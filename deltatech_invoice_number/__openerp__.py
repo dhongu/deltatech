53f3f8fdae0602cbebef73ebee7bacfe080bb8f4
Dorin Hongu <dhongu@gmail.com>
2015-07-16 08:16:36 +0300

    modul pt modificare nuar de factura

---
+++ deltatech_invoice_number/__openerp__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright (c) 2015 Deltatech All Rights Reserved
+#                    Dorin Hongu <dhongu(@)gmail(.)com       
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    "name" : "Deltatech Invoice Number",
+    "version" : "1.0",
+    "author" : "Dorin Hongu",
+    "website" : "",
+    "description": """
+
+Validare data factura sa fie mai mare decat data din ultima factura
+Posibilitatea de a modifica numarul unei facturi
+
+
+    """,
+    "category" : "Generic Modules/Other",
+    "depends" : ['deltatech',"account"],
+ 
+    "data" : ['security/sale_security.xml',
+              'wizard/account_invoice_change_number_view.xml' ],
+    "active": False,
+    "installable": True,
+}
+
+
