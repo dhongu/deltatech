0c35719bc74bb67c6a4f750ffe16687a94882866
Dorin Hongu <dhongu@gmail.com>
2015-08-12 22:20:01 +0300

    Adaugare in raportul de analiza facturi a campurilor: judet, produs si nr de factura

---
+++ deltatech_invoice_report/report/invoice_report.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import models, fields, api, _
+import openerp.addons.decimal_precision as dp
+
+class account_invoice_report(models.Model):
+    _inherit = 'account.invoice.report'
+
+
+    state_id = fields.Many2one("res.country.state", string='Region', readonly=True ) 
+    invoice_id = fields.Many2one('account.invoice', string='Invoice', readonly=True)
+
+
+    def _select(self):
+        return  super(account_invoice_report, self)._select() + ", sub.state_id,  sub.invoice_id"
+
+    def _sub_select(self):
+        return  super(account_invoice_report, self)._sub_select() + ", partner.state_id,  ail.invoice_id"
+
+    def _group_by(self):
+        return super(account_invoice_report, self)._group_by() + ", partner.state_id,  ail.invoice_id"
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
