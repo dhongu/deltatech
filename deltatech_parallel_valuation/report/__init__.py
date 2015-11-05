1fb284c7308fa3dbe71a24669d7bb0b643607864
Dorin Hongu <dhongu@gmail.com>
2015-11-05 08:03:01 +0200

    fix egat de ultimul commit

--- deltatech_parallel_valuation/report/__init__.py
+++ deltatech_parallel_valuation/report/__init__.py
@@ -20,4 +20,5 @@
 ##############################################################################
 
 import invoice_report
+import sale_margin_report
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
