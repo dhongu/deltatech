9117a90b300ef37eb57b23e6f844416cbc497ddf
Dorin Hongu <dhongu@gmail.com>
2016-02-15 16:30:45 +0200

    Modificare logica de afisare stoc pe site.

--- deltatech_website_sale/product.py
+++ deltatech_website_sale/product.py
@@ -25,11 +25,11 @@ class product_template(models.Model):
     def _compute_available(self):
         res = {}
         for product in self:
-            if product.at_order:
-                product.is_qty_available = 'order'
+            if product.sudo().qty_available > 0:
+                product.is_qty_available = 'stock'
             else:
-                if product.sudo().qty_available > 0:
-                    product.is_qty_available = 'stock'
+                if product.at_order:
+                    product.is_qty_available = 'order'
                 else:
                     product.is_qty_available = 'provider'
         return res    
