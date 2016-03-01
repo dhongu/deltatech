9f24a81be2d2e38d63d0797a3f34fe4ee5901aec
Dorin Hongu <dhongu@gmail.com>
2016-03-01 14:37:23 +0200

    Afisare cantitate disponibila in comanda de vanzare

---
+++ deltatech_sale_qty_available/__openerp__.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright (c) 2016 Deltatech All Rights Reserved
+#                    Dorin Hongu <dhongu(@)gmail(.)com       
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    "name" : "Deltatech Sale Qty Available",
+    "version" : "1.0",
+    "author" : "Dorin Hongu",
+    "website" : "",
+    "description": """
+ 
+Features:
+ 
+ - Afisare campuri de cantitate disponibila in comanda de vanzare
+ 
+
+
+    """,
+    "category" : "Generic Modules/Stock",
+    "depends" : ['deltatech',
+                 "sale"],
+ 
+    "data" : [ 'sale_view.xml' ],
+    "active": False,
+    "installable": True,
+}
+
+
