e893f5806c3bfa3a87acd29ff4b5517179d61b9a
Dorin Hongu <dhongu@gmail.com>
2016-03-15 13:22:30 +0200

    modificare analiza operatii mrp

--- deltatech_mrp_operations/mrp_operations.py
+++ deltatech_mrp_operations/mrp_operations.py
@@ -22,7 +22,7 @@
 from openerp import models, fields, api, _
 from openerp.exceptions import except_orm, Warning, RedirectWarning
 import openerp.addons.decimal_precision as dp
-from openerp.tools import float_is_zero
+from openerp import tools
 import logging
 
 _logger = logging.getLogger(__name__)
@@ -66,6 +66,36 @@ class mrp_production_workcenter_line(models.Model):
                 partners |= operator.partner_id 
         self.possible_partner_ids = partners  
 
+class mrp_workorder(models.Model):
+    _inherit = "mrp.workorder"
+
+    partner_id = fields.Many2one('res.partner',string="Operator")
+    total_cost = fields.Float( string="Total Cost")
+
+    def init(self, cr):
+        tools.drop_view_if_exists(cr, 'mrp_workorder')
+        cr.execute("""
+            create or replace view mrp_workorder as (
+                select
+                    date(wl.date_planned) as date,
+                    min(wl.id) as id,
+                    mp.product_id as product_id,
+                    wl.partner_id as partner_id,
+                    sum(wl.hour) as total_hours,
+                    avg(wl.delay) as delay,
+                    (w.costs_hour*sum(wl.hour)) as total_cost,
+                    wl.production_id as production_id,
+                    wl.workcenter_id as workcenter_id,
+                    sum(wl.cycle) as total_cycles,
+                    count(*) as nbr,
+                    sum(mp.product_qty) as product_qty,
+                    wl.state as state
+                from mrp_production_workcenter_line wl
+                    left join mrp_workcenter w on (w.id = wl.workcenter_id)
+                    left join mrp_production mp on (mp.id = wl.production_id)
+                group by
+                    w.costs_hour, mp.product_id, wl.partner_id, mp.name, wl.state, wl.date_planned, wl.production_id, wl.workcenter_id
+        )""")
 
 
 
