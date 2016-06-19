626b04199540d6052dd2d10b8235ba72915c66fb
Dorin Hongu <dhongu@gmail.com>
2016-06-19 17:27:27 +0900

    afisare curs valutar in facura

--- deltatech_invoice/account_invoice.py
+++ deltatech_invoice/account_invoice.py
@@ -29,8 +29,67 @@ from datetime import datetime
 class account_invoice(models.Model):
     _inherit = "account.invoice"
 
- 
+
+    base_rate = fields.Float( string='Rate', digits=(12, 4), readonly=True, default=0.0, store=False, compute="_compute_base_rate" ) 
+    currency_rate = fields.Float( string='Currency Rate', digits=(12, 4), readonly=True, default=0.0 ) 
+    price_currency_id = fields.Many2one('res.currency',  string='Price currency', help="Price currency"  )
+
+    @api.one
+    def _compute_base_rate(self):
+        if self.price_currency_id and self.price_currency_id != self.currency_id:
+            self.base_rate = 1.0
+        else:
+            self.base_rate = 0.0
     
+    
+    @api.onchange('price_currency_id')
+    def onchange_price_currency_id(self):
+        date_eval = self.env.context.get('date',False) or self.date_invoice or fields.Date.context_today(self) 
+        to_currency = self.currency_id or self.env.user.company_id.currency_id
+        #from_currency = self.env.user.company_id.parallel_currency_id
+        from_currency = self.price_currency_id
+        if not from_currency:
+            if self.type in ['out_invoice','out_refund'] and  self.partner_id.property_product_pricelist:
+                from_currency = self.partner_id.property_product_pricelist.currency_id
+            if self.type in ['in_invoice','in_refund'] and self.partner_id.property_product_pricelist_purchase:
+                from_currency = self.partner_id.property_product_pricelist_purchase.currency_id
+            
+        if to_currency and  from_currency:
+            self.currency_rate = from_currency.with_context(date=date_eval).compute(self.base_rate,to_currency,round=False)
+        else:
+            self.currency_rate = self.base_rate
+        
+    @api.multi
+    def get_currency_rate(self):
+        for invoice in self:
+            date_eval =  invoice.date_invoice or fields.Date.context_today(self) 
+            to_currency = invoice.currency_id or self.env.user.company_id.currency_id
+            from_currency = invoice.price_currency_id
+            
+            if invoice.type in ['out_invoice','out_refund'] and  invoice.partner_id.property_product_pricelist:
+                from_currency = invoice.partner_id.property_product_pricelist.currency_id
+            if invoice.type in ['in_invoice','in_refund'] and v.partner_id.property_product_pricelist_purchase:
+                from_currency = invoice.partner_id.property_product_pricelist_purchase.currency_id
+            invoice.price_currency_id = from_currency
+                
+            if to_currency and  from_currency:
+                invoice.currency_rate = from_currency.with_context(date=date_eval).compute(1,to_currency,round=False)
+            else:
+                invoice.currency_rate = 1            
+           
+    @api.multi
+    def onchange_partner_id(self, type, partner_id, date_invoice=False, payment_term=False, partner_bank_id=False, company_id=False):         
+        res = super(account_invoice,self).onchange_partner_id(type, partner_id, date_invoice, payment_term, partner_bank_id, company_id)
+        if partner_id:
+            partner = self.env['res.partner'].browse(partner_id)
+            
+            if type in ['out_invoice','out_refund'] and  partner.property_product_pricelist:
+                price_currency_id = partner.property_product_pricelist.currency_id
+            if type in ['in_invoice','in_refund'] and partner.property_product_pricelist_purchase:
+                price_currency_id = partner.property_product_pricelist_purchase.currency_id
+            res['value']['price_currency_id']  = price_currency_id
+        
+        return res
 
     @api.multi
     def onchange_journal_id(self, journal_id=False):
