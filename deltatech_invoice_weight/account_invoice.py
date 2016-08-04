395a66a797bd7531747db5e2f24e3382dfb88a19
Dorin Hongu <dhongu@gmail.com>
2016-08-04 15:03:28 +0900

    afisare masa in factura

---
+++ deltatech_invoice_weight/account_invoice.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright (c) 2015 Deltatech All Rights Reserved
+#                    Dorin Hongu <dhongu(@)gmail(.)com       
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import models, fields, api, _
+import openerp.addons.decimal_precision as dp
+from openerp.exceptions import except_orm, Warning, RedirectWarning
+from openerp.tools import DEFAULT_SERVER_DATETIME_FORMAT, DEFAULT_SERVER_DATE_FORMAT,DEFAULT_SERVER_TIME_FORMAT
+import time 
+from datetime import datetime
+
+class account_invoice(models.Model):
+    _inherit = "account.invoice"
+
+    weight = fields.Float('Gross Weight', digits =dp.get_precision('Stock Weight'), help="The gross weight in Kg.")
+    weight_net = fields.Float('Net Weight', digits =dp.get_precision('Stock Weight'), help="The net weight in Kg.")
+    weight_package = fields.Float('Package Weight', digits=dp.get_precision('Stock Weight'))
+
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
