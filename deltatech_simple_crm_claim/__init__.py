0c23c4949ef73ce375c2a9d293fc3ba12eaf0c79
Dorin Hongu <dhongu@gmail.com>
2016-09-13 05:23:47 +0300

    Utilizare simple_crm_claim in claim8D

--- deltatech_simple_crm_claim/__init__.py
+++ deltatech_simple_crm_claim/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-# Copyright (c) 2016 Deltatech All Rights Reserved
-#                    Dorin Hongu <dhongu(@)gmail(.)com       
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -18,37 +18,9 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
- 
-{
-    "name" : "Deltatech Claim 8D",
-    "version" : "1.0",
-    "author" : "Dorin Hongu",
-    "website" : "",
-    "description": """
 
-Functionalitati:
- -Raportare 8D
-  
-  https://en.wikipedia.org/wiki/Eight_Disciplines_Problem_Solving
-  
-   
-    """,
-    
-    
-   'category': 'Sales Management',
-    "depends" : [
-                 'deltatech','product',
-                 'deltatech_simple_crm_claim'
-                 #"crm_claim"
-                 ],
- 
-    "data" : [  
-                'security/ir.model.access.csv',
-                'views/report8d.xml',
-                'crm_claim_view.xml'              
-               ],
-    
-    "active": False,
-    "installable": True,
-}
- 
+import crm_claim
+import report
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+
