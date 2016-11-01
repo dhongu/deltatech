2854c3b97786228924e28d3238eec5eb215302d1
Dorin Hongu <dhongu@gmail.com>
2016-11-01 18:46:39 +0200

    Lansare versiune 10.0

--- deltatech_fast_sale/sale.py
+++ deltatech_fast_sale/sale.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-# Copyright (c) 2008 Deltatech All Rights Reserved
+# Copyright (c) 2016 Deltatech All Rights Reserved
 #                    Dorin Hongu <dhongu(@)gmail(.)com       
 #
 #    This program is free software: you can redistribute it and/or modify
@@ -21,35 +21,30 @@
 
 
 
-from openerp.exceptions import except_orm, Warning, RedirectWarning
-from openerp import models, fields, api, _
-from openerp.tools.translate import _
-from openerp import SUPERUSER_ID, api
-import openerp.addons.decimal_precision as dp
+from odoo.exceptions import except_orm, Warning, RedirectWarning
+from odoo import models, fields, api, _
+from odoo.tools.translate import _
+from odoo import SUPERUSER_ID, api
+import odoo.addons.decimal_precision as dp
 
 
 class sale_order(models.Model):
-    _inherit = 'sale.order' 
-    
+    _inherit = 'sale.order'
 
     @api.multi
-    def action_button_confirm_to_invoice(self): 
-        if self.state == 'draft':     
+    def action_button_confirm_to_invoice(self):
+        if self.state == 'draft':
             self.action_confirm()  # confirma comanda
-        
+
         for picking in self.picking_ids:
-            picking.action_assign()   # verifica disponibilitate
+            picking.action_assign()  # verifica disponibilitate
             if not all(move.state == 'assigned' for move in picking.move_lines):
-                raise Warning(_('Not all products are available.')   )
+                raise Warning(_('Not all products are available.'))
             picking.do_transfer()
-  
+
         action_obj = self.env.ref('sale.action_view_sale_advance_payment_inv')
         action = action_obj.read()[0]
 
-
-        return   action
-
-    
-
+        return action
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
