acf13caad71cd2fcc6a3337a7cca60bb2937874e
Dorin Hongu <dhongu@gmail.com>
2016-11-06 18:51:52 +0200

    rearanjare cod

--- deltatech_crm_claim_8D/crm_claim.py
+++ deltatech_crm_claim_8D/crm_claim.py
@@ -35,29 +35,29 @@ class crm_claim(models.Model):
     _inherit = "crm.claim"
     
     
-    product_id = fields.Many2one('product.product',string="Product")
-    quantity = fields.Float(string='Quantity', digits= dp.get_precision('Product Unit of Measure'), required=True, default=1)  
-    value    =   fields.Float(string='Amount', digits= dp.get_precision('Account'),  store=True, readonly=True, compute='_compute_value') 
+    product_id = fields.Many2one('product.product', string="Product")
+    quantity = fields.Float(string='Quantity', digits=dp.get_precision('Product Unit of Measure'), required=True, default=1)  
+    value = fields.Float(string='Amount', digits=dp.get_precision('Account'), store=True, readonly=True, compute='_compute_value') 
     
-    user_ids = fields.Many2many('res.users','crm_claim_team_rel','claim_id','user_id', string='Team')
+    user_ids = fields.Many2many('res.users', 'crm_claim_team_rel', 'claim_id', 'user_id', string='Team')
     
-    action_containment_ids = fields.One2many('crm.claim.action','claim_id', string="Containment actions", domain=[('type', '=', 'containment')])
+    action_containment_ids = fields.One2many('crm.claim.action', 'claim_id', string="Containment actions", domain=[('type', '=', 'containment')])
     action_corrective_ids = fields.One2many('crm.claim.action', 'claim_id', string="Permanent Corrective actions", domain=[('type', '=', 'corrective')])
     action_verification_ids = fields.One2many('crm.claim.action', 'claim_id', string="Effectiveness verification", domain=[('type', '=', 'verification')])
     action_preventive_ids = fields.One2many('crm.claim.action', 'claim_id', string="Preventive actions", domain=[('type', '=', 'preventive')])
 
     action_ids = fields.One2many('crm.claim.action', 'claim_id', string="Actions")
 
-    loc_detected = fields.Many2one("crm.claim.loc.detected",string="Detected in")
+    loc_detected = fields.Many2one("crm.claim.loc.detected", string="Detected in")
     comments = fields.Text(string="Comments")
 
-    date_closed =fields.Datetime(readonly=False)
-    closed_by_user_id = fields.Many2one('res.users',string="Closed by", track_visibility='onchange')
+    date_closed = fields.Datetime(readonly=False)
+    closed_by_user_id = fields.Many2one('res.users', string="Closed by", track_visibility='onchange')
     
 
-    #am incercat sa redefinesc cumpurile originale dar nu am mers
-    date_action_next_comp = fields.Date(string="Next Action Date", compute = "_compute_action_next",   store=False )
-    action_next_comp      = fields.Char(string="Next Action", compute = "_compute_action_next", store=False )
+    # am incercat sa redefinesc cumpurile originale dar nu am mers
+    date_action_next_comp = fields.Date(string="Next Action Date", compute="_compute_action_next", store=False)
+    action_next_comp = fields.Char(string="Next Action", compute="_compute_action_next", store=False)
 
 
     @api.one
@@ -70,12 +70,12 @@ class crm_claim(models.Model):
     @api.depends('action_ids')
     def _compute_action_next(self):
         for claim in self:
-            #actions = self.env['crm.claim.action'].search(['claim_id','=',claim.id])yyyy-mm-dd
-            #print actions
+            # actions = self.env['crm.claim.action'].search(['claim_id','=',claim.id])yyyy-mm-dd
+            # print actions
             for action in claim.action_ids:
                 if action.date_deadline > fields.Date.today() and (action.date_deadline < claim.date_action_next_comp or not claim.date_action_next_comp):
                      claim.date_action_next_comp = action.date_deadline
-                     claim.action_next_comp      = action.name
+                     claim.action_next_comp = action.name
 
 
 
@@ -84,24 +84,24 @@ class crm_claim_action(models.Model):
     _description = "CRM Claim Action"
     _order = "date_deadline" 
 
-    claim_id = fields.Many2one('crm.claim',string='Claim',required=True,index=True)
-    type = fields.Selection([('containment','Containment'),('corrective','Corrective'),
-                             ('verification','Verification'),('preventive','Preventive')], string="Type")
-    name = fields.Char(string="Action",required=True) 
+    claim_id = fields.Many2one('crm.claim', string='Claim', required=True, index=True)
+    type = fields.Selection([('containment', 'Containment'), ('corrective', 'Corrective'),
+                             ('verification', 'Verification'), ('preventive', 'Preventive')], string="Type")
+    name = fields.Char(string="Action", required=True) 
     user_id = fields.Many2one('res.users', 'Responsible', track_visibility='always')
     date_deadline = fields.Date(string='Deadline')
     
     state = fields.Selection([
-            ('draft','Undone'),
-            ('open','In Progress'),
-            ('done','Done'),
-        ], string='Status', index=True,  default='draft',   copy=False )    
+            ('draft', 'Undone'),
+            ('open', 'In Progress'),
+            ('done', 'Done'),
+        ], string='Status', index=True, default='draft', copy=False)    
     
     
 class crm_claim_loc_detected(models.Model):
     _name = "crm.claim.loc.detected"
     _description = "CRM Location Detected"  
     
-    name = fields.Char(string="Detected",required=True, translate=True)
+    name = fields.Char(string="Detected", required=True, translate=True)
     
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
