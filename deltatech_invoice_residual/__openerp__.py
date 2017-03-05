4cbba8e96549000688f971a6d973faaeeb6c0ab6
Dorin Hongu <dhongu@gmail.com>
2017-03-05 14:50:05 +0200

    Calcul sold facturi negative

---
+++ deltatech_invoice_residual/__openerp__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright (c) 2016 Deltatech All Rights Reserved
+#                    Dorin Hongu <dhongu(@)gmail(.)com       
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    "name" : "Deltatech Invoice Residual",
+    "version" : "1.0",
+    "author" : "Dorin Hongu",
+    "website" : "",
+    "description": """
+
+Functionalitati:
+
+ - Calcul Sold factura in cazul in care totalul de pe facura este negativ, standard facturile nu au sold negativ
+
+    """,
+    "category" : "Generic Modules/Other",
+    "depends" : ["account"],
+ 
+    "data" : [ "account_invoice_data.xml"],
+    "active": False,
+    "installable": True,
+}
+
+
