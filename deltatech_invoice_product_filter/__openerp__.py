4b4484a6fd2accb6aef8d655b7c89f5478dd2c37
Dorin Hongu <dhongu@gmail.com>
2017-03-11 15:46:35 +0200

    adaugare site

--- deltatech_invoice_product_filter/__openerp__.py
+++ deltatech_invoice_product_filter/__openerp__.py
@@ -21,8 +21,8 @@
 {
     "name" : "Deltatech Product Filter",
     "version" : "1.0",
-    "author" : "Dorin Hongu",
-    "website" : "",
+    "author": "Dorin Hongu",
+    "website": "www.terrabit.ro",
     "description": """
 
 Functionalitati:
