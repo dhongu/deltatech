8f543cfffb53dcfbf45414fd4e0a18a22c498008
Dorin Hongu <dhongu@gmail.com>
2017-03-15 08:38:56 +0200

    adaugare website

--- deltatech_invoice_number/__manifest__.py
+++ deltatech_invoice_number/__manifest__.py
@@ -21,7 +21,7 @@
 {
     "name" : "Deltatech Invoice Number",
     "version" : "1.0",
-    "author": "Dorin Hongu",
+    "author" : "Terrabit, Dorin Hongu",
     "website": "www.terrabit.ro",
     "description": """
 
