271fc8731f633bc4d55ce6adf91390ef1ac4550e
Dorin Hongu <dhongu@gmail.com>
2017-03-20 06:14:25 +0200

    Inlocuire openerp  cu odoo

--- deltatech_invoice_receipt/stock.py
+++ deltatech_invoice_receipt/stock.py
@@ -23,40 +23,44 @@ from datetime import date, datetime
 from dateutil import relativedelta
 
 import time
-from openerp.exceptions import except_orm, Warning, RedirectWarning
+from odoo.exceptions import except_orm, Warning, RedirectWarning
  
-from openerp import models, fields, api, _
-from openerp.tools.translate import _
-from openerp.tools import DEFAULT_SERVER_DATETIME_FORMAT, DEFAULT_SERVER_DATE_FORMAT
-from openerp import SUPERUSER_ID, api
+from odoo import models, fields, api, _
+from odoo.tools.translate import _
+from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT, DEFAULT_SERVER_DATE_FORMAT
+from odoo import SUPERUSER_ID, api
 
-import openerp.addons.decimal_precision as dp
+import odoo.addons.decimal_precision as dp
 
 
 class stock_move(models.Model):
     _inherit = 'stock.move' 
 
     # metoda standard copiata si am comentat actualizarea pretului
-    def _store_average_cost_price(self, cr, uid, move, context=None):
-        ''' move is a browe record '''
-        product_obj = self.pool.get('product.product')
-        if any([q.qty <= 0 for q in move.quant_ids]) or move.product_qty == 0:
-            #if there is a negative quant, the standard price shouldn't be updated
-            return
-        #Note: here we can't store a quant.cost directly as we may have moved out 2 units (1 unit to 5€ and 1 unit to 7€) and in case of a product return of 1 unit, we can't know which of the 2 costs has to be used (5€ or 7€?). So at that time, thanks to the average valuation price we are storing we will svaluate it at 6€
-        average_valuation_price = 0.0
-        for q in move.quant_ids:
-            average_valuation_price += q.qty * q.cost
-        average_valuation_price = average_valuation_price / move.product_qty
-        # Write the standard price, as SUPERUSER_ID because a warehouse manager may not have the right to write on products
-        ctx = dict(context or {}, force_company=move.company_id.id)
-        #product_obj.write(cr, SUPERUSER_ID, [move.product_id.id], {'standard_price': average_valuation_price}, context=ctx)
-        product = product_obj.browse(cr, SUPERUSER_ID, [move.product_id.id] )
-        if average_valuation_price <> product.standard_price:
-            msg = _('Computed price must to be:  %s') %  str(average_valuation_price)
-            product_obj.message_post(cr, uid, [move.product_id.id], body=msg)
-        
-        self.write(cr, uid, [move.id], {'price_unit': average_valuation_price}, context=context) 
+    def _store_average_cost_price(self):
+        """ Store the average price of the move on the move and product form (costing method 'real')"""
+        for move in self.filtered(lambda move: move.product_id.cost_method == 'real'):
+            # product_obj = self.pool.get('product.product')
+            if any(q.qty <= 0 for q in move.quant_ids) or move.product_qty == 0:
+                # if there is a negative quant, the standard price shouldn't be updated
+                return
+            # Note: here we can't store a quant.cost directly as we may have moved out 2 units
+            # (1 unit to 5€ and 1 unit to 7€) and in case of a product return of 1 unit, we can't
+            # know which of the 2 costs has to be used (5€ or 7€?). So at that time, thanks to the
+            # average valuation price we are storing we will valuate it at 6€
+            valuation_price = sum(q.qty * q.cost for q in move.quant_ids)
+            average_valuation_price = valuation_price / move.product_qty
+
+            #move.product_id.with_context(force_company=move.company_id.id).sudo().write(
+            #    {'standard_price': average_valuation_price})
+            move.write({'price_unit': average_valuation_price})
+
+        for move in self.filtered(lambda move: move.product_id.cost_method != 'real' and not move.origin_returned_move_id):
+            # Unit price of the move should be the current standard price, taking into account
+            # price fluctuations due to products received between move creation (e.g. at SO
+            # confirmation) and move set to done (delivery completed).
+            move.write({'price_unit': move.product_id.standard_price})
+
 
 class stock_picking(models.Model):
     _inherit = 'stock.picking'
