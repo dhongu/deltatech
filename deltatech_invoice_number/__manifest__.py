874517d790d1b845d6b09f0cd6aec79cffd87295
danila12 <danila@terrabit.ro>
2017-04-24 15:17:58 +0300

    Modificare manifest invoice_number

--- deltatech_invoice_number/__manifest__.py
+++ deltatech_invoice_number/__manifest__.py
@@ -25,6 +25,12 @@
     "website": "www.terrabit.ro",
     "description": """
 
+Functions:
+ - At the validation, the invoice date must be greater than the last invoice validated
+ - The number of the invoice can be modified
+ - A number can be allocated to a invoice in the draft state. After the number is allocated, the date of the invoice cannot be changed
+ - The user must be in the account.group_account_invoice group (Accounting & Finance / Billing)
+
 Functionalitati:
  - Validare data factura sa fie mai mare decat data din ultima factura
  - Posibilitatea de a modifica numarul unei facturi
