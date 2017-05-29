2101630a3c750d1a2c8632a4683d898414fbef9c
Dorin Hongu <dhongu@gmail.com>
2017-05-29 18:06:49 +0300

    Creare ridicare

--- deltatech_quant/models/pos.py
+++ deltatech_quant/models/pos.py
@@ -16,11 +16,15 @@ class PosOrder(models.Model):
             orders = self.search([('picking_id', '=', False)], order="date_order")
         for order in orders:
             if not order.picking_id:
-                order.create_picking()
-                _logger.info("%s %s" % (order.name, order.picking_id.name or ''))
-                if order.picking_id:
-                    order.picking_id.write({'min_date': order.date_order})
-                    order.picking_id.move_lines.write({'create_date': order.date_order,
-                                                       'date_expected': order.date_order,
-                                                       'date': order.date_order})
-                self.env.cr.commit()
+                try:
+                    order.create_picking()
+                    _logger.info("%s %s" % (order.name, order.picking_id.name or ''))
+
+                    if order.picking_id:
+                        order.picking_id.write({'min_date': order.date_order})
+                        order.picking_id.move_lines.write({'create_date': order.date_order,
+                                                           'date_expected': order.date_order,
+                                                           'date': order.date_order})
+                    self.env.cr.commit()
+                except:
+                    pass
