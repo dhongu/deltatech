ac1de91d4f4d56fa259cba52e6c1bd000a4af381
Dorin Hongu <dhongu@gmail.com>
2017-09-07 12:33:05 +0300

    update Optimik

--- deltatech_optimik/wizard/mrp_optimik_export.py
+++ deltatech_optimik/wizard/mrp_optimik_export.py
@@ -2,7 +2,9 @@
 
 
 from odoo import models, fields, api
-
+import csv
+from cStringIO import StringIO
+import base64
 
 class MrpOptimikExport(models.TransientModel):
     _name = 'mrp.optimik.export'
@@ -10,10 +12,15 @@ class MrpOptimikExport(models.TransientModel):
 
     state = fields.Selection([('choose', 'choose'),  # choose product
                               ('prepare', 'Prepare'),
-                              ('get', 'get')], default='choose')  # get the file
-    line_ids = fields.One2many("mrp.optimik.export.line", 'optimik_id', string='Lines')
+                              ('get_file', 'Get File')], default='choose')  # get the file
 
-    name = fields.Char(string='File Name', readonly=True)
+    separator = fields.Char(default='|', size=1)
+    no_labels = fields.Boolean(string="Without Label")
+
+    line_ids = fields.One2many("mrp.optimik.select.line", 'optimik_id', string='Select lines')
+    line_export_ids = fields.One2many("mrp.optimik.export.line", 'optimik_id', string='Export lines')
+
+    name = fields.Char(string='File Name', default="Oprimik.csv")
     data_file = fields.Binary(string='File', readonly=True)
 
     @api.model
@@ -86,43 +93,138 @@ class MrpOptimikExport(models.TransientModel):
 
         return res
 
+    @api.multi
+    def do_export(self):
+        lines = self.env['mrp.optimik.select.line'].read_group(
+            domain=[('optimik_id', '=', self.id), ('is_ok', '=', True)],
+            fields=['raw_product', 'fiber', 'length', 'width', 'quantity'],
+            groupby=['raw_product', 'fiber', 'length', 'width'], lazy=False)
+
+        for line in lines:
+            vals = {'optimik_id': self.id,
+                    'raw_product': line['raw_product'][0],
+                    'quantity': line['quantity'],
+                    'length': line['length'],
+                    'width': line['width'],
+                    'fiber': line['fiber']
+                    }
+            self.env["mrp.optimik.export.line"].create(vals)
+
+        self.write({'state': 'prepare'})
+        return {
+            'type': 'ir.actions.act_window',
+            'res_model': 'mrp.optimik.export',
+            'view_mode': 'form',
+            'view_type': 'form',
+            'res_id': self.id,
+            'views': [(False, 'form')],
+            'target': 'new',
+        }
+
+    @api.multi
+    def do_make_file(self):
+        fp = StringIO()
+        writer = csv.writer(fp, quoting=csv.QUOTE_NONE, delimiter=self.separator.encode('ascii','ignore'))
+
+        # D | cod material | nr buc | lungime | latime. | Fibra | nume piesa | Client | cant fata | Cant stanga | cant spate | cant dreapta
+
+
+        fields = ['D', 'cod material', 'nr buc', 'lungime', 'latime', 'Fibra',
+                  'nume piesa', 'client', 'cant fata', 'cant stanga', 'cant spate', 'cant dreapta']
+
+        writer.writerow([name.encode('utf-8') for name in fields])
+
+        for line in self.line_export_ids:
+            row = []
+            row.append('D')
+            row.append(line.raw_product.default_code)
+            row.append(line.quantity)
+
+            row.append(line.length)
+            row.append(line.width)
+
+            if line.fiber:
+                row.append('=')
+            else:
+                row.append('X')
+
+            row.append('')  # nume piesa
+            row.append('')  # client
+            row.append('')  # cant fata
+            row.append('')  # cant stanga
+            row.append('')  # cant spate
+            row.append('')  # cant dreapta
+
+            writer.writerow(row)
+
+        fp.seek(0)
+        data_file =  base64.encodestring(fp.read())
+        fp.close()
+
+        self.write({'state': 'get_file',
+                    'data_file':data_file})
+        return {
+            'type': 'ir.actions.act_window',
+            'res_model': 'mrp.optimik.export',
+            'view_mode': 'form',
+            'view_type': 'form',
+            'res_id': self.id,
+            'views': [(False, 'form')],
+            'target': 'new',
+        }
+
+
+class MrpOptimikSelectLine(models.TransientModel):
+    _name = "mrp.optimik.select.line"
+
+    optimik_id = fields.Many2one('mrp.optimik.export', string="Product Label")
+    product_id = fields.Many2one("product.product", string="Product")
+    quantity = fields.Integer(string="Qty", default=1, group_operator='sum')
+    is_ok = fields.Boolean()
+
+    length = fields.Float()  # related='product_id.length', readonly=True, store=True)
+    width = fields.Float()  # related='product_id.width', readonly=True, store=True)
+
+    raw_product = fields.Many2one('product.product')  # , compute="_compute_is_ok", store=True)
+    fiber = fields.Boolean()
+    strip_top = fields.Many2one('product.product')  # , compute="_compute_is_ok", store=True)
+    strip_left = fields.Many2one('product.product')  # , compute="_compute_is_ok", store=True)
+    strip_right = fields.Many2one('product.product')  # , compute="_compute_is_ok", store=True)
+    strip_bottom = fields.Many2one('product.product')  # , compute="_compute_is_ok", store=True)
+
+    @api.onchange('product_id')
+    def _compute_is_ok(self):
+        for line in self:
+            line.is_ok = False
+            line.raw_product = False
+            line.length = line.product_id.length
+            line.width = line.product_id.width
+            if line.product_id:
+                bom = self.env['mrp.bom']._bom_find(product=line.product_id)
+                if bom:
+                    for line in bom.bom_line_ids:
+                        if line.item_categ in ['cut', 'cut_fiber']:
+                            line.is_ok = True  # Am gasit ceva de taiat!
+                            line.raw_product = line.product_id
+                            if line.item_categ == 'cut_fiber':
+                                line.fiber = True
+                        if line.item_categ == 'top':
+                            line.strip_top = line.product_id
+                        if line.item_categ == 'left':
+                            line.strip_left = line.product_id
+                        if line.item_categ == 'right':
+                            line.strip_right = line.product_id
+                        if line.item_categ == 'bottom':
+                            line.strip_bottom = line.product_id
+
 
 class MrpOptimikExportLine(models.TransientModel):
     _name = "mrp.optimik.export.line"
 
     optimik_id = fields.Many2one('mrp.optimik.export', string="Product Label")
-    product_id = fields.Many2one("product.product", string="Product")
+    raw_product = fields.Many2one('product.product')
+
     quantity = fields.Integer(string="Qty", default=1)
-    is_ok = fields.Boolean(compute="_compute_is_ok")
-
-    length = fields.Float(related='product_id.length', readonly=True)
-    width = fields.Float(related='product_id.width', readonly=True)
-
-    raw_product = fields.Many2one('product.product', compute="_compute_is_ok")
+    length = fields.Float()
+    width = fields.Float()
     fiber = fields.Boolean()
-    strip_top = fields.Many2one('product.product', compute="_compute_is_ok")
-    strip_left = fields.Many2one('product.product', compute="_compute_is_ok")
-    strip_right = fields.Many2one('product.product', compute="_compute_is_ok")
-    strip_bottom = fields.Many2one('product.product', compute="_compute_is_ok")
-
-    @api.depends('product_id')
-    def _compute_is_ok(self):
-        self.is_ok = False
-        self.raw_product = False
-        if self.product_id:
-            bom = self.env['mrp.bom']._bom_find(product=self.product_id)
-            if bom:
-                for line in bom.bom_line_ids:
-                    if line.item_categ in ['cut', 'cut_fiber']:
-                        self.is_ok = True  # Am gasit ceva de taiat!
-                        self.raw_product = line.product_id
-                        if line.item_categ == 'cut_fiber':
-                            self.fiber = True
-                    if line.item_categ == 'top':
-                        self.strip_top = line.product_id
-                    if line.item_categ == 'left':
-                        self.strip_left = line.product_id
-                    if line.item_categ == 'right':
-                        self.strip_right = line.product_id
-                    if line.item_categ == 'bottom':
-                        self.strip_bottom = line.product_id
