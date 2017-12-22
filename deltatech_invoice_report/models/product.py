5864c717c90821de28bbf8d07b27280b583e08c3
Dorin Hongu <dhongu@gmail.com>
2017-12-22 08:08:09 +0200

    afisare pret pe buton

--- deltatech_invoice_report/models/product.py
+++ deltatech_invoice_report/models/product.py
@@ -17,14 +17,20 @@ class product_template(models.Model):
             products = template.product_variant_ids
 
             domain = [
+                ('type', 'in', ['in_invoice', 'in_refund']),
                 ('product_id', 'in', products.ids),
             ]
             product_qty = 0
-            groups = self.env['account.invoice.report'].read_group(domain=domain, fields=['product_id', 'product_qty'],
+            price_average = 0.0
+            groups = self.env['account.invoice.report'].read_group(domain=domain,
+                                                                   fields=['product_id', 'product_qty',
+                                                                           'price_average'],
                                                                    groupby=['product_id'])
             for item in groups:
                 product_qty += item['product_qty']
-            template.invoice_count = -1 * product_qty
+                price_average = item['price_average']
+
+            template.invoice_count = price_average
 
     @api.multi
     def action_view_invoice(self):
@@ -34,6 +40,13 @@ class product_template(models.Model):
         for template in self:
             products |= template.product_variant_ids
         action[
-            'context'] = "{ 'group_by':['type'], 'measures': ['product_qty', 'price_average'], 'col_group_by': ['date:year']  }"
+            'context'] = """{
+             'group_by':['date:year'],
+             'measures': ['product_qty', 'price_average'],
+             'col_group_by': ['type'] ,
+              'group_by_no_leaf': 1,
+              'search_disable_custom_filters': True
+             }"""
+        #
         action['domain'] = "[('product_id','in',[" + ','.join(map(str, products.ids)) + "])]"
         return action
