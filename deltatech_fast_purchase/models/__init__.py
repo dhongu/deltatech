4e59f69c267bfd7f8cc9fd8878b31c1ca69f1635
Dorin Hongu <dhongu@gmail.com>
2018-02-04 19:45:45 +0200

    Upgrade to 11.0

--- deltatech_fast_purchase/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# ©  2015-2018 Deltatech
-#              Dorin Hongu <dhongu(@)gmail(.)com
-# See README.rst file on addons root folder for license details
-
-
-
-import purchase
-import stock
