bd0d816c7b494f052211af2bfa5271d5316614cf
Dorin Hongu <dhongu@gmail.com>
2018-02-04 19:38:29 +0200

    Conversie la 11

---
+++ deltatech_contact/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# ©  2015-2018 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Deltatech Contacts",
+    'version': '11.0.1.3.0',
+    "author": "Dorin Hongu",
+    "website": "www.terrabit.ro",
+    "description": """
+
+Functionalitati:
+ - Adaugare campuri suplimentare in datele de contact: birthdate, CNP,  nr carte de identitate
+
+   
+    """,
+
+    "category": "Generic Modules/Base",
+    "depends": ["base"],
+
+    "data": ['views/res_partner_view.xml',
+             'security/partner_security.xml',
+             'security/ir.model.access.csv'],
+
+    "active": False,
+    "installable": True,
+}
+
+
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ deltatech_contact/models/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# ©  2015-2018 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+
+
+
+import res_partner
+
+
+ 
+
+
+
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
