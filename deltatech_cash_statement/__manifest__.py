bb0a1614061b5297291ef9447d8a6fb6ca5ac4c2
Dorin Hongu <dhongu@gmail.com>
2018-02-18 11:37:05 +0200

    Converie la 10 modul de actualizare sold la casa

---
+++ deltatech_cash_statement/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# ©  2018 Deltatech
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Deltatech Cash Statement Extension",
+    'version': '10.0.1.0.0',
+    "author": "Terrabit, Dorin Hongu",
+    "website": "www.terrabit.ro",
+    "description": """
+
+Functionalitati:
+ - actualizare automata sold la casa
+   
+    """,
+
+    'category': 'Accounting',
+    "depends": ["account"],
+
+    "data": [
+        'wizard/account_cash_update_balances_view.xml'
+    ],
+
+    "active": False,
+    "installable": True,
+}
+
+
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ deltatech_cash_statement/wizard/account_cash_update_balances.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+# ©  2018 Deltatech
+# See README.rst file on addons root folder for license details
+
+
+
+from odoo import models, fields, api, _
+from odoo.exceptions import except_orm, Warning, RedirectWarning
+import odoo.addons.decimal_precision as dp
+
+
+class account_cash_update_balances(models.TransientModel):
+    _name = 'account.cash.update.balances'
+    _description = "Account Cash Update Balances"
+
+    balance_start = fields.Float(string='Starting Balance', digits=dp.get_precision('Account'))
+
+    @api.model
+    def default_get(self, fields):
+        defaults = super(account_cash_update_balances, self).default_get(fields)
+        active_ids = self.env.context.get('active_ids', False)
+        statement = False
+        if active_ids:
+            domain = [('id', 'in', active_ids), ('state', '=', 'open')]
+            statement = self.env['account.bank.statement'].search(domain, order='date', limit=1)
+            if statement:
+                defaults['balance_start'] = statement.balance_start
+        if not statement:
+            raise Warning(_('Please select cash statement'))
+        return defaults
+
+    @api.multi
+    def do_update_balance(self):
+        active_ids = self.env.context.get('active_ids', False)
+
+        if active_ids:
+            domain = [('id', 'in', active_ids), ('state', '=', 'open')]
+            statements = self.env['account.bank.statement'].search(domain,  order='date')
+            balance_start = self.balance_start
+            for statement in statements:
+                statement.write({'balance_start': balance_start})
+                statement.write({'balance_end_real': statement.balance_end})
+                balance_start = statement.balance_end
