caa8df458d3565aaf401df6de97753118eec2275
Dorin Hongu <dhongu@gmail.com>
2018-02-22 18:47:05 +0200

    ajustari mici

--- deltatech_pos/__manifest__.py
+++ deltatech_pos/__manifest__.py
@@ -1,23 +1,7 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-# Copyright (c) 2017 Deltatech All Rights Reserved
-#                    Dorin Hongu <dhongu(@)gmail(.)com       
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# ©  2018 Deltatech
+# See README.rst file on addons root folder for license details
+
 {
     "name": "Deltatech Point Of Sale",
     "version": "1.0",
@@ -32,7 +16,7 @@ Functionalitati:
  
     """,
 
-    "category": "GPoint Of Sale",
+    "category": "Point Of Sale",
     "depends": [
          "point_of_sale",
 
