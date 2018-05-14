c637e22a55ee4a1a52af295326f34e6f0c7c2823
Dorin Hongu <dhongu@gmail.com>
2018-05-14 22:58:47 +0300

    upgrade modul la 11

---
+++ deltatech_payment_to_statement/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# ©  2018 Deltatech
+# See README.rst file on addons root folder for license details
+
+
+
+from . import models
+
+
+ 
+
---
+++ deltatech_payment_to_statement/models/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# ©  2018 Deltatech
+# See README.rst file on addons root folder for license details
+
+
+
+from . import account_payment
+from . import account
+from . import account_bank_statement
+
+
+ 
+
