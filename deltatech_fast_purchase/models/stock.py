1a7ce2c943ef2947552fb27d3f67a0b8c309f7c7
Dorin Hongu <dhongu@gmail.com>
2018-05-17 15:38:12 +0300

    factura negativa pentru cantitatile facturate si neprimite

--- deltatech_fast_purchase/models/stock.py
+++ deltatech_fast_purchase/models/stock.py
@@ -4,7 +4,7 @@
 # See README.rst file on addons root folder for license details
 
 from odoo import api, fields, models, _
-from odoo.exceptions import UserError
+from odoo.tools.float_utils import float_compare
 
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
@@ -18,12 +18,17 @@ class StockPicking(models.Model):
             result = action.read()[0]
 
             # override the context to get rid of the default filtering
-            result['context'] = {'type': 'in_invoice',
-                                 'default_purchase_id': self.purchase_id.id,
+            invoice_type = 'in_invoice'
+            result['context'] = { 'default_purchase_id': self.purchase_id.id,
                                  'default_date_invoice': self.scheduled_date[:10]}
 
+            if self.location_id.usage == 'internal':
+                invoice_type = 'in_refund'
 
-            if not self.purchase_id.invoice_ids:
+            result['context']['type'] = invoice_type
+            invoice_ids = self.purchase_id.invoice_ids.filtered(lambda r:r.type == invoice_type)
+
+            if not invoice_ids:
                 # Choose a default account journal in the same currency in case a new invoice is created
                 journal_domain = [
                     ('type', '=', 'purchase'),
@@ -35,16 +40,16 @@ class StockPicking(models.Model):
                     result['context']['default_journal_id'] = default_journal_id.id
             else:
                 # Use the same account journal than a previous invoice
-                result['context']['default_journal_id'] = self.purchase_id.invoice_ids[0].journal_id.id
+                result['context']['default_journal_id'] = invoice_ids[0].journal_id.id
 
             # choose the view_mode accordingly
-            if len(self.purchase_id.invoice_ids) != 1:
-                result['domain'] = "[('id', 'in', " + str(self.purchase_id.invoice_ids.ids) + ")]"
-            elif len(self.purchase_id.invoice_ids) == 1:
+            if len(invoice_ids) != 1:
+                result['domain'] = "[('id', 'in', " + str(invoice_ids.ids) + ")]"
+            elif len(invoice_ids) == 1:
                 res = self.env.ref('account.invoice_supplier_form', False)
                 result['views'] = [(res and res.id or False, 'form')]
-                result['res_id'] = self.purchase_id.invoice_ids.id
-            if not self.purchase_id.invoice_ids:
+                result['res_id'] = invoice_ids.id
+            if not invoice_ids:
                 #result['target'] = 'new'
                 result['views'] = [[False, "form"]]
             return result
