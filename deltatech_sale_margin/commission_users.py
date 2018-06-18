806531e31c94ef4d867b1190ba4ae9d2e8b0b2ef
Dorin Hongu <dhongu@gmail.com>
2018-06-18 08:20:56 +0300

    fix nr de zecimale

--- deltatech_sale_margin/commission_users.py
+++ deltatech_sale_margin/commission_users.py
@@ -32,8 +32,8 @@ class commission_users(models.Model):
 
     user_id = fields.Many2one('res.users', string='Salesperson', required=True)
     name = fields.Char(string='Name', related='user_id.name')
-    rate = fields.Float(string="Rate", default=0.01)
-    manager_rate = fields.Float(string="Rate manager", default=0)
+    rate = fields.Float(string="Rate", default=0.01, digits=(12,3))
+    manager_rate = fields.Float(string="Rate manager", default=0, digits=(12,3))
     manager_user_id = fields.Many2one('res.users', string='Sales Manager')
     journal_id = fields.Many2one('account.journal', string='Journal',     domain="[('type', 'in', ['sale','sale_refund'])]")
 
