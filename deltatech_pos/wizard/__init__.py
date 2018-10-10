d38a34f89ab3c3364a94a5696b4c1a6f19d025cd
Dorin Hongu <dhongu@gmail.com>
2018-10-10 23:12:14 +0300

    mutare modul

--- deltatech_pos/wizard/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# ©  2008-2018 Deltatech
-#              Dorin Hongu <dhongu(@)gmail(.)com
-# See README.rst file on addons root folder for license details
-
-
-from . import pos_box
-from . import pos_payment
