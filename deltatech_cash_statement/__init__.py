2c37b7937b639b208293b67b539cd560a694fed8
Dorin Hongu <dhongu@gmail.com>
2018-11-19 21:01:57 +0200

    conversie modul pt actualizare sold jurnal de casa

---
+++ deltatech_cash_statement/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# ©  2008-2018 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+
+
+from . import wizard
+
---
+++ deltatech_cash_statement/wizard/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# ©  2008-2018 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details#######################
+
+
+
+from . import account_cash_update_balances
