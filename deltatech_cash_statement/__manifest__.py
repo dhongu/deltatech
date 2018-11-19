69de13711f90c388c0249a73949dc32288013d9a
Dorin Hongu <dhongu@gmail.com>
2018-11-19 21:01:57 +0200

    conversie modul pt actualizare sold jurnal de casa

---
+++ deltatech_cash_statement/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# ©  2008-2018 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Deltatech Cash Statement Extension",
+    "version": "2.0",
+    "author": "Dorin Hongu",
+    "website": "",
+    "description": """
+
+Functionalitati:
+ - actualizare automata sold la casa
+   
+    """,
+
+    'category': 'Accounting & Finance',
+    "depends": ["account"],
+
+    "data": [
+        'wizard/account_cash_update_balances_view.xml'
+    ],
+
+    "active": False,
+    "installable": True,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
