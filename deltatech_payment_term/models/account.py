55d5655b159848c51ea5faa1ded682d989568e05
Dorin Hongu <dhongu@gmail.com>
2018-12-04 14:20:55 +0200

    Plata in rate

--- deltatech_payment_term/models/account.py
+++ deltatech_payment_term/models/account.py
@@ -3,10 +3,69 @@
 # See README.rst file on addons root folder for license details
 
 
+from odoo import models, fields, api, _
+from openerp.exceptions import except_orm, Warning, RedirectWarning
+from dateutil.relativedelta import relativedelta
+from odoo.tools import float_round
 
- 
+
+class AccountPaymentTerm(models.Model):
+    _inherit = "account.payment.term"
+
+
+    @api.one
+    def compute(self, value, date_ref=False):
+
+        date_ref = date_ref or fields.Date.today()
+        amount = value
+        sign = value < 0 and -1 or 1
+        result = []
+        if self.env.context.get('currency_id'):
+            currency = self.env['res.currency'].browse(self.env.context['currency_id'])
+        else:
+            currency = self.env.user.company_id.currency_id
+        for line in self.line_ids:
+            if line.value == 'fixed':
+                amt = sign * currency.round(line.value_amount)
+            elif line.value == 'percent':
+                amt = currency.round(value * (line.value_amount / 100.0))
+            elif line.value == 'balance':
+                amt = currency.round(amount)
+            if amt:
+                next_date = fields.Date.from_string(date_ref)
+                if line.option == 'day_of_month':
+                    next_date += relativedelta(months=int(line.days/30))
+                    if line.days2 < 0:
+                        next_first_date = next_date + relativedelta(day=1, months=1)  # Getting 1st of next month
+                        next_date = next_first_date + relativedelta(days=line.days2)
+                    if line.days2 > 0:
+                        next_date += relativedelta(day=line.days2, months=1)
+
+                if line.option == 'day_after_invoice_date':
+                    next_date += relativedelta(days=line.days)
+                elif line.option == 'fix_day_following_month':
+                    next_first_date = next_date + relativedelta(day=1, months=1)  # Getting 1st of next month
+                    next_date = next_first_date + relativedelta(days=line.days - 1)
+                elif line.option == 'last_day_following_month':
+                    next_date += relativedelta(day=31, months=1)  # Getting last day of next month
+                elif line.option == 'last_day_current_month':
+                    next_date += relativedelta(day=31, months=0)  # Getting last day of next month
+                result.append((fields.Date.to_string(next_date), amt))
+                amount -= amt
+        amount = sum(amt for _, amt in result)
+        dist = currency.round(value - amount)
+        if dist:
+            last_date = result and result[-1][0] or fields.Date.today()
+            result.append((last_date, dist))
+        return result
+
+
+class AccountPaymentTermLine(models.Model):
+    _inherit = "account.payment.term.line"
+
+    option = fields.Selection(selection_add=[('day_of_month', 'Day of the Month')])
+    days2 = fields.Integer('Day of the Month', required=True,
+                            help="Day of the month, set -1 for the last day of the current month. If it's positive, it gives the day of the next month. Set 0 for net days (otherwise it's based on the beginning of the month).")
 
 
 
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
--- deltatech_payment_term/wizard/payment_term.py
+++ deltatech_payment_term/wizard/payment_term.py
@@ -5,6 +5,7 @@
 from odoo import models, fields, api, _
 from odoo.exceptions import except_orm, Warning, RedirectWarning, ValidationError
 import odoo.addons.decimal_precision as dp
+from odoo.tools import float_round
 
 
 class account_payment_term_rate_wizard(models.TransientModel):
@@ -12,9 +13,29 @@ class account_payment_term_rate_wizard(models.TransientModel):
     _description = "Payment Term Rate Wizard"
 
     name = fields.Char(string="Name", required=True)
-    rate = fields.Integer(string="Rate", required=True)
+    rate = fields.Integer(string="Number of rates", required=True)
     advance = fields.Float(string="Advance", digits=dp.get_precision('Payment Term'), required=True)
+    rate_value = fields.Float(string='Rate Value')
     days2 = fields.Integer(string='Day of the Month', required=True)
+    term_id = fields.Many2one('account.payment.term')
+    value = fields.Selection([
+        ('percent', 'Percent'),
+        ('fixed', 'Fixed Amount')
+    ], string='Type', required=True, default='percent',
+        help="Select here the kind of valuation related to this payment terms line.")
+
+    @api.model
+    def default_get(self, fields_list):
+        defaults = super(account_payment_term_rate_wizard, self).default_get(fields_list)
+        active_model = self.env.context.get('active_model', False)
+        active_id = self.env.context.get('active_id', False)
+        if active_model == 'account.payment.term':
+            term = self.env[active_model].browse(active_id)
+            defaults['term_id'] = active_id
+            defaults['name'] = term.name
+            if term.line_ids:
+                defaults['rate'] = len(term.line_ids)-1
+        return defaults
 
     @api.one
     @api.constrains('rate')
@@ -25,21 +46,61 @@ class account_payment_term_rate_wizard(models.TransientModel):
     @api.one
     @api.constrains('advance')
     def _check_advance(self):
-        if self.advance < 0.0 or self.advance > 100.0:
+        if self.value == 'percent' and (self.advance < 0.0 or self.advance > 100.0):
             raise ValidationError("Percentages for Advance must be between 0 and 100.")
 
     @api.multi
     def do_create_rate(self):
         line_ids = []
-        first_rate = {'value': 'percent', 'value_amount': self.advance, 'days': 0, 'days2': self.days2}
-        line_ids.append((0, 0, first_rate))
-        if self.rate > 2:
-            rest = (1 - self.advance/100) / (self.rate - 1)
 
-        for x in range(1, self.rate):
-            norm_rate = {'value': 'percent', 'value_amount': rest*100, 'days': 30 * x, 'days2': self.days2}
-            line_ids.append((0, 0, norm_rate))
+        if self.value == 'percent':
 
-        line_ids[-1] = (0, 0, {'value': 'balance', 'days': 30 * (self.rate - 1), 'days2': self.days2})
+            first_rate = {
+                'value': 'percent',
+                'value_amount': self.advance,
+                'days': 0,
+                'days2': self.days2,
+                'option': 'day_of_month'
+            }
+            line_ids.append((0, 0, first_rate))
 
-        self.env["account.payment.term"].create({'name': self.name, 'line_ids': line_ids})
+            if self.rate > 1:
+                rest = 100 * (1 - self.advance / 100) / (self.rate)
+
+            for x in range(1, self.rate + 1):
+                norm_rate = {
+                    'value': 'percent',
+                    'value_amount': float_round(rest, 6, rounding_method='DOWN'),
+                    'days': 30 * x,
+                    'days2': self.days2,
+                    'option': 'day_of_month'
+                }
+                line_ids.append((0, 0, norm_rate))
+        else:
+
+            first_rate = {
+                'value': 'fixed',
+                'value_amount': self.advance,
+                'days': 0, 'days2': self.days2,
+                'option': 'day_of_month'
+            }
+            line_ids.append((0, 0, first_rate))
+
+            for x in range(1, self.rate + 1):
+                norm_rate = {
+                    'value': 'fixed',
+                    'value_amount': self.rate_value,
+                    'days': 30 * x,
+                    'days2': self.days2,
+                    'option': 'day_of_month'
+                }
+                line_ids.append((0, 0, norm_rate))
+
+        line_ids[-1] = (
+            0, 0, {'value': 'balance', 'days': 30 * (self.rate), 'days2': self.days2, 'option': 'day_of_month'})
+
+        if not self.term_id:
+            self.env["account.payment.term"].create({'name': self.name, 'line_ids': line_ids})
+        else:
+            self.term_id.line_ids.unlink()
+            self.term_id.write({'name': self.name, 'line_ids': line_ids})
