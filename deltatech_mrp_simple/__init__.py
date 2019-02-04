8c02021beabba14405e4e2911357bdaf7156f3be
Dorin Hongu <dhongu@gmail.com>
2019-02-04 15:46:02 +0200

    mrp simple

---
+++ deltatech_mrp_simple/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# ©  2015-2018 Deltatech
+# See README.rst file on addons root folder for license details
+
+ 
+
+
+from . import wizard
+
---
+++ deltatech_mrp_simple/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# ©  2015-2018 Deltatech
+# See README.rst file on addons root folder for license details
+{
+    "name": "Deltatech Simple MRP",
+    "version": "1.0",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "www.terrabit.ro",
+    "description": """
+
+Functionalitati:
+ 
+ - Generare consumuri si nota de predare
+ 
+
+    """,
+
+    "category": "Manufacturing",
+    "depends": [
+        "stock"
+    ]
+    ,
+
+    "data": [
+        'wizard/mrp_simple_view.xml'
+    ],
+
+    "active": False,
+    "installable": True,
+}
