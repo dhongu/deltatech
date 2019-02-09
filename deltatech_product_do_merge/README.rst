54d7c690d5b8605ac775014854b2563fbfebc3b0
Dorin Hongu <dhongu@gmail.com>
2019-02-09 22:03:17 +0200

    product merge

---
+++ deltatech_product_do_merge/README.rst
@@ -0,0 +1,18 @@
+Merge Duplicate Products
+========================
+
+Merge Products
+
+We can merge duplicates products and set the new id in all documents of
+product merged.
+
+We can merge products using like mach parameter these fields:
+
+* Name
+* Reference
+
+We can select which product will be the main product.
+
+This feature do not change anything if the products to be merged have
+operations in different units of measure.
+
---
+++ deltatech_product_do_merge/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+#
+# ©  2013 Vauxoo (info@vauxoo.com)
+#              Sabrina Romero (sabrina@vauxoo.com)
+# ©  2019 Deltatech
+#              Dorin Hongu (dhongu@gmail.com)
+#
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Merge Duplicate Products",
+    "version": "11.0.0.0.7",
+    "author": "Vauxoo, Terrabit, Dorin Hongu",
+    "category": "Generic Modules",
+    "website": "http://www.vauxoo.com/",
+    "license": "",
+    "depends": [
+        "base",
+        "stock"
+    ],
+    "demo": [],
+    "data": [
+        "security/res_groups.xml",
+        "wizard/base_product_merge_view.xml"
+    ],
+    "test": [],
+    "js": [],
+    "css": [],
+    "qweb": [],
+    "installable": True,
+    "auto_install": False,
+}
