3c91b00d01030bc8e40aa94b516b791d3fb494f7
danila12 <danila@terrabit.ro>
2019-02-13 11:57:13 +0200

    verificare pret vanzare doar la facturile de vanzare

--- deltatech_sale_margin/models/account_invoice.py
+++ deltatech_sale_margin/models/account_invoice.py
@@ -52,14 +52,15 @@ class account_invoice_line(models.Model):
     @api.one
     @api.constrains('price_unit', 'purchase_price')
     def _check_sale_price(self):
-        if not self.env['res.users'].has_group('deltatech_sale_margin.group_sale_below_purchase_price'):
-            date_eval = self.invoice_id.date_invoice or fields.Date.context_today(self)
-            if self.invoice_id.currency_id and self.invoice_id.currency_id.id != self.env.user.company_id.currency_id.id:
-                from_currency = self.invoice_id.currency_id.with_context(date=date_eval)
-                price_unit = from_currency.compute(self.price_unit, self.env.user.company_id.currency_id)
-            else:
-                price_unit = self.price_unit
-            if price_unit < self.purchase_price and self.purchase_price > 0 and self.invoice_id.state in ['draft']:
-                raise Warning(_('You can not sell below the purchase price.'))
+        if  self.invoice_id.type == 'out_invoice':
+            if not self.env['res.users'].has_group('deltatech_sale_margin.group_sale_below_purchase_price'):
+                date_eval = self.invoice_id.date_invoice or fields.Date.context_today(self)
+                if self.invoice_id.currency_id and self.invoice_id.currency_id.id != self.env.user.company_id.currency_id.id:
+                    from_currency = self.invoice_id.currency_id.with_context(date=date_eval)
+                    price_unit = from_currency.compute(self.price_unit, self.env.user.company_id.currency_id)
+                else:
+                    price_unit = self.price_unit
+                if price_unit < self.purchase_price and self.purchase_price > 0 and self.invoice_id.state in ['draft']:
+                    raise Warning(_('You can not sell below the purchase price.'))
 
 
