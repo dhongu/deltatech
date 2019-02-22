ef6b19b389f47aacd0dad97013630b5125e90c0d
Dorin Hongu <dhongu@gmail.com>
2019-02-22 13:32:13 +0200

    mofrare la 11  modul deltatech product code

---
+++ deltatech_product_code/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# ©  2015-2018 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Deltatech Products Code",
+    "version": "1.0",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "www.terrabit.ro",
+
+    'category': 'Sales Management',
+
+    "depends": ["product"],
+
+    "description": """
+Features:    
+ - Generare cod de produs  
+ 
+""",
+
+    "license": "LGPL-3",
+    "data": [
+        "views/product_view.xml",
+    ],
+    "active": False,
+    "installable": True,
+
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
