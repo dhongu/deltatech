f410713936bffea43532956e77f3be79dbef41d2
Dorin Hongu <dhongu@gmail.com>
2019-03-20 09:45:21 +0200

    migrare coduri alternative

---
+++ deltatech_alternative/models/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# ©  2015-2018 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+from . import product
+
+
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+
