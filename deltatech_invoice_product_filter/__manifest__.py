df7e55fe942dbff846cff46aa721b609c9245210
Dorin Hongu <dhongu@gmail.com>
2019-03-23 07:36:22 +0200

    Conversie deltatech_invoice_product_filter

---
+++ deltatech_invoice_product_filter/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# ©  2015-2019 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Deltatech Product Filter",
+    'version': '12.0.1.0.0',
+    "author": "Terrabit, Dorin Hongu",
+    "website": "www.terrabit.ro",
+    "description": """
+
+Functionalitati:
+ - Cautare factura dupa produs
+
+
+    """,
+    "category": "Accounting",
+    "depends": ["account"],
+    "images": ['static/description/main_screenshot.png'],
+    "data": ['views/account_invoice_view.xml'],
+    "active": False,
+    "installable": True,
+}
