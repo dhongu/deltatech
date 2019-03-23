9d4c3e31dcf07fde606c24998ad031306669cead
Dorin Hongu <dhongu@gmail.com>
2019-03-23 07:36:22 +0200

    Conversie deltatech_invoice_product_filter

--- deltatech_invoice_product_filter/__init__.py
+++ deltatech_invoice_product_filter/__init__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-# ©  2015-2018 Deltatech
+# ©  2015-2019 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
 # See README.rst file on addons root folder for license details
 
 from . import models
-from . import wizard
 
 
 
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+
