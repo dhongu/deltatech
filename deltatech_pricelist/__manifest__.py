18562cef1193e35a45f16923c7d33e409fbc7ee2
Dorin Hongu <dhongu@gmail.com>
2019-03-27 15:02:43 +0200

    Pret de lista in alta moneda

--- deltatech_pricelist/__manifest__.py
+++ deltatech_pricelist/__manifest__.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
-# ©  2018 Deltatech
+# ©  2015-2019 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
 # See README.rst file on addons root folder for license details
+
 
 {
     "name": "Price List Extension",
@@ -15,10 +17,11 @@ Functionalitati:
     """,
 
     'category': 'Sales',
-    "depends": ["product"],
+    "depends": ['base',"product"],
 
     "data": [
-            'views/product_view.xml'
+        #'views/product_view.xml',
+        'views/res_company_view.xml'
     ],
 
     "active": False,
