59609883171d33c436182b140a2f0a41be340a88
Dorin Hongu <dhongu@gmail.com>
2019-04-01 16:36:12 +0300

    product_do_merge

---
+++ deltatech_product_do_merge/__manifest__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+#
+# ©  2013 Vauxoo (info@vauxoo.com)
+#              Sabrina Romero (sabrina@vauxoo.com)
+# ©  2019 Deltatech
+#              Dorin Hongu (dhongu@gmail.com)
+#
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Merge Duplicate Products",
+    "version": "12.0.0.0.7",
+    "author": "Vauxoo, Terrabit, Dorin Hongu",
+    "category": "Generic Modules",
+    "website": "http://www.vauxoo.com/",
+    "license": "LGPL-3",
+    "depends": [
+        "base",
+        "stock"
+    ],
+    "demo": [],
+    "license": "LGPL-3",
+
+    "data": [
+        "security/res_groups.xml",
+        "wizard/base_product_merge_view.xml"
+    ],
+    "images": ['images/main_screenshot.png'],
+    "test": [],
+    "js": [],
+    "css": [],
+    "qweb": [],
+    "installable": True,
+    "auto_install": False,
+}
---
+++ deltatech_product_do_merge/wizard/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+#
+# ©  2013 Vauxoo (info@vauxoo.com)
+#              Sabrina Romero (sabrina@vauxoo.com)
+# ©  2019 Deltatech
+#              Dorin Hongu (dhongu@gmail.com)
+#
+# See README.rst file on addons root folder for license details
+
+from . import base_product_merge
