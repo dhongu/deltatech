1bce18dcef1beea7e654825a88af071ac86a65e2
Dorin Hongu <dhongu@gmail.com>
2019-04-11 19:32:34 +0300

    sale team

---
+++ deltatech_sale_team/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# ©  2015-2019 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Deltatech Sale Team",
+    'version': '12.0.1.0.0',
+    "author": "Terrabit, Dorin Hongu",
+    "website": "www.terrabit.ro",
+    "description": """
+
+
+    """,
+
+    "category": "Sales",
+    "depends": ["sales_team"],
+
+    "license": "LGPL-3",
+    "data": [
+        "security/sales_team_security.xml",
+    ],
+
+    'application': False,
+
+    "installable": True,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
