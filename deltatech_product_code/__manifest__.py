ce138eba928d0a627dfe80d61cd4462738b7a631
Dorin Hongu <dhongu@gmail.com>
2019-05-02 22:55:48 +0300

    Conversie modul product_code

---
+++ deltatech_product_code/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# ©  2008-2019 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+
+{
+    "name": "Products Code",
+    "version": "1.0",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "www.terrabit.ro",
+
+    'category': 'Sales',
+
+    "depends": ["product"],
+
+    "description": """
+Features:    
+ - Generare cod de produs  
+ 
+""",
+    "data": [
+        "views/product_view.xml",
+    ],
+    "images": ['images/main_screenshot.png'],
+    "installable": True,
+
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
