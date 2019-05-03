3feb7f408f68d6fa29f094167280645c89837a2a
Dorin Hongu <dhongu@gmail.com>
2019-05-03 07:35:24 +0300

    Utilizare README.rst

---
+++ deltatech/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# ©  2015-2019 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+
+{
+    "name" : "Deltatech",
+    'version': '12.0.1.0.0',
+    "author" : "Terrabit, Dorin Hongu",
+    "website": "www.terrabit.ro",
+    "category" : "Generic Modules",
+    "depends" : ['web'],
+
+    'data': [
+
+    ],
+
+    "installable": True,
+    'application': True,
+   
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
