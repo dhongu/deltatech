4b0e5cc7400d4dd307180520e727d4ca2fe2cab3
Dorin Hongu <dhongu@gmail.com>
2019-05-07 08:23:47 +0300

    Adaugare README.rst

--- deltatech_invoice_product_filter/__manifest__.py
+++ deltatech_invoice_product_filter/__manifest__.py
@@ -4,20 +4,13 @@
 # See README.rst file on addons root folder for license details
 
 {
-    "name": "Deltatech Product Filter",
+    "name": "Invoice Product Filter",
     'version': '12.0.1.0.0',
     "author": "Terrabit, Dorin Hongu",
-    "website": "www.terrabit.ro",
-    "description": """
-
-Functionalitati:
- - Cautare factura dupa produs
-
-
-    """,
+    "website": "https://www.terrabit.ro",
     "category": "Accounting",
     "depends": ["account"],
-
+    "license": "LGPL-3",
     "data": ['views/account_invoice_view.xml'],
     "images": ['images/main_screenshot.png'],
     "installable": True,
