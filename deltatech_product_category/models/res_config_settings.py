2350a402bdc9cb8d6841bcbae4ac6805ec422581
Dorin Hongu <dhongu@gmail.com>
2019-06-07 14:22:56 +0300

    Fix

--- deltatech_product_category/models/res_config_settings.py
+++ deltatech_product_category/models/res_config_settings.py
@@ -18,7 +18,7 @@ class ResConfigSettings(models.TransientModel):
         res = super(ResConfigSettings, self).get_values()
         category_rule = self.env.ref('deltatech_product_category.product_category_comp_rule')
         res.update(
-            company_share_product=not bool(category_rule.active),
+            company_share_product_category=not bool(category_rule.active),
         )
 
         return res
