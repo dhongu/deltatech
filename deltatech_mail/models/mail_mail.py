ce9c8fff6beb503415c81befa17a7f9090ab9141
Dorin Hongu <dhongu@gmail.com>
2019-07-30 09:52:48 +0300

    substitutie email

--- deltatech_mail/models/mail_mail.py
+++ deltatech_mail/models/mail_mail.py
@@ -4,21 +4,30 @@
 
 from odoo import _, api, fields, models
 
+
 class MailMail(models.Model):
     _inherit = 'mail.mail'
-
 
     @api.multi
     def _send_prepare_values(self, partner=None):
 
         res = super(MailMail, self)._send_prepare_values(partner)
-
-        substitutions = self.env['mail.substitution'].search([('name','=',self.model)])
+        model = self.model
+        if model:
+            substitutions = self.env['mail.substitution'].search([('name', '=', model)])
+        else:
+            substitutions = self.env['mail.substitution'].search([])
 
         if substitutions:
-            res['email_to'] = []
-            for substitution in substitutions:
-                res['email_to'] += [substitution.email]
+            email_to = []
+            if model:
+                for substitution in substitutions:
+                    email_to += [substitution.email]
+            else:
+                for substitution in substitutions:
+                    if substitution in self.message_id:
+                        email_to += [substitution.email]
+            if email_to:
+                res['email_to'] = email_to
 
-
-        return  res
+        return res
