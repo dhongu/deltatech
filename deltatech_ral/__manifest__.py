b5229a70ad42935412780a8429db28c6f01c9a0b
Dorin Hongu <dhongu@gmail.com>
2019-08-15 07:37:19 +0300

    conversie mmodul ral

---
+++ deltatech_ral/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# ©  2015-2019 Deltatech
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "RAL",
+    'version': '12.0.1.0.0',
+    "author": "Terrabit, Dorin Hongu",
+
+
+    "category": "Generic Modules/Other",
+    "depends": ["base", "stock", "mrp"],
+
+    "data": [
+        "views/stock_view.xml",
+        "views/mrp_view.xml"
+    ],
+
+    "installable": True,
+}
