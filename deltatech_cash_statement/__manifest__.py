bc9e19c2250fdc47bab73fcd18a6b3ef24c9fae9
Dorin Hongu <dhongu@gmail.com>
2019-08-25 15:57:33 +0300

    Upgrade cod 'cash_statement'

---
+++ deltatech_cash_statement/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# ©  2008-2018 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Deltatech Cash Statement Extension",
+    'version': '12.0.3.0.0',
+    "author": "Dorin Hongu",
+    "website": "",
+
+
+    'category': 'Accounting & Finance',
+    "depends": ["account"],
+
+    "data": [
+        'wizard/account_cash_update_balances_view.xml'
+    ],
+
+    "images": ['images/main_screenshot.png'],
+    "installable": True,
+}
