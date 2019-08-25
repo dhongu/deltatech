eb952ff7a8ff66a7b405f5b5cbab9419cc03bf3c
Dorin Hongu <dhongu@gmail.com>
2019-08-25 21:12:48 +0300

    Determinare automata lot

--- deltatech_mrp_process/models/mrp_workorder.py
+++ deltatech_mrp_process/models/mrp_workorder.py
@@ -5,31 +5,28 @@
 
 from odoo import models, fields, _, api
 
+
 class MrpWorkorder(models.Model):
     _inherit = 'mrp.workorder'
 
     location_dest_id = fields.Many2one('stock.location', related='production_id.location_dest_id',
                                        string='Tank', readonly=False)
 
-
-    #todo: de investigat de ce nu se salveaza locatia dest pentru ca e un camp related si a trebuit sa-l pun in write
+    # todo: de investigat de ce nu se salveaza locatia dest pentru ca e un camp related si a trebuit sa-l pun in write
 
     @api.multi
     def write(self, vals):
         if 'location_dest_id' in vals:
             for workorder in self:
-                workorder.production_id.write({'location_dest_id':vals['location_dest_id']})
+                workorder.production_id.write({'location_dest_id': vals['location_dest_id']})
 
         return super(MrpWorkorder, self).write(vals)
-
 
     @api.onchange('qty_producing')
     def _onchange_qty_producing(self):
         if self.state == 'ready':
             return super(MrpWorkorder, self)._onchange_qty_producing()
-        
-        
-        
+
     @api.multi
     def button_finish(self):
         super(MrpWorkorder, self).button_finish()
@@ -40,13 +37,10 @@ class MrpWorkorder(models.Model):
                     values['state'] = 'ready'
                 workorder.next_work_order_id.write(values)
 
-
-
     @api.multi
     def button_next(self):
         if self.next_work_order_id:
             # action = self.env.ref('mrp.action_work_orders').read()[0]
-
 
             action = {
                 'name': _('Work Order'),
@@ -59,7 +53,7 @@ class MrpWorkorder(models.Model):
 
 
         else:
-            action =  {
+            action = {
                 'type': 'ir.actions.act_window',
                 'res_model': 'mrp.production',
                 'views': [[self.env.ref('mrp.mrp_production_form_view').id, 'form']],
@@ -71,7 +65,6 @@ class MrpWorkorder(models.Model):
             }
         return action
 
-
     @api.multi
     def button_done_finish(self):
         self.record_production()
@@ -80,7 +73,17 @@ class MrpWorkorder(models.Model):
                 self.button_finish()
                 workorder.active_move_line_ids.unlink()
 
-
     @api.multi
     def record_production(self):
         super(MrpWorkorder, self).record_production()
+
+    def _generate_lot_ids(self):
+        """ Generate stock move lines """
+        super(MrpWorkorder, self)._generate_lot_ids()
+
+        move_lines = self.active_move_line_ids
+        for move_line in move_lines:
+            line = move_line.move_id.move_line_ids.filtered(lambda line: line.lot_id != False and line.product_id == move_line.product_id)
+            if line:
+                line = line[0]
+                move_line.write({'lot_id': line.lot_id.id})
