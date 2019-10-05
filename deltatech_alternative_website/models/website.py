3950d9f51bf431734ac28edf8eaeb45ab3235117
Dorin Hongu <dhongu@gmail.com>
2019-10-05 20:12:50 +0300

    Fix watermark

--- deltatech_alternative_website/models/website.py
+++ deltatech_alternative_website/models/website.py
@@ -40,4 +40,20 @@ class website(models.Model):
     #     response =  super(website,self)._image(cr, uid, model, id, field, response, max_width, max_height, cache, context)
     #     return response
 
+class IrHttp(models.AbstractModel):
+    _inherit = 'ir.http'
 
+    @classmethod
+    def binary_content(cls, xmlid=None, model='ir.attachment', id=None, field='datas',
+                       unique=False, filename=None, filename_field='datas_fname', download=False,
+                       mimetype=None, default_mimetype='application/octet-stream',
+                       access_token=None, related_id=None, access_mode=None, env=None):
+
+        if model == 'product.template' and field == 'image':
+            field = 'watermark_image'
+
+        return super(IrHttp, cls).binary_content(
+            xmlid=xmlid, model=model, id=id, field=field, unique=unique, filename=filename,
+            filename_field=filename_field, download=download, mimetype=mimetype,
+            default_mimetype=default_mimetype, access_token=access_token, related_id=related_id,
+            access_mode=access_mode, env=env)
