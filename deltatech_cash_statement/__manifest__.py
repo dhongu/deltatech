d633b50c054d4788640bca93ef35c013820fe4a5
Dorin Hongu <dhongu@gmail.com>
2019-10-16 22:06:17 +0300

    Adugare main_image

--- deltatech_cash_statement/__manifest__.py
+++ deltatech_cash_statement/__manifest__.py
@@ -9,7 +9,9 @@
     "author": "Dorin Hongu",
     "website": "",
 
-
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
     'category': 'Accounting & Finance',
     "depends": ["account"],
 
