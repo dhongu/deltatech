7db6c303fe9f461d1693e36ced8c4c06f3c9af2f
Dorin Hongu <dhongu@gmail.com>
2019-10-16 11:28:39 +0300

    Conversie fast purchase

---
+++ deltatech_fast_sale/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# ©  2015-2018 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Fast Sale",
+    'version': '13.0.1.0.0',
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+
+    "category": "Sales",
+    "depends": ["base", "sale_management", 'stock','sale_stock'],
+
+    "license": "LGPL-3",
+    "data": ['views/sale_view.xml',
+             'views/stock_view.xml'],
+    "images": ['images/main_screenshot.png'],
+
+    "installable": True,
+}
---
+++ deltatech_fast_sale/models/stock.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# ©  2015-2018 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+
+from odoo import api, models
+
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    def action_view_sale_invoice(self):
+        if self.sale_id:
+            action_obj = self.env.ref('sale.action_view_sale_advance_payment_inv')
+            action = action_obj.read()[0]
+            action['context'] = {'active_id': self.sale_id.id,
+                                 'active_ids': self.sale_id.ids,
+                                 'notice':True}
+            return action
