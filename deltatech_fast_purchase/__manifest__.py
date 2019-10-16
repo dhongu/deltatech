9c3b76f7322b029f1a70f917610c94d6caceaf37
Dorin Hongu <dhongu@gmail.com>
2019-10-16 11:28:39 +0300

    Conversie fast purchase

---
+++ deltatech_fast_purchase/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# ©  2015-2019 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Fast Purchase",
+    'version': '13.0.1.0.0',
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    'summary': 'Achizitie rapida',
+
+
+    "category": "Generic Modules/Stock",
+    "depends": [
+        "base",
+        "purchase_stock",
+        "stock"
+    ],
+
+    "license": "LGPL-3",
+    "data": [
+        'views/purchase_view.xml',
+             'views/stock_view.xml'
+    ],
+    "images": ['images/main_screenshot.png'],
+
+    "installable": True,
+}
