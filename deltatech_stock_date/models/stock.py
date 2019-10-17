0b3a03c67d8f5a6c325dcb94a3d3ed51eaa47c07
Dorin Hongu <dhongu@gmail.com>
2019-10-17 23:20:21 +0300

    ajustari la modulul pentru selectie data la transfer stoc

--- deltatech_stock_date/models/stock.py
+++ deltatech_stock_date/models/stock.py
@@ -16,35 +16,13 @@ from odoo import SUPERUSER_ID, api
 
 import odoo.addons.decimal_precision as dp
 
-"""
-class stock_pack_operation(models.Model):
-    _inherit = "stock.pack.operation"
 
-    @api.model
-    def create(self,   vals ):
-
-        if vals.get("picking_id"):
-            picking = self.env["stock.picking"].browse( vals['picking_id'])
-            vals['date'] = picking.min_date  # trebuie sa fie minimul dintre data curenta si data din picking
-        res_id = super(stock_pack_operation, self).create( vals)
-        return res_id
-"""
 
 
 class stock_quant(models.Model):
     _inherit = "stock.quant"
 
-    """
-    @api.model
-    def _quant_create_from_move(self, qty, move, lot_id=False, owner_id=False,
-                                src_package_id=False, dest_package_id=False,
-                                force_location_from=False, force_location_to=False):
-        quant = super(stock_quant, self)._quant_create_from_move( qty, move, lot_id=lot_id, owner_id=owner_id,
-                                 src_package_id=src_package_id, dest_package_id=dest_package_id,
-                                 force_location_from=force_location_from, force_location_to=force_location_to)
-        quant.write({'in_date': move.date})
-        return quant    
-    """
+
 
     @api.model
     def _update_available_quantity(self, product_id, location_id, quantity, lot_id=None, package_id=None, owner_id=None,
@@ -61,7 +39,7 @@ class stock_move(models.Model):
     @api.multi
     def write(self, vals):
         date_fields = {'date', 'date_expected'}
-        use_date = self.env.context.get('use_date', False)
+        use_date = self.env.context.get('force_period_date', False)
         if date_fields.intersection(vals):
             if not use_date:
 
@@ -75,18 +53,18 @@ class stock_move(models.Model):
                         move.move_line_ids.write({'date': vals['date']})
                         # move.quant_ids.write({'in_date': vals['date']})
 
-                    if 'date_expected' in vals:
-                        if isinstance(vals['date_expected'], str):  # de unde ajunge aici cu string ?
-                            vals['date_expected'] = fields.Datetime.to_datetime(vals['date_expected'])
-                        move_date = vals.get('date', move.date)
-                        if move_date.date() < today and move_date < vals['date_expected']:
-                            vals['date_expected'] = move_date
+                    # if 'date_expected' in vals:
+                    #     if isinstance(vals['date_expected'], str):  # de unde ajunge aici cu string ?
+                    #         vals['date_expected'] = fields.Datetime.to_datetime(vals['date_expected'])
+                    #     move_date = vals.get('date', move.date)
+                    #     if move_date.date() < today and move_date < vals['date_expected']:
+                    #         vals['date_expected'] = move_date
             else:
 
                 if 'date' in vals:
                     vals['date'] = use_date
-                if 'date_expected' in vals:
-                    vals['date_expected'] = use_date
+                # if 'date_expected' in vals:
+                #     vals['date_expected'] = use_date
 
         return super(stock_move, self).write(vals)
 
@@ -95,10 +73,14 @@ class Picking(models.Model):
     _inherit = "stock.picking"
 
     @api.multi
+    def button_validate(self):
+        return super(Picking, self.with_context(force_period_date=self.scheduled_date)).button_validate()
+
+    @api.multi
     def action_done(self):
         super(Picking, self).action_done()
-        use_date = self.env.context.get('use_date', False)
+        use_date = self.env.context.get('force_period_date', False)
         if use_date:
-            self.write({'date': self.date})
-            self.move_lines.write({'date_expected': use_date, 'date': use_date})
+            self.write({'date': use_date, 'date_done':use_date})
+            self.move_lines.write({ 'date': use_date})  # 'date_expected': use_date,
             self.move_line_ids.write({'date': use_date})
--- deltatech_stock_date/wizard/stock_immediate_transfer.py
+++ deltatech_stock_date/wizard/stock_immediate_transfer.py
@@ -11,17 +11,15 @@ import odoo.addons.decimal_precision as dp
 class StockImmediateTransfer(models.TransientModel):
     _inherit = 'stock.immediate.transfer'
 
-
-    date = fields.Datetime(string="Date", related='pick_ids.scheduled_date',  store=False, readonly=False)
+    date = fields.Datetime(string="Date")
 
     @api.model
     def default_get(self, fields_list):
         res = super(StockImmediateTransfer, self).default_get(fields_list)
+        res['date'] = self.env.context.get('force_period_date',fields.Datetime.now())
         return res
 
     @api.multi
     def process(self):
-        self.pick_ids.write({'date':self.date})
-        return super(StockImmediateTransfer, self.with_context(use_date=self.date)).process()
-
-
+        self.pick_ids.write({'date': self.date, 'date_done': self.date})
+        return super(StockImmediateTransfer, self.with_context(force_period_date=self.date)).process()
