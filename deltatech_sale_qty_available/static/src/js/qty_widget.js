5831ee2f82f91fc8924639314ca18ee71f8448ad
Dorin Hongu <dhongu@gmail.com>
2019-11-05 21:10:32 +0200

    conversie din widget in fieldwidget

--- deltatech_sale_qty_available/static/src/js/qty_widget.js
+++ deltatech_sale_qty_available/static/src/js/qty_widget.js
@@ -8,12 +8,63 @@ var Widget = require('web.Widget');
 var Context = require('web.Context');
 var data_manager = require('web.data_manager');
 var widget_registry = require('web.widget_registry');
+var field_registry = require('web.field_registry');
+
 var config = require('web.config');
 
 var _t = core._t;
 var time = require('web.time');
+var AbstractField = require('web.AbstractField');
+var ListRenderer = require('web.ListRenderer');
+
+
+
+var QtyAvailableField = AbstractField.extend({
+    template: 'deltatech_sale_qty_available.qtyAvailable',
+    events: _.extend({}, Widget.prototype.events, {
+        'click .fa-info-circle': '_onClickButton',
+    }),
+
+    init: function () {
+        this._super.apply(this, arguments);
+        this.data =  this.record.data;
+    },
+    _render: function () {
+        var self = this;
+        self._setPopOver();
+    },
+
+    _setPopOver: function () {
+        var self = this;
+        this.data =  this.record.data;
+        if (!this.data.product_id) {
+            return;
+        }
+        var $content = $(QWeb.render('deltatech_sale_qty_available.QtyDetailPopOver', {
+            data: this.data,
+        }));
+
+        var options = {
+            content: $content,
+            html: true,
+            placement: 'left',
+            title: _t('Availability'),
+            trigger: 'focus',
+            delay: {'show': 0, 'hide': 100 },
+        };
+        this.$el.popover(options);
+    },
+    _onClickButton: function () {
+        // We add the property special click on the widget link.
+        // This hack allows us to trigger the popover (see _setPopOver) without
+        // triggering the _onRowClicked that opens the order line form view.
+        this.$el.find('.fa-info-circle').prop('special_click', true);
+    },
+})
+
 
 var QtyAvailableWidget = Widget.extend({
+
     template: 'deltatech_sale_qty_available.qtyAvailable',
     events: _.extend({}, Widget.prototype.events, {
         'click .fa-info-circle': '_onClickButton',
@@ -84,6 +135,8 @@ var QtyAvailableWidget = Widget.extend({
 });
 
 widget_registry.add('qty_available_widget', QtyAvailableWidget);
+
+field_registry.add('qty_available', QtyAvailableField);
 
 return QtyAvailableWidget;
 });
