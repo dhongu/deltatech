ac5ba930bd2e7629c4c30b08254a4020ebbae7f7
Dorin Hongu <dhongu@gmail.com>
2019-11-21 15:40:23 +0200

    Modul nou Merge Tasks

---
+++ deltatech_task_do_merge/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+#
+# ©  2019 Deltatech
+#              Dorin Hongu (dhongu@gmail.com)
+#
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Merge Tasks",
+    "version": "12.0.0.0.7",
+    "author": "Vauxoo, Terrabit, Dorin Hongu",
+    "category": "Generic Modules",
+    "website": "https://www.terrabit.ro",
+    "license": "LGPL-3",
+    "depends": [
+
+        "project"
+    ],
+    "demo": [],
+    "license": "LGPL-3",
+
+    "data": [
+        "security/res_groups.xml",
+        "wizard/base_task_merge_view.xml"
+    ],
+    "images": ['images/main_screenshot.png'],
+    "installable": True,
+    "auto_install": False,
+}
+
+
+
+
