be9a95e75fa03710d1bbf1ceba71f0ef3f62297d
Dorin Hongu <dhongu@gmail.com>
2019-11-23 10:24:27 +0200

    Adaugare modul Catalog de produse

---
+++ deltatech_product_catalog/__openerp__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# ©  2008-2019 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+
+
+{
+    'name': "Product Catalog",
+    'version': '8.0.1.0.0',
+    'summary': """This module helps to print the catalog of  the multi products""",
+
+    'category': 'Inventory',
+    "author": "Terrabit, Dorin Hongu",
+    'company': 'Terrabit',
+    'maintainer': 'Terrabit',
+    'website': "https://www.terrabit.ro",
+    'depends': ['product'],
+    'data': [
+
+        'report/product_catalog_report.xml',
+        'report/product_catalog_template.xml',
+    ],
+    "images": ['images/main_screenshot.png'],
+    'license': "AGPL-3",
+    'installable': True,
+    'application': False,
+}
---
+++ deltatech_product_catalog/report/product_catalog.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+# ©  2008-2019 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+from openerp import api, models
+
+
+class ProductCatalogReport(models.AbstractModel):
+    _name = "report.deltatech_product_catalog.report_product_catalog"
+    _template = 'deltatech_product_catalog.report_product_catalog'
+
+    @api.multi
+    def render_html(self, data=None):
+        docargs = self._get_report_values(docids=self._ids, data=data)
+        return self.env['report'].render(self._template, docargs)
+
+    @api.model
+    def _get_report_values(self, docids, data=None):
+        products = self.env['product.template'].browse(docids)
+
+        report = self.env['report']._get_report_from_name(self._template)
+        docargs = {
+            'data': data,
+            'doc_ids': docids,
+            'doc_model': report.model,
+            'docs': products,
+        }
+        return docargs
+
+
+class CategoryCatalogReport(models.AbstractModel):
+    _name = "report.deltatech_product_catalog.report_category_catalog"
+    _template = 'deltatech_product_catalog.report_category_catalog'
+
+    @api.multi
+    def render_html(self, data=None):
+        docargs = self._get_report_values(docids=self._ids, data=data)
+        return self.env['report'].render(self._template, docargs)
+
+    @api.model
+    def _get_report_values(self, docids, data=None):
+        domain = [('public_categ_ids', 'in', docids)]
+        products = self.env['product.template'].search(domain)
+
+        report = self.env['ir.actions.report']._get_report_from_name(self._template)
+        docargs = {
+            'data': data,
+            'doc_ids': docids,
+            'doc_model': report.model,
+            'docs': products,
+        }
+        return docargs
