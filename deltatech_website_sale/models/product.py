f2d9cb3f299161bdb9205c131b9baf58a8361e7f
Dorin Hongu <dhongu@gmail.com>
2020-01-06 16:02:52 +0200

    determinare pret de lista din  web_list_price .

--- deltatech_website_sale/models/product.py
+++ deltatech_website_sale/models/product.py
@@ -12,7 +12,7 @@ from dateutil.relativedelta import relativedelta
 from datetime import datetime, date, timedelta
 
 
-class product_template(models.Model):
+class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     # #008a00!important  - in stoc
@@ -26,11 +26,37 @@ class product_template(models.Model):
     inventory_availability = fields.Selection(default='threshold')
     available_threshold = fields.Float(default=1.0)
 
-    published_on_website = fields.Boolean(related='is_published' , readonly=False, string='Published')
-
+    published_on_website = fields.Boolean(related='is_published', readonly=False, string='Published')
 
     @api.multi
-    @api.depends('qty_available','at_order')
+    def _get_combination_info(self, combination=False, product_id=False, add_qty=1, pricelist=False,
+                              parent_combination=False, only_template=False):
+
+        combination_info = super(ProductTemplate, self)._get_combination_info(
+            combination=combination, product_id=product_id, add_qty=add_qty, pricelist=pricelist,
+            parent_combination=parent_combination, only_template=only_template)
+
+        if self.env.context.get('website_id'):
+            current_website = self.env['website'].get_current_website()
+            if not pricelist:
+                pricelist = current_website.get_current_pricelist()
+
+
+            price = combination_info['price']
+
+            list_price = combination_info.get('web_list_price',  combination_info['price'])
+
+            has_discounted_price = pricelist.currency_id.compare_amounts(list_price, price) == 1
+
+            combination_info.update(
+                price=price,
+                list_price=list_price,
+                has_discounted_price=has_discounted_price,
+            )
+        return combination_info
+
+    @api.multi
+    @api.depends('qty_available', 'at_order')
     def _compute_available(self):
         res = {}
         for product in self:
@@ -42,4 +68,3 @@ class product_template(models.Model):
                 else:
                     product.is_qty_available = 'provider'
         return res
-
