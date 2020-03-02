fd82e4c50425c1b2071e9aef61a60aeb4c2e9776
Dorin Hongu <dhongu@gmail.com>
2020-03-02 11:25:26 +0200

    Modul pentru limitare gama de numere (secventa)

---
+++ deltatech_sequence/models/ir_sequence.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# ©  2008-2018 Deltatech
+# See README.rst file on addons root folder for license details
+
+
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
+
+
+class Sequence(models.Model):
+    _inherit = 'ir.sequence'
+
+    number_first = fields.Integer('First Number')
+    number_last = fields.Integer('Last Number')
+
+    def _next(self):
+        if self.number_next_actual > self.number_last and self.number_last:
+            raise ValidationError('The range of numbers has reached its maximum value')
+        return  super(Sequence, self)._next()
