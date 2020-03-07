12713ece686444e15cc5a2a9af05171805eae47b
Dorin Hongu <dhongu@gmail.com>
2020-03-07 14:06:02 +0200

    adaugatre modul deltatech blog

---
+++ deltatech_website_blog/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# ©  2015-2019 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+{
+    'name': 'Deltatech Web Site Blog',
+    'category': 'Website',
+    'summary': 'Blog Extension',
+
+    'version': '1.0',
+
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+
+    'depends': ['website_blog'],
+    'data': [
+
+
+    ],
+
+    "images": ['images/main_screenshot.png'],
+    "installable": True,
+
+}
