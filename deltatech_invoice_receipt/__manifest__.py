84032e06bde0f2605f1c599e4f1fdd34b778f2ef
Dorin Hongu <dhongu@gmail.com>
2020-03-30 09:07:06 +0300

    conversie deltatech invoice receipt

---
+++ deltatech_invoice_receipt/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# ©  2008-2019 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+
+{
+    "name": "Deltatech Invoice Receipt",
+    'version': '13.0.1.0.0',
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+
+
+    "category": "Accounting",
+    "depends": [
+        "purchase_stock",
+    ],
+
+    "data": [
+       # 'account_invoice_view.xml'
+       #  'views/invoice_view.xml'
+    ],
+
+    "images": ['static/description/main_screenshot.png'],
+
+    "active": False,
+    "installable": True,
+}
