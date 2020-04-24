e386917ae75e0d551dd9342926d55404a893ba2d
danila12 <danila@terrabit.ro>
2020-04-24 11:06:04 +0300

    added deltatech_saleorder_report module

---
+++ deltatech_saleorder_report/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# ©  2015-2019 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Deltatech Sale Order Report",
+    'version': '12.0.1.0.0',
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+
+
+    "category": "Sales",
+    "depends": [
+        "sale",
+        "l10n_ro_config",
+    ],
+
+    "license": "LGPL-3",
+    "data": [
+        "views/sale_order.xml",
+    ],
+    "images": ['images/main_screenshot.png'],
+
+    "installable": True,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
