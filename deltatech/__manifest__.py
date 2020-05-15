f078a2e59d28ee42eba928875ad365d92fa2cfcd
Dorin Hongu <dhongu@gmail.com>
2020-05-15 09:03:34 +0300

    test main main screen

--- deltatech/__manifest__.py
+++ deltatech/__manifest__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# ©  2015-2019 Deltatech
+# ©  2008-2019 Deltatech
 #              Dorin Hongu <dhongu(@)gmail(.)com
 # See README.rst file on addons root folder for license details
 
@@ -9,13 +9,16 @@
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     'summary': 'Generic module',
-    "category": "Tools",
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    "category": "Administration",
     "depends": ['web','base'],
     'data': [
         'views/base_view.xml'
     ],
     "license": "LGPL-3",
-    "images": ['images/main_screenshot.png'],
+    "images": ['/static/description/main_screenshot.svg'],
     "installable": True,
     'application': False,
 
