60914871c4eb838438cc36debe20857d7593b15b
Dorin Hongu <dhongu@gmail.com>
2020-07-28 14:38:00 +0300

    SMS la confirmare comanda de vanzare

--- deltatech_sms_sale/models/res_company.py
+++ deltatech_sms_sale/models/res_company.py
@@ -7,16 +7,31 @@ from odoo import fields, models
 class Company(models.Model):
     _inherit = "res.company"
 
-    def _default_confirmation_sms_sale_order_template(self):
+    def _default_sms_sale_order_confirm_template(self):
         try:
-            return self.env.ref('deltatech_sms_sale.sms_template_data_sale_order').id
+            return self.env.ref('deltatech_sms_sale.sms_template_data_sale_order_confirm').id
         except ValueError:
             return False
 
-    sale_order_sms_validation = fields.Boolean("SMS Confirmation", default=True)
-    sale_sms_confirmation_template_id = fields.Many2one(
-        'sms.template', string="SMS Template",
+    def _default_sms_sale_order_post_template(self):
+        try:
+            return self.env.ref('deltatech_sms_sale.sms_template_data_sale_order_post').id
+        except ValueError:
+            return False
+
+    sale_order_sms_post = fields.Boolean("SMS Post", default=True)
+    sale_order_sms_post_template_id = fields.Many2one(
+        'sms.template', string="SMS Template Order Post",
         domain="[('model', '=', 'sale.order')]",
-        default=_default_confirmation_sms_sale_order_template,
+        default=_default_sms_sale_order_post_template,
+        help="SMS sent to the customer once the sale order is posted from website.")
+
+    sale_order_sms_confirm = fields.Boolean("SMS Confirm", default=True)
+    sale_order_sms_confirm_template_id = fields.Many2one(
+        'sms.template', string="SMS Template Order Confirmed",
+        domain="[('model', '=', 'sale.order')]",
+        default=_default_sms_sale_order_confirm_template,
         help="SMS sent to the customer once the sale order is confirmed.")
-    has_received_warning_sale_sms = fields.Boolean()
+
+
+
--- deltatech_sms_sale/models/sale_order.py
+++ deltatech_sms_sale/models/sale_order.py
@@ -12,19 +12,33 @@ import threading
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-
-
-    def action_confirm(self):
-
-        res = super(SaleOrder, self).action_confirm()
+    def action_quotation_send(self):
+        res = super(SaleOrder, self).action_quotation_send()
         if not getattr(threading.currentThread(), 'testing', False) and not self.env.registry.in_test_mode():
-            sales = self.filtered(lambda p: p.company_id.sale_order_sms_validation  and (p.partner_id.mobile or p.partner_id.phone))
+            sales = self.filtered(
+                lambda p: p.company_id.sale_order_sms_post and (p.partner_id.mobile or p.partner_id.phone))
             for sale in sales:
                 # Sudo as the user has not always the right to read this sms template.
-                template = sale.company_id.sudo().sale_sms_confirmation_template_id
+                template = sale.company_id.sudo().sale_order_sms_post_template_id
                 sale._message_sms_with_template(
                     template=template,
                     partner_ids=sale.partner_id.ids,
                     put_in_queue=False
                 )
-        return  res
+
+        return res
+
+    def action_confirm(self):
+        res = super(SaleOrder, self).action_confirm()
+        if not getattr(threading.currentThread(), 'testing', False) and not self.env.registry.in_test_mode():
+            sales = self.filtered(
+                lambda p: p.company_id.sale_order_sms_confirm and (p.partner_id.mobile or p.partner_id.phone))
+            for sale in sales:
+                # Sudo as the user has not always the right to read this sms template.
+                template = sale.company_id.sudo().sale_order_sms_confirm_template_id
+                sale._message_sms_with_template(
+                    template=template,
+                    partner_ids=sale.partner_id.ids,
+                    put_in_queue=False
+                )
+        return res
