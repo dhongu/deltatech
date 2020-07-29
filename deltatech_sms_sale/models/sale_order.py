fb168d5a83a65dccc9d0ff2e40f43ad43a4b48ec
Dorin Hongu <dhongu@gmail.com>
2020-07-29 12:39:02 +0300

    SMS   mail_notify_author

--- deltatech_sms_sale/models/sale_order.py
+++ deltatech_sms_sale/models/sale_order.py
@@ -13,13 +13,15 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
 
+
+
     def _send_order_confirmation_mail(self):
         if not getattr(threading.currentThread(), 'testing', False) and not self.env.registry.in_test_mode():
             sales = self.filtered(lambda o: o.company_id.sale_order_sms_post and ( o.partner_id.mobile or o.partner_id.phone))
             for sale in sales:
                 # Sudo as the user has not always the right to read this sms template.
                 template = sale.company_id.sudo().sale_order_sms_post_template_id
-                sale._message_sms_with_template(
+                sale.with_context(mail_notify_author=True)._message_sms_with_template(
                     template=template,
                     partner_ids=sale.partner_id.ids,
                     put_in_queue=False
@@ -38,7 +40,7 @@ class SaleOrder(models.Model):
             for sale in sales:
                 # Sudo as the user has not always the right to read this sms template.
                 template = sale.company_id.sudo().sale_order_sms_confirm_template_id
-                sale._message_sms_with_template(
+                sale.with_context(mail_notify_author=True)._message_sms_with_template(
                     template=template,
                     partner_ids=sale.partner_id.ids,
                     put_in_queue=False
