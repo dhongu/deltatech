427c3e98848ef30fa6a8f885dbd22a8b130aeb0a
Dorin Hongu <dhongu@gmail.com>
2020-08-03 19:14:17 +0300

    Adaugare modul lista de produse

--- deltatech_product_list/models/__init__.py
+++ deltatech_product_list/models/__init__.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
-# ©  2015-2018 Deltatech
+# ©  2008-2020 Deltatech
 #              Dorin Hongu <dhongu(@)gmail(.)com
 # See README.rst file on addons root folder for license details
 
 
-
-from . import models
+from . import product_list
 
 
