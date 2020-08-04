72b8eb98bb39594eb02fd0aa8b98785e778d2f91
Dorin Hongu <dhongu@gmail.com>
2020-08-04 21:53:40 +0300

    [add]Maintenance Analytic

---
+++ deltatech_maintenance_analytic/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# ©  2008-2020 Deltatech
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Maintenance Analytic",
+    'version': '12.0.1.0.0',
+    "author": "Terrabit, Dorin Hongu",
+    "website": "",
+
+
+    'category': 'Human Resources',
+    "depends": [
+        "analytic",
+        "maintenance"
+    ],
+
+    "data": [
+        'views/maintenance_view.xml',
+    ],
+    "images": ['static/description/main_screenshot.png'],
+    "installable": True,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
