2fc9cee4edfcc5375463cd452cf42f4b4c317ef9
Dorin Hongu <dhongu@gmail.com>
2020-08-04 08:23:47 +0300

    [add] product slider snippet

---
+++ deltatech_website_product_slider_snippet/models/product.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# ©  2015-2020 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+from odoo import api, fields, models, _
+from odoo.tools.translate import html_translate
+
+
+
