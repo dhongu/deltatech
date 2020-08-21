0056e405b7a6bea8f0a829d219097588df8fe7f7
Dorin Hongu <dhongu@gmail.com>
2020-08-21 21:12:23 +0300

    adaugare modul afisare descriere web in produs

---
+++ deltatech_website_short_description/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# ©  2015-2020 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+{
+    'name': 'eCommerce Sale Short Description',
+    'category': 'Website',
+    'summary': 'eCommerce short description',
+
+    'version': '12.0.1.0.0',
+
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+
+    'depends': ['website_sale_stock'],
+    'data': [
+        'views/product_view.xml'
+    ],
+
+    "images": ['static/description/main_screenshot.png'],
+    "installable": True,
+
+}
