09f05197781b950757378167246a4fe777c193e5
Dorin Hongu <dhongu@gmail.com>
2020-09-07 13:42:41 +0300

    Mutare module

--- deltatech_cash_statement/wizard/__init__.py
+++
@@ -1,6 +0,0 @@
-# ©  2008-2018 Deltatech
-#              Dorin Hongu <dhongu(@)gmail(.)com
-# See README.rst file on addons root folder for license details#######################
-
-
-from . import account_cash_update_balances
