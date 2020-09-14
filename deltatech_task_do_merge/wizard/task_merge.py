1332a69471bc896d32722b21df920006d92ada37
Dorin Hongu <dhongu@gmail.com>
2020-09-14 20:14:59 +0300

    fix task_merge

--- deltatech_task_do_merge/wizard/task_merge.py
+++ deltatech_task_do_merge/wizard/task_merge.py
@@ -7,16 +7,16 @@ from odoo import fields, models
 
 class MergeTask(models.TransientModel):
     """
-        The idea behind this wizard is to create a list of potential statement to
+        The idea behind this wizard is to create a list of potential task to
         merge. We use two objects, the first one is the wizard for the end-user.
         And the second will contain the object list to merge.
     """
 
     _inherit = "merge.object.wizard"
-    _name = "merge.product.wizard"
+    _name = "merge.task.wizard"
     _description = "Merge Task Wizard"
     _model_merge = "project.task"
     _table_merge = "project_task"
 
-    object_ids = fields.Many2many(_model_merge, string="Statement")
-    dst_object_id = fields.Many2one(_model_merge, string="Destination Statement")
+    object_ids = fields.Many2many(_model_merge, string="Task")
+    dst_object_id = fields.Many2one(_model_merge, string="Destination Task")
