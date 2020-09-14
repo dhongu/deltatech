f09f6fe6a60977f188f5f40c00da77fb61251eac
cojocariudaniel1 <56277656+cojocariudaniel1@users.noreply.github.com>
2020-09-14 07:37:25 +0300

    14.0.deltatech select journal (#66)
    * 14.0.deltatech_select_journal migration
    
    * 14.0.deltatech_select_journal migration
    
    * 14.0.deltatech_select_journal migration

--- deltatech_select_journal/__manifest__.py
+++ deltatech_select_journal/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Deltatech Select Journal",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "author": "Terrabit, Dorin Hongu",
     "license": "AGPL-3",
     "website": "https://www.terrabit.ro",
