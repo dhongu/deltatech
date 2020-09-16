bb017818357ca38da36fa29abcf1e5b003aaaafd
cojocariudaniel1 <56277656+cojocariudaniel1@users.noreply.github.com>
2020-09-16 07:26:46 +0300

    14.0.deltatech product catalog (#67)
    * 14.0.deltatech_select_journal migration
    
    * 14.0.deltatech_product_catalog migration

--- deltatech_product_catalog/__manifest__.py
+++ deltatech_product_catalog/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "Product Catalog",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "summary": """This module helps to print the catalog of the multi products""",
     "category": "Inventory",
     "author": "Terrabit, Dorin Hongu",
