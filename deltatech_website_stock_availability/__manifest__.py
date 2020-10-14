a5560e0bdbc9c789d24145cc2746622e379b269a
Dorin Hongu <dhongu@gmail.com>
2020-10-14 06:53:47 +0300

    adaugare modul care permite afisarea stocului pe website fara sa blocheze achizitia daca stocul nu este sufieicnt

---
+++ deltatech_website_stock_availability/__manifest__.py
@@ -0,0 +1,18 @@
+# ©  2015-2020 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+{
+    "name": "eCommerce Stock Availability",
+    "category": "Website",
+    "summary": "eCommerce Stock Availability",
+    "version": "13.0.1.0.0",
+    "license": "LGPL-3",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "depends": ["website_sale_stock"],
+    "data": ["views/product_view.xml", "views/website_sale_stock_templates.xml"],
+    "images": ["static/description/main_screenshot.png"],
+    "installable": True,
+    "development_status": "stable",
+    "maintainers": ["dhongu"],
+}
