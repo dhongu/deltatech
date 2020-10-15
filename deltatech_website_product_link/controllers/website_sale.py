1b32b857e566a5538a01f9bd783a3577041bff40
Dorin Hongu <dhongu@gmail.com>
2020-10-15 18:35:03 +0300

    generare 404 daca nu exista pagina

--- deltatech_website_product_link/controllers/website_sale.py
+++ deltatech_website_product_link/controllers/website_sale.py
@@ -9,16 +9,16 @@ from odoo.http import request
 from odoo.addons.web.controllers.main import Binary
 from odoo.addons.website_sale.controllers.main import WebsiteSale
 
-
 class WebsiteSaleAlternativeLink(WebsiteSale):
     @http.route(["/<category>"], type="http", auth="public", website=True, sitemap=False)
     def shop_category(self, page=0, category=None, search="", ppg=False, **post):
-        Category = request.env["product.public.category"]
         if category:
+            Category = request.env["product.public.category"]
             category = Category.search([("alternative_link", "=", category)], limit=1)
             if category:
                 return self.shop(page=page, category=category.id, search=search, ppg=ppg, **post)
 
+        raise request.not_found()
 
 class BinaryAlternativeLink(Binary):
     @http.route(
