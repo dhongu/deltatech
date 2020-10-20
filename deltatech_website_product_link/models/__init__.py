5320988a4ca27712c51e61872dfac2b50f757dcc
Dorin Hongu <dhongu@gmail.com>
2020-10-20 08:33:24 +0300

    fix pre-commit errors

--- deltatech_website_product_link/models/__init__.py
+++ deltatech_website_product_link/models/__init__.py
@@ -2,6 +2,6 @@
 #              Dorin Hongu <dhongu(@)gmail(.)com
 # See README.rst file on addons root folder for license details
 
-# from . import product_template
-# from . import product_image
+from . import product_template
+from . import product_image
 from . import product
