4ffa5aef978beb145b2f75d896545643864cd462
danila12 <danila@terrabit.ro>
2020-10-27 11:09:32 +0200

    added mrp_simple module

---
+++ deltatech_mrp_simple/__manifest__.py
@@ -0,0 +1,16 @@
+# ©  2015-2018 Deltatech
+# See README.rst file on addons root folder for license details
+{
+    "name": "Simple MRP",
+    "version": "12.0.1.0.0",
+    "author": "Terrabit, Dorin Hongu, Dan Stoica",
+    "website": "https://www.terrabit.ro",
+    "category": "Manufacturing",
+    "depends": ["stock"],
+    "license": "LGPL-3",
+    "data": ["wizard/mrp_simple_view.xml"],
+    "images": ["images/main_screenshot.png"],
+    "installable": True,
+    "development_status": "stable",
+    "maintainers": ["dhongu"],
+}
