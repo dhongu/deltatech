d1ccd5ccac25aae190a7dea124a9fe06bdb2be6a
Dorin Hongu <dhongu@gmail.com>
2020-10-28 13:15:04 +0200

    afiseaza un produs dupa cod

---
+++ deltatech_website_product_code/__manifest__.py
@@ -0,0 +1,17 @@
+# ©  2015-2020 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+{
+    "name": "eCommerce Product Code",
+    "summary": "Afisare pagina produs in functie de cod",
+    "category": "Website/Website",
+    "version": "13.0.1.0.0",
+    "author": "Terrabit, Dorin Hongu",
+    "license": "AGPL-3",
+    "website": "https://www.terrabit.ro",
+    "depends": ["website_sale"],
+    "data": [],
+    "images": ["static/description/main_screenshot.png"],
+    "development_status": "stable",
+    "maintainers": ["dhongu"],
+}
