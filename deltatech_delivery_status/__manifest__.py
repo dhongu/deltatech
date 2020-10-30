754edbf0e6cc1fd9e1673a68f9e697567ef49810
Dorin Hongu <dhongu@gmail.com>
2020-10-30 03:14:02 +0200

    adaugate modul deltatech_delivery_status

---
+++ deltatech_delivery_status/__manifest__.py
@@ -0,0 +1,16 @@
+# ©  2015-2019 Deltatech
+# See README.rst file on addons root folder for license details
+{
+    "name": "Deltatech Delivery Status",
+    "version": "13.0.1.0.0",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "support": "odoo@terrabit.ro",
+    "category": "Warehouse",
+    "depends": ["delivery"],
+    "data": ["views/stock_picking_view.xml"],
+    "license": "LGPL-3",
+    "images": ["static/description/main_screenshot.png"],
+    "development_status": "stable",
+    "maintainers": ["dhongu"],
+}
