a43b3d31d7645520b68deed57cf9b3b600e6678e
Dorin Hongu <dhongu@gmail.com>
2020-11-04 10:38:19 +0200

    filtrare valori atribute

--- deltatech_website_sale_attributes/controllers/main.py
+++ deltatech_website_sale_attributes/controllers/main.py
@@ -9,14 +9,21 @@ class WebsiteSaleAttribute(WebsiteSale):
     def shop(self, page=0, category=None, search="", ppg=False, **post):
         response = super(WebsiteSaleAttribute, self).shop(page, category, search, ppg, **post)
 
+        attrib_values = response.qcontext.get("attrib_values")
+        category = response.qcontext.get("category")
+        domain = self._get_search_domain(search, category, attrib_values)
+
         value_ids = request.env["product.attribute.value"]
-        products = response.qcontext.get("products")
+        # products = response.qcontext.get("products")
+        products = request.env["product.template"].search(domain)
 
         domain = [("product_tmpl_id", "in", products.ids)]
         attribute_lines = request.env["product.template.attribute.line"].search(domain)
 
-        for line in attribute_lines:
-            value_ids |= line.value_ids
+        domain = [("pav_attribute_line_ids", "in", attribute_lines.ids)]
+        value_ids = request.env["product.attribute.value"].search(domain)
+        # for line in attribute_lines:
+        #     value_ids |= line.value_ids
 
         response.qcontext.update(value_ids=value_ids)
 
