be3440e75118b253ce0d6c7acdf6436cfb495fee
Dorin Hongu <dhongu@gmail.com>
2020-11-05 04:18:08 +0200

    adaugare serviciu web pentru obtinere produse in format json

--- deltatech_website_product_code/controllers/website_sale.py
+++ deltatech_website_product_code/controllers/website_sale.py
@@ -2,6 +2,7 @@
 #              Dorin Hongu <dhongu(@)gmail(.)com
 # See README.rst file on addons root folder for license details
 
+
 from odoo import http
 from odoo.http import request
 
@@ -15,3 +16,40 @@ class WebsiteSaleAlternativeLink(WebsiteSale):
         if not product:
             raise request.not_found()
         return self.product(product, **kwargs)
+
+    @http.route(["/shop/products-json"], type="json", auth="public", website=True, sitemap=False)
+    def products_json_by_code(self, search="", **kwargs):
+        res = self._search_products_by_code(search)
+        return res
+
+    @http.route(["/shop/products-search"], type="http", auth="public", website=True, sitemap=False)
+    def products_search_by_code(self, search="", **kwargs):
+        res = self._search_products_by_code(search)
+        return str(res)
+
+    def _search_products_by_code(self, search):
+        domain = request.website.sale_product_domain()
+        products = request.env["product.template"].search(domain)
+        res = []
+        alternative_code_mod = request.env["ir.module.module"].search(
+            [("name", "=", "deltatech_alternative"), ("state", "=", "installed")]
+        )
+
+        for product in products:
+            combination_info = product._get_combination_info()
+            values = {
+                "name": combination_info["display_name"],
+                "default_code": product.default_code,
+                "price": combination_info["price"],
+                "list_price": combination_info["list_price"],
+                "image": product.image,
+            }
+            if alternative_code_mod:
+                alternative_code = []
+                for alternative in product.alternative_ids:
+                    alternative_code += [{"name": alternative.name, "hide": alternative.hide}]
+                values["alternative_code"] = alternative_code
+
+            res += [values]
+
+        return res
