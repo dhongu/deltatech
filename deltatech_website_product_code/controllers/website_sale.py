3dca5e71740eeaca0baa0cb21df2a3389ee278e6
Dorin Hongu <dhongu@gmail.com>
2020-11-05 04:45:43 +0200

    adaugare serviciu web pentru obtinere produse in format json

--- deltatech_website_product_code/controllers/website_sale.py
+++ deltatech_website_product_code/controllers/website_sale.py
@@ -19,17 +19,17 @@ class WebsiteSaleAlternativeLink(WebsiteSale):
 
     @http.route(["/shop/products-json"], type="json", auth="public", website=True, sitemap=False)
     def products_json_by_code(self, search="", **kwargs):
-        res = self._search_products_by_code(search)
+        res = self._search_products_by_code()
         return res
 
     @http.route(["/shop/products-search"], type="http", auth="public", website=True, sitemap=False)
     def products_search_by_code(self, search="", **kwargs):
-        res = self._search_products_by_code(search)
+        res = self._search_products_by_code()
         return str(res)
 
-    def _search_products_by_code(self, search):
+    def _search_products_by_code(self):
         domain = request.website.sale_product_domain()
-        products = request.env["product.template"].search(domain)
+        products = request.env["product.template"].sudo().search(domain, limit=100)
         res = []
         alternative_code_mod = request.env["ir.module.module"].search(
             [("name", "=", "deltatech_alternative"), ("state", "=", "installed")]
