50df4e3f90f8ab27021526e8838cd0487fbda512
Dorin Hongu <dhongu@gmail.com>
2020-11-11 18:31:23 +0200

    generare inventar din actualizare cantitate directa din produse

--- deltatech_stock_inventory/wizard/__init__.py
+++ deltatech_stock_inventory/wizard/__init__.py
@@ -2,6 +2,4 @@
 #              Dorin Hongu <dhongu(@)gmail(.)com
 # See README.rst file on addons root folder for license details
 
-
-from . import models
-from . import wizard
+from . import stock_change_product_qty
