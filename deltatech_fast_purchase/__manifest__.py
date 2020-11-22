9e29b73bd9931d32247b1740fddfd5e4d2dcb5c3
Dorin Hongu <dhongu@gmail.com>
2020-11-22 08:07:42 +0200

    Preluare modificări din 13 in 14 (#89)
    * Preluare modificari din 13 in 14
    
    * modificare travis
    
    * modificare pre-commit

--- deltatech_fast_purchase/__manifest__.py
+++ deltatech_fast_purchase/__manifest__.py
@@ -16,6 +16,6 @@
     "data": ["views/purchase_view.xml", "views/stock_view.xml"],
     "images": ["images/main_screenshot.png"],
     "installable": True,
-    "development_status": "stable",
+    "development_status": "Mature",
     "maintainers": ["dhongu"],
 }
