f7c23329803d91033bc91acce9d40d9bb9e9802d
Dorin Hongu <dhongu@gmail.com>
2020-11-24 06:14:58 +0200

    adaugare modul quick_create

---
+++ deltatech_no_quick_create/__manifest__.py
@@ -0,0 +1,18 @@
+# ©  2020 Deltatech
+# See README.rst file on addons root folder for license details
+
+
+{
+    "name": "No quick_create",
+    "summary": "disable quick_create",
+    "version": "14.0.1.0.0",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "category": "Tools",
+    "depends": ["base", "web"],
+    "license": "LGPL-3",
+    "data": ["views/assets.xml"],
+    "images": ["static/description/main_screenshot.png"],
+    "development_status": "Beta",
+    "maintainers": ["dhongu"],
+}
