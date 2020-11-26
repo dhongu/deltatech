d423136904f113efac6f108db08c4bce55bba71b
danila12 <danila@terrabit.ro>
2020-11-26 13:44:39 +0200

    code cleaning

--- deltatech_move_negative_stock/__manifest__.py
+++ deltatech_move_negative_stock/__manifest__.py
@@ -13,6 +13,6 @@
     "data": ["views/stock_picking_view.xml"],
     "images": ["static/description/main_screenshot.png"],
     "installable": True,
-    "development_status": "stable",
+    "development_status": "Beta",
     "maintainers": ["danila"],
 }
