e46b9db5d0036a15e739fe1337ae77c2e99c3752
danila12 <danila@terrabit.ro>
2020-11-26 13:38:35 +0200

    added move_negative_stock module

---
+++ deltatech_move_negative_stock/__manifest__.py
@@ -0,0 +1,18 @@
+# ©  2015-2019 Deltatech
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Replenish negative stock",
+    "summary": "Replenish negative stock from other location",
+    "version": "13.0.1.0.0",
+    "author": "Terrabit, Dan Stoica",
+    "website": "https://www.terrabit.ro",
+    "category": "Generic Modules/Stock",
+    "depends": ["stock"],
+    "license": "LGPL-3",
+    "data": ["views/stock_picking_view.xml"],
+    "images": ["static/description/main_screenshot.png"],
+    "installable": True,
+    "development_status": "stable",
+    "maintainers": ["danila"],
+}
