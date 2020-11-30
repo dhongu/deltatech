dbb991c647b0923789aca60b0964d262c5755c96
Dorin Hongu <dhongu@gmail.com>
2020-11-30 10:58:38 +0200

    tiparire bon fiscal din factura (#94)

---
+++ deltatech_partner_generic/__manifest__.py
@@ -0,0 +1,18 @@
+# ©  2008-2019 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Deltatech Generic Partner",
+    "version": "14.0.1.0.0",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "summary": "Gneric partner",
+    "category": "Generic Modules",
+    "depends": ["sale"],
+    "license": "LGPL-3",
+    "data": ["data/data.xml", "views/res_config_settings_views.xml"],
+    "images": ["images/main_screenshot.png"],
+    "development_status": "Mature",
+    "maintainers": ["dhongu"],
+}
