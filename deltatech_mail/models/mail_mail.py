a2743ad134048e9091c054cd45e35d44a23e713b
Dorin Hongu <dhongu@gmail.com>
2020-11-30 04:01:16 +0200

    adaugat modul Mail Extension

---
+++ deltatech_mail/models/mail_mail.py
@@ -0,0 +1,45 @@
+# ©  2015-2020 Deltatech
+# See README.rst file on addons root folder for license details
+
+from email.utils import formataddr
+
+from odoo import _, models
+from odoo.exceptions import UserError
+from odoo.tools import safe_eval
+
+
+class MailMail(models.Model):
+    _inherit = "mail.mail"
+
+    def _send_prepare_values(self, partner=None):
+
+        res = super(MailMail, self)._send_prepare_values(partner)
+
+        use_company_email = self.env["ir.config_parameter"].sudo().get_param("mail.use_company_email")
+        if safe_eval(use_company_email):
+            if self.author_id.company_id.email:
+                self.write(
+                    {"email_from": formataddr((self.author_id.company_id.name, self.author_id.company_id.email))}
+                )
+            else:
+                raise UserError(_("Unable to post message, please configure the company's email address."))
+
+        model = self.model
+        if model:
+            substitutions = self.env["mail.substitution"].search(["|", ("name", "=", model), ("name", "=", False)])
+        else:
+            substitutions = self.env["mail.substitution"].search([])
+
+        if substitutions:
+            email_to = []
+            if model:
+                for substitution in substitutions:
+                    email_to += [substitution.email]
+            else:
+                for substitution in substitutions:
+                    if not substitution.name or substitution.name in self.message_id:
+                        email_to += [substitution.email]
+            if email_to:
+                res["email_to"] = email_to
+
+        return res
---
+++ deltatech_mail/models/mail_substitution.py
@@ -0,0 +1,12 @@
+# ©  2015-2020 Deltatech
+# See README.rst file on addons root folder for license details
+
+from odoo import fields, models
+
+
+class MailSubstitution(models.Model):
+    _name = "mail.substitution"
+    _description = "Mail Substitution"
+
+    name = fields.Char("Related Document Model", index=True)
+    email = fields.Char("Substitution", help="Substitution with this email")
