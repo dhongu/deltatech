c4cdc4b4a03bf3218413cff32c2f898c1176287a
Dorin Hongu <dhongu@gmail.com>
2020-12-01 18:54:28 +0200

    paletizare

---
+++ deltatech_sale_pallet/models/product_template.py
@@ -0,0 +1,13 @@
+# ©  2008-2020 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+
+from odoo import fields, models
+
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    pallet_product_id = fields.Many2one("product.product")
+    pallet_qty_min = fields.Integer()  # cantitatea minima pe palet
