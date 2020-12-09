c88cf53ecdc519b4a30fd9fe4a4e6ccbdb823f01
Dorin Hongu <dhongu@gmail.com>
2020-12-09 07:49:24 +0200

    adaugare modul deltatech_website_phone_validation

---
+++ deltatech_website_phone_validation/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Website Phone Validation",
+    "summary": "Phone Validation",
+    "version": "13.0.1.0.0",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "external_dependencies": {"python": ["phonenumbers"]},
+    "license": "AGPL-3",
+    "category": "Generic Modules",
+    "depends": ["website_sale", "phone_validation"],
+    "images": ["static/description/main_screenshot.png"],
+    "development_status": "Mature",
+    "maintainers": ["dhongu"],
+}
