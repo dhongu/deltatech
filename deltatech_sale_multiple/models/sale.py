1a7caff7ac8c3c3d89864436ba876eb88c961391
Dorin Hongu <dhongu@gmail.com>
2020-12-15 13:39:14 -0800

    multiple quantity

--- deltatech_sale_multiple/models/sale.py
+++ deltatech_sale_multiple/models/sale.py
@@ -1,19 +1,14 @@
-# -*- coding: utf-8 -*-
 # ©  2008-2018 Deltatech
 #              Dorin Hongu <dhongu(@)gmail(.)com
 # See README.rst file on addons root folder for license details
 
 
-from odoo.exceptions import UserError, RedirectWarning
-from odoo import models, fields, api, _
-from odoo.tools.translate import _
-from odoo import SUPERUSER_ID, api
-import odoo.addons.decimal_precision as dp
-from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT, float_compare, float_round
+from odoo import api, models
+from odoo.tools import float_compare, float_round
 
 
 class SaleOrderLine(models.Model):
-    _inherit = 'sale.order.line'
+    _inherit = "sale.order.line"
 
     @api.model
     def fix_qty_multiple(self, product, product_uom, qty):
@@ -26,25 +21,27 @@ class SaleOrderLine(models.Model):
 
             if float_compare(qty, 0.0, precision_rounding=product_uom.rounding) > 0:
                 qty = float_round(qty, precision_rounding=product_uom.rounding)
+        if product.qty_minim and product.qty_minim > 0:
+            if qty < product.qty_minim:
+                qty = product.qty_minim
 
         return qty
 
-    @api.onchange('product_uom_qty')
+    @api.onchange("product_uom_qty")
     def _onchange_product_uom_qty(self):
         self.product_uom_qty = self.fix_qty_multiple(self.product_id, self.product_uom, self.product_uom_qty)
         super(SaleOrderLine, self)._onchange_product_uom_qty()
 
-    @api.multi
     def write(self, vals):
-        if len(self) == 1 and 'product_uom_qty' in vals:
-            if 'product_id' in vals:
-                product = self.env['product.product'].browse(vals['product_id'])
+        if len(self) == 1 and "product_uom_qty" in vals:
+            if "product_id" in vals:
+                product = self.env["product.product"].browse(vals["product_id"])
             else:
                 product = self.product_id
-            if 'product_uom' in vals:
-                product_uom = self.env['uom.uom'].browse(vals['product_uom'])
+            if "product_uom" in vals:
+                product_uom = self.env["uom.uom"].browse(vals["product_uom"])
             else:
                 product_uom = self.product_uom
-            vals['product_uom_qty'] = self.fix_qty_multiple(product, product_uom, vals['product_uom_qty'])
+            vals["product_uom_qty"] = self.fix_qty_multiple(product, product_uom, vals["product_uom_qty"])
 
         super(SaleOrderLine, self).write(vals)
