9258681a898a702896068df1ed1fb51939d0036e
Dorin Hongu <dhongu@gmail.com>
2020-12-18 17:05:30 +0200

    valori atribute

--- deltatech_website_sale_attributes/controllers/main.py
+++ deltatech_website_sale_attributes/controllers/main.py
@@ -18,27 +18,30 @@ class WebsiteSaleAttribute(WebsiteSale):
     def shop(self, page=0, category=None, search="", ppg=False, **post):
         response = super(WebsiteSaleAttribute, self).shop(page, category, search, ppg, **post)
 
-        # attrib_values = response.qcontext.get("attrib_values")
-        category = response.qcontext.get("category")
-        # domain = self._get_search_domain(search, category, attrib_values)
-        domain = request.env.context.get("shop_search_domain", [])
+        if category and search:
+            # attrib_values = response.qcontext.get("attrib_values")
+            category = response.qcontext.get("category")
+            # domain = self._get_search_domain(search, category, attrib_values)
+            domain = request.env.context.get("shop_search_domain", [])
 
-        # value_ids = request.env["product.attribute.value"]
-        # products = response.qcontext.get("products")
-        products = request.env["product.template"].search(domain)
+            # value_ids = request.env["product.attribute.value"]
+            # products = response.qcontext.get("products")
+            products = request.env["product.template"].search(domain)
 
-        domain = [("product_tmpl_id", "in", products.ids)]
-        attribute_lines = request.env["product.template.attribute.line"].search(domain)
+            domain = [("product_tmpl_id", "in", products.ids)]
+            attribute_lines = request.env["product.template.attribute.line"].search(domain)
 
-        # value_ids = attribute_lines.mapped("value_ids")
-        domain = [("pav_attribute_line_ids", "in", attribute_lines.ids)]
-        value_ids = request.env["product.attribute.value"].search(domain)
+            value_ids = attribute_lines.mapped("value_ids")
+            # domain = [("pav_attribute_line_ids", "in", attribute_lines.ids)]
+            # value_ids = request.env["product.attribute.value"].search(domain)
 
-        if category:
-            # se ascund restul de caterorii
-            # categories = request.env['product.public.category'].search([('id','child_of',category.id)])
-            categories = category
-            response.qcontext.update(categories=categories)
+            if category:
+                # se ascund restul de caterorii
+                # categories = request.env['product.public.category'].search([('id','child_of',category.id)])
+                categories = category
+                response.qcontext.update(categories=categories)
+        else:
+            value_ids = request.env["product.attribute.value"].search([])
 
         response.qcontext.update(value_ids=value_ids)
 
