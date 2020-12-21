218e7efcf18ea034541b3d0fc95657ef7944e6d0
Dorin Hongu <dhongu@gmail.com>
2020-12-21 21:33:23 +0200

    import lini comanda de vanzare din excel

---
+++ deltatech_purchase_xls/__manifest__.py
@@ -0,0 +1,19 @@
+# ©  2015-2020 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Purchase XLS",
+    "summary": "Import purchase line from Excel",
+    "version": "13.0.1.0.0",
+    "author": "Terrabit, Dorin Hongu",
+    "license": "LGPL-3",
+    "website": "https://www.terrabit.ro",
+    "category": "Purchase",
+    "depends": ["purchase_stock"],
+    "data": ["wizard/import_purchase_line_view.xml", "security/ir.model.access.csv"],
+    "images": ["static/description/main_screenshot.png"],
+    "installable": True,
+    "development_status": "Mature",
+    "maintainers": ["dhongu"],
+}
