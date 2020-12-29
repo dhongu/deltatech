00e4405dea1264181e6d1c267c174483bc62c6d1
Dorin Hongu <dhongu@gmail.com>
2020-12-29 05:17:12 +0200

    Descarcare fisier si inchiderere fereastra

--- deltatech_download/static/src/js/action_manager.js
+++ deltatech_download/static/src/js/action_manager.js
@@ -2,26 +2,13 @@ odoo.define("deltatech_download.ActionManager", function (require) {
     "use strict";
 
     var ActionManager = require("web.ActionManager");
-    // Var session = require("web.session");
-    // var framework = require("web.framework");
 
     ActionManager.include({
-        // Ir_actions_act_url: function (action, options) {
-        //     var url = action.url;
-        //     if (session.debug && url && url.length && url[0] === "/") {
-        //         url = $.param.querystring(url, {debug: session.debug});
-        //     }
-        //     this.dialog_stop();
-        //     if (action.target === "self") {
-        //         framework.redirect(url);
-        //         // The action is finished only when the redirection is done
-        //         return $.Deferred();
-        //     }
-        //     window.open(url, "_blank");
-        //     options.on_close();
-        //
-        //     return $.when();
-        // },
+        _executeURLAction: function (action) {
+            var result = this._super.apply(this, arguments);
+            this._closeDialog({infos: action.infos});
+            return result;
+        },
 
         _triggerDownload: function (action, options, type) {
             var self = this;
