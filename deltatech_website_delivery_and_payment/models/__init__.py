cd9666d165e42d5a2791d21e079e65854b42bf4c
Dorin Hongu <dhongu@gmail.com>
2021-01-19 07:16:21 +0200

    adaugare taxa pentru plata la livrare (#120)

--- deltatech_website_delivery_and_payment/models/__init__.py
+++ deltatech_website_delivery_and_payment/models/__init__.py
@@ -1,6 +1,7 @@
-# ©  2015-2020 Deltatech
+# ©  2015-2021 Deltatech
 #              Dorin Hongu <dhongu(@)gmail(.)com
 # See README.rst file on addons root folder for license details
 
 from . import delivery
 from . import payment
+from . import sale
--- deltatech_website_delivery_and_payment/static/src/js/website_sale_delivery.js
+++ deltatech_website_delivery_and_payment/static/src/js/website_sale_delivery.js
@@ -2,13 +2,19 @@ odoo.define("deltatech_website_delivery_and_payment.checkout", function (require
     "use strict";
 
     var publicWidget = require("web.public.widget");
+    require("website_sale_delivery.checkout");
+    var websiteSaleDelivery = publicWidget.registry.websiteSaleDelivery;
 
-    publicWidget.registry.websiteSaleDeliveryConstrain = publicWidget.Widget.extend({
+    var concurrency = require("web.concurrency");
+    var dp = new concurrency.DropPrevious();
+
+    // PublicWidget.registry.websiteSaleDeliveryConstrain = publicWidget.Widget.extend({
+    websiteSaleDelivery.include({
         selector: ".oe_website_sale",
-        events: {
-            "click #delivery_carrier .o_delivery_carrier_select": "_onCarrierClick",
-            "click #payment_method .o_payment_acquirer_select": "_onAcquirerClick",
-        },
+        events: _.extend({}, websiteSaleDelivery.prototype.events, {
+            //   "click #delivery_carrier .o_delivery_carrier_select": "_onCarrierClick",
+            "click #payment_method .o_payment_acquirer_select": "_onAcquirerClickCheck",
+        }),
 
         /**
          * @override
@@ -22,6 +28,27 @@ odoo.define("deltatech_website_delivery_and_payment.checkout", function (require
             if (result.status === true) {
                 $payButton.data("disabled_reasons").acquirer_selection = false;
                 $payButton.prop("disabled", _.contains($payButton.data("disabled_reasons"), true));
+            }
+            var $acquirers = $('#payment_method input[name="pm_id"]');
+            if (result.all_acquirer === false) {
+                $acquirers.each(function (index, acquirer) {
+                    var acquirer_id = $(acquirer).data("acquirer-id");
+                    if (result.acquirer_allowed_ids.includes(acquirer_id)) {
+                        $(acquirer).parent().parent().show();
+                        $(acquirer).parent().show();
+                        $(acquirer).show();
+                    } else {
+                        $(acquirer).parent().parent().hide();
+                        $(acquirer).parent().hide();
+                        $(acquirer).hide();
+                    }
+                });
+            } else {
+                $acquirers.each(function (index, acquirer) {
+                    $(acquirer).parent().parent().show();
+                    $(acquirer).parent().show();
+                    $(acquirer).show();
+                });
             }
         },
 
@@ -50,11 +77,26 @@ odoo.define("deltatech_website_delivery_and_payment.checkout", function (require
         },
 
         _onCarrierClick: function () {
+            this._super.apply(this, arguments);
+            var $acquirer = $('#payment_method input[name="pm_id"]').filter(":checked");
+            $acquirer.prop("checked", false);
+
             this._doCheckSelection();
         },
 
-        _onAcquirerClick: function () {
+        _onAcquirerClickCheck: function () {
             this._doCheckSelection();
+
+            var $carrier = $('#delivery_carrier input[name="delivery_type"]').filter(":checked");
+            var carrier_id = $carrier.val();
+            dp.add(
+                this._rpc({
+                    route: "/shop/update_carrier",
+                    params: {
+                        carrier_id: carrier_id,
+                    },
+                })
+            ).then(this._handleCarrierUpdateResult.bind(this));
         },
     });
 });
