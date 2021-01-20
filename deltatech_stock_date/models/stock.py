99cba9ccc0897eeef026e0803d3374717c3ec4fd
Dorin Hongu <dhongu@gmail.com>
2021-01-20 07:37:41 +0200

    inregistrare miscari de stoc la data contabila din inventar

--- deltatech_stock_date/models/stock.py
+++ deltatech_stock_date/models/stock.py
@@ -3,21 +3,25 @@
 # See README.rst file on addons root folder for license details
 
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
-class StockQuant(models.Model):
-    _inherit = "stock.quant"
+class StockInventory(models.Model):
+    _inherit = "stock.inventory"
 
-    @api.model
-    def _update_available_quantity(
-        self, product_id, location_id, quantity, lot_id=None, package_id=None, owner_id=None, in_date=None
-    ):
-        res = super(StockQuant, self)._update_available_quantity(
-            product_id, location_id, quantity, lot_id, package_id, owner_id, in_date
-        )
+    def post_inventory(self):
+        super(StockInventory, self).post_inventory()
+        for inventory in self:
+            inventory.move_ids.write({"date": inventory.accounting_date or inventory.date})
 
-        return res
+
+class InventoryLine(models.Model):
+    _inherit = "stock.inventory.line"
+
+    def _get_move_values(self, qty, location_id, location_dest_id, out):
+        values = super(InventoryLine, self)._get_move_values(qty, location_id, location_dest_id, out)
+        values["date"] = self.inventory_id.accounting_date or self.inventory_id.date
+        return values
 
 
 class StockMove(models.Model):
@@ -28,29 +32,17 @@ class StockMove(models.Model):
         use_date = self.env.context.get("force_period_date", False)
         if date_fields.intersection(vals):
             if not use_date:
-
                 for move in self:
                     today = fields.Date.today()
                     if "date" in vals:
                         if move.date_expected.date() < today and move.date_expected < vals["date"]:
                             vals["date"] = move.date_expected
-                        if move.date.date() < today and move.date < vals["date"]:
+                        if move.date.date() < today and move.date.date() < vals["date"]:
                             vals["date"] = move.date
                         move.move_line_ids.write({"date": vals["date"]})
-                        # move.quant_ids.write({'in_date': vals['date']})
-
-                    # if 'date_expected' in vals:
-                    #     if isinstance(vals['date_expected'], str):  # de unde ajunge aici cu string ?
-                    #         vals['date_expected'] = fields.Datetime.to_datetime(vals['date_expected'])
-                    #     move_date = vals.get('date', move.date)
-                    #     if move_date.date() < today and move_date < vals['date_expected']:
-                    #         vals['date_expected'] = move_date
             else:
-
                 if "date" in vals:
                     vals["date"] = use_date
-                # if 'date_expected' in vals:
-                #     vals['date_expected'] = use_date
 
         return super(StockMove, self).write(vals)
 
