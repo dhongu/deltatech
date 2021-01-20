4de6442b86a2e0e72d90297cac09bca1d07b00f9
Dorin Hongu <dhongu@gmail.com>
2021-01-20 09:42:49 +0200

    fitrare facturi dupa produs (#122)

---
+++ deltatech_invoice_product_filter/__manifest__.py
@@ -0,0 +1,18 @@
+# ©  2015-2021 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Invoice Product Filter",
+    "summary": "Searching invoice using product",
+    "version": "14.0.1.0.0",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "category": "Accounting",
+    "depends": ["account"],
+    "license": "LGPL-3",
+    "data": ["views/account_invoice_view.xml"],
+    "images": ["static/description/main_screenshot.png"],
+    "development_status": "Mature",
+    "maintainers": ["dhongu"],
+}
