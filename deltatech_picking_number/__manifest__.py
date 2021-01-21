2f244e74e07578782bcb6a07e85a5d7286b7ce17
Dorin Hongu <dhongu@gmail.com>
2021-01-21 08:46:17 +0200

    adaugare modul deltatech_stock_report

---
+++ deltatech_picking_number/__manifest__.py
@@ -0,0 +1,15 @@
+# ©  2015-2018 Deltatech
+# See README.rst file on addons root folder for license details
+{
+    "name": "Picking Number",
+    "version": "13.0.2.0.0",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "category": "Warehouse",
+    "depends": ["stock"],
+    "license": "LGPL-3",
+    "data": ["views/stock_view.xml"],
+    "images": ["images/main_screenshot.png"],
+    "development_status": "Mature",
+    "maintainers": ["dhongu"],
+}
