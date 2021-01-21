9a2494d9e30306c8797c2af93473bcec0bf0d6e3
Dorin Hongu <dhongu@gmail.com>
2021-01-21 08:16:44 +0200

    adaugare modul deltatech_average_payment_period

---
+++ deltatech_average_payment_period/__manifest__.py
@@ -0,0 +1,19 @@
+# ©  2015-2019 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+
+{
+    "name": "Deltatech Average Payment Period",
+    "version": "13.0.1.0.0",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "category": "Accounting & Finance",
+    "depends": ["account"],
+    "license": "AGPL-3",
+    "data": ["views/account_view.xml", "report/account_average_payment_view.xml", "security/ir.model.access.csv"],
+    "images": ["images/main_screenshot.png"],
+    "installable": True,
+    "development_status": "Mature",
+    "maintainers": ["dhongu"],
+}
