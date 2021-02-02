20c6e162156cdac1618dfcf794eb80e07d3aa293
Dorin Hongu <dhongu@gmail.com>
2021-02-02 13:51:20 +0200

    Conversie bruta deltatech_quant

---
+++ deltatech_quant/wizard/__init__.py
@@ -0,0 +1,8 @@
+# ©  2008-2021 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+
+from . import stock_quant_change_lot
+from . import stock_quant_split
+from . import stock_quant_report
