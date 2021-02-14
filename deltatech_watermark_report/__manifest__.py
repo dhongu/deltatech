23445384b0b4245bb586adc12dce2165a06835bf
Dorin Hongu <dhongu@gmail.com>
2021-02-14 05:37:39 +0200

    adaugare watermark_report

---
+++ deltatech_watermark_report/__manifest__.py
@@ -0,0 +1,19 @@
+# ©  2015-2020 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Watermark report",
+    "summary": "Watermark in report",
+    "version": "14.0.1.0.0",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "category": "Tools",
+    "depends": ["deltatech_watermark"],
+    "license": "LGPL-3",
+    "data": ["views/report_templates.xml"],
+    "images": ["static/description/main_screenshot.png"],
+    "installable": True,
+    "development_status": "Beta",
+    "maintainers": ["dhongu"],
+}
