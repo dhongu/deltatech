ef6efc3a96f1b373bf3f5688c1d45e69d50e7674
Dorin Hongu <dhongu@gmail.com>
2021-02-22 05:03:12 +0200

    fix stergere produs din cos

--- deltatech_sale_multiple_website/controllers/main.py
+++ deltatech_sale_multiple_website/controllers/main.py
@@ -11,12 +11,15 @@ from odoo.addons.website_sale.controllers.main import WebsiteSale
 class WebsiteSaleQty(WebsiteSale):
     @http.route()
     def cart_update_json(self, product_id, line_id=None, add_qty=None, set_qty=None, display=True):
+
         product = request.env["product.product"].sudo().browse(product_id)
-        set_qty = request.env["sale.order.line"].sudo().fix_qty_multiple(product, product.uom_id, set_qty)
+        if add_qty or set_qty:
+            line = request.env["sale.order.line"].sudo()
+            set_qty = line.fix_qty_multiple(product, product.uom_id, set_qty)
 
         value = super(WebsiteSaleQty, self).cart_update_json(product_id, line_id, add_qty, set_qty, display)
-        product = request.env["product.product"].sudo().browse(product_id)
-        value["quantity"] = (
-            request.env["sale.order.line"].sudo().fix_qty_multiple(product, product.uom_id, value["quantity"])
-        )
+        if add_qty or set_qty:
+            line = request.env["sale.order.line"].sudo()
+            value["quantity"] = line.fix_qty_multiple(product, product.uom_id, value["quantity"])
+
         return value
