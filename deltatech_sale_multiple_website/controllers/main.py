a86f4cddccfcd0fb10cdca56eb8e42875480442e
Dorin Hongu <dhongu@gmail.com>
2021-02-22 12:00:46 +0200

    conversie cantitate multipla (#143)
    * conversie cantitate multipla
    
    * modificare cantitate in checkout
    
    * fix stergere produs din cos
    
    * adaugare imagini module
    
    Co-authored-by: Dan Stoica <danila@terrabit.ro>

---
+++ deltatech_sale_multiple_website/controllers/main.py
@@ -0,0 +1,25 @@
+# ©  2008-2021 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+from odoo import http
+from odoo.http import request
+
+from odoo.addons.website_sale.controllers.main import WebsiteSale
+
+
+class WebsiteSaleQty(WebsiteSale):
+    @http.route()
+    def cart_update_json(self, product_id, line_id=None, add_qty=None, set_qty=None, display=True):
+
+        product = request.env["product.product"].sudo().browse(product_id)
+        if add_qty or set_qty:
+            line = request.env["sale.order.line"].sudo()
+            set_qty = line.fix_qty_multiple(product, product.uom_id, set_qty)
+
+        value = super(WebsiteSaleQty, self).cart_update_json(product_id, line_id, add_qty, set_qty, display)
+        if add_qty or set_qty:
+            line = request.env["sale.order.line"].sudo()
+            value["quantity"] = line.fix_qty_multiple(product, product.uom_id, value["quantity"])
+
+        return value
