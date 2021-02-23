70b1ff1319f7f16d4159742bfd0f3d12430fb45a
Dorin Hongu <dhongu@gmail.com>
2021-02-23 12:51:49 +0200

    adaugare modul pentru cautare produs in comenzile de pos (#147)

---
+++ deltatech_pos_product_filter/__manifest__.py
@@ -0,0 +1,18 @@
+# ©  2015-2021 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "POS Order Product Filter",
+    "summary": "Searching pos order using product",
+    "version": "14.0.1.0.0",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "category": "Accounting",
+    "depends": ["point_of_sale"],
+    "license": "LGPL-3",
+    "data": ["views/pos_order_view.xml"],
+    "images": ["static/description/main_screenshot.png"],
+    "development_status": "Mature",
+    "maintainers": ["dhongu"],
+}
