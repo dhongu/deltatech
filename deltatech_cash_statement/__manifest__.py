512f3aacee8f8845da57c30465ce8b6e0bb301eb
Dorin Hongu <dhongu@gmail.com>
2021-02-24 07:32:40 +0200

    upgrade modul Cash Statement Extension (#150)
    Co-authored-by: Dan Stoica <danila@terrabit.ro>

---
+++ deltatech_cash_statement/__manifest__.py
@@ -0,0 +1,18 @@
+# ©  2008-2018 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Deltatech Cash Statement Extension",
+    "summary": "Update cash balance",
+    "version": "14.0.3.0.0",
+    "author": "Terrabit,Dorin Hongu",
+    "license": "LGPL-3",
+    "website": "https://www.terrabit.ro",
+    "category": "Accounting",
+    "depends": ["account"],
+    "data": ["wizard/account_cash_update_balances_view.xml", "security/ir.model.access.csv"],
+    "images": ["static/description/main_screenshot.png"],
+    "development_status": "Mature",
+    "maintainers": ["dhongu"],
+}
