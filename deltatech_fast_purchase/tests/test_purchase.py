4a360adf7281a22aa908690bb7e20ca4e29a3885
Dorin Hongu <dhongu@gmail.com>
2021-03-01 09:36:12 +0200

    14.0 add tests (#156)
    * adaugare folder test
    
    * adaugare test in deltatech alternative
    
    * adaugaretest in alternative_website
    
    * adaugaretest indeltatech_contact
    
    * adaugare test deltatech_expenses
    
    * adaugare test deltatech_invoice_receipt
    
    * adaugare test deltatech_invoice_delivery
    
    * modificare test deltatech_invoice_delivery
    
    * adaugare test in fast purchase

---
+++ deltatech_fast_purchase/tests/test_purchase.py
@@ -0,0 +1,54 @@
+# ©  2015-2021 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+from odoo.tests import Form
+from odoo.tests.common import TransactionCase
+
+
+class TestPurchase(TransactionCase):
+    def setUp(self):
+        super(TestPurchase, self).setUp()
+        self.partner_a = self.env["res.partner"].create({"name": "Test"})
+
+        seller_ids = [(0, 0, {"name": self.partner_a.id})]
+        self.product_a = self.env["product.product"].create(
+            {"name": "Test A", "type": "product", "standard_price": 100, "list_price": 150, "seller_ids": seller_ids}
+        )
+        self.product_b = self.env["product.product"].create(
+            {"name": "Test B", "type": "product", "standard_price": 100, "list_price": 150, "seller_ids": seller_ids}
+        )
+
+    def test_purchase_confirm_to_invoice(self):
+        form_purchase = Form(self.env["purchase.order"])
+        form_purchase.partner_id = self.partner_a
+        with form_purchase.order_line.new() as po_line:
+            po_line.product_id = self.product_a
+            po_line.product_qty = 10
+            po_line.price_unit = 10
+
+        po = form_purchase.save()
+        po.action_button_confirm_to_invoice()
+
+    def test_purchase_receipt_to_invoice(self):
+        form_purchase = Form(self.env["purchase.order"])
+        form_purchase.partner_id = self.partner_a
+        with form_purchase.order_line.new() as po_line:
+            po_line.product_id = self.product_a
+            po_line.product_qty = 10
+            po_line.price_unit = 10
+
+        po = form_purchase.save()
+        po.button_confirm()
+        po.action_button_confirm_to_invoice()
+
+    def test_purchase_confirm_notice(self):
+        form_purchase = Form(self.env["purchase.order"])
+        form_purchase.partner_id = self.partner_a
+        with form_purchase.order_line.new() as po_line:
+            po_line.product_id = self.product_a
+            po_line.product_qty = 10
+            po_line.price_unit = 10
+
+        po = form_purchase.save()
+        po.action_button_confirm_notice()
