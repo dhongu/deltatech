309a4d4e6d634a4e1146fe03adbf2c2fdffd86a3
Dorin Hongu <dhongu@gmail.com>
2021-03-01 10:54:45 +0200

    adaugare modul product margin (#152)
    * adaugare modul product margin
    
    * fix division by zero
    
    Co-authored-by: Dan Stoica <danila@terrabit.ro>

---
+++ deltatech_product_margin/__manifest__.py
@@ -0,0 +1,18 @@
+# ©  2015-2021 Deltatech
+# See README.rst file on addons root folder for license details
+
+
+{
+    "name": "Deltatech Product Margin",
+    "summary": "Product margin and trade markup",
+    "version": "14.0.1.0.0",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "license": "AGPL-3",
+    "category": "Generic Modules/Other",
+    "depends": ["product"],
+    "data": ["views/product_view.xml"],
+    "development_status": "Beta",
+    "images": ["static/description/main_screenshot.png"],
+    "maintainers": ["dhongu"],
+}
