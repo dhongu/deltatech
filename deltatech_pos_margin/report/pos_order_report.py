7e27237570401266ab29341da0b643d12cc0c63a
Dorin Hongu <dhongu@gmail.com>
2021-03-01 15:53:53 +0200

    [14.0][ADD] adaugare modul POS Margin (#158)
    Pare ok, poti sa faci si diferentierea pe pos/sales prin sale team

---
+++ deltatech_pos_margin/report/pos_order_report.py
@@ -0,0 +1,19 @@
+# ©  2015-2021 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+from odoo import fields, models
+
+
+class PosOrderReport(models.Model):
+    _inherit = "report.pos.order"
+
+    margin = fields.Float(string="Margin")
+
+    def _select(self):
+        sql_select = super(PosOrderReport, self)._select()
+        return (
+            sql_select
+            + ",SUM(l.margin / CASE COALESCE(s.currency_rate, 0) "
+            + " WHEN 0 THEN 1.0 ELSE s.currency_rate END) AS margin"
+        )
