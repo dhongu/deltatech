53668beab3623052eb58cfb93780ce3f38e20cc5
Dorin Hongu <dhongu@gmail.com>
2021-03-06 07:17:59 +0200

    Cautare dupa is_ready

--- deltatech_sale_qty_available/models/sale.py
+++ deltatech_sale_qty_available/models/sale.py
@@ -3,22 +3,57 @@
 # See README.rst file on addons root folder for license details
 
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    is_ready = fields.Boolean(string="Is ready", compute="_compute_is_ready")
+    is_ready = fields.Boolean(string="Is ready", compute="_compute_is_ready", store=False, search="_search_is_ready")
 
-    # aceasta functie poate sa fie consumatoare de resurse !
-    # trebuie sa scaneze stocurile pentru toate produsele din comenzile de vanzare afisate
-
+    # @api.depends('state', 'invoice_status', 'order_line.product_id.qty_available', 'order_line.qty_to_deliver',
+    #              'picking_ids.move_lines.reserved_availability')
     def _compute_is_ready(self):
         for order in self:
+            is_ready = order.state in ["draft", "sent", "sale"] and order.invoice_status not in ["invoiced"]
+            if is_ready and order.state == "draft":
+                if order.picking_policy == "direct":
+                    is_ready = False
+                    for line in order.order_line:
+                        available = line.product_id.qty_available - line.product_id.outgoing_qty
+                        is_ready = is_ready or (available >= line.qty_to_deliver)
+                else:
+                    for line in order.order_line:
+                        available = line.product_id.qty_available - line.product_id.outgoing_qty
+                        is_ready = is_ready and (available >= line.qty_to_deliver)
 
-            is_ready = order.state in ["draft", "sent", "sale", "done"] and order.invoice_status != "invoiced"
-            if is_ready:
-                for line in order.order_line:
-                    is_ready = is_ready and (line.qty_available_today >= line.product_uom_qty)
+            if is_ready and order.state != "draft":
+                # verific daca comanzile de livrare au stocul rezervat
+                if order.picking_policy == "direct":
+                    is_ready = False
+                    for picking in order.picking_ids:
+                        for move in picking.move_lines:
+                            is_ready = is_ready or move.reserved_availability > 0
+                else:
+                    for picking in order.picking_ids:
+                        if picking.state in ["done"]:
+                            continue
+                        for move in picking.move_lines:
+                            is_ready = is_ready and (move.reserved_availability == move.product_uom_qty)
             order.is_ready = is_ready
+
+    @api.model
+    def _search_is_ready(self, operator, value):
+        # comenzi deschise
+        orders = self.env["sale.order"].search(
+            [("state", "in", ["draft", "sent", "sale"]), ("invoice_status", "!=", "invoiced")]
+        )
+        ready_orders = self.env["sale.order"]
+        for order in orders:
+            if order.is_ready:
+                ready_orders += order
+        if operator == "=" and value:
+            domain = [("id", "in", ready_orders.ids)]
+        else:
+            domain = [("id", "not in", ready_orders.ids)]
+        return domain
