6b8109aab610f988c8cca8085907d58c62919650
Dorin Hongu <dhongu@gmail.com>
2021-03-06 10:46:05 +0200

    adaugare deltatech_tracking_2performant

---
+++ deltatech_tracking_2performant/models/company.py
@@ -0,0 +1,12 @@
+# ©  2015-2020 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+from odoo import fields, models
+
+
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    campaign_unique_2p = fields.Char(string="2Performant Campaign Unique")
+    confirm_2p = fields.Char(string="2Performant Confirm")
