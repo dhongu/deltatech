c38a545764331df03202b951ccab0c7bdea6be29
Dorin Hongu <dhongu@gmail.com>
2021-03-08 11:58:58 +0200

    Adaugare modul deltatech_mrp_simple

---
+++ deltatech_mrp_simple/__manifest__.py
@@ -0,0 +1,17 @@
+# ©  2015-2018 Deltatech
+# See README.rst file on addons root folder for license details
+{
+    "name": "Simple MRP",
+    "summary": "Simple production",
+    "version": "14.0.1.0.0",
+    "author": "Terrabit, Dorin Hongu, Dan Stoica",
+    "website": "https://www.terrabit.ro",
+    "category": "Manufacturing",
+    "depends": ["stock", "sale"],
+    "license": "LGPL-3",
+    "data": ["wizard/mrp_simple_view.xml", "security/ir.model.access.csv"],
+    "images": ["static/description/main_screenshot.png"],
+    "installable": True,
+    "development_status": "Mature",
+    "maintainers": ["dhongu"],
+}
