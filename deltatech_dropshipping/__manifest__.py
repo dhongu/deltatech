693eb017d2346af3fd74656d40d0a4efcc7d0659
Dorin Hongu <dhongu@gmail.com>
2021-03-11 13:35:43 +0200

    adaugare modul afisare adresa dropshipping (#183)
    * adaugare modul afisare adresa dropshipping
    
    * [upd] added translation
    
    Co-authored-by: Dan Stoica <danila@terrabit.ro>

---
+++ deltatech_dropshipping/__manifest__.py
@@ -0,0 +1,17 @@
+# ©  2015-2019 Deltatech
+# See README.rst file on addons root folder for license details
+{
+    "name": "Deltatech Drop Shipping",
+    "summary": "Delivery address in picking",
+    "version": "14.0.1.0.0",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "support": "odoo@terrabit.ro",
+    "category": "Warehouse",
+    "depends": ["stock_dropshipping"],
+    "data": ["views/stock_picking_view.xml"],
+    "license": "LGPL-3",
+    "images": ["static/description/main_screenshot.png"],
+    "development_status": "Beta",
+    "maintainers": ["dhongu"],
+}
