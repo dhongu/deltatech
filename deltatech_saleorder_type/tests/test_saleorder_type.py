c835cf1918a22d0f5c868375766c7ca8188108c5
danila12 <danila@terrabit.ro>
2021-03-12 16:21:03 +0200

    [upd] moved terrabit_saleorder_type to deltatech/deltatech_saleorder_type

---
+++ deltatech_saleorder_type/tests/test_saleorder_type.py
@@ -0,0 +1,28 @@
+# ©  2015-2021 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+
+from odoo.tests.common import TransactionCase
+
+
+class TestCatalog(TransactionCase):
+    def setUp(self):
+        super(TestCatalog, self).setUp()
+        values = {
+            "name": "Type 1",
+        }
+        self.type1 = self.env["sale.order.type"].create(values)
+        self.type1.write({"is_default": True})
+        values = {
+            "name": "Type 2",
+        }
+        self.type2 = self.env["sale.order.type"].create(values)
+
+    def test_update_saleorder(self):
+        test_saleorder = self.env["sale.order"].create(
+            {
+                "partner_id": 1,
+            }
+        )
+        test_saleorder.write({"so_type": self.type1.id})
