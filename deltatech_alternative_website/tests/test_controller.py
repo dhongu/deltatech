19fc7cbc7b32944cb1268e7572bbaa7765399d3f
Dorin Hongu <dhongu@gmail.com>
2021-03-18 11:56:37 +0200

    test alternative website

---
+++ deltatech_alternative_website/tests/test_controller.py
@@ -0,0 +1,12 @@
+# ©  2015-2021 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+
+from odoo.tests.common import HttpCase, tagged
+
+
+@tagged("-at_install", "post_install")
+class TestUi(HttpCase):
+    def test_page(self):
+        self.url_open("/shop?&search=cod1")
