c63e986cef15b407b386dd6f4450b5bf92429dc0
Dorin Hongu <dhongu@gmail.com>
2021-03-22 13:38:42 +0200

    Generare out_receipt in loc de out_invoice pentru vanzare cu bon fiscal

--- deltatech_print_bf/models/__init__.py
+++ deltatech_print_bf/models/__init__.py
@@ -3,4 +3,7 @@
 # See README.rst file on addons root folder for license details
 
 
-from . import account_invoice, account_journal, res_config_settings
+from . import account_invoice
+from . import account_journal
+from . import res_config_settings
+from . import sale
--- deltatech_print_bf/models/account_invoice.py
+++ deltatech_print_bf/models/account_invoice.py
@@ -3,32 +3,21 @@
 # See README.rst file on addons root folder for license details
 
 
-from odoo import api, models
-from odoo.tools.translate import _
+from odoo import _, fields, models
 
 
 class AccountInvoice(models.Model):
     _inherit = "account.move"
 
+    receipt_print = fields.Boolean()  # bon fiscal tiparit
+
     def print_bf(self):
         wizard = self.env["account.invoice.export.bf"].with_context({"active_id": self.id}).create({})
 
-        message = _("Fisier Bon Fiscal  %s generat") % wizard.name
+        message = _("File receipt %s has been generated") % wizard.name
         self.message_post(body=message)
         wizard_download = self.env["wizard.download.file"].create(
             {"data_file": wizard.data_file, "file_name": wizard.name}
         )
-
+        self.write({"receipt_print": True})
         return wizard_download.do_download_file()
-
-
-class SaleOrder(models.Model):
-    _inherit = "sale.order"
-
-    @api.model
-    def default_get(self, fields):
-        defaults = super(SaleOrder, self).default_get(fields)
-        is_bf = self.env.context.get("is_bf", False)
-        if is_bf:
-            defaults["partner_id"] = self.env.ref("deltatech_partner_generic.partner_generic").id
-        return defaults
