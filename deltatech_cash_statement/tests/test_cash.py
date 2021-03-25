9864c24466affda72f32e11b33ebaffc49c527d5
Dorin Hongu <dhongu@gmail.com>
2021-03-25 08:50:00 +0200

    14.0 add tests (#197)
    * adaugare folder test
    
    * adaugare test in deltatech alternative
    
    * adaugaretest in alternative_website
    
    * adaugaretest indeltatech_contact
    
    * adaugare test deltatech_expenses
    
    * adaugare test deltatech_invoice_receipt
    
    * adaugare test deltatech_invoice_delivery
    
    * modificare test deltatech_invoice_delivery
    
    * adaugare test in fast purchase
    
    * add test to purchase_xls
    
    * add test to payment_to_statement
    
    * add tests
    
    * add test deltatech_procurement
    
    * adaugare stoc
    
    * adaugare stoc
    
    * adaugare test la deltatech_fast_purchase
    
    * fix fast_sale
    
    * add test deltatech_select_journal
    
    * add test commission
    
    * adaugare test la mrp simple
    
    * adaugare test la invoice number
    
    * adaugare test la payment to statement
    
    * adaugare test la stock_inventory
    
    * test deltatech_contact
    
    * modificare invoice_number
    
    * fix
    
    * adaugare test la select journal
    
    * test alternative website
    
    * deltatech_promissory_note
    
    * update test to deltatech_sale_commission
    
    * update test to deltatech_website_stock_availability
    
    * update test to deltatech_sale_payment
    
    * update test to deltatech_sale_pallet
    
    * update test to deltatech_sale_multiple
    
    * update target
    
    * update deltatech_sale_multiple
    
    * update test deltatech_stock_inventory
    
    * update test deltatech_cash_statement
    
    Co-authored-by: Dan Stoica <danila@terrabit.ro>

---
+++ deltatech_cash_statement/tests/test_cash.py
@@ -0,0 +1,23 @@
+# ©  2015-2021 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+
+from odoo.tests import Form
+from odoo.tests.common import TransactionCase
+
+
+class TestCash(TransactionCase):
+    def setUp(self):
+        super(TestCash, self).setUp()
+        journal_id = self.env["account.journal"].search([("type", "=", "cash")], limit=1)
+        self.statement = self.env["account.bank.statement"].create(
+            {
+                "journal_id": journal_id.id,
+            }
+        )
+
+    def test_cash_update(self):
+        form_cash = Form(self.env["account.cash.update.balances"].with_context(active_ids=self.statement.ids))
+        wizard = form_cash.save()
+        wizard.do_update_balance()
