44420a73de9143223df6b75ae542e6fba5e1d973
Dorin Hongu <dhongu@gmail.com>
2021-03-29 10:30:26 +0300

    [14.0][MIG] deltatech website product url image (#205)
    * update test deltatech_sale_qty_available
    
    * add deltatech_website_product_url_image
    
    Co-authored-by: Dan Stoica <danila@terrabit.ro>

---
+++ deltatech_website_product_url_image/__manifest__.py
@@ -0,0 +1,17 @@
+# ©  2015-2020 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+{
+    "name": "eCommerce Product Image URL Link",
+    "category": "Website",
+    "summary": "Load image for product from URL",
+    "version": "14.0.1.0.0",
+    "author": "Terrabit, Dorin Hongu",
+    "license": "AGPL-3",
+    "website": "https://www.terrabit.ro",
+    "depends": ["website_sale"],
+    "data": ["views/product_template_view.xml"],
+    "images": ["static/description/main_screenshot.png"],
+    "development_status": "Mature",
+    "maintainers": ["dhongu"],
+}
