66c8e6e90397c734e5fd6ecaf9757aa1cab8dc37
Dorin Hongu <dhongu@gmail.com>
2021-04-01 13:05:27 +0300

    Fix precommit

--- deltatech_mrp_simple/models/stock_picking.py
+++ deltatech_mrp_simple/models/stock_picking.py
@@ -4,7 +4,8 @@
 
 from odoo import fields, models
 
+
 class StockPicking(models.Model):
-    _inherit = 'stock.picking'
+    _inherit = "stock.picking"
 
     sale_simple_mrp_id = fields.Many2one("sale.order", string="Sales Order", store=True, readonly=False)
