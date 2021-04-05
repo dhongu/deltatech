64e6ad4cf5041ea9530a31f0ed1231937459e70c
Dorin Hongu <dhongu@gmail.com>
2021-04-05 09:36:35 +0300

    fix licenta

--- deltatech_select_journal/__manifest__.py
+++ deltatech_select_journal/__manifest__.py
@@ -6,7 +6,7 @@
     "name": "Deltatech Select Journal",
     "version": "13.0.1.0.0",
     "author": "Terrabit, Dorin Hongu",
-    "license": "AGPL-3",
+    "license": "LGPL-3",
     "website": "https://www.terrabit.ro",
     "summary": "Selectie jurnal",
     "category": "Sales",
