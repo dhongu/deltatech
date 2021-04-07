49a8e26270af6bb39cbab0bdad0cd5e3348ed765
danila12 <danila@terrabit.ro>
2021-04-07 14:08:06 +0300

    [upd] check if unit price is with taxes

--- deltatech_sale_margin/models/sale.py
+++ deltatech_sale_margin/models/sale.py
@@ -13,7 +13,16 @@ class SaleOrderLine(models.Model):
         if not res:
             res = {}
         if not res.get("warning", False):
-            if self.price_unit < self.purchase_price and self.purchase_price > 0:
+            # check if price_unit is with taxes
+            with_taxes = False
+            for tax in self.tax_id:
+                if tax.price_include:
+                    with_taxes = True
+            if with_taxes:
+                price_unit = self.price_unit - self.price_tax / self.product_uom_qty
+            else:
+                price_unit = self.price_unit
+            if price_unit < self.purchase_price and self.purchase_price > 0:
                 warning = {"title": _("Price Error!"), "message": _("Do not sell below the purchase price.")}
                 res["warning"] = warning
         return res
@@ -41,8 +50,16 @@ class SaleOrderLine(models.Model):
                 else:
                     message = _("Sale %s without price.") % line.product_id.name
                     self.order_id.message_post(body=message)
-
-            if line.price_unit < line.purchase_price:
+            # check if price_unit is with taxes
+            with_taxes = False
+            for tax in self.tax_id:
+                if tax.price_include:
+                    with_taxes = True
+            if with_taxes:
+                price_unit = self.price_unit - self.price_tax / self.product_uom_qty
+            else:
+                price_unit = self.price_unit
+            if price_unit < line.purchase_price:
                 if not self.env["res.users"].has_group("deltatech_sale_margin.group_sale_below_purchase_price"):
                     raise UserError(_("You can not sell below the purchase price."))
                 else:
