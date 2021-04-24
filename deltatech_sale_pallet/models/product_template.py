3d0692405ad1fb8486cfabdc6a13261bc274f3cd
Dorin Hongu <dhongu@gmail.com>
2021-04-24 08:35:29 +0300

    [14.0][IMP] sale pallet (#217)
    * update test deltatech_sale_qty_available
    
    * afisare pert la palet pe website
    
    * adaugare palet din frontend
    
    Co-authored-by: Dan Stoica <danila@terrabit.ro>

--- deltatech_sale_pallet/models/product_template.py
+++ deltatech_sale_pallet/models/product_template.py
@@ -11,3 +11,11 @@ class ProductTemplate(models.Model):
 
     pallet_product_id = fields.Many2one("product.product")
     pallet_qty_min = fields.Float(digits="Product Unit of Measure")  # cantitatea minima pe palet
+    pallet_price = fields.Float("Pallet Price", default=1.0, digits="Product Price", compute="_compute_pallet_price")
+
+    def _compute_pallet_price(self):
+        main_pricelist = self.env.ref("product.list0", False)
+        for template in self:
+            template = template.with_context(quantity=template.pallet_qty_min, pricelist=main_pricelist.id)
+            prices = template._compute_template_price_no_inverse()
+            template.pallet_price = prices.get(template.id, 0.0)
---
+++ deltatech_sale_pallet_website/__manifest__.py
@@ -0,0 +1,17 @@
+# ©  2008-2019 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+{
+    "name": "Sale Pallet Website",
+    "summary": "Sale pallet",
+    "version": "14.0.1.0.1",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "category": "Sales",
+    "depends": ["deltatech_sale_pallet", "website_sale"],
+    "license": "LGPL-3",
+    "data": ["views/templates.xml"],
+    "images": ["static/description/main_screenshot.png"],
+    "development_status": "Beta",
+    "maintainers": ["dhongu"],
+}
