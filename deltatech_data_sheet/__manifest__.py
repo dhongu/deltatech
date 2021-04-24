3c91442ea0acf2e418d3d5344f135851d8dd3b26
Dorin Hongu <dhongu@gmail.com>
2021-04-24 07:50:31 +0300

    [14.0][ADD] data sheet (#218)
    * update test deltatech_sale_qty_available
    
    * data sheet
    
    Co-authored-by: Dan Stoica <danila@terrabit.ro>

---
+++ deltatech_data_sheet/__manifest__.py
@@ -0,0 +1,18 @@
+# ©  2015-2021 Deltatech
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Product Data Sheet",
+    "summary": "Data Sheet",
+    "version": "14.0.1.0.0",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "category": "Administration",
+    "depends": ["product"],
+    "license": "AGPL-3",
+    "data": ["views/product_view.xml"],
+    "images": ["static/description/main_screenshot.png"],
+    "installable": True,
+    "development_status": "Production/Stable",
+    "maintainers": ["dhongu"],
+}
