aeaa5258c8a711dd59677378d6e93c1aa9f4e1f6
Dorin Hongu <dhongu@gmail.com>
2021-04-27 11:54:28 +0300

    [14.0][MIG] adaugare Product Link (#170)
    * adaugare Product Link
    
    * wip
    
    * update product link
    
    Co-authored-by: Dan Stoica <danila@terrabit.ro>

--- deltatech_website_product_link/__manifest__.py
+++ deltatech_website_product_link/__manifest__.py
@@ -1,16 +1,16 @@
-# ©  2015-2020 Deltatech
+# ©  2015-2021 Deltatech
 #              Dorin Hongu <dhongu(@)gmail(.)com
 # See README.rst file on addons root folder for license details
 {
-    "name": "eCommerce Product Category",
+    "name": "eCommerce Product Link",
     "category": "Website",
-    "summary": "Public category",
+    "summary": "Alternative product url",
     "version": "14.0.1.0.0",
     "author": "Terrabit, Dorin Hongu",
     "license": "AGPL-3",
     "website": "https://www.terrabit.ro",
-    "depends": ["website_sale"],
-    "data": ["views/shop_template.xml"],
+    "depends": ["website_sale", "deltatech_website_product_url_image", "deltatech_website_category"],
+    "data": ["views/product_template_view.xml", "views/product_view.xml"],
     "images": ["static/description/main_screenshot.png"],
     "development_status": "Alpha",
     "maintainers": ["dhongu"],
