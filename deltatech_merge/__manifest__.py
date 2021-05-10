b2356c75cd16b1b7d76be3aa4fb1bdbf563a1c76
danila12 <danila@terrabit.ro>
2021-05-10 18:13:58 +0300

    [upd] added security group, inherit partner merge action for security

--- deltatech_merge/__manifest__.py
+++ deltatech_merge/__manifest__.py
@@ -11,7 +11,9 @@
     "category": "Tools",
     "depends": ["base"],
     "license": "LGPL-3",
-    "data": [],
+    "data": [
+        "security/merge_security.xml"
+    ],
     "images": ["static/description/main_screenshot.png"],
     "development_status": "Beta",
     "maintainers": ["dhongu"],
