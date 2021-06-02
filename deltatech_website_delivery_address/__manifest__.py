8cf9f6e18c0b6c4f3f7b9c70ceacd7239bac2f58
Dorin Hongu <dhongu@gmail.com>
2021-06-02 07:18:37 +0300

    adaugare deltatech_website_delivery_address

---
+++ deltatech_website_delivery_address/__manifest__.py
@@ -0,0 +1,18 @@
+# ©  2015-2019 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+{
+    "name": "eCommerce Delivery Address",
+    "category": "Website",
+    "summary": "Default delivery address",
+    "version": "12.0.1.0.0",
+    "author": "Terrabit, Dorin Hongu",
+    "license": "LGPL-3",
+    "website": "https://www.terrabit.ro",
+    "depends": ["website_sale"],
+    "data": ["views/res_users_view.xml"],
+    "images": ["static/description/main_screenshot.png"],
+    "installable": True,
+    "development_status": "stable",
+    "maintainers": ["dhongu"],
+}
