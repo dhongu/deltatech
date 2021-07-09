3f9076110066734258cd4d18f2f51c17afee96db
Dan Stoica <danila@terrabit.ro>
2021-07-09 09:12:55 +0300

    [fix] get previous domain also (#246)
    * [fix] get previous domain also
    
    * [fix] code cleaning
    
    * [fix] code cleaning

--- deltatech_generic_partner_restriction/models/invoicing.py
+++ deltatech_generic_partner_restriction/models/invoicing.py
@@ -3,20 +3,23 @@ from odoo import api, fields, models
 
 class AccountPayment(models.Model):
     _inherit = "account.payment"
-    _description = "Restrict Journals"
 
     @api.onchange("journal_id")
     def _onchange_journal(self):
+        res = super(AccountPayment, self)._onchange_journal()
         customer_id = self.partner_id.id
         generic_partner_id = int(self.env["ir.config_parameter"].sudo().get_param("sale.partner_generic_id"))
         if customer_id == generic_partner_id:
-            return {"domain": {"journal_id": [("restriction", "=", False), ("type", "in", ("bank", "cash"))]}}
-        else:
-            return {"domain": {"journal_id": [("type", "in", ("bank", "cash"))]}}
+            if "domain" in res:
+                res["domain"].update(
+                    {
+                        "journal_id": [("restriction", "=", False), ("type", "in", ("bank", "cash"))],
+                    }
+                )
+        return res
 
 
 class AccountJournal(models.Model):
     _inherit = "account.journal"
-    _description = ""
 
     restriction = fields.Boolean(string="Generic Restriction")
