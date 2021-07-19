5acef27f90ab5c30d5a25d93b9863149ed36f6d8
Dorin Hongu <dhongu@gmail.com>
2021-07-19 12:49:03 +0300

    Adaugare optiune de download sau deschidere in pagina noua

--- deltatech_download/__manifest__.py
+++ deltatech_download/__manifest__.py
@@ -4,14 +4,14 @@
 
 {
     "name": "Download File",
-    "version": "13.0.0.0.0",
+    "version": "13.0.0.1.0",
     "author": "Terrabit,Dorin Hongu",
     "summary": "Generare fisier",
     "website": "https://www.terrabit.ro",
     "category": "Base",
     "depends": [],
     "license": "LGPL-3",
-    "data": ["views/assets.xml"],
+    "data": ["views/assets.xml", "views/ir_action_report_view.xml"],
     "images": ["images/main_screenshot.png"],
     "development_status": "Mature",
     "maintainers": ["dhongu"],
