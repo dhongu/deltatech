9f4200892f11030dbc21a64fdf982487de9404ff
Dorin Hongu <dhongu@gmail.com>
2021-07-27 08:28:04 +0300

    [14.0][MIG] picking number (#256)
    * fix division by zero
    
    * add deltatech_picking_number
    
    * [upd] updated description
    
    Co-authored-by: Dan Stoica <danila@terrabit.ro>

---
+++ deltatech_picking_number/__manifest__.py
@@ -0,0 +1,16 @@
+# ©  2015-2018 Deltatech
+# See README.rst file on addons root folder for license details
+{
+    "name": "Picking Number",
+    "summary": "Picking Number",
+    "version": "14.0.2.0.0",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "category": "Warehouse",
+    "depends": ["stock"],
+    "license": "LGPL-3",
+    "data": ["views/stock_view.xml"],
+    "images": ["images/main_screenshot.png"],
+    "development_status": "Mature",
+    "maintainers": ["dhongu"],
+}
