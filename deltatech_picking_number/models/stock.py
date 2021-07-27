a4d39f61d60343c28d308508e536b410bd0f15cd
Dorin Hongu <dhongu@gmail.com>
2021-07-27 08:28:04 +0300

    [14.0][MIG] picking number (#256)
    * fix division by zero
    
    * add deltatech_picking_number
    
    * [upd] updated description
    
    Co-authored-by: Dan Stoica <danila@terrabit.ro>

---
+++ deltatech_picking_number/models/stock.py
@@ -0,0 +1,31 @@
+# ©  2015-2018 Deltatech
+# See README.rst file on addons root folder for license details
+
+
+from odoo import _, fields, models
+from odoo.exceptions import UserError
+
+
+class StockPicking(models.Model):
+    _inherit = "stock.picking"
+
+    request_number = fields.Char(string="Request Number")
+
+    def action_get_number(self):
+        if not self.request_number:
+            if self.picking_type_id.request_sequence_id:
+                request_number = self.picking_type_id.request_sequence_id.next_by_id()
+                if request_number:
+                    self.write({"request_number": request_number, "name": request_number})
+
+    def unlink(self):
+        for picking in self:
+            if picking.request_number:
+                raise UserError(_("The document %s has been numbered") % picking.request_number)
+        return super(StockPicking, self).unlink()
+
+
+class StockPickingType(models.Model):
+    _inherit = "stock.picking.type"
+
+    request_sequence_id = fields.Many2one("ir.sequence", string="Sequence on Request")
