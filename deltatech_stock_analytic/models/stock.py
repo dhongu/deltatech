14b7f0d061f430d67c413c93cb8e08ae7b800c79
danila12 <danila@terrabit.ro>
2021-08-12 10:59:46 +0300

    code cleaning

--- deltatech_stock_analytic/models/stock.py
+++ deltatech_stock_analytic/models/stock.py
@@ -2,7 +2,7 @@
 #              Dan Stoica <danila(@)terrabit(.)ro
 # See README.rst file on addons root folder for license details
 
-from odoo import fields, models
+from odoo import models
 
 
 class StockMove(models.Model):
