d069eff65d8a1086ab1895cac65d42a1af817ea0
Dorin Hongu <dhongu@gmail.com>
2021-10-10 10:06:37 +0300

    Migrare module

--- deltatech_invoice_payment/__manifest__.py
+++ deltatech_invoice_payment/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Invoice Payment",
     "summary": "Show payments from invoice",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "category": "Accounting",
