74888350411b5ce545fb4fd3e0a86c8a8b1a6837
Dorin Hongu <dhongu@gmail.com>
2021-10-10 10:06:37 +0300

    Migrare module

--- deltatech_fast_purchase/__manifest__.py
+++ deltatech_fast_purchase/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Fast Purchase",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "summary": "Achizitie rapida",
