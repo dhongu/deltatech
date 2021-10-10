ff82541c92d462900adbd22be46cbcd4dc81853a
Dorin Hongu <dhongu@gmail.com>
2021-10-10 10:06:37 +0300

    Migrare module

--- deltatech_alternative/__manifest__.py
+++ deltatech_alternative/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "Products Alternative",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "summary": "Alternative product codes",
