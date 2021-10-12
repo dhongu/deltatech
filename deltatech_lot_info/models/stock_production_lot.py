a30954b3557d33727d96df41f908ec0344a507e4
danila12 <danila@terrabit.ro>
2021-10-12 11:04:34 +0300

    added rsy fields

--- deltatech_lot_info/models/stock_production_lot.py
+++ deltatech_lot_info/models/stock_production_lot.py
@@ -34,6 +34,34 @@ class ProductionLot(models.Model):
     sale_invoice_id = fields.Many2one("account.move", string="Invoice")
     tag_ids = fields.Many2many("stock.lot.tag", string="Tags")
 
+    # RSY fields
+    contor_monocrom = fields.Integer(string="Contor monocrom")
+    contor_color = fields.Integer(string="Contor color")
+    rezervat = fields.Boolean(string='Rezervat')
+    rezervat_pentru = fields.Many2one('res.partner', string='Rezervat pentru')
+    rezervat_nota = fields.Char('Nota rezervare')
+    rezervat_user_id = fields.Many2one('res.users', string="Rezervat de")
+    rezervat_date = fields.Date('Data rezervarii')
+    equi_categ_id = fields.Many2one('service.equipment.category', string="Categorie echipament")
+
+    # pret_final = fields.Float(string="Pret client final", compute='_compute_price')
+    # pret_final_currency_id = fields.Many2one('res.currency', string='Moneda pret lista', compute='_compute_price',
+    #                                          readonly=True)
+    # pret_revanzator = fields.Float(string="Pret revanzator", compute='_compute_price')
+    # pret_revanzator_currency_id = fields.Many2one('res.currency', string='Moneda pret revanzator',
+    #                                               compute='_compute_price', readonly=True)
+
+    pret_final = fields.Float(string="Pret client final")
+    pret_final_currency_id = fields.Many2one('res.currency', string='Moneda pret lista')
+    pret_revanzator = fields.Float(string="Pret revanzator")
+    pret_revanzator_currency_id = fields.Many2one('res.currency', string='Moneda pret revanzator')
+
+    verificat = fields.Boolean('Echipament Verificat')
+    verificat_user_id = fields.Many2one('res.users', string="Verificat de")
+    verificat_date = fields.Date('Data verificarii')
+
+    nota_interna = fields.Text()
+
     @api.depends("input_price", "product_qty")
     def _compute_input_amount(self):
         for lot in self:
@@ -43,3 +71,5 @@ class ProductionLot(models.Model):
     def _compute_output_amount(self):
         for lot in self:
             lot.output_amount = lot.output_price * lot.product_qty
+
+
