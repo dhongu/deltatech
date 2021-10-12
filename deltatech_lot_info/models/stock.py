781fdb3123fe4d18623eabdec1fabb96dd1eb2c3
danila12 <danila@terrabit.ro>
2021-10-12 12:13:19 +0300

    added lot_info module

---
+++ deltatech_lot_info/models/stock.py
@@ -0,0 +1,25 @@
+# ©  2008-2021 Deltatech
+#              Dan Stoica <danila(@)terrabit(.)ro
+# See README.rst file on addons root folder for license details
+
+from odoo import models
+
+
+class StockPicking(models.Model):
+    _inherit = "stock.picking"
+
+    # TODO:
+    # def button_validate(self):
+    #     res = super(StockPicking, self).button_validate()
+    #     for picking in self:
+    #         for move_line in picking.move_line_ids_without_package:
+    #             if move_line.lot_id:
+    #                 values = {}
+    #                 # update lot info for delivery
+    #                 if move_line.location_dest_id.usage == "customer" and move_line.location_id.usage in ["internal", "supplier"]:
+    #                     if move_line.picking_id.partner_id:
+    #                         values["customer_id"] = move_line.picking_id.partner_id.id
+    #                     values["output_date"] = move_line.picking_id.date_done
+    #                     if move_line.move_id.sale_line_id:
+    #                         # values["output_price"]
+    #                         pass
---
+++ deltatech_lot_info/models/stock_production_lot.py
@@ -0,0 +1,123 @@
+# ©  2008-2021 Deltatech
+#              Dan Stoica <danila(@)terrabit(.)ro
+# See README.rst file on addons root folder for license details
+
+from odoo import api, fields, models
+
+
+class StockLotTag(models.Model):
+    _name = "stock.lot.tag"
+    _description = "Stock Lot Tag"
+
+    name = fields.Char(string="Name")
+
+
+class ProductionLot(models.Model):
+    _inherit = "stock.production.lot"
+
+    has_info = fields.Boolean("Has other info")
+    inventory_value = fields.Float(store=True)
+    # categ_id = fields.Many2one(
+    #     "product.category", string="Internal Category", related="product_id.categ_id", store=True, readonly=True
+    # )
+    categ_id = fields.Many2one("product.category", related="product_id.categ_id", store=True)
+    input_price = fields.Float(string="Input Price")
+    output_price = fields.Float(string="Output Price")
+    input_date = fields.Date(string="Input date")
+    output_date = fields.Date(string="Output date")
+    input_amount = fields.Float(string="Input Amount", compute="_compute_input_amount", store=True)
+    output_amount = fields.Float(string="Output Amount", compute="_compute_output_amount", store=True)
+
+    customer_id = fields.Many2one("res.partner", string="Customer")
+    supplier_id = fields.Many2one("res.partner", string="Supplier")
+    origin = fields.Char(string="Source Document")
+    sale_invoice_id = fields.Many2one("account.move", string="Invoice")
+    tag_ids = fields.Many2many("stock.lot.tag", string="Tags")
+
+    # RSY fields
+    contor_monocrom = fields.Integer(string="Contor monocrom")
+    contor_color = fields.Integer(string="Contor color")
+    rezervat = fields.Boolean(string='Rezervat')
+    rezervat_pentru = fields.Many2one('res.partner', string='Rezervat pentru')
+    rezervat_nota = fields.Char('Nota rezervare')
+    rezervat_user_id = fields.Many2one('res.users', string="Rezervat de")
+    rezervat_date = fields.Date('Data rezervarii')
+    equi_categ_id = fields.Many2one('service.equipment.category', string="Categorie echipament")
+
+    pret_final = fields.Float(string="Pret client final", compute='_compute_price')
+    pret_final_currency_id = fields.Many2one('res.currency', string='Moneda pret lista', compute='_compute_price',
+                                             readonly=True)
+    pret_revanzator = fields.Float(string="Pret revanzator", compute='_compute_price')
+    pret_revanzator_currency_id = fields.Many2one('res.currency', string='Moneda pret revanzator',
+                                                  compute='_compute_price', readonly=True)
+
+
+    # pret_final = fields.Float(string="Pret client final")
+    # pret_final_currency_id = fields.Many2one('res.currency', string='Moneda pret lista')
+    # pret_revanzator = fields.Float(string="Pret revanzator")
+    # pret_revanzator_currency_id = fields.Many2one('res.currency', string='Moneda pret revanzator')
+
+    verificat = fields.Boolean('Echipament Verificat')
+    verificat_user_id = fields.Many2one('res.users', string="Verificat de")
+    verificat_date = fields.Date('Data verificarii')
+
+    nota_interna = fields.Text()
+
+    @api.depends("input_price", "product_qty")
+    def _compute_input_amount(self):
+        for lot in self:
+            lot.input_amount = lot.input_price * lot.product_qty
+
+    @api.depends("output_price", "product_qty")
+    def _compute_output_amount(self):
+        for lot in self:
+            lot.output_amount = lot.output_price * lot.product_qty
+
+    def _compute_price(self):
+        if "pricelist_id" in self.env.context:
+            pricelist = self.env["product.pricelist"].browse(self.env.context["pricelist_id"])
+            list_currency_id = pricelist.currency_id
+        else:
+            pricelist = False
+
+        for serial in self:
+            serial.pret_final = serial.product_id.list_price
+            serial.pret_final_currency_id = serial.product_id.currency_id
+            if pricelist:
+                price = pricelist.price_get(serial.product_id.id, serial.qty)
+                if price:
+                    serial.pret_revanzator = price
+                    serial.pret_revanzator_currency_id = list_currency_id
+                else:
+                    serial.pret_revanzator = serial.product_id.list_price
+                    serial.pret_revanzator_currency_id = serial.product_id.currency_id
+            else:
+                serial.pret_revanzator = serial.product_id.list_price
+                serial.pret_revanzator_currency_id = serial.product_id.currency_id
+
+        #
+        # price_type = self.env['product.price.type'].search([('field', '=', 'list_price')])
+        # if price_type:
+        #     sale_currency_id = price_type.currency_id
+        # else:
+        #     sale_currency_id = self.env.user.company_id
+        #
+        # if 'pricelist_id' in self.env.context:
+        #     pricelist = self.env['product.pricelist'].browse(self.env.context['pricelist_id'])
+        #     list_currency_id = pricelist.currency_id
+        # else:
+        #     pricelist = False
+        #
+        # for quant in self:
+        #     quant.pret_final = quant.product_id.list_price
+        #     quant.pret_final_currency_id = sale_currency_id
+        #     if pricelist:
+        #         price = pricelist.price_get(quant.product_id.id, quant.qty)[pricelist.id]
+        #         if price:
+        #             quant.pret_revanzator = price
+        #             quant.pret_revanzator_currency_id = list_currency_id
+        #     else:
+        #         quant.pret_revanzator = quant.product_id.list_price
+        #         quant.pret_revanzator_currency_id = sale_currency_id
+
+
