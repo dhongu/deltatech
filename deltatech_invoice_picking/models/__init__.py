860d792c63bf27611a59bc2195306d9a0eddbeab
Dorin Hongu <dhongu@gmail.com>
2021-10-18 08:52:19 +0300

    eliminare produse care nu sunt in picking

--- deltatech_invoice_picking/models/__init__.py
+++ deltatech_invoice_picking/models/__init__.py
@@ -4,3 +4,4 @@
 
 
 from . import stock_picking
+from . import sale
---
+++ deltatech_invoice_picking/models/sale.py
@@ -0,0 +1,32 @@
+# ©  2021 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+
+from odoo import models
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    def _create_invoices(self, grouped=False, final=False, date=None):
+        moves = super(SaleOrder, self)._create_invoices(grouped, final, date)
+
+        if "pinking_ids" in self.env.context:
+            products_qty = {}
+            pinkings = self.env["stock.picking"].browse(self.env.context["pinking_ids"])
+            for move_line in pinkings.move_lines:
+                if move_line.product_id.id in products_qty:
+                    products_qty[move_line.product_id.id] += move_line.quantity_done
+                else:
+                    products_qty[move_line.product_id.id] = move_line.quantity_done
+
+            for move in moves:
+                for line in move.line_ids:
+                    if line.product_id.type == "product" and line.product_id.id not in products_qty:
+                        line.with_context(check_move_validity=False).unlink()
+                    else:
+                        products_qty[line.product_id.id] -= line.quantity
+                move.with_context(check_move_validity=False)._recompute_dynamic_lines()
+            # todo: de verificat catitatile ramase
+        return moves
