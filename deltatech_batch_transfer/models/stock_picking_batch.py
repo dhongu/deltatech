03ef5daaffe70ccaeb79ab2caf319e1d57f54afa
Dorin Hongu <dhongu@gmail.com>
2021-10-18 08:52:19 +0300

    eliminare produse care nu sunt in picking

--- deltatech_batch_transfer/models/stock_picking_batch.py
+++ deltatech_batch_transfer/models/stock_picking_batch.py
@@ -3,6 +3,7 @@
 
 
 from odoo import models
+from odoo.tools.safe_eval import safe_eval
 
 # from odoo.exceptions import UserError
 
@@ -11,7 +12,9 @@ class StockPickingBatch(models.Model):
     _inherit = "stock.picking.batch"
 
     def action_done(self):
-        if self.env["ir.config_parameter"].get_param("deltatech_batch_keep_pickings", False):
+        get_param = self.env["ir.config_parameter"].sudo().get_param
+        batch_keep_pickings = safe_eval(get_param("deltatech_batch_keep_pickings", "False"))
+        if batch_keep_pickings:
             return super(StockPickingBatch, self.with_context(from_batch=True)).action_done()
         else:
             pickings = self.mapped("picking_ids").filtered(lambda p: p.state not in ("cancel", "done"))
--- deltatech_invoice_picking/wizard/sale_make_invoice_advance.py
+++ deltatech_invoice_picking/wizard/sale_make_invoice_advance.py
@@ -2,25 +2,8 @@
 #              Dorin Hongu <dhongu(@)gmail(.)com
 # See README.rst file on addons root folder for license details
 
-from odoo import api, models
+from odoo import models
 
 
 class SaleAdvancePaymentInv(models.TransientModel):
     _inherit = "sale.advance.payment.inv"
-
-    @api.model
-    def default_get(self, fields_list):
-        active_model = self.env.context.get("active_model", False)
-        active_ids = self.env.context.get("active_ids", [])
-        self_with_context = self
-        if active_model == "stock.picking":
-            pickings = self.env[active_model].browse(active_ids)
-            active_id = pickings.sale_id.id if len(pickings) == 1 else False
-            active_ids = pickings.mapped("sale_id").ids
-            self_with_context = self.with_context(
-                active_id=active_id, active_ids=active_ids, active_model="sale.order", active_domain=[]
-            )
-
-        defaults = super(SaleAdvancePaymentInv, self_with_context).default_get(fields_list)
-
-        return defaults
