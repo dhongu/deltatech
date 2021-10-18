f5c3f4ef0340f61e53c7284adc9256ed3d9f0ce2
danila12 <danila@terrabit.ro>
2021-10-18 09:58:11 +0300

    added invoice_picking module

---
+++ deltatech_invoice_picking/models/stock_picking.py
@@ -0,0 +1,26 @@
+# ©  2008-2021 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+
+from ast import literal_eval
+
+from odoo import models
+
+
+class StockPicking(models.Model):
+    _inherit = "stock.picking"
+
+    def action_create_invoice(self):
+        action = self.env["ir.actions.actions"]._for_xml_id("sale.action_view_sale_advance_payment_inv")
+        context = literal_eval(action.get("context", "{}"))
+        context.update(
+            {
+                "active_id": self.sale_id.id if len(self) == 1 else False,
+                "active_ids": self.mapped("sale_id").ids,
+                # "active_model": "stock.picking",
+                "default_company_id": self.company_id.id,
+            }
+        )
+        action["context"] = context
+        return action
