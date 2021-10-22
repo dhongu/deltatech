03146d78c906a16663f1ad2ce4ce99c35345750e
Dorin Hongu <dhongu@gmail.com>
2021-10-22 08:30:10 +0300

    migrare Generic Partner Restriction

---
+++ deltatech_generic_partner_restriction/__manifest__.py
@@ -0,0 +1,16 @@
+# ©  2015-2021 Deltatech
+# See README.rst file on addons root folder for license details
+{
+    "name": "Deltatech Generic Partner Restriction",
+    "summary": " Generic Partner restriction",
+    "version": "13.0.2.0.0",
+    "author": "Terrabit, cojocariudaniel1",
+    "license": "LGPL-3",
+    "website": "https://www.terrabit.ro",
+    "category": "",
+    "depends": ["account", "deltatech_partner_generic"],
+    "data": ["views/account_view_jurnal.xml"],
+    "images": [],
+    "development_status": "Production/Stable",
+    "maintainers": ["cojocariudaniel1"],
+}
