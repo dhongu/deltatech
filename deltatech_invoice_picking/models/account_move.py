cd79bb398eeabf21b61bcae18748306b47e53f68
Dorin Hongu <dhongu@gmail.com>
2021-10-23 13:44:14 +0300

    fix

--- deltatech_invoice_picking/models/account_move.py
+++ deltatech_invoice_picking/models/account_move.py
@@ -35,7 +35,8 @@ class AccountMove(models.Model):
                 sale_line_ids = account_move_line.sale_line_ids
                 for stock_move in sale_line_ids.move_ids.filtered(lambda m: m.state == "done"):
                     if not stock_move.picking_id.account_move_id or (
-                        stock_move.picking_id.account_move_id and stock_move.picking_id.to_invoice
+                        stock_move.picking_id.account_move_id
+                        and stock_move.picking_id.to_invoice
                         # picking seems to be partially invoiced
                     ):
                         # Check if qty in invoice is not all the qty in stock move.
