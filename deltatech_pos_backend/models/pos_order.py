d3ea6ba0afd933056bdec8699f41e27bb7ed558f
Dorin Hongu <dhongu@gmail.com>
2021-10-31 08:32:39 +0200

    adaugare modul editare comenzi POS

---
+++ deltatech_pos_backend/models/pos_order.py
@@ -0,0 +1,35 @@
+# ©  2015-2021 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+from odoo import api, models
+
+
+class PosOrder(models.Model):
+    _inherit = "pos.order"
+
+    @api.model
+    def default_get(self, fields_list):
+        defaults = super(PosOrder, self).default_get(fields_list)
+        active_model = self.env.context.get("active_model", False)
+        active_id = self.env.context.get("active_id", False)
+        if active_model == "pos.config":
+            pos_config = self.env[active_model].browse(active_id)
+            session_id = pos_config.current_session_id
+            defaults["session_id"] = session_id.id
+            defaults["pricelist_id"] = pos_config.pricelist_id.id
+            defaults["fiscal_position_id"] = pos_config.default_fiscal_position_id.id
+            defaults["company_id"] = pos_config.company_id.id
+            defaults["amount_return"] = 0
+
+        return defaults
+
+
+class PosOrderLine(models.Model):
+    _inherit = "pos.order.line"
+
+    @api.onchange("product_id")
+    def _onchange_product_id(self):
+        super(PosOrderLine, self)._onchange_product_id()
+        if self.product_id:
+            self.full_product_name = self.product_id.display_name
