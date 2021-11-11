080e88df1194da32cfd14724e537264987ce1c1c
danila12 <danila@terrabit.ro>
2021-11-11 06:13:52 +0200

    fix error message on partner creation, updated translations

--- deltatech_partner_discount/models/res_partner.py
+++ deltatech_partner_discount/models/res_partner.py
@@ -13,4 +13,13 @@ class ResPartner(models.Model):
     @api.onchange("discount")
     def check_discount_group(self):
         if not self.env.user.has_group("deltatech_partner_discount.group_partner_discount"):
-            raise UserError(_("Your user cannot modify the discount."))
+            if self._origin and self._origin.discount != self.discount:
+                raise UserError(_("Your user cannot modify the discount."))
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        if not self.env.user.has_group("deltatech_partner_discount.group_partner_discount"):
+            for vals in vals_list:
+                if "discount" in vals and vals["discount"] > 0.0:
+                    raise UserError(_("Your user cannot create a partner with discount."))
+        return super(ResPartner, self).create(vals_list)
