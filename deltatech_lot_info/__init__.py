3d584fc921b43b475822690113a7c1f75de4ced3
danila12 <danila@terrabit.ro>
2021-11-15 11:56:03 +0200

    [ADD] added lot_info module

---
+++ deltatech_lot_info/__init__.py
@@ -0,0 +1,8 @@
+# ©  2008-2021 Deltatech
+#              Dan Stoica <danila(@)terrabit(.)ro
+# See README.rst file on addons root folder for license details
+
+from . import models
+
+# from . import report
+# from . import wizard
