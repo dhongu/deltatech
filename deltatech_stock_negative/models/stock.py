07ad929b75f0ea2b8b57e4498b88c0db250f5e5a
danila12 <danila@terrabit.ro>
2021-11-17 07:25:01 +0200

    use float_compare

--- deltatech_stock_negative/models/stock.py
+++ deltatech_stock_negative/models/stock.py
@@ -4,6 +4,7 @@
 
 from odoo import _, api, models
 from odoo.exceptions import UserError
+from odoo.tools import float_compare
 
 
 class StockQuant(models.Model):
@@ -23,7 +24,9 @@ class StockQuant(models.Model):
                 lot_qty += quant.quantity
         else:
             lot_qty = product_id.qty_available
-        if not location_id.allow_negative_stock and location_id.usage == "internal" and (lot_qty + quantity) < 0:
+        uom_precision_digits = self.env['decimal.precision'].precision_get('Product Unit of Measure')
+        result_qty = float_compare(lot_qty + quantity, 0.0, uom_precision_digits)
+        if not location_id.allow_negative_stock and location_id.usage == "internal" and result_qty < 0:
             if location_id.company_id.no_negative_stock:
                 if not lot_id:
                     err = _(
@@ -38,15 +41,7 @@ class StockQuant(models.Model):
                         "Please adjust your quantities or correct your stock with an inventory adjustment."
                     ) % (lot_qty, product_id.name, location_id.name, lot_id.name, quantity)
                 raise UserError(err)
-                # raise UserError(
-                #     _(
-                #         "You have chosen to avoid negative stock. \
-                #         %s pieces of %s are remaining in location %s  but you want to transfer  \
-                #         %s pieces. Please adjust your quantities or \
-                #         correct your stock with an inventory adjustment."
-                #     )
-                #     % (product_id.qty_available, product_id.name, location_id.name, quantity)
-                # )
+
 
         return super(StockQuant, self)._update_available_quantity(
             product_id, location_id, quantity, lot_id, package_id, owner_id, in_date
