e93389eef149a6183afec899cb162985d7d74b09
Dorin Hongu <dhongu@gmail.com>
2021-11-18 20:46:39 +0200

    adaugare butoane in sale team

--- deltatech_sale_team/models/sale_team.py
+++ deltatech_sale_team/models/sale_team.py
@@ -16,3 +16,23 @@ class CrmTeam(models.Model):
         if action and "context" in action:
             action["context"]["default_warehouse_id "] = self.warehouse_id.id
         return action
+
+    def show_products(self):
+        self.ensure_one()
+        action = self.env.ref("stock.product_template_action_product").read()[0]
+
+        action["context"] = {"warehouse": self.warehouse_id.id, "search_default_real_stock_available": 1}
+        return action
+
+    def show_validated_quotations(self):
+        self.ensure_one()
+        action = self.env.ref("sale.action_quotations_salesteams").read()[0]
+        action["domain"] = [("state", "=", "sent")]
+        action["context"] = {
+            "search_default_team_id": [self.id],
+            "default_team_id": self.id,
+            "show_address": 1,
+            "search_default_draft": False,
+            "search_default_sent": True,
+        }
+        return action
