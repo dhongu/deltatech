be63004ac7fc87c8b2f1425f820d3a82d9892b73
Dorin Hongu <dhongu@gmail.com>
2021-11-25 12:10:59 +0200

    [14.0][ADD] Merge Unit of Measure (#309)
    * add Merge Unit of Measure
    
    * <act_window -> <record
    
    * fix drepturi
    
    Co-authored-by: Dan Stoica <danila@terrabit.ro>

---
+++ deltatech_merge_uom/__manifest__.py
@@ -0,0 +1,17 @@
+# ©  2021 Deltatech
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Merge Unit of Measure",
+    "summary": "Merge Unit of Measure",
+    "version": "14.0.0.0.1",
+    "author": "Terrabit, Dorin Hongu",
+    "category": "Generic Modules",
+    "website": "https://www.terrabit.ro",
+    "license": "LGPL-3",
+    "depends": ["uom", "deltatech_merge"],
+    "data": ["security/res_groups.xml", "wizard/uom_merge_view.xml", "security/ir.model.access.csv"],
+    "images": ["static/description/main_screenshot.png"],
+    "development_status": "Beta",
+    "maintainers": ["dhongu"],
+}
