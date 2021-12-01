7c6e6483553e1ea3b26d75eef43a67c28ad307e1
Dorin Hongu <dhongu@gmail.com>
2021-12-01 08:35:10 +0200

    reopen last order

---
+++ deltatech_website_sale_last/__manifest__.py
@@ -0,0 +1,18 @@
+# ©  2015-2020 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+{
+    "name": "eCommerce Reopen Last Order",
+    "category": "Website",
+    "summary": "Reopen Last Order",
+    "version": "14.0.1.0.1",
+    "license": "LGPL-3",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "depends": ["website_sale"],
+    "data": ["views/templates.xml"],
+    "images": ["static/description/main_screenshot.png"],
+    "installable": True,
+    "development_status": "Beta",
+    "maintainers": ["dhongu"],
+}
