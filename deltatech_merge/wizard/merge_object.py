4d4d6518b1b54e110158828ad2b292661dc23683
Dan Stoica <danila@terrabit.ro>
2021-12-10 12:31:22 +0200

    added system parameter for max number of objects merged

--- deltatech_merge/wizard/merge_object.py
+++ deltatech_merge/wizard/merge_object.py
@@ -306,13 +306,18 @@ class MergeObject(models.TransientModel):
         object_ids = Object.browse(object_ids).exists()
         if len(object_ids) < 2:
             return
-
-        if len(object_ids) > 3:
+        #
+        # get_param = self.env["ir.config_parameter"].sudo().get_param
+        # max_no_objects = safe_eval(get_param("deltatech_merge.merge_objects_max_number", default=3))
+        params = self.env["ir.config_parameter"].sudo()
+        max_no_objects = int(params.get_param("deltatech_merge.merge_objects_max_number", default=3))
+        if len(object_ids) > max_no_objects:
             raise UserError(
                 _(
-                    "For safety reasons, you cannot merge more than 3 objects together."
+                    "For safety reasons, you cannot merge more than %s objects together."
                     " You can re-open the wizard several times if needed."
                 )
+                % max_no_objects
             )
 
         # check if the list of objects to merge contains child/parent relation
