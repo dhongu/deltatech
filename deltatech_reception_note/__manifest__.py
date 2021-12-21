8e6fd897bc24fd49f471fe2bf3415f5afaa63b77
Dorin Hongu <dhongu@gmail.com>
2021-12-21 09:57:54 +0200

    deltatech_reception_note (#367)

---
+++ deltatech_reception_note/__manifest__.py
@@ -0,0 +1,18 @@
+# ©  2015-2020 Deltatech
+# See README.rst file on addons root folder for license details
+{
+    "name": "Deltatech reception_note",
+    "summary": "Batch reception_note",
+    "version": "14.0.0.0.1",
+    "author": "Terrabit, Dan Stoica",
+    "website": "https://www.terrabit.ro",
+    "support": "odoo@terrabit.ro",
+    "category": "Stock",
+    "external_dependencies": {"python": []},
+    "depends": ["purchase_stock"],
+    "data": ["views/purchase_view.xml"],
+    "license": "LGPL-3",
+    "development_status": "Beta",
+    "images": ["static/description/main_screenshot.png"],
+    "maintainers": ["dhongu"],
+}
