01d22550a3437075e429c31978f10cf376a3da67
Dorin Hongu <dhongu@gmail.com>
2021-12-28 18:22:13 +0200

    [12.0][ADD] sale transfer (#373)
    * fix migration
    
    * sale transfer

---
+++ deltatech_sale_transfer/__manifest__.py
@@ -0,0 +1,15 @@
+# ©  2021 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Sale Prepare Transfer",
+    "version": "12.0.1.0.0",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "category": "Sales",
+    "depends": ["sale_stock"],
+    "license": "LGPL-3",
+    "data": [],
+    "images": ["static/description/main_screenshot.png"],
+}
