1f7e70b51f322302e372aa146fb7a66f8d43355f
Dorin Hongu <dhongu@gmail.com>
2022-01-06 13:51:28 +0200

    adaugare modul deltatech_sale_purchase (#387)
    Co-authored-by: Dan Stoica <danila@terrabit.ro>

---
+++ deltatech_sale_purchase/__manifest__.py
@@ -0,0 +1,16 @@
+# ©  2015-2022 Deltatech
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Sale Purchase",
+    "summary": "Sale Purchase",
+    "version": "14.0.1.0.0",
+    "category": "Sales",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "depends": ["sale_purchase", "sale_stock"],
+    "license": "LGPL-3",
+    "images": ["static/description/main_screenshot.png"],
+    "development_status": "Beta",
+    "maintainers": ["dhongu"],
+}
