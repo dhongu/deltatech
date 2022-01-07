398147ef78c58f5e9284b0096457ec70f3f814e4
Dorin Hongu <dhongu@gmail.com>
2022-01-07 12:46:56 +0200

    stock_account

--- deltatech_sale_margin/__manifest__.py
+++ deltatech_sale_margin/__manifest__.py
@@ -4,11 +4,11 @@
 {
     "name": "Sale Margin",
     "summary": "Check price in sale order",
-    "version": "14.0.1.0.2",
+    "version": "14.0.1.0.3",
     "category": "Sales",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
-    "depends": ["sale_margin", "account"],
+    "depends": ["sale_margin", "account", "stock_account"],
     "license": "LGPL-3",
     "data": ["security/sale_security.xml", "views/sale_margin_view.xml"],
     "images": ["static/description/main_screenshot.png"],
