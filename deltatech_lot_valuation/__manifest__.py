1da915b54c1a87e30a0657c9b01c5cc2a4d1bf55
Dorin Hongu <dhongu@gmail.com>
2022-01-08 10:38:16 +0200

    Lot valuation - version

--- deltatech_lot_valuation/__manifest__.py
+++ deltatech_lot_valuation/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Lot valuation",
     "summary": "Lot valuation",
-    "version": "14.0.1.0.1",
+    "version": "14.0.1.0.2",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "category": "Inventory/Inventory",
