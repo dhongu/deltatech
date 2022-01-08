f1ef16cdc014aa53508adf2605f0f7bb262325c0
Dorin Hongu <dhongu@gmail.com>
2022-01-08 10:58:12 +0200

    add stock_move

--- deltatech_lot_valuation/models/__init__.py
+++ deltatech_lot_valuation/models/__init__.py
@@ -3,6 +3,6 @@
 # See README.rst file on addons root folder for license details
 
 # from . import stock_valuation_layer
-# from . import stock_move
+from . import stock_move
 
 # from . import stock_quant
