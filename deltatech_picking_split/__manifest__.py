764cbdd3c734f03d8ac9a17788466494f88d6b9a
Dorin Hongu <dhongu@gmail.com>
2022-01-08 14:32:00 +0200

    Manual backorder

---
+++ deltatech_picking_split/__manifest__.py
@@ -0,0 +1,19 @@
+# ©  2015-2018 Deltatech
+# See README.rst file on addons root folder for license details
+{
+    "name": "Picking Split",
+    "summary": "Picking Manual Backorder",
+    "version": "14.0.1.0.1",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "category": "Warehouse",
+    "depends": ["stock"],
+    "license": "LGPL-3",
+    "data": [
+        "wizard/stock_picking_manual_backorder_view.xml",
+        "security/ir.model.access.csv",
+    ],
+    "images": ["static/description/main_screenshot.png"],
+    "development_status": "Beta",
+    "maintainers": ["dhongu"],
+}
