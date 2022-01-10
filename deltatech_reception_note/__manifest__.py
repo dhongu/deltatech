4f92172bf9017f1199ede820d9b2dccb55c89cf4
Dan Stoica <danila@terrabit.ro>
2022-01-10 10:32:29 +0200

    added views/menus for reception notes

--- deltatech_reception_note/__manifest__.py
+++ deltatech_reception_note/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Deltatech reception_note",
     "summary": "Batch reception note",
-    "version": "14.0.0.0.2",
+    "version": "14.0.0.0.3",
     "author": "Terrabit, Dan Stoica",
     "website": "https://www.terrabit.ro",
     "support": "odoo@terrabit.ro",
