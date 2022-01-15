1abce071b31ed78c840ee05bbc5afd63f79ec71a
Dan Stoica <danila@terrabit.ro>
2022-01-15 06:05:35 +0200

    [14.0][FIX]fix invoice button (#415)
    * fix invoice button
    
    * updated version

--- deltatech_invoice_picking/__manifest__.py
+++ deltatech_invoice_picking/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Invoice Pickings",
-    "version": "14.0.1.0.3",
+    "version": "14.0.1.0.4",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "summary": "Facturare livrari",
