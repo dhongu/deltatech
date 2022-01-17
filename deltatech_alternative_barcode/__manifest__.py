e492ddb8c5ffcb9214582c768e8171fa1dcba5ce
Dorin Hongu <dhongu@gmail.com>
2022-01-17 18:26:01 +0200

    fix

--- deltatech_alternative_barcode/__manifest__.py
+++ deltatech_alternative_barcode/__manifest__.py
@@ -1,8 +1,6 @@
 # ©  2015-2022 Deltatech
 #              Dorin Hongu <dhongu(@)gmail(.)com
 # See README.rst file on addons root folder for license details
-
-
 {
     "name": "Products Alternative Barcode",
     "version": "14.0.1.0.1",
@@ -12,7 +10,6 @@
     "category": "Inventory/Inventory",
     "depends": ["deltatech_alternative", "stock_barcode"],
     "license": "LGPL-3",
-    "data": [""],
     "images": ["static/description/main_screenshot.png"],
     "development_status": "Beta",
     "maintainers": ["dhongu"],
