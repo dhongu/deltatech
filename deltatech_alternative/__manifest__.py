ce18da08ee7ce6165b67a635e97fdccaca171bcb
Dan Stoica <danila@terrabit.ro>
2022-01-17 13:02:09 +0200

    updated version

--- deltatech_alternative/__manifest__.py
+++ deltatech_alternative/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "Products Alternative",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.0.1",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "summary": "Alternative product codes",
