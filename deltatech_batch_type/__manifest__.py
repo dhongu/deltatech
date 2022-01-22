2ad81cc0898f3fd6509a260b6a61361bb046bd72
Dan Stoica <danila@terrabit.ro>
2022-01-22 12:11:10 +0200

    updated translations

---
+++ deltatech_batch_type/__manifest__.py
@@ -0,0 +1,18 @@
+# ©  2015-2021 Terrabit Solutions
+#              Dan Stoica <danila(@)terrabit(.)ro
+# See README.rst file on addons root folder for license details
+
+
+{
+    "name": "Deltatech - Batch Type",
+    "summary": "Batch types from sale order",
+    "version": "14.0.1.0.0",
+    "author": "Terrabit, Dan Stoica",
+    "website": "https://www.terrabit.ro",
+    "category": "Generic Modules/Other",
+    "depends": ["deltatech_saleorder_type", "deltatech_batch_transfer"],
+    "license": "LGPL-3",
+    "data": ["views/sale.xml", "security/ir.model.access.csv"],
+    "images": ["static/description/main_screenshot.png"],
+    "development_status": "Beta",
+}
---
+++ deltatech_batch_type/readme/DESCRIPTION.rst
@@ -0,0 +1,4 @@
+Features:
+ - Link between Sale Order Type and Deltatech Batch Transfer modules
+ - Sale order type is propagated to batch
+
