90cbf9e347e771fa73d070bef4329dbe2e817da3
Dorin Hongu <dhongu@gmail.com>
2022-01-26 08:55:18 +0200

    adaugare optiune de SearchMore (#432)

--- deltatech_list_view/__manifest__.py
+++ deltatech_list_view/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "List View Select Text",
     "summary": "List View Select Text",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.0.1",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "category": "Generic Modules",
