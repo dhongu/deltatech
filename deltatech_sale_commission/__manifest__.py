92ef36586de2b442dece6d05456630e90c4b46a0
Dorin Hongu <dhongu@gmail.com>
2022-02-08 07:59:18 +0200

    fix viteza rulare raport (#450)

--- deltatech_sale_commission/__manifest__.py
+++ deltatech_sale_commission/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Sale Commission",
     "summary": "Compute sale commission",
-    "version": "14.0.1.0.2",
+    "version": "14.0.1.0.3",
     "category": "Sales",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
