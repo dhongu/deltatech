a46d07b2b94c977c988b8e4c4c7d75db3cff7b37
Dorin Hongu <dhongu@gmail.com>
2022-02-08 09:07:32 +0200

    fix valoare facturi retur

--- deltatech_sale_commission/__manifest__.py
+++ deltatech_sale_commission/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Sale Commission",
     "summary": "Compute sale commission",
-    "version": "14.0.1.0.3",
+    "version": "14.0.1.0.4",
     "category": "Sales",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
