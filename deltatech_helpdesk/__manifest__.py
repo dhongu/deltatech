777f8926c50908e17be4bc6d75c34e789a8a5317
Dorin Hongu <dhongu@gmail.com>
2022-02-15 08:01:24 +0200

    helpdesk

---
+++ deltatech_helpdesk/__manifest__.py
@@ -0,0 +1,19 @@
+# ©  2008-2022 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+
+{
+    "name": "Deltatech Helpdesk",
+    "summary": "Helpdesk addons",
+    "version": "15.0.0.0.1",
+    "author": "Terrabit, Dorin Hongu",
+    "license": "AGPL-3",
+    "website": "https://www.terrabit.ro",
+    "category": "Hidden",
+    "depends": ["website_helpdesk", "website_helpdesk_form"],
+    "data": ["views/templates.xml"],
+    "images": ["static/description/main_screenshot.png"],
+    "development_status": "Beta",
+    "maintainers": ["dhongu"],
+}
