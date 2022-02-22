8a955c7ca0fcd5240e340d3ab20cb5127af2510c
Dorin Hongu <dhongu@gmail.com>
2022-02-22 08:25:31 +0200

    notification sound (#465)

---
+++ deltatech_notification_sound/__manifest__.py
@@ -0,0 +1,18 @@
+# ©  2020 Deltatech
+# See README.rst file on addons root folder for license details
+
+
+{
+    "name": "Notification Sound",
+    "summary": "Notification Sound",
+    "version": "14.0.1.0.0",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "category": "Tools",
+    "depends": ["base", "web"],
+    "license": "LGPL-3",
+    "data": ["views/assets.xml"],
+    "images": ["static/description/main_screenshot.png"],
+    "development_status": "Beta",
+    "maintainers": ["dhongu"],
+}
