328bde33d1b3c678d5c148a6af946944e2d3148d
Dorin Hongu <dhongu@gmail.com>
2022-02-23 02:38:19 +0200

    eCommerce Stock location

---
+++ deltatech_website_stock_location/models/product.py
@@ -0,0 +1,38 @@
+# ©  2015-2022 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+from odoo import models
+
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    def _get_combination_info(
+        self,
+        combination=False,
+        product_id=False,
+        add_qty=1,
+        pricelist=False,
+        parent_combination=False,
+        only_template=False,
+    ):
+
+        website = self.env["website"].get_current_website()
+        location = website.sudo().location_id
+        if location:
+            self = self.with_context(location=location.id)
+
+        combination_info = super(ProductTemplate, self)._get_combination_info(
+            combination=combination,
+            product_id=product_id,
+            add_qty=add_qty,
+            pricelist=pricelist,
+            parent_combination=parent_combination,
+            only_template=only_template,
+        )
+
+        if not self.env.context.get("website_sale_stock_get_quantity"):
+            return combination_info
+
+        return combination_info
