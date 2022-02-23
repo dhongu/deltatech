b239f78123c2ab6f71ca32f440e839f076947ef9
Dan Stoica <danila@terrabit.ro>
2022-02-23 10:27:46 +0200

    removed comments, increased version

--- deltatech_notification_sound/__manifest__.py
+++ deltatech_notification_sound/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Notification Sound",
     "summary": "Notification Sound",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.0.1",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "category": "Tools",
--- deltatech_notification_sound/static/src/js/notification.js
+++ deltatech_notification_sound/static/src/js/notification.js
@@ -1,29 +1,7 @@
 odoo.define("deltatech_notification_sound.Notification", function (require) {
     "use strict";
 
-    // Var Notification = require("web.Notification");
     const AbstractWebClient = require("web.AbstractWebClient");
-
-    // Notification.include({
-    //     init: function () {
-    //         this._super.apply(this, arguments);
-    //         this.play_sound(this.type);
-    //     },
-    //
-    //     play_sound: function (sound) {
-    //         var src = "";
-    //         if (sound === "danger") {
-    //             src = "/deltatech_notification_sound/static/src/sounds/error.wav";
-    //         } else if (sound === "warning") {
-    //             src = "/deltatech_notification_sound/static/src/sounds/exclamation.wav";
-    //         } else if (sound === "success") {
-    //             src = "/deltatech_notification_sound/static/src/sounds/bell.wav";
-    //         } else if (sound !== "default") {
-    //             src = "/deltatech_notification_sound/static/src/sounds/notify.wav";
-    //         }
-    //         $("body").append('<audio src="' + src + '" autoplay="true"></audio>');
-    //     },
-    // });
 
     AbstractWebClient.include({
         _onDisplayWarning: function (e) {
