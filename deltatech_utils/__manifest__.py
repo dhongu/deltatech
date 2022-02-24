ae8e74273765a69470727556b2c3a3993a671268
Dorin Hongu <dhongu@gmail.com>
2022-02-24 06:52:01 +0200

    stergere views

---
+++ deltatech_utils/__manifest__.py
@@ -0,0 +1,19 @@
+# ©  2008-2021 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+
+{
+    "name": "Deltatech Utils",
+    "summary": "Utils",
+    "version": "15.0.1.0.1",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "category": "Warehouse",
+    "depends": ["base", "website"],
+    "license": "LGPL-3",
+    "data": [],
+    "images": ["static/description/main_screenshot.png"],
+    "development_status": "Beta",
+    "maintainers": ["dhongu"],
+}
---
+++ deltatech_utils/models/ir_ui_view.py
@@ -0,0 +1,16 @@
+# ©  2008-2022 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+
+from odoo import models
+
+
+class View(models.Model):
+    _inherit = "ir.ui.view"
+
+    def unlink(self):
+        for view in self:
+            if view.website_id:
+                view.inherit_children_ids.unlink()
+        super(View, self).unlink()
