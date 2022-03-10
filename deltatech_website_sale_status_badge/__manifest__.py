3469fbe8e8fe0ac8f78746fc72f9d1ab488f6c28
Dorin Hongu <dhongu@gmail.com>
2022-03-10 07:12:11 +0200

    separare badge din sale status

--- deltatech_website_sale_status_badge/__manifest__.py
+++ deltatech_website_sale_status_badge/__manifest__.py
@@ -2,20 +2,16 @@
 #              Dorin Hongu <dhongu(@)gmail(.)com
 # See README.rst file on addons root folder for license details
 {
-    "name": "eCommerce Sale Order status",
+    "name": "eCommerce Sale Order status - badge",
     "category": "Website",
-    "summary": "Additional filters sales orders by status ",
-    "version": "13.0.2.0.1",
+    "summary": "eCommerce Sale Order status - badge",
+    "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
-    "depends": [
-        "website_sale_stock",
-        "deltatech_delivery_status",
-        "web_list_view_general_buttons",
-    ],
-    "data": ["views/sale_view.xml", "views/assets.xml"],
-    "qweb": ["static/src/xml/*.xml"],
+    "depends": ["deltatech_website_sale_status"],
+    "data": ["views/sale_view.xml"],
+    "qweb": [],
     "images": ["static/description/main_screenshot.png"],
     "installable": True,
     "development_status": "Mature",
