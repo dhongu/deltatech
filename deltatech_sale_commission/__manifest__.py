ac2f165430703b2f5f415d0c326570bdf706a53f
Dorin Hongu <dhongu@gmail.com>
2022-03-17 08:56:15 +0200

    update

--- deltatech_sale_commission/__manifest__.py
+++ deltatech_sale_commission/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Sale Commission",
     "summary": "Compute sale commission",
-    "version": "14.0.1.0.6",
+    "version": "14.0.1.0.8",
     "category": "Sales",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
