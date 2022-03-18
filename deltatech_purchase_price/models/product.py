e5fa516edae8396a8ee9ea714df44c9f71451cb8
Dan Stoica <danila@terrabit.ro>
2022-03-18 08:38:28 +0200

    fix sale price, added system parameter

--- deltatech_purchase_price/models/product.py
+++ deltatech_purchase_price/models/product.py
@@ -23,16 +23,19 @@ class ProductTemplate(models.Model):
     def onchange_last_purchase_price(self):
         AccountTax = self.env["account.tax"]
         for product in self:
-            if not product.last_purchase_price:
-                product.last_purchase_price = product.standard_price
-            if not product.trade_markup:
-                list_price = AccountTax._fix_tax_included_price_company(
-                    product.list_price, product.taxes_id, AccountTax, product.company_id
-                )
-                trade_markup = (list_price - product.last_purchase_price) / product.last_purchase_price * 100
-                product.trade_markup = trade_markup
-            list_price = product.last_purchase_price * (1 + product.trade_markup / 100)
-            list_price_tax = 0
-            if product.taxes_id.price_include:
-                list_price_tax = product.taxes_id.with_context(force_price_include=False)._compute_amount(list_price, 1)
-            product.list_price = list_price + list_price_tax
+            if product._origin and product.trade_markup:
+                if not product.last_purchase_price:
+                    product.last_purchase_price = product.standard_price
+                if not product.trade_markup:
+                    list_price = AccountTax._fix_tax_included_price_company(
+                        product.list_price, product.taxes_id, AccountTax, product.company_id
+                    )
+                    trade_markup = (list_price - product.last_purchase_price) / product.last_purchase_price * 100
+                    product.trade_markup = trade_markup
+                list_price = product.last_purchase_price * (1 + product.trade_markup / 100)
+                list_price_tax = 0
+                if product.taxes_id.price_include:
+                    list_price_tax = product.taxes_id.with_context(force_price_include=False)._compute_amount(
+                        list_price, 1
+                    )
+                product.list_price = list_price + list_price_tax
