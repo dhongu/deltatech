22b7b52fc89529e1bdbbf494d9f3656a70b27b7c
Dorin Hongu <dhongu@gmail.com>
2022-03-22 18:38:26 +0200

    fix

--- deltatech_purchase_price/models/product.py
+++ deltatech_purchase_price/models/product.py
@@ -3,10 +3,10 @@
 # See README.rst file on addons root folder for license details
 
 from odoo import api, fields, models
+from odoo.tools import safe_eval
 
 
 class ProductTemplate(models.Model):
-    _name = "product.template"
     _inherit = "product.template"
 
     list_price = fields.Float(tracking=True)
@@ -31,6 +31,10 @@ class ProductTemplate(models.Model):
 
     @api.onchange("last_purchase_price", "trade_markup")
     def onchange_last_purchase_price(self):
+        get_param = self.env["ir.config_parameter"].sudo().get_param
+        change_list_price = safe_eval(get_param("purchase.update_list_price", "True"))
+        if not change_list_price:
+            return
         AccountTax = self.env["account.tax"]
         for product in self:
             if product._origin and product.trade_markup:
@@ -40,8 +44,9 @@ class ProductTemplate(models.Model):
                     list_price = AccountTax._fix_tax_included_price_company(
                         product.list_price, product.taxes_id, AccountTax, product.company_id
                     )
-                    trade_markup = (list_price - product.last_purchase_price) / product.last_purchase_price * 100
-                    product.trade_markup = trade_markup
+                    if product.last_purchase_price:
+                        trade_markup = (list_price - product.last_purchase_price) / product.last_purchase_price * 100
+                        product.trade_markup = trade_markup
                 list_price = product.last_purchase_price * (1 + product.trade_markup / 100)
                 list_price_tax = 0
                 if product.taxes_id.price_include:
@@ -49,3 +54,27 @@ class ProductTemplate(models.Model):
                         list_price, 1
                     )
                 product.list_price = list_price + list_price_tax
+
+
+class SupplierInfo(models.Model):
+    _inherit = "product.supplierinfo"
+
+    def update_last_purchase_price(self):
+        for item in self:
+            price = item.product_uom._compute_price(item.price, item.product_tmpl_id.uom_id)
+            if item.currency_id:
+                price = item.currency_id.compute(price, self.env.user.company_id.currency_id)
+            if price:
+                item.product_tmpl_id.last_purchase_price = price
+                item.product_tmpl_id.onchange_last_purchase_price()
+
+    def write(self, vals):
+        res = super(SupplierInfo, self).write(vals)
+        if "price" in vals:
+            self.update_last_purchase_price()
+        return res
+
+    def create(self, vals_list):
+        res = super(SupplierInfo, self).create(vals_list)
+        res.update_last_purchase_price()
+        return res
