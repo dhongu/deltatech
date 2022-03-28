be6ad2745948a6a7128de70576e7c47c664183e1
Dan Stoica <danila@terrabit.ro>
2022-03-28 13:26:27 +0300

    added aml version

--- deltatech_followup/wizard/followup_send.py
+++ deltatech_followup/wizard/followup_send.py
@@ -3,7 +3,8 @@
 
 from string import Template
 
-from odoo import api, models
+from odoo import api, fields, models
+from odoo.tools import float_compare
 
 
 class FollowupSend(models.TransientModel):
@@ -86,4 +87,67 @@ class FollowupSend(models.TransientModel):
                                 "body_html": body,
                                 "recipient_ids": [(4, partner_id)],
                             }
+                        followup.mail_template.send_mail(partner.id, False, False, email_values)
+
+    @api.model
+    def run_followup_aml(self, codes=False):
+        # run by cron job
+        if not codes:
+            followups = self.env["account.invoice.followup"].search([])
+        else:
+            domain = [("code", "in", codes)]
+            followups = self.env["account.invoice.followup"].search(domain)
+        partners = self.env["res.partner"].search([("send_followup", "=", True)])
+        if followups:
+            for followup in followups:
+                for partner in partners:
+                    partner.env.cr.execute(
+                        """SELECT l.partner_id, p.name, p.vat, a.user_type_id, t.type, l.debit,l.credit
+                                                            FROM account_move_line l
+                                                            LEFT JOIN account_account a ON (l.account_id=a.id)
+                                                            LEFT JOIN res_partner p ON (l.partner_id=p.id)
+                                                            LEFT JOIN account_account_type t ON (a.user_type_id=t.id)
+                                                            WHERE t.type IN ('receivable','payable')
+                                                            AND l.parent_state = 'posted'
+                                                            AND l.full_reconcile_id IS NULL
+                                                            AND l.partner_id = %s
+                                                            AND l.date <= %s
+                                                            """,
+                        (partner.id, fields.Date.today()),
+                    )
+                    partner_moves = self.env.cr.fetchall()
+                    debit_payable = 0.0
+                    credit_payable = 0.0
+                    debit_receivable = 0.0
+                    credit_receivable = 0.0
+                    for move in partner_moves:
+                        if move[4] == "payable":
+                            debit_payable += move[5]
+                            credit_payable += move[6]
+                        elif move[4] == "receivable":
+                            debit_receivable += move[5]
+                            credit_receivable += move[6]
+
+                    if float_compare(debit_receivable, credit_receivable, precision_digits=2) == 1:
+                        mail_values = followup.mail_template.with_context(
+                            template_preview_lang=partner.lang
+                        ).generate_email(partner.id)
+                        body = mail_values["body"]
+                        override_id = (
+                            self.env["ir.config_parameter"]
+                            .sudo()
+                            .get_param("followup.override_partner_id", default=False)
+                        )
+                        if override_id:
+                            try:
+                                partner_id = int(override_id)
+                            except ValueError:
+                                partner_id = partner.id
+                        else:
+                            partner_id = partner.id
+                        body = body.replace("$total_debit", "{:,.2f}".format(debit_receivable - credit_receivable))
+                        email_values = {
+                            "body_html": body,
+                            "recipient_ids": [(4, partner_id)],
+                        }
                         followup.mail_template.send_mail(partner.id, False, False, email_values)
