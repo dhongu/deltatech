dfbc8940be6305f44cbce0077645ac4d8a96051b
Dorin Hongu <dhongu@gmail.com>
2022-03-28 11:16:31 +0300

    comisioane pt director si jurnale

--- deltatech_sale_commission/__manifest__.py
+++ deltatech_sale_commission/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Sale Commission",
     "summary": "Compute sale commission",
-    "version": "14.0.1.1.1",
+    "version": "14.0.1.1.2",
     "category": "Sales",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
