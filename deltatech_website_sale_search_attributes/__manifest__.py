29b6688474d8b87ce950da41c8fb336d7cf2d841
Dorin Hongu <dhongu@gmail.com>
2022-04-01 15:31:21 +0300

    [14.0][ADD] cautare in atribute (#498)
    * cautare in atribute
    
    * fix

---
+++ deltatech_website_sale_search_attributes/__manifest__.py
@@ -0,0 +1,18 @@
+# ©  2015-2020 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+{
+    "name": "eCommerce Search in attribute values",
+    "category": "Website",
+    "summary": "Search in attribute values",
+    "version": "14.0.1.0.0",
+    "license": "LGPL-3",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "depends": ["website_sale"],
+    "data": [],
+    "images": ["static/description/main_screenshot.png"],
+    "installable": True,
+    "development_status": "Beta",
+    "maintainers": ["dhongu"],
+}
