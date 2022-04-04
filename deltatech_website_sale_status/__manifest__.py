7132dd745d5004d4f7de2b1acef403016782826a
Dan Stoica <danila@terrabit.ro>
2022-04-04 10:49:46 +0300

    removed stage tracking (#500)

--- deltatech_website_sale_status/__manifest__.py
+++ deltatech_website_sale_status/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "eCommerce Sale Order status",
     "category": "Website",
     "summary": "Additional filters sales orders by status ",
-    "version": "13.0.2.0.1",
+    "version": "13.0.2.0.2",
     "license": "AGPL-3",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
