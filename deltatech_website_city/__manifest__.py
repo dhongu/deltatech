d7414d3269d9cff1c4a362f941c72f770cf3b065
Dorin Hongu <dhongu@gmail.com>
2022-04-05 10:24:02 +0300

    fix selectie localitate

--- deltatech_website_city/__manifest__.py
+++ deltatech_website_city/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Website City",
     "category": "Website/Website",
     "summary": "City extension",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.0.1",
     "author": "Terrabit, Dorin Hongu",
     "license": "LGPL-3",
     "website": "https://www.terrabit.ro",
