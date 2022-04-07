43330dae76b2abea00acb96ded167ed10274b2e2
Dorin Hongu <dhongu@gmail.com>
2022-04-07 07:44:45 +0300

    fix search

--- deltatech_lot_valuation/models/stock_valuation_layer.py
+++ deltatech_lot_valuation/models/stock_valuation_layer.py
@@ -3,7 +3,7 @@
 # See README.rst file on addons root folder for license details
 
 
-from odoo import models
+from odoo import api, models
 
 
 class StockValuationLayer(models.Model):
@@ -11,15 +11,25 @@ class StockValuationLayer(models.Model):
 
     # lot_id = fields.Many2one("stock.production.lot", "Lot/Serial Number")
 
-    def search(self, args, offset=0, limit=None, order=None, count=False):
+    @api.model
+    def _search(self, args, offset=0, limit=None, order=None, count=False, access_rights_uid=None):
         if self.env.context.get("lot_ids", False):
             lots = self.env.context["lot_ids"]
             domain = [("move_line_ids.lot_id", "in", lots.ids)]
             moves = self.env["stock.move"].search(domain)
             if moves:
                 args += [("stock_move_id", "in", moves.ids)]
+        return super(StockValuationLayer, self)._search(args, offset, limit, order, count, access_rights_uid)
 
-        return super(StockValuationLayer, self).search(args, offset, limit, order, count)
+    # def search(self, args, offset=0, limit=None, order=None, count=False):
+    #     if self.env.context.get("lot_ids", False):
+    #         lots = self.env.context["lot_ids"]
+    #         domain = [("move_line_ids.lot_id", "in", lots.ids)]
+    #         moves = self.env["stock.move"].search(domain)
+    #         if moves:
+    #             args += [("stock_move_id", "in", moves.ids)]
+    #
+    #     return super(StockValuationLayer, self).search(args, offset, limit=limit, order=order, count=count)
 
     def read_group(self, domain, fields, groupby, offset=0, limit=None, orderby=False, lazy=True):
         if self.env.context.get("lot_ids", False):
