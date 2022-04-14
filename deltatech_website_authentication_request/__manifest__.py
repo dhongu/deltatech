2473ee0b657b0eb7c1a81f06633ce9b066324afa
Dorin Hongu <dhongu@gmail.com>
2022-04-14 14:49:26 +0300

    migrare modul Website authentication request (#513)

---
+++ deltatech_website_authentication_request/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Website authentication request",
+    "summary": "Authentication request",
+    "version": "14.0.1.0.0",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "license": "AGPL-3",
+    "category": "Generic Modules",
+    "depends": ["website_sale"],
+    "images": ["static/description/main_screenshot.png"],
+    "development_status": "Mature",
+    "maintainers": ["dhongu"],
+}
