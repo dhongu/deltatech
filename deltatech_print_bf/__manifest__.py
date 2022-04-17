88b12267cbdd9fde18c5d962434e419f09bc013d
Dorin Hongu <dhongu@gmail.com>
2022-04-17 03:48:16 +0300

    adaugare modul dummy deltatech_print_bf

---
+++ deltatech_print_bf/__manifest__.py
@@ -0,0 +1,18 @@
+# ©  2008-2021 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Deltatech Print Invoice to ECR",
+    "summary": "Deltatech Print Invoice to ECR",
+    "version": "15.0.1.0.0",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "category": "Generic Modules/Stock",
+    "depends": ['deltatech_sale_store'],
+    "license": "LGPL-3",
+    "data": [],
+    "images": [],
+    "development_status": "Mature",
+    "maintainers": ["dhongu"],
+}
