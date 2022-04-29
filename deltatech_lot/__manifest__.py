91a7670a7b0f2266f22d91161141ed7414fe8608
Dorin Hongu <dhongu@gmail.com>
2022-04-29 13:54:27 +0300

    evaluare lot

---
+++ deltatech_lot/__manifest__.py
@@ -0,0 +1,16 @@
+# ©  2008-2022 Deltatech
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Generate/Select lot",
+    "summary": "Generate/Select lot",
+    "version": "15.0.1.0.2",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "category": "Generic Modules",
+    "depends": ["stock"],
+    "data": ["views/stock_move_line_view.xml", "views/stock_production_lot.xml"],
+    "license": "LGPL-3",
+    "images": ["static/description/main_screenshot.png"],
+    "development_status": "Beta",
+}
