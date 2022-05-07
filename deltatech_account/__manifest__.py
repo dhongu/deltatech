702f6d3df10d4f46dc7a9e154ac83cb2a8be69e4
Dorin Hongu <dhongu@gmail.com>
2022-05-07 05:26:12 +0300

    migrare modul deltatech account

---
+++ deltatech_account/__manifest__.py
@@ -0,0 +1,16 @@
+# ©  2015-2019 Deltatech
+# See README.rst file on addons root folder for license details
+{
+    "name": "Deltatech Account",
+    "summary": "Deltatech Account",
+    "version": "15.0.2.0.0",
+    "author": "Terrabit, Dorin Hongu",
+    "license": "LGPL-3",
+    "website": "https://www.terrabit.ro",
+    "category": "Accounting & Finance",
+    "depends": ["account"],
+    "data": ["views/account_invoice_view.xml", "views/res_config_settings_views.xml"],
+    "images": ["images/main_screenshot.png"],
+    "development_status": "Production/Stable",
+    "maintainers": ["dhongu"],
+}
