352cc2ab1534759120b95b81521fddd642f8b8c2
Dorin Hongu <dhongu@gmail.com>
2022-05-07 07:43:41 +0300

    raport profit

---
+++ deltatech_sale/__manifest__.py
@@ -0,0 +1,19 @@
+# ©  2015-2019 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Sale Extension",
+    "summary": "Sale Extension Obsolete",
+    "version": "15.0.1.0.0",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "category": "Sales",
+    "depends": ["sale"],
+    "license": "LGPL-3",
+    "data": ["report/deltatech_sale_report.xml", "security/ir.model.access.csv"],
+    "images": ["images/main_screenshot.png"],
+    "installable": True,
+    "development_status": "stable",
+    "maintainers": ["dhongu"],
+}
---
+++ deltatech_sale/models/sale.py
@@ -0,0 +1,21 @@
+# ©  2015-2019 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+from odoo import _, api, models
+from odoo.exceptions import UserError
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+
+    @api.onchange("product_id")
+    def product_id_change(self):
+
+        if not self.order_id.partner_id:
+            raise UserError(_("Before choosing a product,\n select a customer in the sales form."))
+
+        result = super(SaleOrderLine, self).product_id_change()
+
+        return result
