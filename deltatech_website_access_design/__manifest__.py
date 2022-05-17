28fb07bcc531e3b4a5a6e0547410c452a191b9bc
Dorin Hongu <dhongu@gmail.com>
2022-05-17 10:31:04 +0300

    Migrare modul Website web designer access

---
+++ deltatech_website_access_design/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Website web designer access",
+    "summary": "Access for web designer",
+    "version": "14.0.1.0.1",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "license": "AGPL-3",
+    "category": "Generic Modules",
+    "depends": ["website", "contacts", "sale", "account"],
+    "data": ["data/data.xml"],
+    "images": ["static/description/main_screenshot.png"],
+    "development_status": "Mature",
+    "maintainers": ["dhongu"],
+}
