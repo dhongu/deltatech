5229e36941f5d9db006e4154d97bbb299de2d2b9
Dorin Hongu <dhongu@gmail.com>
2022-05-18 07:06:53 +0300

    fix salvare configurare

--- deltatech_product_category/models/product_category.py
+++ deltatech_product_category/models/product_category.py
@@ -9,4 +9,4 @@ from odoo import fields, models
 class ProductCategory(models.Model):
     _inherit = "product.category"
 
-    company_id = fields.Many2one("res.company", string="Company", default=lambda self: self.env.company, required=True)
+    company_id = fields.Many2one("res.company", string="Company", default=lambda self: self.env.company)
--- deltatech_product_category/readme/DESCRIPTION.rst
+++ deltatech_product_category/readme/DESCRIPTION.rst
@@ -1,2 +1,4 @@
 Features:
  - Product category per company
+
+ - Categoria All trebuie sa ramana fara companie
