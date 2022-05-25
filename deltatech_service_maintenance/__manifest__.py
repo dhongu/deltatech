86a8e364def96ccb06e24f353ed4621f72058ad6
Dan Stoica <danila@terrabit.ro>
2022-05-25 15:33:59 +0300

    service notifications: removed obsolete categories

--- deltatech_service_maintenance/__manifest__.py
+++ deltatech_service_maintenance/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Deltatech Services Maintenance",
     "summary": "Services Maintenance",
-    "version": "14.0.1.0.2",
+    "version": "14.0.1.0.3",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "category": "Services/Maintenance",
