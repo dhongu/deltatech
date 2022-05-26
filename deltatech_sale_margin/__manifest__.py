113eaf1e6a3eb54b65177e5a5bd50d7500dbed16
Dorin Hongu <dhongu@gmail.com>
2022-05-26 12:10:39 +0300

    ignorare verificare pret la comenzile de pe website (#537)

--- deltatech_sale_margin/__manifest__.py
+++ deltatech_sale_margin/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Sale Margin",
     "summary": "Check price in sale order",
-    "version": "14.0.1.0.3",
+    "version": "14.0.1.0.4",
     "category": "Sales",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
