a349fcd357ccb32f87cc3b26b6e3abae24ed00db
Dorin Hongu <dhongu@gmail.com>
2022-05-30 15:20:17 +0300

    modul pentru indicare sistem de test (#540)

---
+++ deltatech_test_system/__manifest__.py
@@ -0,0 +1,19 @@
+# ©  2008-2021 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+
+{
+    "name": "Deltatech Test System",
+    "summary": "Utils",
+    "version": "14.0.0.0.1",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "category": "Tools",
+    "depends": ["web"],
+    "license": "LGPL-3",
+    "data": ["views/templates.xml"],
+    "images": ["static/description/main_screenshot.png"],
+    "development_status": "Beta",
+    "maintainers": ["dhongu"],
+}
