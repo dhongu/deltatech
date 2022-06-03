d825c50c0b86c9a38fe4fddbfb35151350753047
Dorin Hongu <dhongu@gmail.com>
2022-06-03 11:15:16 +0300

    [14.0][ADD] Sale request Feedback (#541)
    * Sale request Feedback
    
    * added ircron_data to manifest, updated DESCRIPTION.rst
    
    Co-authored-by: Dan Stoica <danila@terrabit.ro>

---
+++ deltatech_sale_feedback/__manifest__.py
@@ -0,0 +1,17 @@
+# ©  2015-2019 Deltatech
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Sale Feedback",
+    "summary": "Sale Feedback",
+    "version": "14.0.1.0.1",
+    "category": "Sales",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "depends": ["sale", "account", "portal_rating"],
+    "license": "LGPL-3",
+    "data": ["data/mail_data.xml", "views/account_move_view.xml", "data/ir_cron_data.xml"],
+    "images": ["static/description/main_screenshot.png"],
+    "development_status": "Beta",
+    "maintainers": ["dhongu"],
+}
---
+++ deltatech_sale_feedback/models/account_invoice.py
@@ -0,0 +1,30 @@
+# ©  2015-2022 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+from datetime import date
+
+from dateutil.relativedelta import relativedelta
+
+from odoo import api, models
+from odoo.tools.safe_eval import safe_eval
+
+
+class AccountInvoice(models.Model):
+    _inherit = "account.move"
+
+    @api.model
+    def cron_request_feedback(self):
+        get_param = self.env["ir.config_parameter"].sudo().get_param
+        days_request_feedback = safe_eval(get_param("sale.days_request_feedback", "3"))
+        invoice_date = date.today() + relativedelta(days=-days_request_feedback)
+        domain = [("move_type", "=", "out_invoice"), ("invoice_date", "=", invoice_date)]
+        invoices = self.env["account.move"].search(domain)
+        invoices.request_feedback()
+
+    def request_feedback(self):
+        res_id = "deltatech_sale_feedback.mail_template_sale_feedback"
+        template_id = self.env["ir.model.data"].xmlid_to_res_id(res_id, raise_if_not_found=True)
+        for invoice in self.filtered(lambda i: i.move_type == "out_invoice"):
+            message_post = invoice.with_context(force_send=True).message_post_with_template
+            message_post(template_id, composition_mode="comment", email_layout_xmlid="mail.mail_notification_light")
