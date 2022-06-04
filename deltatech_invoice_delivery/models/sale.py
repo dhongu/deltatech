591410d8cf00bf6b6a4a0c694a493110145ed04d
Dorin Hongu <dhongu@gmail.com>
2022-06-04 12:44:24 +0300

    fix dublare linii cu negativ

--- deltatech_invoice_delivery/models/sale.py
+++ deltatech_invoice_delivery/models/sale.py
@@ -32,72 +32,72 @@ class SaleOrder(models.Model):
                     picking.with_context(force_period_date=order.date_order)._action_done()
 
 
-class SaleOrderLine(models.Model):
-    _inherit = "sale.order.line"
-
-    def _action_launch_stock_rule(self, previous_product_uom_qty=False):
-
-        super(SaleOrderLine, self)._action_launch_stock_rule(previous_product_uom_qty)
-        precision = self.env["decimal.precision"].precision_get("Product Unit of Measure")
-
-        for line in self:
-            if line.state != "sale" or line.product_id.type not in ("consu", "product"):
-                continue
-            qty = line._get_qty_procurement(previous_product_uom_qty)
-            if float_compare(qty, line.product_uom_qty, precision_digits=precision) < 0:
-                continue
-            if line.product_uom_qty >= 0:
-                continue
-
-            group_id = line.order_id.procurement_group_id
-            if not group_id:
-                group_id = self.env["procurement.group"].create(line._prepare_procurement_group_vals())
-                line.order_id.procurement_group_id = group_id
-
-            values = line._prepare_procurement_values(group_id=group_id)
-            product_qty = line.product_uom_qty - qty
-
-            line_uom = line.product_uom
-            quant_uom = line.product_id.uom_id
-            product_qty, procurement_uom = line_uom._adjust_uom_quantities(-product_qty, quant_uom)
-            location_id = line.order_id.partner_shipping_id.property_stock_customer
-            rule = group_id._get_rule(line.product_id, location_id, values)
-
-            picking = self.env["stock.picking"].create(
-                {
-                    "picking_type_id": rule.picking_type_id.return_picking_type_id.id or rule.picking_type_id.id,
-                    # "picking_type_id": rule.picking_type_id.id,
-                    "partner_id": line.order_id.partner_shipping_id.id,
-                    "origin": line.order_id.name,
-                    "group_id": group_id.id,
-                    "location_id": rule.location_id.id,
-                    "location_dest_id": rule.location_src_id.id,
-                    "sale_id": line.order_id.id,
-                    "state": "draft",
-                    "move_type": "one",
-                }
-            )
-            self.env["stock.move"].create(
-                {
-                    "picking_id": picking.id,
-                    "location_id": rule.location_id.id,
-                    "location_dest_id": rule.location_src_id.id,
-                    "product_id": line.product_id.id,
-                    "name": line.product_id.name,
-                    "product_uom_qty": product_qty,
-                    "product_uom": procurement_uom.id,
-                    "state": "draft",
-                    "to_refund": True,
-                    "sale_line_id": line.id,
-                }
-            )
-            picking.write(
-                {
-                    "group_id": group_id.id,
-                    "sale_id": line.order_id.id,
-                }
-            )
-
-            picking.action_assign()
-
-        return True
+# class SaleOrderLine(models.Model):
+#     _inherit = "sale.order.line"
+#
+#     def _action_launch_stock_rule(self, previous_product_uom_qty=False):
+#
+#         super(SaleOrderLine, self)._action_launch_stock_rule(previous_product_uom_qty)
+#         precision = self.env["decimal.precision"].precision_get("Product Unit of Measure")
+#
+#         for line in self:
+#             if line.state != "sale" or line.product_id.type not in ("consu", "product"):
+#                 continue
+#             qty = line._get_qty_procurement(previous_product_uom_qty)
+#             if float_compare(qty, line.product_uom_qty, precision_digits=precision) < 0:
+#                 continue
+#             if line.product_uom_qty >= 0:
+#                 continue
+#
+#             group_id = line.order_id.procurement_group_id
+#             if not group_id:
+#                 group_id = self.env["procurement.group"].create(line._prepare_procurement_group_vals())
+#                 line.order_id.procurement_group_id = group_id
+#
+#             values = line._prepare_procurement_values(group_id=group_id)
+#             product_qty = line.product_uom_qty - qty
+#
+#             line_uom = line.product_uom
+#             quant_uom = line.product_id.uom_id
+#             product_qty, procurement_uom = line_uom._adjust_uom_quantities(-product_qty, quant_uom)
+#             location_id = line.order_id.partner_shipping_id.property_stock_customer
+#             rule = group_id._get_rule(line.product_id, location_id, values)
+#
+#             picking = self.env["stock.picking"].create(
+#                 {
+#                     "picking_type_id": rule.picking_type_id.return_picking_type_id.id or rule.picking_type_id.id,
+#                     # "picking_type_id": rule.picking_type_id.id,
+#                     "partner_id": line.order_id.partner_shipping_id.id,
+#                     "origin": line.order_id.name,
+#                     "group_id": group_id.id,
+#                     "location_id": rule.location_id.id,
+#                     "location_dest_id": rule.location_src_id.id,
+#                     "sale_id": line.order_id.id,
+#                     "state": "draft",
+#                     "move_type": "one",
+#                 }
+#             )
+#             self.env["stock.move"].create(
+#                 {
+#                     "picking_id": picking.id,
+#                     "location_id": rule.location_id.id,
+#                     "location_dest_id": rule.location_src_id.id,
+#                     "product_id": line.product_id.id,
+#                     "name": line.product_id.name,
+#                     "product_uom_qty": product_qty,
+#                     "product_uom": procurement_uom.id,
+#                     "state": "draft",
+#                     "to_refund": True,
+#                     "sale_line_id": line.id,
+#                 }
+#             )
+#             picking.write(
+#                 {
+#                     "group_id": group_id.id,
+#                     "sale_id": line.order_id.id,
+#                 }
+#             )
+#
+#             picking.action_assign()
+#
+#         return True
