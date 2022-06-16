272828c46276c316ac5d796e4d2cdacfdf62edd4
Dorin Hongu <dhongu@gmail.com>
2022-06-16 11:28:05 +0300

    fix eCommerce Product Slider

--- deltatech_website_product_slider_snippet/__manifest__.py
+++ deltatech_website_product_slider_snippet/__manifest__.py
@@ -5,18 +5,21 @@
     "name": "eCommerce Product Slider",
     "category": "Website",
     "summary": "eCommerce extension",
-    "version": "15.0.1.0.0",
+    "version": "15.0.1.0.1",
     "author": "Terrabit, Dorin Hongu",
     "license": "LGPL-3",
     "website": "https://www.terrabit.ro",
-    "depends": ["website_sale", "deltatech_product_list"],
-    "data": ["views/templates.xml", "views/snippets.xml"],
+    "depends": ["website","website_sale", "deltatech_product_list"],
+    "data": [ "views/snippets.xml"],
     "images": ["static/description/main_screenshot.png"],
     "installable": True,
     "qweb": ["static/src/xml/*.xml"],
     "development_status": "Mature",
     "maintainers": ["dhongu"],
     "assets": {
+        "website.assets_frontend":[
+            "deltatech_website_product_slider_snippet/static/src/js/website_products_slider.js"
+        ],
         "website.assets_wysiwyg": [
             "deltatech_website_product_slider_snippet/static/src/js/website_products_slider_editor.js"
         ],
