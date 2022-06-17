74f2d900b931c03cf20a8a6b05c9055eeaed0a15
Dorin Hongu <dhongu@gmail.com>
2022-06-17 08:02:50 +0300

    fix

--- deltatech_service_consumable/models/__init__.py
+++ deltatech_service_consumable/models/__init__.py
@@ -2,7 +2,9 @@
 # See README.rst file on addons root folder for license details
 
 from . import service_consumable
-from . import service_efficiency_report
+
 from . import service_equipment
 from . import service_agreement
 from . import stock_picking
+
+from . import service_efficiency_report
