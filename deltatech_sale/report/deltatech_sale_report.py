e156e06eba2757d6bcdf8c1006dc6857a73e74ea
Dorin Hongu <dhongu@gmail.com>
2022-06-24 11:07:43 +0300

    fix raport vanzare

--- deltatech_sale/report/deltatech_sale_report.py
+++ deltatech_sale/report/deltatech_sale_report.py
@@ -2,55 +2,13 @@
 #              Dorin Hongu <dhongu(@)gmail(.)com
 # See README.rst file on addons root folder for license details
 
-from odoo import api, fields, models, tools
+from odoo import fields, models, tools
 
 
 class DeltatechSaleReport(models.Model):
     _name = "deltatech.sale.report"
     _description = "Deltatech sale report"
     _auto = False
-
-    @api.model
-    def read_group(self, domain, fields, groupby, offset=0, limit=None, orderby=False, lazy=True):
-
-        res = super(DeltatechSaleReport, self).read_group(
-            domain, fields, groupby, offset=offset, limit=limit, orderby=orderby, lazy=lazy
-        )
-
-        prod_dict = {}
-        if "stock_val" in fields or "profit_val" in fields:
-            for line in res:
-                lines = self.search(line.get("__domain", []))
-                inv_value = 0.0
-                sale_val = 0.0
-                # product_tmpl_obj = self.pool.get("product.template")
-
-                for line_rec in lines:
-                    if line_rec.product_id.cost_method == "real":
-                        # price = line_rec.price_unit_on_quant
-                        # trbuie sa determin care este miscare cu care a fost facuta iesire din
-                        # stoc si de acolo sa gasesc care e quntul din care sa citesc pretul!!! fack!
-                        # nu mai exista get_history_price
-                        if line_rec.product_id.id not in prod_dict:
-                            prod_dict[line_rec.product_id.id] = line_rec.product_id.standard_price
-                            # prod_dict[line_rec.product_id.id] = line_rec.product_id.get_history_price(
-                            #     line_rec.company_id.id, date=line_rec.date
-                            # )
-                        price = prod_dict[line_rec.product_id.id] or line_rec.product_id.standard_price
-                    else:
-                        if line_rec.product_id.id not in prod_dict:
-                            # nu mai exista get_history_price
-                            prod_dict[line_rec.product_id.id] = line_rec.product_id.standard_price
-
-                            # prod_dict[line_rec.product_id.id] = line_rec.product_id.get_history_price(
-                            #     line_rec.company_id.id, date=line_rec.date
-                            # )
-                        price = prod_dict[line_rec.product_id.id] or line_rec.product_id.standard_price
-                    inv_value += price * line_rec.product_uom_qty
-                    sale_val += line_rec.sale_val
-                line["stock_val"] = inv_value
-                line["profit_val"] = sale_val - inv_value
-        return res
 
     date = fields.Datetime("Date", readonly=True)
     categ_id = fields.Many2one("product.category", "Category", readonly=True)
@@ -59,8 +17,9 @@ class DeltatechSaleReport(models.Model):
     product_uom_qty = fields.Float("Quantity", readonly=True)
     sale_val = fields.Float("Sale value", readonly=True)
 
-    stock_val = fields.Float(string="Stock value", readonly=True, compute="_compute_stock_val", store=True)
-    profit_val = fields.Float(string="Profit", readonly=True, compute="_compute_profit_val", store=True)
+    stock_val = fields.Float(string="Stock value", readonly=True)
+    profit_val = fields.Float(string="Profit", readonly=True)
+
     partner_id = fields.Many2one("res.partner", "Partner", readonly=True)
     user_id = fields.Many2one("res.users", "Salesperson", readonly=True)
     warehouse_id = fields.Many2one("stock.warehouse", "Warehouse", required=True)
@@ -69,11 +28,9 @@ class DeltatechSaleReport(models.Model):
     state = fields.Selection(
         [
             ("draft", "Quotation"),
-            ("waiting_date", "Waiting Schedule"),
-            ("manual", "Manual In Progress"),
-            ("progress", "In Progress"),
-            ("invoice_except", "Invoice Exception"),
-            ("done", "Done"),
+            ("sent", "Quotation Sent"),
+            ("sale", "Sales Order"),
+            ("done", "Locked"),
             ("cancel", "Cancelled"),
         ],
         "Order Status",
@@ -81,26 +38,16 @@ class DeltatechSaleReport(models.Model):
     )
     pricelist_id = fields.Many2one("product.pricelist", "Pricelist", readonly=True)
 
-    def _compute_stock_val(self):
-        for line in self:
-            # nu mai exista get_history_price
-            price_unit = (
-                line.product_id.standard_price
-            )  # product_tmpl_id.get_history_price(line.company_id.id, date=line.date)
-            line.stock_val = line.product_uom_qty * price_unit
-
-    def _compute_profit_val(self):
-        for line in self:
-            line.profit_val = line.sale_val - line.stock_val
-
     def init(self):
         tools.drop_view_if_exists(self.env.cr, "deltatech_sale_report")
         self.env.cr.execute(
             """
 
-         create or replace view deltatech_sale_report as (
+            create or replace view deltatech_sale_report as (
+             select *,  sale_val - stock_val as profit_val
+            from (
                 select
-                    min(l.id) as id,
+                    l.id as id,
                     s.date_order as date,
                     t.categ_id as categ_id,
                     l.product_id as product_id,
@@ -116,10 +63,9 @@ class DeltatechSaleReport(models.Model):
                     s.company_id as company_id,
 
                     s.state,
+                    sum(l.product_uom_qty / u.factor * u2.factor) * l.purchase_price  as stock_val,
+                    s.pricelist_id as pricelist_id
 
-                    s.pricelist_id as pricelist_id,
-                    0 as stock_val,
-                    0 as profit_val
 
                 from
                     sale_order s
@@ -132,6 +78,7 @@ class DeltatechSaleReport(models.Model):
 
 
                 group by
+                    l.id,
                     l.product_id,
                     l.product_uom_qty,
                     l.order_id,
@@ -146,5 +93,5 @@ class DeltatechSaleReport(models.Model):
                     s.state,
                     s.pricelist_id
 
-       ) """
+       ) as sub) """
         )
