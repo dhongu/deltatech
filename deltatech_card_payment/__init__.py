c17ba37ed5886b5c40c6a2f857cdf6899e5f0468
Dorin Hongu <dhongu@gmail.com>
2022-06-27 11:33:21 +0300

    fix

--- deltatech_card_payment/__init__.py
+++ deltatech_card_payment/__init__.py
@@ -1,9 +1,6 @@
-# ©  2008-2020 Deltatech
+# ©  2008-2022 Deltatech
 #              Dorin Hongu <dhongu(@)gmail(.)com
 # See README.rst file on addons root folder for license details
 
 
 from . import models
-from . import wizard
-
-# from . import controllers
