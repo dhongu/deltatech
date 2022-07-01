72b5a4dd9f095ddf89833ed8b95d05fe00d30d5b
Dorin Hongu <dhongu@gmail.com>
2022-07-01 17:32:09 +0300

    migrare modul card payment

---
+++ deltatech_purchase_refund/__manifest__.py
@@ -0,0 +1,21 @@
+# ©  2015-2019 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Refund Purchase",
+    "summary": "Generare factura storno pentru retururi",
+    "version": "15.0.1.0.0",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "category": "Purchases",
+    "depends": ["base", "purchase_stock", "stock"],
+    "price": 5.00,
+    "currency": "EUR",
+    "license": "LGPL-3",
+    "data": ["views/account_invoice_view.xml"],
+    "images": ["static/description/main_screenshot.png"],
+    "installable": True,
+    "development_status": "Mature",
+    "maintainers": ["dhongu"],
+}
