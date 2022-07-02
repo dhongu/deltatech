8bb7bd9fc67b5c1a6b9eed5cc17aa6702877186e
Dorin Hongu <dhongu@gmail.com>
2022-07-02 16:52:03 +0300

    migrare modul sale transfer

---
+++ deltatech_sale_transfer/models/warehouse.py
@@ -0,0 +1,17 @@
+# ©  2015-2019 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+
+import logging
+
+from odoo import fields, models
+
+_logger = logging.getLogger(__name__)
+
+
+class Warehouse(models.Model):
+    _inherit = "stock.warehouse"
+
+    pick_type_auto_transfer_id = fields.Many2one("stock.picking.type", string="Type auto transfer")
+    group_transfer_with_delivery = fields.Boolean()
---
+++ deltatech_sale_transfer/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+Features:
+ - La validarea unei comenzi de vanzre se verifica stocul existent in alte deposite si se genereaza un transfer
