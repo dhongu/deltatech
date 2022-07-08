62eadf6d418d6f4e9906ce1cf99d3334f264d3f8
Dan Stoica <danila@terrabit.ro>
2022-07-08 10:02:37 +0300

    added stock account extension module

---
+++ deltatech_stock_account/__manifest__.py
@@ -0,0 +1,18 @@
+# ©  2015-2022 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Stock Account Extension",
+    "summary": "Stock Account Extension",
+    "version": "13.0.1.0.1",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "category": "Generic Modules",
+    "depends": ["stock_account"],
+    "license": "AGPL-3",
+    "data": ["views/stock_picking_view.xml"],
+    "images": ["static/description/main_screenshot.png"],
+    "development_status": "Beta",
+    "maintainers": ["dhongu"],
+}
