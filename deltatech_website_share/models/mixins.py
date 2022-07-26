36cbd0670a53acfc0a518b6373e210c1d7c8772a
Dorin Hongu <dhongu@gmail.com>
2022-07-26 18:18:22 +0300

    14.0 website share (#613)
    * partajare produse intre websituri
    
    * mix published
    
    * domeniu
    
    * added help/description
    
    Co-authored-by: Dan Stoica <danila@terrabit.ro>

---
+++ deltatech_website_share/models/mixins.py
@@ -0,0 +1,34 @@
+# ©  2015-2022 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+from odoo import models
+from odoo.http import request
+
+
+class WebsiteMultiMixin(models.AbstractModel):
+    _inherit = "website.multi.mixin"
+
+    def can_access_from_current_website(self, website_id=False):
+        can_access = super(WebsiteMultiMixin, self).can_access_from_current_website(website_id)
+        if not can_access:
+            for record in self:
+                website_ids = [] or request.website.website_access_ids.ids
+                if (website_id or record.website_id.id) in website_ids:
+                    can_access = True
+        return can_access
+
+
+class WebsitePublishedMultiMixin(models.AbstractModel):
+
+    _inherit = "website.published.multi.mixin"
+
+    def _compute_website_published(self):
+        current_website_id = self._context.get("website_id")
+        super(WebsitePublishedMultiMixin, self)._compute_website_published()
+        for record in self:
+            if current_website_id and record.website_id:
+                record.website_published = record.is_published and (
+                    record.website_id.id == current_website_id
+                    or current_website_id in record.website_id.website_access_ids.ids
+                )
---
+++ deltatech_website_share/models/website.py
@@ -0,0 +1,25 @@
+# ©  2015-2022 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+from odoo import api, fields, models
+
+
+class Website(models.Model):
+
+    _inherit = "website"
+
+    website_access_ids = fields.Many2many(
+        "website",
+        relation="website_access_rel",
+        column2="website_id",
+        column1="website_access_id",
+        string="Access",
+        help="Other websites from which the objects can be showed",
+    )
+
+    @api.model
+    def website_domain(self, website_id=False):
+        website_ids = self.website_access_ids.ids or []
+        website_ids += [False, website_id or self.id]
+        return [("website_id", "in", website_ids)]
