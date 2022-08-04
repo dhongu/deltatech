d07967e2637b9adca5ff96549eb6d1586d717fd9
Dorin Hongu <dhongu@gmail.com>
2022-08-04 03:46:46 +0300

    dialog

---
+++ deltatech_error_dialog/__manifest__.py
@@ -0,0 +1,22 @@
+# ©  2008-2021 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Error Dialog",
+    "version": "15.0.0.1.1",
+    "author": "Terrabit,Dorin Hongu",
+    "summary": "Error Dialog",
+    "website": "https://www.terrabit.ro",
+    "category": "Base",
+    "depends": ['web'],
+    "license": "LGPL-3",
+    "data": [
+
+    ],
+    "qweb": ["static/src/xml/error_dialog.xml"],
+    "images": ["images/main_screenshot.png"],
+    "development_status": "Beta",
+    "maintainers": ["dhongu"],
+
+}
---
+++ deltatech_error_dialog/static/src/js/error_dialog.js
@@ -0,0 +1,5 @@
+odoo.define("deltatech_error_dialog.PaymentScreen", function (require) {
+    "use strict";
+
+
+});
