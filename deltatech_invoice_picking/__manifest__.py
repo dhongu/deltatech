9c8132949e8c31ae92143f042ea858d7a9c035f5
Dan Stoica <danila@terrabit.ro>
2022-08-24 09:24:23 +0300

    [14.0][UPD]reception note number (#639)
    * updated ReadMe
    
    * separare nr. fact furnizor de nr. nota receptie
    
    * updated translation

--- deltatech_invoice_picking/__manifest__.py
+++ deltatech_invoice_picking/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Invoice Pickings",
-    "version": "14.0.1.0.6",
+    "version": "14.0.1.0.7",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "summary": "Facturare livrari",
