f1d6369ada646c6895f6ad591e5aa61d399352db
Dan Stoica <danila@terrabit.ro>
2022-08-26 14:50:54 +0300

    sudo when creating equipment at lot creation (#644)

--- deltatech_service_equipment/__manifest__.py
+++ deltatech_service_equipment/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Services Equipment",
     "summary": "Service Equipment Management",
-    "version": "14.0.1.0.9",
+    "version": "14.0.1.1.0",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "category": "Services/Equipment",
