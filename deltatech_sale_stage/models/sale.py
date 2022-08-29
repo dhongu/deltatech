541271526d8b8aa2be9e436b0496aed72041f8a4
Dorin Hongu <dhongu@gmail.com>
2022-08-29 13:04:16 +0300

    eliminare cod dublat

--- deltatech_sale_stage/models/sale.py
+++ deltatech_sale_stage/models/sale.py
@@ -36,19 +36,3 @@ class SaleOrder(models.Model):
         for order in self:
             if not order.stage_id or not order.stage_id[stage_step]:
                 order.stage_id = stage
-
-
-class SaleOrderStage(models.Model):
-    _name = "sale.order.stage"
-    _description = "SaleOrderStage"
-    _order = "sequence, name"
-
-    name = fields.Char()
-    color = fields.Integer()
-
-    sequence = fields.Integer()
-    send_email = fields.Boolean()  # comanda a fost transmita catre client
-    confirmed = fields.Boolean()  # comanda a fost confirmata
-    delivered = fields.Boolean()  # comanda a fost livrata
-    invoiced = fields.Boolean()  # comanda a fost facturata
-    paid = fields.Boolean()  # comanda a fost platita
