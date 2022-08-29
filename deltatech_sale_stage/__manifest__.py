ea9d747d14dc62106037308cd192c036a6433101
Dan Stoica <danila@terrabit.ro>
2022-08-29 18:04:36 +0300

    fix delivered field name

--- deltatech_sale_stage/__manifest__.py
+++ deltatech_sale_stage/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "Deltatech Sale Order Stage",
-    "version": "14.0.1.0.4",
+    "version": "14.0.1.0.5",
     "author": "Terrabit, Dorin Hongu",
     "license": "LGPL-3",
     "website": "https://www.terrabit.ro",
