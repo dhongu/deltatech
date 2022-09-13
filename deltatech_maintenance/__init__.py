f94f62ef35a30947bba97efa60b2d51e4f5731a3
Dorin Hongu <dhongu@gmail.com>
2022-09-13 13:23:27 +0300

    migrare modul

---
+++ deltatech_maintenance/__init__.py
@@ -0,0 +1,5 @@
+# ©  2008-2022  Deltatech
+# See README.rst file on addons root folder for license details
+
+
+from . import models
---
+++ deltatech_maintenance/__manifest__.py
@@ -0,0 +1,22 @@
+# ©  2008-2022 Deltatech
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Maintenance Extension",
+    "summary": "Maintenance Extension",
+    "version": "15.0.1.0.0",
+    "author": "Terrabit, Dorin Hongu",
+    "license": "LGPL-3",
+    "website": "https://www.terrabit.ro",
+    "category": "Human Resources",
+    "depends": ["maintenance", "stock"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/maintenance_view.xml",
+        "views/stock_picking_view.xml",
+        # 'views/res_config_view.xml',
+    ],
+    "images": ["static/description/main_screenshot.png"],
+    "development_status": "Production/Stable",
+    "maintainers": ["dhongu"],
+}
