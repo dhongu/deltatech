324cb8d23266774b744ee334529cfc0b82a1ff8a
Dorin Hongu <dhongu@gmail.com>
2022-09-30 17:31:42 +0300

    fix js payment and delivery

--- deltatech_website_delivery_and_payment/static/src/js/website_sale_delivery.js
+++ deltatech_website_delivery_and_payment/static/src/js/website_sale_delivery.js
@@ -8,12 +8,11 @@ odoo.define("deltatech_website_delivery_and_payment.checkout", function (require
     var concurrency = require("web.concurrency");
     var dp = new concurrency.DropPrevious();
 
-    // PublicWidget.registry.websiteSaleDeliveryConstrain = publicWidget.Widget.extend({
     websiteSaleDelivery.include({
         selector: ".oe_website_sale",
         events: _.extend({}, websiteSaleDelivery.prototype.events, {
             //   "click #delivery_carrier .o_delivery_carrier_select": "_onCarrierClick",
-            "click #payment_method .o_payment_acquirer_select": "_onAcquirerClickCheck",
+            "click #payment_method .o_payment_option_card": "_onAcquirerClickCheck",
         }),
 
         /**
@@ -61,8 +60,8 @@ odoo.define("deltatech_website_delivery_and_payment.checkout", function (require
             var carrier_id = $carrier.val();
             // Var $radio = $(ev.currentTarget).find('input[type="radio"]');
             // var carrier_id =  $radio.val();
-            var $acquirer = $('#payment_method input[name="pm_id"]').filter(":checked");
-            var acquirer_id = $acquirer.data("acquirer-id");
+            var $acquirer = $('#payment_method input[name="o_payment_radio"]').filter(":checked");
+            var acquirer_id = $acquirer.data("payment-option-id");
 
             var $payButton = $("#o_payment_form_pay");
             $payButton.prop("disabled", true);
@@ -87,7 +86,7 @@ odoo.define("deltatech_website_delivery_and_payment.checkout", function (require
 
         _onCarrierClick: function () {
             this._super.apply(this, arguments);
-            var $acquirer = $('#payment_method input[name="pm_id"]').filter(":checked");
+            var $acquirer = $('#payment_method input[name="o_payment_radio"]').filter(":checked");
             $acquirer.prop("checked", false);
 
             this._doCheckSelection();
