6172408db2f976f2dc0b63bbb8a05ff758bb93fa
danila12 <danila@terrabit.ro>
2022-10-04 12:06:53 +0300

    fix compute number of pallets

--- deltatech_sale_pallet/models/sale.py
+++ deltatech_sale_pallet/models/sale.py
@@ -15,7 +15,8 @@ class SaleOrder(models.Model):
             for line in self.order_line:
                 pallet = pallets.pop(line.product_id.id, False)
                 if pallet:
-                    line.product_uom_qty = max(pallet["product_uom_qty"], line.product_uom_qty)
+                    # line.product_uom_qty = max(pallet["product_uom_qty"], line.product_uom_qty)
+                    line.product_uom_qty = pallet["product_uom_qty"]
 
         for product_id in pallets:
             order_line = self.order_line.new(pallets[product_id])
@@ -30,15 +31,16 @@ class SaleOrder(models.Model):
                 and line.product_id.pallet_qty_min
                 and line.product_uom_qty >= line.product_id.pallet_qty_min
             ):
-                pallet = pallets.get(line.product_id, False)
-                if pallet:
-                    qty = pallet["product_uom_qty"]
-                else:
-                    qty = 0
-                qty = qty + line.product_uom_qty / line.product_id.pallet_qty_min
-                qty = line.product_id.pallet_product_id.uom_id._compute_quantity(
-                    qty, line.product_id.pallet_product_id.uom_id
+                # search for lines with same pallet
+                prod_with_pallet_lines = self.order_line.filtered(
+                    lambda p: p.product_id.pallet_product_id == line.product_id.pallet_product_id
                 )
+
+                # compute sum for all lines with pallets
+                qty = 0
+                for prod_with_pallet_line in prod_with_pallet_lines:
+                    qty += prod_with_pallet_line.compute_pallet_number()
+
                 pallets[line.product_id.pallet_product_id.id] = {
                     "product_uom_qty": qty,
                     "product_id": line.product_id.pallet_product_id.id,
@@ -46,3 +48,16 @@ class SaleOrder(models.Model):
                     "order_id": self.id,
                 }
         return pallets
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    def compute_pallet_number(self):
+        self.ensure_one()
+        if self.product_id.pallet_product_id and self.product_id.pallet_qty_min:
+            if self.product_id.pallet_qty_min > self.product_uom_qty:
+                return 1
+            else:
+                pallets = self.product_uom_qty / self.product_id.pallet_qty_min
+                return round(pallets + 0.49)  # round up
