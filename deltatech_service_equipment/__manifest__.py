9a7e93d78b5bebe72034cdbb2edd8ded2fcb3b46
Dorin Hongu <dhongu@gmail.com>
2022-10-10 12:06:01 +0300

    fix

--- deltatech_service_equipment/__manifest__.py
+++ deltatech_service_equipment/__manifest__.py
@@ -39,6 +39,6 @@
         "views/product_view.xml",
     ],
     "images": ["static/description/main_screenshot.png"],
-    "development_status": "Mature",
+    "development_status": "Production/Stable",
     "maintainers": ["dhongu"],
 }
