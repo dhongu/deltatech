75e47ca5a7e5929404744b189221a49c45802b00
Dorin Hongu <dhongu@gmail.com>
2022-10-14 09:07:57 +0300

    conversie la 16.0

--- deltatech/__manifest__.py
+++ deltatech/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Deltatech",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "summary": "Generic module",
