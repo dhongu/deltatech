5f7f534ab0aa149721c5afe5e88a04bceb5ec5d9
Dorin Hongu <dhongu@gmail.com>
2022-10-14 09:07:57 +0300

    conversie la 16.0

--- deltatech_partner_discount/__manifest__.py
+++ deltatech_partner_discount/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Deltatech partner discount",
     "summary": "Creates a discount field on partner and alerts the user on invoice",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "Terrabit, Dan Stoica",
     "website": "https://www.terrabit.ro",
     "category": "Sale",
