010c1120c01cbbb51fb14c0d0dcd61dcc3448cf7
Dorin Hongu <dhongu@gmail.com>
2022-10-14 09:07:57 +0300

    conversie la 16.0

--- deltatech_card_payment/__manifest__.py
+++ deltatech_card_payment/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Deltatech Payment Method Card",
-    "version": "15.0.1.0.1",
+    "version": "16.0.1.0.1",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "summary": "Deltatech Payment Method Card",
