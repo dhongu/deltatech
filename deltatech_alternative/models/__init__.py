02847cb85255737b77ea9df9eafb7beb7f347912
Dorin Hongu <dhongu@gmail.com>
2022-10-14 22:39:38 +0300

    coduri alternative (#689)

--- deltatech_alternative/models/__init__.py
+++ deltatech_alternative/models/__init__.py
@@ -2,5 +2,6 @@
 #              Dorin Hongu <dhongu(@)gmail(.)com
 # See README.rst file on addons root folder for license details
 
+from . import product_catalog
 from . import product
 from . import res_config_settings
