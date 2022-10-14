7881d21cfa0f7a93b2df999bb310938c7db1c690
Dorin Hongu <dhongu@gmail.com>
2022-10-14 23:56:39 +0300

    sale team

---
+++ deltatech_sale_team/__manifest__.py
@@ -0,0 +1,18 @@
+# ©  2015-2019 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Sale Team Access",
+    "summary": "Sale Team Access",
+    "version": "15.0.1.0.0",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "category": "Sales",
+    "depends": ["sales_team", "account"],
+    "license": "LGPL-3",
+    "data": ["views/crm_team_views.xml", "security/sales_team_security.xml"],
+    "development_status": "Production/Stable",
+    "images": ["static/description/main_screenshot.png"],
+    "installable": True,
+}
