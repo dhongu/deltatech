cf7ee1826d8af7ce5a707c9ce0db3043cb427025
Dorin Hongu <dhongu@gmail.com>
2022-10-28 21:22:49 +0300

    Adaugare modul stock location

---
+++ deltatech_stock_location/__manifest__.py
@@ -0,0 +1,20 @@
+# ©  2008-2021 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Deltatech Location",
+    "summary": "Generate route, picking operation for location",
+    "version": "15.0.1.0.0",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "license": "LGPL-3",
+    "category": "Generic Modules/Other",
+    "depends": [
+        "stock",
+    ],
+    "data": ["views/stock_location_view.xml"],
+    "images": ["static/description/main_screenshot.png"],
+    "development_status": "Beta",
+    "maintainers": ["dhongu"],
+}
