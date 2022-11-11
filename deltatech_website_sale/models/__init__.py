f0455ee446cf9ff2e6a2e50a7547dc4f19dd8aa6
Dorin Hongu <dhongu@gmail.com>
2022-11-11 08:34:28 +0200

    [15.0][FIX]  website_sale (#719)
    * fix caclul cost
    
    * izolare localizare
    
    * fix website_sale

--- deltatech_website_sale/models/__init__.py
+++ deltatech_website_sale/models/__init__.py
@@ -2,6 +2,6 @@
 #              Dorin Hongu <dhongu(@)gmail(.)com
 # See README.rst file on addons root folder for license details
 
-# from . import product
+
 from . import website_keyword
 from . import website
