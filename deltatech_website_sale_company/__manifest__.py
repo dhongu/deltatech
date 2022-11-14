b57ea0578bb28c6febea5adf06d1882d7fcb1aa8
Dorin Hongu <dhongu@gmail.com>
2022-11-14 21:15:45 +0200

    generare companie

---
+++ deltatech_website_sale_company/__manifest__.py
@@ -0,0 +1,18 @@
+# ©  2015-2020 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+{
+    "name": "eCommerce sale to company ",
+    "category": "Website",
+    "summary": "eCommerce sale to company",
+    "version": "14.0.1.0.0",
+    "license": "LGPL-3",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "depends": ["website_sale"],
+    "data": [],
+    "images": ["static/description/main_screenshot.png"],
+    "installable": True,
+    "development_status": "Beta",
+    "maintainers": ["dhongu"],
+}
---
+++ deltatech_website_sale_company/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+Features:
+ -  generare companie dupa complatare datelor de firma
