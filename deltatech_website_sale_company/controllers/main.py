2070c28d0115d9288819ec6db6f8a6cc85a6dbcd
Dorin Hongu <dhongu@gmail.com>
2022-11-15 08:35:42 +0200

    generare companie (#725)

---
+++ deltatech_website_sale_company/controllers/main.py
@@ -0,0 +1,28 @@
+from odoo.http import request
+
+from odoo.addons.website_sale.controllers.main import WebsiteSale
+
+
+class WebsiteSaleAttribute(WebsiteSale):
+    def _checkout_form_save(self, mode, checkout, all_values):
+        partner_id = super(WebsiteSaleAttribute, self)._checkout_form_save(mode, checkout, all_values)
+
+        Partner = request.env["res.partner"]
+        partner_id = Partner.browse(partner_id)
+        if partner_id.company_name and partner_id.vat:
+            if not partner_id.parent_id:
+                values = {
+                    "name": partner_id.company_name,
+                    "is_company": True,
+                    "vat": partner_id.vat,
+                    "city": partner_id.city,
+                    "state_id": partner_id.state_id.id,
+                    "country_id": partner_id.country_id.id,
+                    "street": partner_id.street,
+                }
+                company_partner_id = Partner.sudo().with_context(tracking_disable=True).create(values)
+                partner_id.sudo().write({"parent_id": company_partner_id.id})
+
+            order = request.website.sale_get_order()
+            order.partner_invoice_id = partner_id.commercial_partner_id
+        return partner_id.id
