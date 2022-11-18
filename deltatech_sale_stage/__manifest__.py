74f3a4e27ad83aa519f0d3f00b532eab676903f6
Dorin Hongu <dhongu@gmail.com>
2022-11-18 17:28:47 +0200

    colorare etape

--- deltatech_sale_stage/__manifest__.py
+++ deltatech_sale_stage/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "Deltatech Sale Order Stage",
-    "version": "15.0.1.0.5",
+    "version": "15.0.1.0.6",
     "author": "Terrabit, Dorin Hongu",
     "license": "LGPL-3",
     "website": "https://www.terrabit.ro",
