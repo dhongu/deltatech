b6ff83c349110554712d3f689058b940e962bf21
danila12 <danila@terrabit.ro>
2022-11-29 11:24:09 +0200

    fix lot inventory value

--- deltatech_lot_valuation/__manifest__.py
+++ deltatech_lot_valuation/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Lot valuation",
     "summary": "Lot valuation",
-    "version": "14.0.1.1.2",
+    "version": "14.0.1.1.3",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "category": "Inventory/Inventory",
