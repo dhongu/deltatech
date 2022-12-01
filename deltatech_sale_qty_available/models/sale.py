fdfee1a4bb96f0cbdcbec82d5e9ddb4825712417
Dorin Hongu <dhongu@gmail.com>
2022-12-01 16:33:26 +0200

    qty_available_text

--- deltatech_sale_qty_available/models/sale.py
+++ deltatech_sale_qty_available/models/sale.py
@@ -57,3 +57,34 @@ class SaleOrder(models.Model):
         else:
             domain = [("id", "not in", ready_orders.ids)]
         return domain
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    qty_available_text = fields.Char(string="Available", compute="_compute_qty_available_text")
+
+    def _compute_qty_available_text(self):
+        for line in self:
+            product = line.product_id
+            if line.route_id:
+                location = False
+                for pull in line.route_id.pull_ids:
+                    location = pull.location_src_id
+                if location:
+                    product = line.product_id.with_context(location=location.id)
+            qty_available_text = "N/A"
+
+            qty_available, virtual_available = product.qty_available, product.virtual_available
+            outgoing_qty, incoming_qty = product.outgoing_qty, product.incoming_qty
+
+            if qty_available or virtual_available or outgoing_qty or incoming_qty:
+                qty_available_text = "%s = " % virtual_available
+                if qty_available:
+                    qty_available_text += " %s " % qty_available
+                if outgoing_qty:
+                    qty_available_text += " -%s " % outgoing_qty
+                if incoming_qty:
+                    qty_available_text += " +%s " % incoming_qty
+
+            line.qty_available_text = qty_available_text
