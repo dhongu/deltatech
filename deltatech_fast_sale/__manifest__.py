fa99f939bccb5229ab76b913214709f52a592ebe
danila12 <danila@terrabit.ro>
2022-12-06 15:02:11 +0200

    Fix invoice date

--- deltatech_fast_sale/__manifest__.py
+++ deltatech_fast_sale/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Fast Sale",
-    "version": "15.0.1.0.1",
+    "version": "15.0.1.0.2",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "summary": "Vanzare rapida",
