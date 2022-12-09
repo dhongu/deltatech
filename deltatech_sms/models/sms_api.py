4eb8da26640b1bcee2ea87809c9ab33422c79bad
Dorin Hongu <dhongu@gmail.com>
2022-12-09 04:49:37 +0200

    fix

--- deltatech_sms/models/sms_api.py
+++ deltatech_sms/models/sms_api.py
@@ -22,18 +22,23 @@ class SmsApi(models.AbstractModel):
         # endpoint = self.env['ir.config_parameter'].sudo().get_param('sms.endpoint')
 
         res = []
-        endpoint = self.env["ir.config_parameter"].sudo().get_param("sms.endpoint", "")
+        # endpoint = self.env["ir.config_parameter"].sudo().get_param("sms.endpoint", "")
+        # endpoint =  account.endpoint or endpoint
         for message in params["messages"]:
+            res_value = {"state": "success", "res_id": message["res_id"]}
 
             endpoint = account.endpoint
+            if not endpoint:
+                res_value["state"] = "Endpoint is not defined."
             endpoint = endpoint.format(**message)
             self.env.cr.execute("select unaccent(%s);", [endpoint])
             endpoint_unaccent = self.env.cr.fetchone()[0]
             result = requests.get(endpoint_unaccent)
             response = result.content.decode("utf-8")
-            res_value = {"state": "success", "res_id": message["res_id"]}
+
             if "OK" not in response:
-                res_value["state"] = "server_error: %s " % response
+                _logger.error("SMS: %s" % response)
+                res_value["state"] = "server_error"
             res += [res_value]
 
         return res
