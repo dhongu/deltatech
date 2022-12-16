c25851929122b682bbe3116858a6c16240f10faa
danila12 <danila@terrabit.ro>
2022-12-16 15:38:05 +0200

    fix compute_costs in agreements

--- deltatech_service_consumable/__manifest__.py
+++ deltatech_service_consumable/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Services Consumable",
     "summary": "Service Consumable",
-    "version": "14.0.1.0.8",
+    "version": "14.0.1.0.9",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "category": "Services/Equipment",
