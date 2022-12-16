bc49a2310f9355f8aec8974ede17d81871d9c514
danila12 <danila@terrabit.ro>
2022-12-16 11:29:11 +0200

    fix account move post if not linked to equipments

--- deltatech_service_equipment/__manifest__.py
+++ deltatech_service_equipment/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Services Equipment",
     "summary": "Service Equipment Management",
-    "version": "14.0.1.1.2",
+    "version": "14.0.1.1.3",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "category": "Services/Equipment",
