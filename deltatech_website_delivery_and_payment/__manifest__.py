7a4809e2abc3fd6d9dbd488c92bd108e64531a30
Dorin Hongu <dhongu@gmail.com>
2022-12-19 09:32:01 +0200

    fix payment restrictions (#801)

--- deltatech_website_delivery_and_payment/__manifest__.py
+++ deltatech_website_delivery_and_payment/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Delivery and Payment",
     "category": "Website",
     "summary": "eCommerce Delivery and Payment constrains",
-    "version": "15.0.2.0.4",
+    "version": "15.0.2.0.5",
     "author": "Terrabit, Dorin Hongu",
     "license": "LGPL-3",
     "website": "https://www.terrabit.ro",
