5d514d38b3d3828769c0f8e689e5d0a672994a98
Dorin Hongu <dhongu@gmail.com>
2023-01-02 22:31:02 +0200

    Pregatire spargere module

---
+++ deltatech_service_base/__manifest__.py
@@ -0,0 +1,18 @@
+# ©  2015-2019 Deltatech
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Services Base",
+    "summary": "Manage Services Base",
+    "version": "14.0.2.0.5",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "category": "Services/Agreement",
+    "depends": ["product", "account"],
+    "license": "AGPL-3",
+    "data": [],
+    "images": ["static/description/main_screenshot.png"],
+    "application": True,
+    "development_status": "Production/Stable",
+    "maintainers": ["dhongu"],
+}
