4797aaaadfc27713c244e61f673aae93972d05d7
Dorin Hongu <dhongu@gmail.com>
2023-01-12 10:20:49 +0200

    pret de vanzare

--- deltatech_sale_margin/__manifest__.py
+++ deltatech_sale_margin/__manifest__.py
@@ -1,10 +1,10 @@
-# ©  2008-2021 Deltatech
+# ©  2008-2023 Deltatech
 # See README.rst file on addons root folder for license details
 
 {
     "name": "Sale Margin",
     "summary": "Check price in sale order",
-    "version": "15.0.1.0.4",
+    "version": "15.0.1.0.7",
     "category": "Sales",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
