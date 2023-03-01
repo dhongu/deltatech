4dd21f427c89809b6547c4a80523234fedbd20f4
danila12 <danila@terrabit.ro>
2023-03-01 11:49:25 +0200

    added validation restrict module

---
+++ deltatech_picking_restrict/__manifest__.py
@@ -0,0 +1,18 @@
+# ©  2015-2018 Deltatech
+# See README.rst file on addons root folder for license details
+{
+    "name": "Picking Validation Restrict",
+    "summary": "Restrict picking validation to a certain security group",
+    "version": "15.0.1.0.0",
+    "author": "Terrabit, Dan Stoica",
+    "website": "https://www.terrabit.ro",
+    "category": "Warehouse",
+    "depends": ["stock"],
+    "license": "LGPL-3",
+    "data": [
+        "views/stock_view.xml",
+    ],
+    "images": ["static/description/main_screenshot.png"],
+    "development_status": "Beta",
+    "maintainers": ["danila12"],
+}
