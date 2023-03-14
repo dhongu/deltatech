2276b4371213c8a164bab7338cb1d7b3983723e3
Dorin Hongu <dhongu@gmail.com>
2023-03-14 18:28:29 +0200

    modificare domeniu pentru facturile din portal

--- deltatech_portal_invoice/controllers/portal.py
+++ deltatech_portal_invoice/controllers/portal.py
@@ -9,6 +9,12 @@ from odoo.addons.account.controllers.portal import PortalAccount
 
 
 class PortalAccountInherit(PortalAccount):
+    def _get_invoices_domain(self):
+        return [
+            ("state", "not in", ("cancel", "draft")),
+            ("move_type", "in", ("out_invoice", "out_refund", "in_invoice", "in_refund", "out_receipt", "in_receipt")),
+        ]
+
     @http.route(["/my/invoices_in", "/my/invoices_in/page/<int:page>"], type="http", auth="user", website=True)
     def portal_my_invoices_in(self, page=1, date_begin=None, date_end=None, sortby=None, filterby=None, **kw):
         return self.portal_my_invoices(page, date_begin, date_end, sortby, filterby="bills", **kw)
