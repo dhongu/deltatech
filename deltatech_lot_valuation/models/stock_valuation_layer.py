831104a9ff0243a1069920c1853413a6e161f98e
Dan Stoica <danila@terrabit.ro>
2023-03-23 08:49:40 +0200

    [14.0] lot valuation: removed svl search (#886)
    * removed search modifications for SVL
    
    * readme
    
    * code cleaning

--- deltatech_lot_valuation/models/stock_valuation_layer.py
+++ deltatech_lot_valuation/models/stock_valuation_layer.py
@@ -3,7 +3,7 @@
 # See README.rst file on addons root folder for license details
 
 
-from odoo import api, models
+from odoo import models
 
 
 class StockValuationLayer(models.Model):
@@ -11,15 +11,15 @@ class StockValuationLayer(models.Model):
 
     # lot_id = fields.Many2one("stock.production.lot", "Lot/Serial Number")
 
-    @api.model
-    def _search(self, args, offset=0, limit=None, order=None, count=False, access_rights_uid=None):
-        if self.env.context.get("lot_ids", False):
-            lots = self.env.context["lot_ids"]
-            domain = [("move_line_ids.lot_id", "in", lots.ids)]
-            moves = self.env["stock.move"].search(domain)
-            if moves:
-                args += [("stock_move_id", "in", moves.ids)]
-        return super(StockValuationLayer, self)._search(args, offset, limit, order, count, access_rights_uid)
+    # @api.model
+    # def _search(self, args, offset=0, limit=None, order=None, count=False, access_rights_uid=None):
+    #     if self.env.context.get("lot_ids", False):
+    #         lots = self.env.context["lot_ids"]
+    #         domain = [("move_line_ids.lot_id", "in", lots.ids)]
+    #         moves = self.env["stock.move"].search(domain)
+    #         if moves:
+    #             args += [("stock_move_id", "in", moves.ids)]
+    #     return super(StockValuationLayer, self)._search(args, offset, limit, order, count, access_rights_uid)
 
     # def search(self, args, offset=0, limit=None, order=None, count=False):
     #     if self.env.context.get("lot_ids", False):
@@ -31,11 +31,11 @@ class StockValuationLayer(models.Model):
     #
     #     return super(StockValuationLayer, self).search(args, offset, limit=limit, order=order, count=count)
 
-    def read_group(self, domain, fields, groupby, offset=0, limit=None, orderby=False, lazy=True):
-        if self.env.context.get("lot_ids", False):
-            lots = self.env.context["lot_ids"]
-            local_domain = [("move_line_ids.lot_id", "in", lots.ids)]
-            moves = self.env["stock.move"].search(local_domain)
-            if moves:
-                domain += [("stock_move_id", "in", moves.ids)]
-        return super(StockValuationLayer, self).read_group(domain, fields, groupby, offset, limit, orderby, lazy)
+    # def read_group(self, domain, fields, groupby, offset=0, limit=None, orderby=False, lazy=True):
+    #     if self.env.context.get("lot_ids", False):
+    #         lots = self.env.context["lot_ids"]
+    #         local_domain = [("move_line_ids.lot_id", "in", lots.ids)]
+    #         moves = self.env["stock.move"].search(local_domain)
+    #         if moves:
+    #             domain += [("stock_move_id", "in", moves.ids)]
+    #     return super(StockValuationLayer, self).read_group(domain, fields, groupby, offset, limit, orderby, lazy)
