8b0d8f08558bf0b621142bc73fc0f37f7c866b01
Dan Stoica <danila@terrabit.ro>
2023-05-08 14:03:39 +0300

    fix qty available (#919)

--- deltatech_website_stock_availability/__manifest__.py
+++ deltatech_website_stock_availability/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "eCommerce Stock Availability",
     "category": "Website",
     "summary": "eCommerce Stock Availability",
-    "version": "15.0.1.0.2",
+    "version": "15.0.1.0.3",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "depends": [
