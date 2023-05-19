e05bbece2f145015c7a6244ff86fd9c6518d712e
Dorin Hongu <dhongu@gmail.com>
2023-05-19 15:09:19 +0300

    modificare mod da afisare disponibilitate stoc la furnizor

--- deltatech_vendor_stock/__manifest__.py
+++ deltatech_vendor_stock/__manifest__.py
@@ -6,7 +6,7 @@
 {
     "name": "Vendor Stock",
     "summary": "Vendor stock availability",
-    "version": "15.0.1.0.2",
+    "version": "15.0.1.0.3",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "category": "Warehouse",
