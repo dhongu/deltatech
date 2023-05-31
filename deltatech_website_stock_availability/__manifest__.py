975dd914c37ae9af8ffb810ba67e76e5e65f3661
danila12 <danila@terrabit.ro>
2023-05-31 13:35:43 +0300

    color for vendor stock

--- deltatech_website_stock_availability/__manifest__.py
+++ deltatech_website_stock_availability/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "eCommerce Stock Availability",
     "category": "Website",
     "summary": "eCommerce Stock Availability",
-    "version": "15.0.1.0.4",
+    "version": "15.0.1.0.5",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "depends": [
