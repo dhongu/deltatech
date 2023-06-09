dc5eca939977ac77318e1bc6634c3b060f01a523
Dorin Hongu <dhongu@gmail.com>
2023-06-09 07:57:34 +0300

    adaugare cantitate disponibila

--- deltatech_website_sale_wishlist/models/product_wishlist.py
+++ deltatech_website_sale_wishlist/models/product_wishlist.py
@@ -4,7 +4,7 @@
 
 import logging
 
-from odoo import _, models
+from odoo import _, fields, models
 from odoo.exceptions import UserError
 from odoo.tools.misc import clean_context
 
@@ -13,6 +13,23 @@ _logger = logging.getLogger(__name__)
 
 class ProductWishlist(models.Model):
     _inherit = "product.wishlist"
+
+    qty_available = fields.Float(
+        "Quantity On Hand",
+        compute="_compute_quantities",
+        search="_search_qty_available",
+        compute_sudo=False,
+        digits="Product Unit of Measure",
+    )
+
+    def _compute_quantities(self):
+        for item in self:
+            item.qty_available = item.product_id.qty_available
+
+    def _search_qty_available(self, operator, value):
+        domain = [("qty_available", operator, value)]
+        products = self.env["product.product"]._search(domain)
+        return [("product_id", "in", products)]
 
     def action_launch_replenishment(self):
 
