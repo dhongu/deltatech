489ed78b1b425db67a47c1aa46f4a32d18453552
Dan Stoica <danila@terrabit.ro>
2023-07-13 11:50:12 +0200

    [14.0][UPD] service buttons (#985)
    * rearanjare butoane agreement/equipment
    
    * new line in po's

--- deltatech_service_consumable/__manifest__.py
+++ deltatech_service_consumable/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Services Consumable",
     "summary": "Service Consumable",
-    "version": "14.0.1.1.2",
+    "version": "14.0.1.1.3",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "category": "Services/Equipment",
