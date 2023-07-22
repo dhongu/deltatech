ced09d73160fb6528902e578d1febb37ee609920
Dorin Hongu <dhongu@gmail.com>
2023-07-22 16:10:07 +0300

    migrare la 15

---
+++ deltatech_agreement_management/models/res_partner.py
@@ -0,0 +1,35 @@
+# ©  2008-2023 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+
+from odoo import _, api, fields, models
+
+
+class Partner(models.Model):
+    _inherit = "res.partner"
+
+    agreement_count = fields.Integer(string="Agreements", compute="_compute_agreements_no", search="_search_agreement")
+
+    def view_agreements(self):
+        self.ensure_one()
+        action = {
+            "name": _("Agreement"),
+            "type": "ir.actions.act_window",
+            "view_mode": "tree,form",
+            "res_model": "service.agreement",
+            "domain": [("partner_id", "=", self.id)],
+            "context": {"default_partner_id": self.id},
+        }
+        return action
+
+    @api.model
+    def _search_agreement(self, operator, value):
+        agreements = self.env["service.agreement"].search([])
+        domain = [("id", "in", agreements.partner_id.ids)]
+        return domain
+
+    def _compute_agreements_no(self):
+        for partner in self:
+            agreement_count = self.env["service.agreement"].search_count([("partner_id", "=", partner.id)])
+            partner.agreement_count = agreement_count
