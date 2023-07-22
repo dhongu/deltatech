a9c4feb7c6ef76abf6486792eca0323aa408a056
Dorin Hongu <dhongu@gmail.com>
2023-07-22 16:10:07 +0300

    migrare la 15

--- deltatech_delivery_status/models/sale.py
+++ deltatech_delivery_status/models/sale.py
@@ -15,7 +15,13 @@ class SaleOrder(models.Model):
         for order in self:
             pickings |= order.picking_ids.filtered(lambda p: p.state not in ["done", "cancel"])
 
-        pickings.write({"postponed": True})
+        if pickings:
+            res = True
+            pickings.write({"postponed": True})
+        else:
+            res = False
+
+        return res
 
     def release_delivery(self):
         pickings = self.env["stock.picking"]
@@ -26,3 +32,13 @@ class SaleOrder(models.Model):
     def _compute_postponed_delivery(self):
         for order in self:
             order.postponed_delivery = any([p.postponed for p in order.picking_ids])
+
+    def _action_confirm(self):
+        res = super(SaleOrder, self)._action_confirm()
+        for order in self:
+            if order.team_id.postpone_payment_transfer:
+                tx = self.sudo().transaction_ids._get_last()
+                if not tx or tx.provider == "transfer":
+                    order.postpone_delivery()
+
+        return res
