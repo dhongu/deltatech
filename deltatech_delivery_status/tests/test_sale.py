895db77de3ab2d8a4b674bbc109524bc0db40e96
Dorin Hongu <dhongu@gmail.com>
2023-07-31 08:38:23 +0300

    fix seller_ids

--- deltatech_delivery_status/tests/test_sale.py
+++ deltatech_delivery_status/tests/test_sale.py
@@ -10,7 +10,7 @@ class TestSale(TransactionCase):
         super(TestSale, self).setUp()
         self.partner_a = self.env["res.partner"].create({"name": "Test"})
 
-        seller_ids = [(0, 0, {"name": self.partner_a.id})]
+        seller_ids = [(0, 0, {"partner_id": self.partner_a.id})]
         self.product_a = self.env["product.product"].create(
             {"name": "Test A", "type": "product", "standard_price": 100, "list_price": 150, "seller_ids": seller_ids}
         )
