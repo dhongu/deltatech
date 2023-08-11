351a839b10f422891f44f3b5eb03d606650250d6
danila12 <danila@terrabit.ro>
2023-08-11 11:34:47 +0300

    code cleaning

--- deltatech_stock_inventory/tests/test_stock_inventory.py
+++ deltatech_stock_inventory/tests/test_stock_inventory.py
@@ -19,21 +19,23 @@ class TestStockInventory(TransactionCase):
             {"name": "Test B", "type": "product", "standard_price": 70, "list_price": 150, "seller_ids": seller_ids}
         )
         # self.stock_location = self.env.ref("stock.stock_location_stock")
-        self.stock_location = self.env["stock.location"].create(
-            {"name": "Test location", "usage": "internal"}
-        )
+        self.stock_location = self.env["stock.location"].create({"name": "Test location", "usage": "internal"})
         # Create a user with rights
-        group_inventory_user = self.env.ref('deltatech_stock_inventory.group_view_inventory_button')
-        group_stock_manager = self.env.ref('stock.group_stock_manager')
+        group_inventory_user = self.env.ref("deltatech_stock_inventory.group_view_inventory_button")
+        group_stock_manager = self.env.ref("stock.group_stock_manager")
 
-        self.inventory_user = self.env['res.users'].with_context(
-            no_reset_password=True
-        ).create({
-            'name': 'Inv user',
-            'login': 'invUser',
-            'email': 'inv@odoo.com',
-            'groups_id': [(6, 0, [group_inventory_user.id, group_stock_manager.id])],
-        })
+        self.inventory_user = (
+            self.env["res.users"]
+            .with_context(no_reset_password=True)
+            .create(
+                {
+                    "name": "Inv user",
+                    "login": "invUser",
+                    "email": "inv@odoo.com",
+                    "groups_id": [(6, 0, [group_inventory_user.id, group_stock_manager.id])],
+                }
+            )
+        )
 
     def test_stock_inventory(self):
         inv_line_a = {
@@ -46,14 +48,18 @@ class TestStockInventory(TransactionCase):
             "product_qty": 10000,
             "location_id": self.stock_location.id,
         }
-        inventory = self.env["stock.inventory"].with_user(self.inventory_user).create(
-            {
-                "name": "Inv. productserial1",
-                "line_ids": [
-                    (0, 0, inv_line_a),
-                    (0, 0, inv_line_b),
-                ],
-            }
+        inventory = (
+            self.env["stock.inventory"]
+            .with_user(self.inventory_user)
+            .create(
+                {
+                    "name": "Inv. productserial1",
+                    "line_ids": [
+                        (0, 0, inv_line_a),
+                        (0, 0, inv_line_b),
+                    ],
+                }
+            )
         )
         inventory.with_user(self.inventory_user).action_start()
         inventory.with_user(self.inventory_user).action_validate()
@@ -68,11 +74,19 @@ class TestStockInventory(TransactionCase):
     #     self.product_b.product_tmpl_id.confirm_actual_inventory()
 
     def test_new_inventory(self):
-        inventory = self.env["stock.inventory"].with_user(self.inventory_user).create({"location_ids": [(6, 0, self.stock_location.ids)]})
+        inventory = (
+            self.env["stock.inventory"]
+            .with_user(self.inventory_user)
+            .create({"location_ids": [(6, 0, self.stock_location.ids)]})
+        )
         inventory.with_user(self.inventory_user).action_start()
 
     def test_stock_change_product_qty(self):
-        wizard = Form(self.env["stock.change.product.qty"].with_user(self.inventory_user).with_context(active_ids=self.product_a.ids))
+        wizard = Form(
+            self.env["stock.change.product.qty"]
+            .with_user(self.inventory_user)
+            .with_context(active_ids=self.product_a.ids)
+        )
         wizard.product_id = self.product_a
         wizard.product_tmpl_id = self.product_a.product_tmpl_id
         wizard.new_quantity = 50
