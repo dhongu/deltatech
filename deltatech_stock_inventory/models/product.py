8f22edb4e1b963afb7d2930d37262fedd85909c1
Dan Stoica <danila@terrabit.ro>
2023-08-14 07:13:41 +0200

    [15.0][UPD] deltatech inventory - security (#1000)
    * security group for inventory users
    
    * added admin user to inventory group
    
    * upd translation
    
    * code cleaning
    
    * fix field name
    
    * tests with user
    
    * code cleaning

--- deltatech_stock_inventory/models/product.py
+++ deltatech_stock_inventory/models/product.py
@@ -71,37 +71,3 @@ class ProductTemplate(models.Model):
                 loc.write(values)
             else:
                 self.env["product.warehouse.location"].sudo().create(values)
-
-    # def confirm_actual_inventory(self):
-    #     products = self.env["product.product"]
-    #     for template in self:
-    #         products |= template.product_variant_ids
-    #
-    #     products.confirm_actual_inventory()
-
-
-# class ProductProduct(models.Model):
-#     _inherit = "product.product"
-#
-#     def confirm_actual_inventory(self):
-#         products = self
-#         inventory_values = {"state": "confirm", "line_ids": []}
-#         quants = self.env["stock.quant"].search([("product_id", "in", products.ids)])
-#         for quant in quants:
-#             if quant.location_id.usage == "internal" and (
-#                 not quant.product_id.last_inventory_date
-#                 or (quant.product_id.last_inventory_date and quant.product_id.last_inventory_date < fields.Date.today())
-#             ):
-#                 values = {
-#                     "product_id": quant.product_id.id,
-#                     "product_uom_id": quant.product_id.uom_id.id,
-#                     "location_id": quant.location_id.id,
-#                     "theoretical_qty": quant.quantity,
-#                     "product_qty": quant.quantity,
-#                     "standard_price": quant.product_id.product_tmpl_id.standard_price,
-#                     "is_ok": True,
-#                 }
-#                 inventory_values["line_ids"].append((0, 0, values))
-#         if inventory_values["line_ids"]:
-#             inventory = self.env["stock.inventory"].create(inventory_values)
-#             inventory.action_validate()
--- deltatech_stock_inventory/readme/DESCRIPTION.rst
+++ deltatech_stock_inventory/readme/DESCRIPTION.rst
@@ -1,4 +1,6 @@
 Features:
+ - Adds the old stock.inventory model, with its functionalities
  - Display stock price column at inventory
+ - Security group "Can update quantities" is added. Only users in this group can update product quantities
 
-Parametrul stock.use_inventory_price setat pe True permite actualizarea pretului pentru produsele cu metoda de cost FIFO
+If system parameter "stock.use_inventory_price" is set to True, the cost price of the product is updated with the price on the inventory line (only if product has FIFO evaluation), so the stock valuation that is generated from the inventory has the line's unit price
