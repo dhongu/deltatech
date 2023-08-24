f8a89ae8736677a5665686f9eba1aa216058e25f
Dorin Hongu <dhongu@gmail.com>
2023-08-24 08:20:04 +0300

    [IMP] deltatech_stock_report: pre-commit stuff

--- deltatech_stock_report/__manifest__.py
+++ deltatech_stock_report/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Stock Reports",
     "summary": "Report with positions from picking lists",
-    "version": "15.0.1.0.1",
+    "version": "16.0.1.0.1",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "category": "Generic Modules",
