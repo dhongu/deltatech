a425c6f7d2e061a7f520318bb604fbbc204b369e
Dorin Hongu <dhongu@gmail.com>
2023-08-27 08:23:39 +0300

    update views

--- deltatech_business_process/models/business_development.py
+++ deltatech_business_process/models/business_development.py
@@ -15,6 +15,7 @@ class BusinessDevelopment(models.Model):
     area_id = fields.Many2one(string="Business area", comodel_name="business.area", required=True)
     type_id = fields.Many2one(string="Type", comodel_name="business.development.type", required=True)
 
+    project_id = fields.Many2one(string="Project", comodel_name="business.project")
     approved = fields.Selection(
         [("draft", "Draft"), ("approved", "Approved"), ("rejected", "Rejected"), ("pending", "Pending")],
         string="Approved",
@@ -40,6 +41,7 @@ class BusinessDevelopment(models.Model):
     date_start_fs = fields.Date(string="Start FS", help="Start date functional specification")
     date_end_fs = fields.Date(string="End FS", help="End date functional specification")
     completion_fs = fields.Float(string="Completion FS", help="Completion of functional specification")
+    effort_fs = fields.Float(string="Effort FS", help="Effort for functional specification")
 
     developer_id = fields.Many2one(
         string="Developer", domain="[('is_company', '=', False)]", comodel_name="res.partner"
@@ -47,11 +49,13 @@ class BusinessDevelopment(models.Model):
     date_start_dev = fields.Date(help="Start date development")
     date_end_dev = fields.Date(help="End date development")
     completion_dev = fields.Float(help="Completion development")
+    effort_dev = fields.Float(string="Effort Dev", help="Effort for development")
 
     tester_id = fields.Many2one(string="Tester", domain="[('is_company', '=', False)]", comodel_name="res.partner")
     date_start_test = fields.Date(help="Start date test")
     date_end_test = fields.Date(help="End date test")
     completion_test = fields.Float(help="Completion test")
+    effort_test = fields.Float(string="Effort Test", help="Effort for test")
 
     customer_id = fields.Many2one(string="Customer", comodel_name="res.partner")
 
