00c76844e8c6de2eddddc4d176b6a0fd148d84fa
Dorin Hongu <dhongu@gmail.com>
2023-09-17 10:14:29 +0300

    fix sale_get_order

--- deltatech_website_delivery_and_payment/models/website.py
+++ deltatech_website_delivery_and_payment/models/website.py
@@ -9,10 +9,8 @@ from odoo.http import request
 class Website(models.Model):
     _inherit = "website"
 
-    def sale_get_order(self, force_create=False, code=None, update_pricelist=False, force_pricelist=False):
-        so = super().sale_get_order(
-            force_create=force_create, code=code, update_pricelist=update_pricelist, force_pricelist=force_pricelist
-        )
+    def sale_get_order(self, *args, **kwargs):
+        so = super().sale_get_order(*args, **kwargs)
         acquirer_id = request.context.get("acquirer_id")
         if acquirer_id:
             so = so.with_context(force_acquirer_id=acquirer_id)
