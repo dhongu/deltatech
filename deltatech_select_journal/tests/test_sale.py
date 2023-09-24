e8e205e91889b2cec2927951939b69a22a92043c
Dorin Hongu <dhongu@gmail.com>
2023-09-24 07:01:53 +0300

    modificare test script

--- deltatech_select_journal/tests/test_sale.py
+++ deltatech_select_journal/tests/test_sale.py
@@ -20,7 +20,7 @@ class TestSale(TransactionCase):
                 "standard_price": 100,
                 "list_price": 150,
                 "seller_ids": seller_ids,
-                "company_id": self.env.company,
+                "company_id": self.env.company.id,
             }
         )
         self.product_b = self.env["product.product"].create(
@@ -30,33 +30,28 @@ class TestSale(TransactionCase):
                 "standard_price": 70,
                 "list_price": 150,
                 "seller_ids": seller_ids,
-                "company_id": self.env.company,
+                "company_id": self.env.company.id,
             }
         )
         self.stock_location = self.env.ref("stock.stock_location_stock")
-        inv_line_a = {
-            "product_id": self.product_a.id,
-            "product_qty": 10000,
-            "location_id": self.stock_location.id,
-        }
-        inv_line_b = {
-            "product_id": self.product_b.id,
-            "product_qty": 10000,
-            "location_id": self.stock_location.id,
-        }
-        inventory = self.env["stock.inventory"].create(
-            {
-                "name": "Inv. productserial1",
-                "line_ids": [
-                    (0, 0, inv_line_a),
-                    (0, 0, inv_line_b),
-                ],
-            }
-        )
-        inventory.action_start()
-        inventory.action_validate()
 
-    def test_sale(self):
+        self.env["stock.quant"].with_context(inventory_mode=True).create(
+            {
+                "product_id": self.product_a.id,
+                "inventory_quantity": 10000,
+                "location_id": self.stock_location.id,
+            }
+        ).action_apply_inventory()
+
+        self.env["stock.quant"].with_context(inventory_mode=True).create(
+            {
+                "product_id": self.product_b.id,
+                "inventory_quantity": 10000,
+                "location_id": self.stock_location.id,
+            }
+        ).action_apply_inventory()
+
+    def test_sale_wizard_invoice(self):
         so = Form(self.env["sale.order"])
         so.partner_id = self.partner_a
 
@@ -76,6 +71,30 @@ class TestSale(TransactionCase):
         wizard.amount = "50"
         wizard = wizard.save()
         wizard.create_invoices()
+
+    def test_sale(self):
+        so = Form(self.env["sale.order"])
+        so.partner_id = self.partner_a
+
+        with so.order_line.new() as so_line:
+            so_line.product_id = self.product_a
+            so_line.product_uom_qty = 100
+
+        with so.order_line.new() as so_line:
+            so_line.product_id = self.product_b
+            so_line.product_uom_qty = 10
+
+        self.so = so.save()
+        self.so.action_confirm()
+
+        pick = self.so.picking_ids
+        pick.move_lines.write({"quantity_done": 1})
+        pick.button_validate()
+
+        self.so.with_context(default_journal_id=self.so.team_id.journal_id.id)._create_invoices()
+
+        self.so.invoice_ids.action_post()
+        # self.so.invoice_ids.action_switch_invoice_into_refund_credit_note()
 
     def test_sale_eur_percentage(self):
         so = Form(self.env["sale.order"])
