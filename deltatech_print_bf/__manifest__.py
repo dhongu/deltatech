0e6d469a3c65c5d438ad67c75a98082bf885c137
Dorin Hongu <dhongu@gmail.com>
2023-09-25 13:25:58 +0300

    update

--- deltatech_print_bf/__manifest__.py
+++ deltatech_print_bf/__manifest__.py
@@ -3,7 +3,7 @@
 # See README.rst file on addons root folder for license details
 
 {
-    "name": "Deltatech Print Invoice to ECR",
+    "name": "Deltatech Print Invoice to ECR - Obsolete",
     "summary": "Deltatech Print Invoice to ECR",
     "version": "16.0.1.0.0",
     "author": "Terrabit, Dorin Hongu",
