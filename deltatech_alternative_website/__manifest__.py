d04aaad49412995d5e618db6c349d53396034e93
Dorin Hongu <dhongu@gmail.com>
2023-09-25 10:40:29 +0300

    upgrade module

--- deltatech_alternative_website/__manifest__.py
+++ deltatech_alternative_website/__manifest__.py
@@ -6,16 +6,19 @@
 {
     "name": "Website alternative code",
     "summary": "Show alternative code in website",
-    "version": "16.0.1.0.1",
+    "version": "16.0.1.0.6",
     "author": "Terrabit, Dorin Hongu",
-    "license": "LGPL-3",
+    "license": "OPL-1",
     "website": "https://www.terrabit.ro",
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     "category": "Website",
-    "depends": ["website_sale", "deltatech_alternative"],
-    "data": ["views/product_view.xml", "views/templates.xml"],
+    "depends": ["website", "website_sale", "deltatech_alternative"],
+    "data": [
+        # "views/product_catalog_view.xml",
+        "views/templates.xml"
+    ],
     "images": ["images/main_screenshot.png"],
     "development_status": "Mature",
     "maintainers": ["dhongu"],
