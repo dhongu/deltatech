75a695b053f4800c3fe6bf532fbcd5464d8cfb92
Dorin Hongu <dhongu@gmail.com>
2023-09-26 05:33:25 +0300

    update

--- deltatech_purchase_discount/models/purchase.py
+++ deltatech_purchase_discount/models/purchase.py
@@ -11,7 +11,7 @@ class PurchaseOrderLine(models.Model):
 
     @api.onchange("product_id")
     def onchange_product_id(self):
-        res = super(PurchaseOrderLine, self).onchange_product_id()
+        res = super().onchange_product_id()
         self.price_without_discount = self.price_unit
         return res
 
