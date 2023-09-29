e1821e7913cf461c285535822cb6bb6ad975f238
danila12 <danila@terrabit.ro>
2023-09-29 11:36:48 +0300

    fix access rights

--- deltatech_agreement_management/__manifest__.py
+++ deltatech_agreement_management/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Agreement Management",
     "summary": "Manage agreements numbers, date, state",
-    "version": "15.0.0.0.2",
+    "version": "15.0.0.0.3",
     "author": "Terrabit, Dan Stoica",
     "website": "https://www.terrabit.ro",
     "category": "Services/Agreement",
