842ad8be88c25b40b554155e08d8e54db9ac07e4
danila12 <danila@terrabit.ro>
2023-11-02 15:46:58 +0200

    added date filter

--- deltatech_average_payment_period/__manifest__.py
+++ deltatech_average_payment_period/__manifest__.py
@@ -6,7 +6,7 @@
 {
     "name": "Deltatech Average Payment Period",
     "summary": "Computes average duration of cash accounting",
-    "version": "14.0.1.0.2",
+    "version": "14.0.1.0.3",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "category": "Accounting & Finance",
