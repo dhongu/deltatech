1ec23d3af3775ad9f5bca79b7c9b30208c97b259
Dorin Hongu <dhongu@gmail.com>
2023-11-06 08:11:59 +0200

    modificare preluata din 15

--- deltatech_website_sale_status/__manifest__.py
+++ deltatech_website_sale_status/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "eCommerce Sale Order status",
     "category": "Website",
     "summary": "Additional filters sales orders by status ",
-    "version": "16.0.2.0.2",
+    "version": "16.0.2.0.3",
     "license": "OPL-1",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
