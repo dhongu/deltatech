0e794e160da70ed1b9d923c41e4d4e9cd135cf9b
Dorin Hongu <dhongu@gmail.com>
2023-11-17 03:33:23 +0200

    update

--- deltatech_purchase_xls/__manifest__.py
+++ deltatech_purchase_xls/__manifest__.py
@@ -10,8 +10,15 @@
     "license": "AGPL-3",
     "website": "https://www.terrabit.ro",
     "category": "Purchase",
-    "depends": ["purchase_stock", "report_xlsx"],
-    "data": ["wizard/import_purchase_line_view.xml", "report/purchase_xls.xml", "security/ir.model.access.csv"],
+    "depends": [
+        "purchase_stock",
+        # "report_xlsx"
+    ],
+    "data": [
+        "wizard/import_purchase_line_view.xml",
+        # "report/purchase_xls.xml",
+        "security/ir.model.access.csv",
+    ],
     "images": ["static/description/main_screenshot.png"],
     "installable": True,
     "development_status": "Mature",
