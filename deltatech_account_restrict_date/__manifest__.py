b53efc2b8fe4497696c2281ed9888d030ca82491
VoicuStefan2001 <150918937+VoicuStefan2001@users.noreply.github.com>
2023-11-28 16:51:48 +0200

    Add files via upload

---
+++ deltatech_account_restrict_date/__manifest__.py
@@ -0,0 +1,21 @@
+# ©  2015-2018 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+
+{
+    "name": "Restrict account date",
+    "version": "17.0.1.0.1",
+    "author": "Terrabit, Dan Stoica",
+    "website": "https://www.terrabit.ro",
+    "summary": "Restrict certain operations depending on date",
+    "category": "Accounting",
+    "depends": [
+        "account",
+    ],
+    "license": "OPL-1",
+    # "data": ["views/product_view.xml", "security/ir.model.access.csv"],
+    "images": ["images/main_screenshot.png"],
+    "development_status": "Beta",
+    "maintainers": ["danila12"],
+}
