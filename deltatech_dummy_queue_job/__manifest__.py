ef1af14e0488f2ae9babab6e160237963897bf1e
VoicuStefan2001 <150918937+VoicuStefan2001@users.noreply.github.com>
2023-11-28 16:55:19 +0200

    Add files via upload

---
+++ deltatech_dummy_queue_job/__manifest__.py
@@ -0,0 +1,17 @@
+# ©  2008-2021 Deltatech
+# See README.rst file on addons root folder for license details
+{
+    "name": "Deltatech Dummy Queue Job",
+    "summary": "Dummy Queue Job",
+    "version": "17.0.1.0.0",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "support": "odoo@terrabit.ro",
+    "category": "Warehouse",
+    "depends": ["base"],
+    "data": [],
+    "license": "OPL-1",
+    "images": ["static/description/main_screenshot.png"],
+    "development_status": "Beta",
+    "maintainers": ["dhongu"],
+}
