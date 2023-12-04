3417284c874906e2047bff18aa983ca12a83a923
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2023-12-04 14:08:03 +0200

    [MIG]deltatech_website_country:Migration to 17.0

---
+++ deltatech_website_country/__manifest__.py
@@ -0,0 +1,18 @@
+# ©  2008-2021 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+{
+    "name": "eCommerce Country",
+    "category": "Website",
+    "summary": "eCommerce extension",
+    "version": "17.0.1.0.0",
+    "author": "Terrabit, Dorin Hongu",
+    "license": "LGPL-3",
+    "website": "https://www.terrabit.ro",
+    "depends": ["website_sale"],
+    "data": [],
+    "images": ["static/description/main_screenshot.png"],
+    "installable": True,
+    "development_status": "Mature",
+    "maintainers": ["dhongu"],
+}
