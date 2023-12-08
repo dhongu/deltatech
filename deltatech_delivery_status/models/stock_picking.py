2941786739dddd99b1d986cc24e546a1712c3d20
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2023-12-08 17:06:16 +0200

    [MIG] deltatech_delivery_status:Migration to 17.0

---
+++ deltatech_delivery_status/models/stock_picking.py
@@ -0,0 +1,88 @@
+# ©  2008-2021 Deltatech
+# See README.rst file on addons root folder for license details
+
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError
+from odoo.tools.safe_eval import safe_eval
+
+
+class StockPickingType(models.Model):
+    _inherit = "stock.picking.type"
+
+    postponed = fields.Boolean(string="Postponed")
+
+
+class StockPicking(models.Model):
+    _inherit = "stock.picking"
+
+    @api.model
+    def _default_postponed(self):
+        picking_type = self.env["stock.picking.type"].browse(self._context.get("default_picking_type_id"))
+
+        return picking_type.postponed
+
+    postponed = fields.Boolean(
+        string="Postponed",
+        tracking=True,
+        states={"done": [("readonly", True)], "cancel": [("readonly", True)]},
+        default=_default_postponed,
+    )
+    delivery_state = fields.Selection(
+        [
+            ("draft", "Draft"),
+            ("pre_advice", "Pre advice"),  # awb generat
+            ("in_transit", "In Transit"),  # colet ridicat de curier
+            ("in_warehouse", "In Warehouse"),  # colet in depozitul curierului
+            ("in_delivery", "In delivery"),  # coletul este livrare
+            ("delivered", "Delivered"),
+        ],
+        string="State",
+        default="draft",
+        readonly=False,
+    )
+    available_state = fields.Selection(
+        [("unavailable", "Unavailable"), ("partially", "Partially available"), ("available", "Available")],
+        default=False,
+        store=True,
+        compute="_compute_state",
+    )
+
+    def _action_done(self):
+        res = super()._action_done()
+        for picking in self:
+            if picking.state == "done" and not picking.carrier_id:
+                picking.write({"delivery_state": "delivered"})
+        return res
+
+    @api.depends("move_type", "move_ids.state", "move_ids.picking_id", "postponed")
+    def _compute_state(self):
+        super()._compute_state()
+
+        for picking in self.filtered(lambda p: p.state == "assigned"):
+            if picking.postponed:
+                picking.state = "waiting"
+
+        picking_in_progress = self.filtered(lambda p: p.state in ["assigned", "waiting", "confirmed"])
+        remaining = self - picking_in_progress
+        for picking in picking_in_progress:
+            move_state = picking.move_ids._get_relevant_state_among_moves()
+            map_state = {"assigned": "available", "partially_available": "partially"}
+            picking.available_state = map_state.get(move_state, "unavailable")
+
+        remaining.available_state = False
+
+    def button_validate(self):
+        for picking in self:
+            if picking.postponed:
+                raise UserError(_("The transfer %s is postponed") % picking.name)
+
+        return super().button_validate()
+
+    def _create_backorder(self):
+        backorders = super()._create_backorder()
+        get_param = self.env["ir.config_parameter"].sudo().get_param
+        postponed = get_param("backorders.postponed", default="False")
+        postponed = safe_eval(postponed)
+        if postponed:
+            backorders.write({"postponed": True})
+        return backorders
---
+++ deltatech_delivery_status/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+Features:
+ - delivery status
+ -  Stare livare
