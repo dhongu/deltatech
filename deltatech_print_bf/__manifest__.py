5eaeff9753ef3e640f93446738133d5e5503031e
Dorin Hongu <dhongu@gmail.com>
2023-12-09 06:32:54 +0200

    Vendor stock availability

--- deltatech_print_bf/__manifest__.py
+++ deltatech_print_bf/__manifest__.py
@@ -3,13 +3,13 @@
 # See README.rst file on addons root folder for license details
 
 {
-    "name": "Deltatech Print Invoice to ECR",
+    "name": "Deltatech Print Invoice to ECR - Obsolete",
     "summary": "Deltatech Print Invoice to ECR",
-    "version": "15.0.1.0.0",
+    "version": "15.0.1.0.1",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "category": "Generic Modules/Stock",
-    "depends": [],
+    "depends": ["account"],
     "license": "OPL-1",
     "data": [],
     "images": [],
