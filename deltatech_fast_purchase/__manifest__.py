966bb579ea84568368f57a51210d51ff1e3bbac4
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2023-12-11 17:52:16 +0200

    [MIG] deltatech_fast_purchase:Migration to 17.0

---
+++ deltatech_fast_purchase/__manifest__.py
@@ -0,0 +1,21 @@
+# ©  2008-2021 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Fast Purchase",
+    "version": "17.0.1.0.0",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "summary": "Achizitie rapida",
+    "category": "Purchases",
+    "depends": ["base", "purchase_stock", "stock"],
+    "price": 5.00,
+    "currency": "EUR",
+    "license": "OPL-1",
+    "data": ["views/purchase_view.xml", "views/stock_view.xml"],
+    "images": ["images/main_screenshot.png"],
+    "installable": True,
+    "development_status": "Mature",
+    "maintainers": ["dhongu"],
+}
