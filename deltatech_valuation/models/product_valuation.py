0de53c9fdc6e842c8e6844adb78a2e5d8e675397
Dorin Hongu <dhongu@gmail.com>
2023-12-18 08:55:28 +0200

    Update product valuation with in/out quantities
    Added "quantity_in" and "quantity_out" fields to product valuation to distinguish incoming and outgoing stock. They have been integrated into the product valuation view and model for transparency and better tracking of stock changes. The database query in "product_valuation.py" was updated to handle these new fields.

--- deltatech_valuation/models/product_valuation.py
+++ deltatech_valuation/models/product_valuation.py
@@ -15,7 +15,10 @@ class ProductValuation(models.Model):
     product_id = fields.Many2one("product.product", string="Product", required=True, index=True)
     valuation_area_id = fields.Many2one("valuation.area", string="Valuation Area", index=True)
 
-    quantity = fields.Float(string="Quantity", digits="Product Unit of Measure", required=True, default=1.0)
+    quantity = fields.Float(string="Quantity", digits="Product Unit of Measure")
+    quantity_in = fields.Float(string="Quantity In", digits="Product Unit of Measure")
+    quantity_out = fields.Float(string="Quantity Out", digits="Product Unit of Measure")
+
     amount = fields.Monetary(string="Amount")
     debit = fields.Monetary(string="Debit")
     credit = fields.Monetary(string="Credit")
@@ -120,25 +123,57 @@ class ProductValuation(models.Model):
         self.env.cr.execute(
             """
             INSERT INTO product_valuation
-              (product_id, valuation_area_id, account_id, company_id, quantity, debit, credit, amount)
-            select product_id, valuation_area_id, account_id, company_id, quantity, debit, credit, debit-credit as amount
-            from (
-            SELECT product_id, valuation_area_id, account_id, m.company_id,
-                sum(l.debit) as debit, sum(l.credit) as credit, sum(
-                    l.quantity / NULLIF(COALESCE(uom_line.factor, 1) / COALESCE(uom_template.factor, 1), 0.0) * (
-                    CASE WHEN m.move_type IN ('out_invoice','in_refund') THEN -1 ELSE 1 END
+                (product_id, valuation_area_id, account_id, company_id,
+                quantity, quantity_in, quantity_out, debit, credit, amount)
+            select product_id, valuation_area_id, account_id, company_id,
+                        quantity, quantity_in, quantity_out, debit, credit, debit-credit as amount
+            FROM (
+            SELECT product_id, valuation_area_id, account_id, company_id,
+                sum(debit) as debit, sum(credit) as credit,
+                sum(
+                    quantity * (
+                    CASE
+                        WHEN move_type ='in_invoice' THEN 1
+                        WHEN move_type ='in_refund' THEN -1
+                        WHEN move_type IN ('out_invoice','out_refund') THEN 0
+                        ELSE
+                            CASE WHEN debit > 0 THEN 1 ELSE 0 END
+                    END
+                    )
+                ) as quantity_in,
+
+                sum(
+                    quantity * (
+                    CASE
+                        WHEN move_type ='out_invoice' THEN 1
+                        WHEN move_type ='out_refund' THEN -1
+                        WHEN move_type IN ('in_invoice','in_refund') THEN 0
+                        ELSE CASE WHEN credit > 0 THEN -1 ELSE 0 END
+                    END
+                    )
+                ) as quantity_out,
+
+                sum(
+                    quantity * (
+                    CASE WHEN move_type IN ('out_invoice','in_refund') THEN -1 ELSE 1 END
                     )
                 ) as quantity
+
+            FROM (
+                SELECT product_id, valuation_area_id, account_id, m.company_id,
+                    debit, credit, move_type,
+                    l.quantity / NULLIF(COALESCE(uom_line.factor, 1) / COALESCE(uom_template.factor, 1), 0.0) as quantity
                 FROM account_move_line as l
-                LEFT JOIN account_move as m ON l.move_id=m.id
-                LEFT JOIN product_product product ON product.id = l.product_id
-                LEFT JOIN product_template template ON template.id = product.product_tmpl_id
-                LEFT JOIN uom_uom uom_line ON uom_line.id = l.product_uom_id
-                LEFT JOIN uom_uom uom_template ON uom_template.id = template.uom_id
+                    LEFT JOIN account_move as m ON l.move_id=m.id
+                    LEFT JOIN product_product product ON product.id = l.product_id
+                    LEFT JOIN product_template template ON template.id = product.product_tmpl_id
+                    LEFT JOIN uom_uom uom_line ON uom_line.id = l.product_uom_id
+                    LEFT JOIN uom_uom uom_template ON uom_template.id = template.uom_id
                 WHERE
                     account_id in %(account_ids)s
                     AND m.state = 'posted'
-                GROUP BY  product_id, valuation_area_id, account_id, m.company_id
+                ) as sub
+             GROUP BY  product_id, valuation_area_id, account_id, company_id
            ) as a """,
             params,
         )
