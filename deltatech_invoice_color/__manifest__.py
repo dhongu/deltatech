3faee4031456e867277edfc8a4d431d74b9b219f
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2023-12-19 12:44:53 +0200

    [FIX]manifest deltatech_invoice_color

--- deltatech_invoice_color/__manifest__.py
+++ deltatech_invoice_color/__manifest__.py
@@ -8,7 +8,9 @@
     "license": "OPL-1",
     "website": "https://www.terrabit.ro",
     "depends": ["account"],
-    "data": ["views/account_invoice_view.xml"],
+    "data": [
+   # "views/account_invoice_view.xml"
+    ],
     # "images": ["static/description/main_screenshot.png"],
     "development_status": "Beta",
     "maintainers": ["VoicuStefan2001 "],
