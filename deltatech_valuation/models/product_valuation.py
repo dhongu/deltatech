fa9b6d14b026719e5fb49594965010905976fcb8
Dorin Hongu <dhongu@gmail.com>
2023-12-20 13:55:59 +0200

    Refactor product valuation calculations in Deltatech module
    The refactoring aims to simplify and optimize the product valuation calculations by restructuring the SQL queries. The calculation has been split into smaller, more manageable sub-queries resulting in increased readability and performance. The changes also include better control over data selection and updates for product valuation.

--- deltatech_valuation/models/product_valuation.py
+++ deltatech_valuation/models/product_valuation.py
@@ -84,66 +84,30 @@ class ProductValuation(models.Model):
             "company_ids": tuple(companies.ids),
             "valuation_area_ids": tuple(valuation_areas.ids) or (None,),
         }
-        self.env.cr.execute(
-            """
-            SELECT product_id, valuation_area_id, account_id, m.company_id,
-                sum(l.debit) as debit, sum(l.credit) as credit, sum(
-                    l.quantity / NULLIF(COALESCE(uom_line.factor, 1) / COALESCE(uom_template.factor, 1), 0.0) * (
-                    CASE WHEN m.move_type IN ('out_invoice','in_refund') THEN -1 ELSE 1 END
-                    )
-                ) as quantity
-                FROM account_move_line as l
-                LEFT JOIN account_move as m ON l.move_id=m.id
-                LEFT JOIN product_product product ON product.id = l.product_id
-                LEFT JOIN product_template template ON template.id = product.product_tmpl_id
-                LEFT JOIN uom_uom uom_line ON uom_line.id = l.product_uom_id
-                LEFT JOIN uom_uom uom_template ON uom_template.id = template.uom_id
-                WHERE product_id in %(product_ids)s
-                    AND account_id in %(account_ids)s
-                    AND m.company_id in %(company_ids)s
-                    AND (valuation_area_id in %(valuation_area_ids)s or valuation_area_id is null)
-                    AND m.state = 'posted'
-                GROUP BY  product_id, valuation_area_id, account_id, m.company_id
-           """,
-            params,
+
+        sql = """
+          UPDATE product_valuation AS pv
+          SET quantity = sub.quantity,
+                quantity_in = sub.quantity_in,
+                quantity_out = sub.quantity_out,
+                debit = sub.debit,
+                credit = sub.credit,
+                amount = sub.debit - sub.credit
+            FROM ( %s ) as sub
+            WHERE
+                pv.product_id = sub.product_id AND
+                pv.account_id = sub.account_id AND
+                pv.valuation_area_id = sub.valuation_area_id AND
+                pv.company_id = sub.company_id
+        """ % self._get_sql_select(
+            all=False
         )
 
-        res = self.env.cr.dictfetchall()
+        self.env.cr.execute(sql, params)
 
-        for valuation in self:
-            valuation.amount = 0
-            valuation.debit = 0
-            valuation.credit = 0
-            valuation.quantity = 0
-            for line in res:
-                if not line["valuation_area_id"]:
-                    line["valuation_area_id"] = False
-                if (
-                    line["product_id"] == valuation.product_id.id
-                    and line["valuation_area_id"] == valuation.valuation_area_id.id
-                    and line["account_id"] == valuation.account_id.id
-                    and line["company_id"] == valuation.company_id.id
-                ):
-                    valuation.quantity += line["quantity"]
-                    valuation.amount += line["debit"] - line["credit"]
-                    valuation.debit += line["debit"]
-                    valuation.credit += line["credit"]
-                    res.remove(line)
-
-    def recompute_all_amount(self):
-        params = {
-            "account_ids": tuple(self.env["account.account"].search([("stock_valuation", "=", True)]).ids),
-        }
-        self.env.cr.execute("DELETE FROM product_valuation WHERE account_id in %(account_ids)s", params)
-        self.env.cr.execute(
-            """
-            INSERT INTO product_valuation
-                (product_id, valuation_area_id, account_id, company_id,
-                quantity, quantity_in, quantity_out, debit, credit, amount)
-            select product_id, valuation_area_id, account_id, company_id,
-                        quantity, quantity_in, quantity_out, debit, credit, debit-credit as amount
-            FROM (
-            SELECT product_id, valuation_area_id, account_id, company_id,
+    def _get_sql_select(self, all=True):
+        sql = """
+        SELECT product_id, valuation_area_id, account_id, company_id,
                 sum(debit) as debit, sum(credit) as credit,
                 sum(
                     quantity * (
@@ -175,6 +139,17 @@ class ProductValuation(models.Model):
                 ) as quantity
 
             FROM (
+               %s
+                ) as sub
+             GROUP BY  product_id, valuation_area_id, account_id, company_id
+
+        """ % self._get_sql_sub_select(
+            all
+        )
+        return sql
+
+    def _get_sql_sub_select(self, all=True):
+        sql = """
                 SELECT product_id, valuation_area_id, account_id, m.company_id,
                     debit, credit, move_type,
                     l.quantity / NULLIF(COALESCE(uom_line.factor, 1) / COALESCE(uom_template.factor, 1), 0.0) as quantity
@@ -187,11 +162,92 @@ class ProductValuation(models.Model):
                 WHERE
                     account_id in %(account_ids)s
                     AND m.state = 'posted'
-                ) as sub
-             GROUP BY  product_id, valuation_area_id, account_id, company_id
-           ) as a """,
-            params,
+
+        """
+        if not all:
+            sql += """
+                    AND product_id in %(product_ids)s
+                    AND account_id in %(account_ids)s
+                    AND valuation_area_id in %(valuation_area_ids)s
+            """
+
+        return sql
+
+    def recompute_all_amount(self):
+        params = {
+            "account_ids": tuple(self.env["account.account"].search([("stock_valuation", "=", True)]).ids),
+        }
+        self.env.cr.execute("DELETE FROM product_valuation WHERE account_id in %(account_ids)s", params)
+
+        sql = (
+            """
+            INSERT INTO product_valuation
+                (product_id, valuation_area_id, account_id, company_id,
+                quantity, quantity_in, quantity_out, debit, credit, amount)
+            FROM ( %s )
+            """
+            % self._get_sql_select()
         )
+
+        self.env.cr.execute(sql, params)
+
+        # self.env.cr.execute(
+        #     """
+        #     INSERT INTO product_valuation
+        #         (product_id, valuation_area_id, account_id, company_id,
+        #         quantity, quantity_in, quantity_out, debit, credit, amount)
+        #     select product_id, valuation_area_id, account_id, company_id,
+        #                 quantity, quantity_in, quantity_out, debit, credit, debit-credit as amount
+        #     FROM (
+        #     SELECT product_id, valuation_area_id, account_id, company_id,
+        #         sum(debit) as debit, sum(credit) as credit,
+        #         sum(
+        #             quantity * (
+        #             CASE
+        #                 WHEN move_type ='in_invoice' THEN 1
+        #                 WHEN move_type ='in_refund' THEN -1
+        #                 WHEN move_type IN ('out_invoice','out_refund') THEN 0
+        #                 ELSE
+        #                     CASE WHEN debit > 0 THEN 1 ELSE 0 END
+        #             END
+        #             )
+        #         ) as quantity_in,
+        #
+        #         sum(
+        #             quantity * (
+        #             CASE
+        #                 WHEN move_type ='out_invoice' THEN 1
+        #                 WHEN move_type ='out_refund' THEN -1
+        #                 WHEN move_type IN ('in_invoice','in_refund') THEN 0
+        #                 ELSE CASE WHEN credit > 0 THEN -1 ELSE 0 END
+        #             END
+        #             )
+        #         ) as quantity_out,
+        #
+        #         sum(
+        #             quantity * (
+        #             CASE WHEN move_type IN ('out_invoice','in_refund') THEN -1 ELSE 1 END
+        #             )
+        #         ) as quantity
+        #
+        #     FROM (
+        #         SELECT product_id, valuation_area_id, account_id, m.company_id,
+        #             debit, credit, move_type,
+        #             l.quantity / NULLIF(COALESCE(uom_line.factor, 1) / COALESCE(uom_template.factor, 1), 0.0) as quantity
+        #         FROM account_move_line as l
+        #             LEFT JOIN account_move as m ON l.move_id=m.id
+        #             LEFT JOIN product_product product ON product.id = l.product_id
+        #             LEFT JOIN product_template template ON template.id = product.product_tmpl_id
+        #             LEFT JOIN uom_uom uom_line ON uom_line.id = l.product_uom_id
+        #             LEFT JOIN uom_uom uom_template ON uom_template.id = template.uom_id
+        #         WHERE
+        #             account_id in %(account_ids)s
+        #             AND m.state = 'posted'
+        #         ) as sub
+        #      GROUP BY  product_id, valuation_area_id, account_id, company_id
+        #    ) as a """,
+        #     params,
+        # )
 
 
 class ProductValuationHistory(models.Model):
@@ -257,6 +313,7 @@ class ProductValuationHistory(models.Model):
             ("product_id", "in", products.ids),
             ("account_id", "in", accounts.ids),
             ("company_id", "in", companies.ids),
+            ("valuation_area_id", "in", valuation_areas.ids),
         ]
         if valuation_areas:
             domain.append(("valuation_area_id", "in", valuation_areas.ids))
