d9669a42117825124f920f1596e01aa7ac814d31
Dorin Hongu <dhongu@gmail.com>
2023-12-20 15:09:41 +0200

    Refactor and optimize SQL operations in product valuation
    Implemented improvements to SQL code in the product valuation module to enhance its readability and maintainability. The commit simplifies the implementation of an "INSERT INTO" statement and consolidates multiple instances of repetitive code, thereby optimizing the overall codebase without compromising on functionality.

--- deltatech_valuation/models/product_valuation.py
+++ deltatech_valuation/models/product_valuation.py
@@ -184,7 +184,9 @@ class ProductValuation(models.Model):
             INSERT INTO product_valuation
                 (product_id, valuation_area_id, account_id, company_id,
                 quantity, quantity_in, quantity_out, debit, credit, amount)
-            FROM ( %s )
+            select product_id, valuation_area_id, account_id, company_id,
+                         quantity, quantity_in, quantity_out, debit, credit, debit-credit as amount
+            FROM ( %s ) as a
             """
             % self._get_sql_select()
         )
@@ -304,122 +306,9 @@ class ProductValuationHistory(models.Model):
             )
         return valuation
 
-    def recompute_amount(self):
-        valuation_areas = self.mapped("valuation_area_id")
-        products = self.mapped("product_id")
-        accounts = self.mapped("account_id")
-        companies = self.mapped("company_id")
-        domain = [
-            ("product_id", "in", products.ids),
-            ("account_id", "in", accounts.ids),
-            ("company_id", "in", companies.ids),
-            ("valuation_area_id", "in", valuation_areas.ids),
-        ]
-        if valuation_areas:
-            domain.append(("valuation_area_id", "in", valuation_areas.ids))
-
-        params = {
-            "product_ids": tuple(products.ids),
-            "account_ids": tuple(accounts.ids),
-            "year": tuple(self.mapped("year")),
-            "month": tuple(self.mapped("month")),
-            "company_ids": tuple(companies.ids),
-            "valuation_area_ids": tuple(valuation_areas.ids) or (None,),
-        }
-        self.env.cr.execute(
-            """
-            SELECT product_id, valuation_area_id, account_id, m.company_id,
-             EXTRACT(YEAR FROM m.date) as year, EXTRACT(MONTH FROM m.date) as month,
-                 (date_trunc('month', m.date) + interval '1 month - 1 day')::date as date,
-                sum(l.debit) as debit, sum(l.credit) as credit, sum(
-                    l.quantity / NULLIF(COALESCE(uom_line.factor, 1) / COALESCE(uom_template.factor, 1), 0.0) * (
-                    CASE WHEN m.move_type IN ('out_invoice','in_refund') THEN -1 ELSE 1 END
-                    )
-                ) as quantity
-                FROM account_move_line as l
-                LEFT JOIN account_move as m ON l.move_id=m.id
-                LEFT JOIN product_product product ON product.id = l.product_id
-                LEFT JOIN product_template template ON template.id = product.product_tmpl_id
-                LEFT JOIN uom_uom uom_line ON uom_line.id = l.product_uom_id
-                LEFT JOIN uom_uom uom_template ON uom_template.id = template.uom_id
-                WHERE product_id in %(product_ids)s
-                    AND account_id in %(account_ids)s
-                    AND m.company_id in %(company_ids)s
-                    AND EXTRACT(YEAR FROM m.date)  in %(year)s
-                    AND EXTRACT(MONTH FROM m.date) in %(month)s
-                    AND (valuation_area_id in %(valuation_area_ids)s or valuation_area_id is null)
-                    AND m.state = 'posted'
-                GROUP BY  product_id, valuation_area_id, account_id, m.company_id,
-                EXTRACT(YEAR FROM m.date), EXTRACT(MONTH FROM m.date),
-                (date_trunc('month', m.date) + interval '1 month - 1 day')::date
-           """,
-            params,
-        )
-
-        res = self.env.cr.dictfetchall()
-
-        for valuation in self:
-            valuation.amount = 0
-            valuation.quantity = 0
-            valuation.debit = 0
-            valuation.credit = 0
-            for line in res:
-                if not line["valuation_area_id"]:
-                    line["valuation_area_id"] = False
-                if (
-                    line["product_id"] == valuation.product_id.id
-                    and line["valuation_area_id"] == valuation.valuation_area_id.id
-                    and line["account_id"] == valuation.account_id.id
-                    and line["company_id"] == valuation.company_id.id
-                    and line["year"] == valuation.year
-                    and line["month"] == valuation.month
-                ):
-                    valuation.quantity += line["quantity"]
-                    valuation.amount += line["debit"] - line["credit"]
-                    valuation.debit += line["debit"]
-                    valuation.credit += line["credit"]
-                    valuation.date = line["date"]
-                    res.remove(line)
-
-    def recompute_all_amount(self):
-        params = {
-            "account_ids": tuple(self.env["account.account"].search([("stock_valuation", "=", True)]).ids),
-        }
-
-        if self.env.company.valuation_area_level == "company" and not self.env.company.valuation_area_id:
-            _logger.info("Creare zona de evaluare pentru companie.")
-            valuation_area = self.env["valuation.area"].create(
-                {
-                    "name": self.env.company.name,
-                    "company_id": self.env.company.id,
-                }
-            )
-
-            self.env.company.valuation_area_id = valuation_area.id
-            params["valuation_area_id"] = valuation_area.id
-            _logger.info("Actualizre zona de evaluare pentru notele contabile")
-            self.env.cr.execute(
-                """
-                UPDATE account_move_line SET   valuation_area_id = %(valuation_area_id)s
-                where account_id in %(account_ids)s
-                """,
-                params,
-            )
-
-            return
-
-        _logger.info("Stergere linii istoric")
-        self.env.cr.execute("DELETE FROM product_valuation_history WHERE account_id in %(account_ids)s", params)
-        _logger.info("Calculare linii istoric miscari lunare")
-        self.env.cr.execute(
-            """
-            INSERT INTO product_valuation_history
-                (product_id, valuation_area_id, account_id, company_id, year, month, date,
-                quantity, quantity_in, quantity_out, debit, credit, amount)
-            SELECT product_id, valuation_area_id, account_id, company_id,  year, month, date,
-                        quantity, quantity_in, quantity_out, debit, credit, debit-credit as amount
-            FROM (
-            SELECT product_id, valuation_area_id, account_id, company_id,  year, month, date,
+    def _get_sql_select(self, all=True):
+        sql = """
+                    SELECT product_id, valuation_area_id, account_id, company_id,  year, month, date,
                 sum(debit) as debit, sum(credit) as credit,
                 sum(
                     quantity * (
@@ -451,7 +340,17 @@ class ProductValuationHistory(models.Model):
                 ) as quantity
 
             FROM (
-                SELECT product_id, valuation_area_id, account_id, m.company_id,
+                 %s
+                ) as sub
+             GROUP BY  product_id, valuation_area_id, account_id, company_id, year, month, date
+        """ % self._get_sql_sub_select(
+            all
+        )
+        return sql
+
+    def _get_sql_sub_select(self, all=True):
+        sql = """
+            SELECT product_id, valuation_area_id, account_id, m.company_id,
                     debit, credit, move_type,
                     EXTRACT(YEAR FROM m.date) as year, EXTRACT(MONTH FROM m.date) as month,
                     (date_trunc('month', m.date) + interval '1 month - 1 day')::date as date,
@@ -465,11 +364,106 @@ class ProductValuationHistory(models.Model):
                 WHERE
                     account_id in %(account_ids)s
                     AND m.state = 'posted'
-                ) as sub
-             GROUP BY  product_id, valuation_area_id, account_id, company_id, year, month, date
-           ) as a """,
-            params,
+        """
+        if not all:
+            sql += """
+                    AND product_id in %(product_ids)s
+                    AND account_id in %(account_ids)s
+                    AND valuation_area_id in %(valuation_area_ids)s
+                    AND EXTRACT(YEAR FROM m.date)  in %(year)s
+                    AND EXTRACT(MONTH FROM m.date) in %(month)s
+            """
+
+        return sql
+
+    def recompute_amount(self):
+        valuation_areas = self.mapped("valuation_area_id")
+        products = self.mapped("product_id")
+        accounts = self.mapped("account_id")
+        companies = self.mapped("company_id")
+        domain = [
+            ("product_id", "in", products.ids),
+            ("account_id", "in", accounts.ids),
+            ("company_id", "in", companies.ids),
+            ("valuation_area_id", "in", valuation_areas.ids),
+        ]
+        if valuation_areas:
+            domain.append(("valuation_area_id", "in", valuation_areas.ids))
+
+        params = {
+            "product_ids": tuple(products.ids),
+            "account_ids": tuple(accounts.ids),
+            "year": tuple(self.mapped("year")),
+            "month": tuple(self.mapped("month")),
+            "company_ids": tuple(companies.ids),
+            "valuation_area_ids": tuple(valuation_areas.ids) or (None,),
+        }
+        sql = """
+           UPDATE product_valuation_history AS pv
+            SET quantity = sub.quantity,
+                quantity_in = sub.quantity_in,
+                quantity_out = sub.quantity_out,
+                debit = sub.debit,
+                credit = sub.credit,
+                amount = sub.debit - sub.credit
+            FROM ( %s ) as sub
+            WHERE
+                pv.product_id = sub.product_id AND
+                pv.account_id = sub.account_id AND
+                pv.valuation_area_id = sub.valuation_area_id AND
+                pv.company_id = sub.company_id AND
+                pv.year = sub.year AND
+                pv.month = sub.month AND
+                pv.date = sub.date
+        """ % self._get_sql_select(
+            all=False
         )
+        self.env.cr.execute(sql, params)
+
+    def recompute_all_amount(self):
+        params = {
+            "account_ids": tuple(self.env["account.account"].search([("stock_valuation", "=", True)]).ids),
+        }
+
+        if self.env.company.valuation_area_level == "company" and not self.env.company.valuation_area_id:
+            _logger.info("Creare zona de evaluare pentru companie.")
+            valuation_area = self.env["valuation.area"].create(
+                {
+                    "name": self.env.company.name,
+                    "company_id": self.env.company.id,
+                }
+            )
+
+            self.env.company.valuation_area_id = valuation_area.id
+            params["valuation_area_id"] = valuation_area.id
+            _logger.info("Actualizre zona de evaluare pentru notele contabile")
+            self.env.cr.execute(
+                """
+                UPDATE account_move_line SET valuation_area_id = %(valuation_area_id)s
+                where account_id in %(account_ids)s
+                """,
+                params,
+            )
+
+            return
+
+        _logger.info("Stergere linii istoric")
+        self.env.cr.execute("DELETE FROM product_valuation_history WHERE account_id in %(account_ids)s", params)
+        _logger.info("Calculare linii istoric miscari lunare")
+
+        sql = (
+            """
+            INSERT INTO product_valuation_history
+                (product_id, valuation_area_id, account_id, company_id, year, month, date,
+                quantity, quantity_in, quantity_out, debit, credit, amount)
+            SELECT product_id, valuation_area_id, account_id, company_id,  year, month, date,
+                        quantity, quantity_in, quantity_out, debit, credit, debit-credit as amount
+            FROM ( %s ) as a
+        """
+            % self._get_sql_select()
+        )
+
+        self.env.cr.execute(sql, params)
 
         _logger.info("resetare Sold initial si final")
         self.env.cr.execute(
