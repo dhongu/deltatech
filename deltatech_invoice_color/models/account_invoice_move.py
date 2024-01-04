87e9c248f1c886abac79756fa4658f89738ad2ea
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2024-01-04 15:34:04 +0200

    [MIG] deltatech_invoice_color update

--- deltatech_invoice_color/models/account_invoice_move.py
+++ deltatech_invoice_color/models/account_invoice_move.py
@@ -2,21 +2,28 @@ from odoo import api, fields, models
 
 
 class AccountMoveLine(models.Model):
-    """Override AccountInvoice_line to add the link to the purchase order line it is related to"""
-
     _inherit = "account.move.line"
 
-    color_trigger = fields.Boolean(string="Trigger", compute="_compute_color_trigger")
+    color_trigger = fields.Selection(
+        [("danger", "Danger"), ("warning", "Warning")], string="Trigger", compute="_compute_color_trigger"
+    )
 
     @api.depends("purchase_line_id", "sale_line_ids")
     def _compute_color_trigger(self):
         for line in self:
-            if (
-                line.purchase_line_id
-                and line.move_id.move_type == "in_invoice"
-                or line.sale_line_ids
-                and line.move_id.move_type == "out_invoice"
-            ):
-                line.color_trigger = True
+            color_trigger = False
+            if line.move_id.move_type not in ("in_invoice", "in_refund", "out_invoice", "out_refund"):
+                line.color_trigger = color_trigger
+                continue
+            if not line.product_id:
+                color_trigger = "warning"
             else:
-                line.color_trigger = False
+                if line.product_id.type == "product":
+                    if line.move_id.move_type in ("in_invoice", "in_refund"):
+                        if not line.purchase_line_id:
+                            color_trigger = "danger"
+                    else:
+                        if not line.sale_line_ids:
+                            color_trigger = "danger"
+
+            line.color_trigger = color_trigger
