ec822142e0ad694cdd78ebc7f3495925e7525a82
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2024-01-05 12:06:52 +0200

    [MIG] deltatech_sale_pallet:Migration to 17.0

---
+++ deltatech_sale_pallet/__manifest__.py
@@ -0,0 +1,17 @@
+# ©  2008-2021 Deltatech
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Sale Pallet",
+    "summary": "Sale pallet",
+    "version": "17.0.1.0.6",
+    "category": "Sales",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "depends": ["sale_margin", "account"],
+    "license": "LGPL-3",
+    "data": ["views/product_view.xml", "views/invoice_view.xml"],
+    "images": ["static/description/main_screenshot.png"],
+    "development_status": "Beta",
+    "maintainers": ["dhongu"],
+}
---
+++ deltatech_sale_pallet/models/product_category.py
@@ -0,0 +1,12 @@
+# ©  2008-2021 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+
+from odoo import fields, models
+
+
+class ProductCategory(models.Model):
+    _inherit = "product.category"
+
+    pallet = fields.Boolean()
