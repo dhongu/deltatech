522520366eb0c47ea5184d8c7e75974f0f0f4bea
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2024-01-15 12:26:57 +0200

    [MIG] deltatech_product_margin:Migration to 17.0

---
+++ deltatech_product_margin/__manifest__.py
@@ -0,0 +1,18 @@
+# ©  2008-2021 Deltatech
+# See README.rst file on addons root folder for license details
+
+
+{
+    "name": "Deltatech Product Margin",
+    "summary": "Product margin and trade markup",
+    "version": "17.0.1.0.1",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "license": "OPL-1",
+    "category": "Generic Modules/Other",
+    "depends": ["product", "deltatech_product_trade_markup"],
+    "data": ["views/product_view.xml"],
+    "development_status": "Beta",
+    "images": ["static/description/main_screenshot.png"],
+    "maintainers": ["dhongu"],
+}
