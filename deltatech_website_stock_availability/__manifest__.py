cd4bdfebcea5ae395ce493b7af97aa02ee2cca7f
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2024-01-18 19:00:20 +0200

    [FIX] deltatech_website_stock_avilability

--- deltatech_website_stock_availability/__manifest__.py
+++ deltatech_website_stock_availability/__manifest__.py
@@ -8,7 +8,7 @@
     "version": "16.0.1.0.2",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
-    "depends": ["website", "website_sale_stock", "purchase"],
+    "depends": ["website", "website_sale_stock", "purchase", "deltatech_vendor_stock"],
     "data": [
         "views/product_view.xml",
         "views/website_sale_stock_templates.xml",
