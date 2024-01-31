eb5b37d02c73fe358be14accdf6bdea288bb22cf
Dorin Hongu <dhongu@gmail.com>
2024-01-31 05:01:03 +0200

    update

--- deltatech_website_stock_location/models/__init__.py
+++ deltatech_website_stock_location/models/__init__.py
@@ -4,3 +4,5 @@
 
 from . import product
 from . import website
+from . import res_config_settings
+from . import sale_order
--- deltatech_website_stock_location/models/website.py
+++ deltatech_website_stock_location/models/website.py
@@ -9,3 +9,21 @@ class Website(models.Model):
     _inherit = "website"
 
     location_id = fields.Many2one("stock.location", string="Stock Location")
+
+    def _get_warehouse_available(self):
+        if not self.warehouse_id:
+            self = self.with_context(all_warehouses=True)
+        if not self.location_id:
+            self = self.with_context(all_locations=True)
+        return super()._get_warehouse_available()
+
+    def sale_get_order(self, force_create=False, code=None, update_pricelist=False, force_pricelist=False):
+        so = super().sale_get_order(
+            force_create=force_create, code=code, update_pricelist=update_pricelist, force_pricelist=force_pricelist
+        )
+        if so and so.website_id:
+            if not so.website_id.warehouse_id:
+                so = so.with_context(all_warehouses=True)
+            if not so.website_id.location_id:
+                so = so.with_context(all_locations=True)
+        return so
