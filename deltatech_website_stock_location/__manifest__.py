e70fe7f627df945bd3f58d0e9356d4bb251a927e
Dorin Hongu <dhongu@gmail.com>
2024-01-31 12:35:48 +0200

    Obsolete eCommerce Stock location

--- deltatech_website_stock_location/__manifest__.py
+++ deltatech_website_stock_location/__manifest__.py
@@ -2,17 +2,15 @@
 #              Dorin Hongu <dhongu(@)gmail(.)com
 # See README.rst file on addons root folder for license details
 {
-    "name": "eCommerce Stock location",
+    "name": "Obsolete eCommerce Stock location",
     "category": "Website",
     "summary": "Stock Availability From Location",
     "version": "16.0.1.0.2",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "depends": ["website", "website_sale_stock"],
-    "data": ["views/website_view.xml", "views/res_config_settings_views.xml"],
+    "data": [],
     "images": ["static/description/main_screenshot.png"],
-    "price": 10.00,
-    "currency": "EUR",
     "license": "OPL-1",
     "development_status": "Beta",
     "maintainers": ["dhongu"],
