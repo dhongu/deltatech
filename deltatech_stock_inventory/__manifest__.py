f900c545c1ee2aeffed87c850e534028d36b2110
danila12 <danila@terrabit.ro>
2024-02-02 10:28:48 +0200

    allow users to create inventory if no qty change

--- deltatech_stock_inventory/__manifest__.py
+++ deltatech_stock_inventory/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Stock Inventory",
     "summary": "Inventory Old Method",
-    "version": "15.0.2.2.9",
+    "version": "15.0.2.3.0",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "category": "Warehouse",
