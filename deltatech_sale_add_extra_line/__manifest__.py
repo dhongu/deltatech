1c098333160b9bcc11ed0318bde304f6d659e82c
danila12 <danila@terrabit.ro>
2024-02-06 12:27:38 +0200

    added website_sale functions

--- deltatech_sale_add_extra_line/__manifest__.py
+++ deltatech_sale_add_extra_line/__manifest__.py
@@ -4,11 +4,11 @@
 {
     "name": "Sale Add Extra Line",
     "summary": "Sale Add Extra Line",
-    "version": "15.0.1.0.6",
+    "version": "15.0.1.0.7",
     "category": "Sales",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
-    "depends": ["sale"],
+    "depends": ["sale", "website_sale"],
     "license": "OPL-1",
     "data": ["views/product_view.xml", "views/sale.xml"],
     "images": ["static/description/main_screenshot.png"],
