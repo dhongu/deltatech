83515a8469705797db970739e9743cacaee773df
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2024-02-07 09:32:02 +0200

    [UPD] deltatech_invoice_delivery: Modifications from 15.0

--- deltatech_invoice_delivery/tests/test_delivery.py
+++ deltatech_invoice_delivery/tests/test_delivery.py
@@ -19,6 +19,9 @@ class TestInvoiceDelivery(TransactionCase):
         self.product_b = self.env["product.product"].create(
             {"name": "Test B", "type": "product", "standard_price": 100, "list_price": 150, "seller_ids": seller_ids}
         )
+        self.stock_location = self.env.ref("stock.stock_location_stock")
+        self.env["stock.quant"]._update_available_quantity(self.product_a, self.stock_location, 1000)
+        self.env["stock.quant"]._update_available_quantity(self.product_b, self.stock_location, 1000)
 
     def create_in_invoice(self):
         invoice = self.env["account.move"].create(
@@ -64,6 +67,17 @@ class TestInvoiceDelivery(TransactionCase):
         invoice.action_post()
 
     def test_create_invoice_with_negative_qty(self):
+        Journal = self.env["account.journal"]
+
+        Journal.create(
+            {
+                "name": "Test Purchase Journal",
+                "code": "TPUR",
+                "type": "purchase",
+                "sequence": 10,
+                "active": True,
+            }
+        )
         self.create_in_invoice()
 
         invoice = self.env["account.move"].create(
