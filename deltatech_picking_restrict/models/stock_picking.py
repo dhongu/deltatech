42fa3a529bed4289d9b29aeeb87e9d11bfa27e7b
VoicuStefan2001 <150918937+VoicuStefan2001@users.noreply.github.com>
2024-02-13 13:42:28 +0200

    [UPD] deltatech_picking_restrict: changes from 15.0 (#1230)

--- deltatech_picking_restrict/models/stock_picking.py
+++ deltatech_picking_restrict/models/stock_picking.py
@@ -4,12 +4,23 @@
 
 from odoo import _, fields, models
 from odoo.exceptions import UserError
+from odoo.tools import float_compare
 
 
 class StockPickingType(models.Model):
     _inherit = "stock.picking.type"
 
     validate_group_id = fields.Many2one("res.groups", string="Group for validation")
+    restrict_quantities = fields.Boolean(
+        string="Restrict done quantities to reserved",
+        default=False,
+        help="If set, no picking with different reserved quantity and done quantity can be validated",
+    )
+    restrict_new_products = fields.Boolean(
+        string="Restrict new products",
+        default=False,
+        help="If set, no picking with extra products and done quantity can be validated",
+    )
 
 
 class StockPicking(models.Model):
@@ -21,4 +32,30 @@ class StockPicking(models.Model):
                 group_id = picking.picking_type_id.validate_group_id
                 if self.env.user not in group_id.users:
                     raise UserError(_("Your user cannot validate this type of transfer"))
+
+            # new product or wrong quantities check
+            if picking.picking_type_id.restrict_quantities or picking.picking_type_id.restrict_new_products:
+                precision = self.env["decimal.precision"].precision_get("Product Unit of Measure")
+                for move_line in picking.move_line_ids_without_package:
+                    if (
+                        picking.picking_type_id.restrict_quantities
+                        and move_line.reserved_uom_qty
+                        and float_compare(move_line.reserved_uom_qty, move_line.qty_done, precision_digits=precision)
+                        != 0
+                    ):
+                        raise UserError(
+                            _("Done quantities not equal to reserved for [{}] {}").format(
+                                move_line.product_id.default_code, move_line.product_id.name
+                            )
+                        )
+                    if (
+                        picking.picking_type_id.restrict_new_products
+                        and float_compare(move_line.reserved_uom_qty, 0.0, precision_digits=precision) == 0
+                        and float_compare(move_line.qty_done, 0.0, precision_digits=precision) != 0
+                    ):
+                        raise UserError(
+                            _("Unrecognized product: [{}] {}").format(
+                                move_line.product_id.default_code, move_line.product_id.name
+                            )
+                        )
         return super().button_validate()
