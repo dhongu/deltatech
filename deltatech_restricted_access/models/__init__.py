fab03cee9d0ee5e4cedb80c46ffdb838a7679c12
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2024-02-20 11:34:45 +0200

    [UPD] deltatech_restricted_access:descrimprion+changes from 16.0

---
+++ deltatech_restricted_access/models/__init__.py
@@ -0,0 +1,8 @@
+# ©  2008-2023 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+
+from . import product_category
+from . import stock_location
+from . import uom
