d9973e3e9b9611d9f6fd997972819b1bf004fdc6
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2024-03-04 13:40:05 +0200

    [17.0][MIG] deltatech_fleet_geo

---
+++ deltatech_fleet_geo/__manifest__.py
@@ -0,0 +1,20 @@
+# ©  2008-2021 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+{
+    "name": "Deltatech Fleet Geo",
+    "summary": "Fleet Geographical Information",
+    "version": "17.0.1.0.0",
+    "author": "Dorin Hongu, Terrabit",
+    "website": "https://www.terrabit.ro",
+    "category": "Human Resources/Fleet",
+    "license": "OPL-1",
+    "depends": [
+        "deltatech_fleet",
+        # 'web_map'
+    ],
+    "data": ["views/fleet_view.xml", "views/fleet_vehicle_view.xml"],
+    "images": ["static/description/main_screenshot.png"],
+    "development_status": "Beta",
+    "maintainers": ["dhongu"],
+}
