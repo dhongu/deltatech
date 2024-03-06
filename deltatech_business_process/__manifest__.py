3a27dd9f033e8dfc7d7c982e696e9f8774402f62
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2024-03-06 16:11:26 +0200

    [17.0][UPd]deltatech_business_process: exp/imp test + diferite modificari vizuale

--- deltatech_business_process/__manifest__.py
+++ deltatech_business_process/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Business process",
     "summary": "Business process",
-    "version": "17.0.1.0.5",
+    "version": "17.0.1.0.6",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "license": "OPL-1",
