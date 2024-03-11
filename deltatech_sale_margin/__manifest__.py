909467198a90622ed660449bb47ed0d769f56769
danila12 <danila@terrabit.ro>
2024-03-11 11:53:21 +0200

    fix readonly fields

--- deltatech_sale_margin/__manifest__.py
+++ deltatech_sale_margin/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Sale Margin",
     "summary": "Check price in sale order",
-    "version": "16.0.1.0.5",
+    "version": "16.0.1.0.6",
     "category": "Sales",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
