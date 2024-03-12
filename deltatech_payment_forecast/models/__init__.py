4d5e2b94e9df60b9c39d946209d9062290339347
Dan Stoica <danila@terrabit.ro>
2024-03-12 16:55:45 +0200

    add payment_forecast module (#1320)

---
+++ deltatech_payment_forecast/models/__init__.py
@@ -0,0 +1,4 @@
+# Copyright (c) 2024-now Terrabit Solutions All Rights Reserved
+
+
+from . import payment_forecast
---
+++ deltatech_payment_forecast/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+This module generates a report which shows what payments should be expected until a certain date
