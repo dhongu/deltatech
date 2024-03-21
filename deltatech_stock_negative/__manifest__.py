7b52ffde4dfe825afcef183bfcf06933538a389c
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2024-03-21 12:02:11 +0200

    [17.0][MIG] deltatech_stock_negative

---
+++ deltatech_stock_negative/__manifest__.py
@@ -0,0 +1,18 @@
+# ©  2008-2021 Deltatech
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "No Negative Stock",
+    "summary": "Negative stocks are not allowed",
+    "version": "17.0.2.0.2",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "category": "Generic Modules/Stock",
+    "depends": ["stock"],
+    "license": "OPL-1",
+    "data": ["views/res_config_view.xml", "views/stock_location_view.xml"],
+    "images": ["static/description/main_screenshot.png"],
+    "installable": True,
+    "development_status": "Mature",
+    "maintainers": ["dhongu"],
+}
---
+++ deltatech_stock_negative/models/stock_location.py
@@ -0,0 +1,11 @@
+# ©  2008-2021 Deltatech
+# See README.rst file on addons root folder for license details
+
+
+from odoo import fields, models
+
+
+class StockLocation(models.Model):
+    _inherit = "stock.location"
+
+    allow_negative_stock = fields.Boolean(string="Allow Negative Stock")
