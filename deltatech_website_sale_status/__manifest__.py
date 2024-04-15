b6c84d97dadd9f59cf27e92a4d45bada8d9d3a61
Dan Stoica <danila@terrabit.ro>
2024-04-15 15:15:59 +0300

    stage in sale report (#1403)

--- deltatech_website_sale_status/__manifest__.py
+++ deltatech_website_sale_status/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "eCommerce Sale Order status",
     "category": "Website",
     "summary": "Additional filters sales orders by status ",
-    "version": "15.0.2.0.4",
+    "version": "15.0.2.0.5",
     "license": "OPL-1",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
---
+++ deltatech_website_sale_status/models/sale_report.py
@@ -0,0 +1,30 @@
+# ©  2008-2021 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+
+from odoo import fields, models
+
+
+class SaleReport(models.Model):
+    _inherit = "sale.report"
+
+    stage = fields.Selection(
+        [
+            ("placed", "Placed"),  # comanda plasta pe website
+            ("in_process", "In Process"),  # comanda in procesare de catre agentul de vanzare
+            ("waiting", "Waiting availability"),  # nu sunt in stoc toate produsele din comanda
+            ("postponed", "Postponed"),  # livrarea a fost amanata
+            ("to_be_delivery", "To Be Delivery"),  # comanda este de livrat
+            ("in_delivery", "In Delivery"),  # marfa a fost predata la curier
+            ("delivered", "Delivered"),  # comanda a fost livrata la client
+            ("canceled", "Canceled"),
+            ("returned", "Returned"),
+        ],
+        string="Stage",
+        index=True,
+    )
+
+    def _select_additional_fields(self, fields):
+        fields["stage"] = ", s.stage as stage"
+        return super()._select_additional_fields(fields)
