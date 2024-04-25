2902af042c26689492fdfa18232e8f82071c2b05
Dorin Hongu <dhongu@gmail.com>
2024-04-26 01:08:20 +0300

    recuperare modul sters

---
+++ deltatech_business_process_documentation/__manifest__.py
@@ -0,0 +1,21 @@
+# ©  2023 Deltatech
+# See README.rst file on addons root folder for license details
+
+
+{
+    "name": "Business process documentation",
+    "summary": "Business process documentation",
+    "version": "16.0.1.0.3",
+    "author": "Terrabit, Nexterp, Fekete Mihai",
+    "website": "https://www.terrabit.ro",
+    "license": "OPL-1",
+    "category": "Generic Modules/Other",
+    "depends": ["deltatech_business_process", "website_slides"],
+    "data": [
+        "views/business_project_view.xml",
+        "views/templates.xml",
+    ],
+    "development_status": "Beta",
+    "maintainers": ["dhongu"],
+    "application": True,
+}
