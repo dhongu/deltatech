586521c5a8ab0a6edeb8ee737ae5c97befce604e
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2024-04-26 14:59:04 +0300

    [16.0][UPD] deltatech_sale_pallet

--- deltatech_sale_pallet/models/product_template.py
+++ deltatech_sale_pallet/models/product_template.py
@@ -3,7 +3,7 @@
 # See README.rst file on addons root folder for license details
 
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ProductTemplate(models.Model):
@@ -13,8 +13,12 @@ class ProductTemplate(models.Model):
     pallet_qty_min = fields.Float(digits="Product Unit of Measure")  # cantitatea minima pe palet
     pallet_price = fields.Float("Pallet Price", default=1.0, digits="Product Price", compute="_compute_pallet_price")
 
+    @api.onchange("pallet_product_id", "pallet_qty_min")
     def _compute_pallet_price(self):
         main_pricelist = self.env.ref("product.list0", False)
         for template in self:
             price = main_pricelist._get_product_price(template, template.pallet_qty_min)
+            price = price * template.pallet_qty_min
+            if template.pallet_product_id:
+                price = price + template.pallet_product_id.list_price
             template.pallet_price = price
