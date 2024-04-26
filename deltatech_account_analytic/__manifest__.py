bad573429e6cec24030e29654041d5734d1a8ec1
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2024-04-26 09:51:28 +0300

    [17.0][MIG] deltatech_account_analytic

---
+++ deltatech_account_analytic/__manifest__.py
@@ -0,0 +1,19 @@
+# ©  2023-now Terrabit
+# See README.rst file on addons root folder for license details
+{
+    "name": "Deltatech Account Analytic",
+    "summary": "Analytic lines enhancements",
+    "version": "17.0.0.0.2",
+    "author": "Terrabit, Dan Stoica",
+    "license": "OPL-1",
+    "website": "https://www.terrabit.ro",
+    "category": "Accounting & Finance",
+    "depends": ["account", "analytic"],
+    "data": [
+        "views/res_config_settings.xml",
+        "views/account_analytic_default.xml",
+    ],
+    "images": ["static/description/main_screenshot.png"],
+    "development_status": "Production/Stable",
+    "maintainers": ["danila12"],
+}
