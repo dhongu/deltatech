b356db9f5853a2cf843e8a6617d514e7a37a8b11
Dorin Hongu <dhongu@gmail.com>
2024-05-03 09:22:25 +0300

    precommit

--- deltatech_invoice_receipt/tests/test_receipt.py
+++ deltatech_invoice_receipt/tests/test_receipt.py
@@ -15,10 +15,22 @@ class TestInvoiceReceipt(TransactionCase):
 
         seller_ids = [(0, 0, {"partner_id": self.partner_a.id})]
         self.product_a = self.env["product.product"].create(
-            {"name": "Test A", "type": "product", "standard_price": 100, "list_price": 150, "seller_ids": seller_ids}
+            {
+                "name": "Test A",
+                "type": "product",
+                "standard_price": 100,
+                "list_price": 150,
+                "seller_ids": seller_ids,
+            }
         )
         self.product_b = self.env["product.product"].create(
-            {"name": "Test B", "type": "product", "standard_price": 100, "list_price": 150, "seller_ids": seller_ids}
+            {
+                "name": "Test B",
+                "type": "product",
+                "standard_price": 100,
+                "list_price": 150,
+                "seller_ids": seller_ids,
+            }
         )
         self.stock_location = self.env.ref("stock.stock_location_stock")
         self.env["stock.quant"]._update_available_quantity(self.product_a, self.stock_location, 1000)
