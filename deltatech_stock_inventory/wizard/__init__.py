d52c81be2bfcd7da286106236982eb2305d5a96e
Dorin Hongu <dhongu@gmail.com>
2024-05-03 08:34:35 +0300

    migrare stock inventory

---
+++ deltatech_stock_inventory/wizard/__init__.py
@@ -0,0 +1,8 @@
+# ©  2015-2018 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+# from . import stock_change_product_qty
+
+
+# from . import stock_request_count
