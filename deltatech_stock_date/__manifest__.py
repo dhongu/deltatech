c6dbad54a79a8fa4ab761144cbe2c405d8aecdf4
Dorin Hongu <dhongu@gmail.com>
2024-05-10 12:51:53 +0300

    add deltatech_stock_date

---
+++ deltatech_stock_date/__manifest__.py
@@ -0,0 +1,19 @@
+# ©  2024 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Stock Date - Obsolete",
+    "summary": "Obsolete use l10n_ro_stock_account_date",
+    "version": "17.0.1.0.9",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "category": "Warehouse",
+    "depends": ["base", "stock_account", "purchase_stock"],
+    "license": "OPL-1",
+    "data": [],
+    "application": False,
+    "installable": True,
+    "development_status": "Mature",
+    "maintainers": ["dhongu"],
+}
