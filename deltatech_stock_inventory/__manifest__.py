79913fa64ebf47279efd46dec5faf229f76cc9cd
Dan Stoica <danila@terrabit.ro>
2024-05-24 15:18:59 +0300

    fix location view (#1456)

--- deltatech_stock_inventory/__manifest__.py
+++ deltatech_stock_inventory/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Stock Inventory",
     "summary": "Inventory Old Method",
-    "version": "16.0.2.2.4",
+    "version": "16.0.2.2.5",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "category": "Warehouse",
