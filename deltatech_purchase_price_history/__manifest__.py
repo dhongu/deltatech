35cf7a0bef1e7eb8ce5a53e36ac52f0cf471e893
danila12 <danila@terrabit.ro>
2024-06-13 12:12:49 +0300

    add purchase price history module

---
+++ deltatech_purchase_price_history/__manifest__.py
@@ -0,0 +1,22 @@
+# ©  2008-2021 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Purchase Price History",
+    "summary": "View minim, maxim and average purchase price from the last 12 months in product template",
+    "version": "16.0.0.0.1",
+    "author": "Terrabit, Dan Stoica",
+    "license": "OPL-1",
+    "website": "https://www.terrabit.ro",
+    "category": "Purchase",
+    "depends": ["purchase"],
+    "data": [
+        "views/product_template.xml",
+        "data/ir_cron.xml",
+    ],
+    "images": ["static/description/main_screenshot.png"],
+    "installable": True,
+    "development_status": "Beta",
+    "maintainers": ["danila12"],
+}
