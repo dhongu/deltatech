145b08364f2e18eadcf518ad879c002a62095b38
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2024-07-01 12:55:12 +0300

    version update

--- deltatech_alternative/__manifest__.py
+++ deltatech_alternative/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Products Alternative",
-    "version": "17.0.2.0.5",
+    "version": "17.4.2.0.5",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "summary": "Alternative product codes",
