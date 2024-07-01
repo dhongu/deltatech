eee57ed448ee81db90e941ae02b12c6768b35d60
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2024-07-01 13:18:16 +0300

    18.0 changes

--- deltatech_auto_reorder_rule/__manifest__.py
+++ deltatech_auto_reorder_rule/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Deltatech Auto Reorder Rule",
     "category": "Stock",
     "summary": "Auto create reorder rule",
-    "version": "17.0.0.0.1",
+    "version": "17.4.0.0.1",
     "author": "Terrabit, Dan Stoica",
     "website": "https://www.terrabit.ro",
     "license": "OPL-1",
