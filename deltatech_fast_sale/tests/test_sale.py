108cc44fe10c9d424e8d029784f10c377877c55c
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2024-07-01 14:26:50 +0300

    version and test update

--- deltatech_fast_sale/tests/test_sale.py
+++ deltatech_fast_sale/tests/test_sale.py
@@ -16,7 +16,8 @@ class TestSale(TransactionCase):
         self.product_a = self.env["product.product"].create(
             {
                 "name": "Test A",
-                "type": "product",
+                "type": "consu",
+                "is_storable": True,
                 "standard_price": 100,
                 "list_price": 150,
                 "seller_ids": seller_ids,
@@ -25,7 +26,8 @@ class TestSale(TransactionCase):
         self.product_b = self.env["product.product"].create(
             {
                 "name": "Test B",
-                "type": "product",
+                "type": "consu",
+                "is_storable": True,
                 "standard_price": 70,
                 "list_price": 150,
                 "seller_ids": seller_ids,
