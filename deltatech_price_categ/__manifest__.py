925a755e633520a5b29af00b90b0069477002c23
Dan Stoica <danila@terrabit.ro>
2024-07-02 14:04:23 +0300

    error if price resulted is negative (#1517)

--- deltatech_price_categ/__manifest__.py
+++ deltatech_price_categ/__manifest__.py
@@ -6,7 +6,7 @@
 {
     "name": "Price Category ",
     "summary": "Price List: Bronze Silver and Gold in product",
-    "version": "15.0.1.0.1",
+    "version": "15.0.1.0.2",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "category": "Generic Modules/Stock",
