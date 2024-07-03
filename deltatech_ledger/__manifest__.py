466d397dccf290e70675e39a56142717b0cc963f
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2024-07-03 13:30:39 +0300

    [17.0][MIG]deltatech_ledger

---
+++ deltatech_ledger/__manifest__.py
@@ -0,0 +1,19 @@
+# a module named Dealtatech Ledger version 16.0 made by VoicuStefan2001 that depends on stock account sale purchase
+
+{
+    "name": "Deltatech Ledger",
+    "summary": "Deltatech Ledger",
+    "version": "17.0.0.0.0",
+    "author": "Terrabit, Voicu Stefan",
+    "depends": ["base", "mail"],
+    "website": "https://www.terrabit.ro",
+    "license": "OPL-1",
+    "data": [
+        "security/ir.model.access.csv",
+        "data/ir_sequence_data.xml",
+        "views/ledger_view.xml",
+    ],
+    "development_status": "Alpha",
+    "maintainers": ["VoicuStefan2001"],
+    "application": True,
+}
