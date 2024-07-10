9285f838be45e7f8599bd039dfb34a5924ce4c92
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2024-07-10 10:18:14 +0300

    [16.0][UPD] deltatech_sale_pallet_website

--- deltatech_sale_pallet_website/__manifest__.py
+++ deltatech_sale_pallet_website/__manifest__.py
@@ -4,15 +4,13 @@
 {
     "name": "Sale Pallet Website",
     "summary": "Sale pallet",
-    "version": "16.0.1.0.2",
+    "version": "16.0.1.0.3",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "category": "Sales",
     "depends": ["deltatech_sale_pallet", "website_sale"],
     "license": "OPL-1",
-    "data": [
-        # "views/templates.xml"
-    ],
+    "data": ["views/templates.xml"],
     "images": ["static/description/main_screenshot.png"],
     "development_status": "Production/Stable",
     "maintainers": ["dhongu"],
