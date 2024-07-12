817d7c473d288a82e03b3d8630ef226d9461f8a0
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2024-07-12 13:30:51 +0300

    [16.0][UPD] deltatech_invoice_product_filter

---
+++ deltatech_invoice_product_filter/tests/test_invoice_filter.py
@@ -0,0 +1,46 @@
+from odoo.tests.common import TransactionCase
+
+
+class TestAccountInvoiceView(TransactionCase):
+    def setUp(self):
+        super().setUp()
+
+        # Create necessary records for the test
+        self.product = self.env["product.product"].create(
+            {
+                "name": "Test A",
+                "type": "consu",
+            }
+        )
+
+        self.partner = self.env["res.partner"].create(
+            {
+                "name": "Test Partner",
+            }
+        )
+
+        self.invoice = self.env["account.move"].create(
+            {
+                "partner_id": self.partner.id,
+                "invoice_line_ids": [
+                    (
+                        0,
+                        0,
+                        {
+                            "product_id": self.product.id,
+                            "quantity": 1,
+                            "name": "Test Product",
+                            "price_unit": 1.0,
+                            "account_id": self.env["account.account"].search([], limit=1).id,
+                        },
+                    )
+                ],
+            }
+        )
+
+    def test_product_filter(self):
+        # Use the product filter to search for invoices
+        invoices = self.env["account.move"].search([("line_ids.product_id", "ilike", "Test A")])
+
+        # Check that the correct invoice was found
+        self.assertEqual(invoices, self.invoice)
