7ac667c9a1853ebd2ffa787542d4c12f8a025f6d
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2024-07-24 10:22:26 +0300

    [16.0][UPD] deltatech_sale_multiple tests

--- deltatech_sale_multiple/tests/test_sale.py
+++ deltatech_sale_multiple/tests/test_sale.py
@@ -74,3 +74,35 @@ class TestSale(TransactionCase):
             so_line.product_uom_qty = 1
 
         self.so = so.save()
+
+    def test_write_sale_order_line(self):
+        product = self.env["product.product"].create(
+            {
+                "name": "Test Product",
+                "type": "product",
+                "purchase_ok": True,
+                "list_price": 100.0,  # Sale price
+                "standard_price": 50.0,  # Purchase price
+            }
+        )
+        partner = self.env["res.partner"].create(
+            {
+                "name": "Test Partner",
+            }
+        )
+
+        # Create a sale order with one order line
+        sale_order = self.env["sale.order"].create(
+            {
+                "partner_id": partner.id,
+            }
+        )
+        sale_order_line = self.env["sale.order.line"].create(
+            {
+                "order_id": sale_order.id,
+                "product_id": product.id,
+                "product_uom_qty": 1.0,
+                "price_unit": 40.0,  # Below the purchase price
+            }
+        )
+        sale_order_line.write({"product_uom_qty": 2.0})
