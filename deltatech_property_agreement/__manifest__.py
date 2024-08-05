6ea6b9c7551d02a8703d2ee7ad00fd6f0935755b
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2024-08-05 12:57:52 +0300

    [17.0][MIG] deltatech_property_agreement

---
+++ deltatech_property_agreement/__manifest__.py
@@ -0,0 +1,18 @@
+# ©  2015-2020 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Property Agreement",
+    "summary": "Manage property agreements",
+    "version": "17.0.1.0.0",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "category": "Property",
+    "depends": ["deltatech_property", "deltatech_service_agreement", "deltatech_service_equipment"],
+    "license": "OPL-1",
+    "data": ["views/property_building_view.xml", "security/ir.model.access.csv"],
+    "images": ["static/description/main_screenshot.png"],
+    "development_status": "Production/Stable",
+    "maintainers": ["dhongu"],
+}
