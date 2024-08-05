58a46f2c214270ccc5e1d6a9249b3e8a9ce615a1
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2024-08-05 13:43:15 +0300

    [17.0][UPD] deltatech_purchase_refund

--- deltatech_purchase_refund/tests/test_purchase.py
+++ deltatech_purchase_refund/tests/test_purchase.py
@@ -1,61 +1,85 @@
-# ©  2008-2021 Deltatech
-#              Dorin Hongu <dhongu(@)gmail(.)com
-# See README.rst file on addons root folder for license details
-
-from odoo.tests import Form
+from odoo import fields
 from odoo.tests.common import TransactionCase
+from odoo.tools import float_compare
 
 
-class TestPurchase(TransactionCase):
+class TestPurchaseOrder(TransactionCase):
     def setUp(self):
         super().setUp()
-        # se creeaza un furnizor
-        self.partner_a = self.env["res.partner"].create({"name": "Test"})
+        self.Product = self.env["product.product"]
+        self.PurchaseOrder = self.env["purchase.order"]
+        self.PurchaseOrderLine = self.env["purchase.order.line"]
+        self.AccountMove = self.env["account.move"]
 
-        seller_ids = [(0, 0, {"partner_id": self.partner_a.id})]
-        self.product_a = self.env["product.product"].create(
+        # Create a product
+        self.product = self.Product.create(
             {
-                "name": "Test A",
+                "name": "Test Product",
                 "type": "product",
-                "standard_price": 100,
-                "list_price": 150,
-                "seller_ids": seller_ids,
-            }
-        )
-        self.product_b = self.env["product.product"].create(
-            {
-                "name": "Test B",
-                "type": "product",
-                "standard_price": 100,
-                "list_price": 150,
-                "seller_ids": seller_ids,
+                "purchase_method": "purchase",
             }
         )
 
-    def test_purchase(self):
-        # se creeaza o comanda de achizitie
-        form_purchase = Form(self.env["purchase.order"])
-        form_purchase.partner_id = self.partner_a
-        with form_purchase.order_line.new() as po_line:
-            po_line.product_id = self.product_a
-            po_line.product_qty = 10
-            po_line.price_unit = 10
+        # Create a purchase order
+        self.purchase_order = self.PurchaseOrder.create(
+            {
+                "partner_id": self.env.ref("base.res_partner_1").id,
+                "date_order": fields.Date.today(),
+                "order_line": [
+                    (
+                        0,
+                        0,
+                        {
+                            "name": self.product.name,
+                            "product_id": self.product.id,
+                            "product_qty": 10,
+                            "product_uom": self.env.ref("uom.product_uom_unit").id,
+                            "price_unit": 100,
+                            "date_planned": fields.Date.today(),
+                        },
+                    )
+                ],
+            }
+        )
 
-        with form_purchase.order_line.new() as po_line:
-            po_line.product_id = self.product_b
-            po_line.product_qty = -10
-            po_line.price_unit = 10
+    def test_action_view_invoice(self):
+        # Confirm the purchase order
+        self.purchase_order.button_confirm()
 
-        po = form_purchase.save()
+        # Create a picking and mark it as done to receive the products
+        picking = self.purchase_order.picking_ids[0]
+        picking.move_line_ids.write({"quantity": 10})
+        picking.button_validate()
+        self.purchase_order.action_create_invoice()
+        # Create an invoice
+        self.purchase_order.action_view_invoice()
 
-        # se valideaza comanda de achizitie
-        po.button_confirm()
-        self.picking = po.picking_ids[0]
+    def test_prepare_account_move_line(self):
+        # Confirm the purchase order
+        self.purchase_order.button_confirm()
 
-        # se confirma primirea produselor
-        for move_line in self.picking.move_line_ids:
-            if move_line.product_id == self.product_a:
-                move_line.write({"picked": 10})
+        # Create an invoice with negative quantity
+        line = self.purchase_order.order_line[0]
+        move = self.AccountMove.create(
+            {
+                "partner_id": self.purchase_order.partner_id.id,
+                "move_type": "in_refund",
+                "invoice_date": fields.Date.today(),
+            }
+        )
 
-        # se valideaza primirea
-        self.picking.button_validate()
+        move_line_vals = line._prepare_account_move_line(move=move)
+        move_line_vals["move_id"] = move.id  # Ensure move_id is set
+
+        move_line = self.env["account.move.line"].create(move_line_vals)
+
+        # Check the quantity on the move line
+        if line.product_id.purchase_method == "purchase":
+            expected_qty = line.qty_invoiced - line.product_qty
+        else:
+            expected_qty = line.qty_invoiced - line.qty_received
+
+        if float_compare(expected_qty, 0.0, precision_rounding=line.product_uom.rounding) <= 0:
+            expected_qty = 0.0
+
+        self.assertEqual(move_line.quantity, expected_qty)
