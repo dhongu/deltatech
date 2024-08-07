4b3aeba0bf3577640a199b7fa0471069422c2fcb
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2024-08-07 11:31:23 +0300

    [17.0][UPD] deltatech_sale_payment tests

--- deltatech_website_stock_availability/tests/test_product.py
+++ deltatech_website_stock_availability/tests/test_product.py
@@ -3,16 +3,15 @@
 # See README.rst file on addons root folder for license details
 
 
-from odoo.tests import Form
 from odoo.tests.common import TransactionCase
 
 
-class TestSale(TransactionCase):
+class TestProduct(TransactionCase):
     def setUp(self):
         super().setUp()
         self.partner_a = self.env["res.partner"].create({"name": "Test"})
 
-        seller_ids = [(0, 0, {"partner_id": self.partner_a.id})]
+        seller_ids = [(0, 0, {"partner_id": self.partner_a.id, "date_start": "2099-01-01", "delay": 5})]
         self.product_a = self.env["product.product"].create(
             {
                 "name": "Test A",
@@ -26,12 +25,26 @@ class TestSale(TransactionCase):
             {
                 "name": "Test B",
                 "type": "product",
+                # "inventory_availability": "preorder",
                 "standard_price": 70,
                 "list_price": 150,
                 "seller_ids": seller_ids,
             }
         )
+        seller_ids = [(0, 0, {"partner_id": self.partner_a.id, "delay": 5})]
+        self.product_c = self.env["product.product"].create(
+            {
+                "name": "Test C",
+                "type": "product",
+                # "inventory_availability": "preorder",
+                "standard_price": 70,
+                "list_price": 150,
+                "seller_ids": seller_ids,
+            }
+        )
+
         self.stock_location = self.env.ref("stock.stock_location_stock")
+
         self.env["stock.quant"]._update_available_quantity(self.product_a, self.stock_location, 1000)
         self.env["stock.quant"]._update_available_quantity(self.product_b, self.stock_location, 1000)
 
@@ -40,42 +53,26 @@ class TestSale(TransactionCase):
         #     "product_qty": 10000,
         #     "location_id": self.stock_location.id,
         # }
-        # inv_line_b = {
-        #     "product_id": self.product_b.id,
-        #     "product_qty": 10000,
-        #     "location_id": self.stock_location.id,
-        # }
+
         # inventory = self.env["stock.inventory"].create(
         #     {
         #         "name": "Inv. productserial1",
         #         "line_ids": [
         #             (0, 0, inv_line_a),
-        #             (0, 0, inv_line_b),
         #         ],
         #     }
         # )
         # inventory.action_start()
         # inventory.action_validate()
 
-    def test_sale(self):
-        so = Form(self.env["sale.order"])
-        so.partner_id = self.partner_a
+    # def test_product(self):
+    #     product = self.product_a
+    #     self.assertIsNotNone(product.availability_text)
+    #     product = self.product_b
+    #     self.assertIsNotNone(product.availability_text)
+    #     product = self.product_c
+    #     self.assertIsNotNone(product.availability_text)
 
-        with so.order_line.new() as so_line:
-            so_line.product_id = self.product_a
-            so_line.product_uom_qty = 100
-
-        with so.order_line.new() as so_line:
-            so_line.product_id = self.product_b
-            so_line.product_uom_qty = 10
-
-        self.so = so.save()
-        self.so.action_confirm()
-
-        self.so.action_payment_link()
-
-        wizard = Form(self.env["sale.confirm.payment"].with_context(active_id=self.so.id))
-        wizard.amount = 100
-        wizard.acquirer_id = self.env["payment.provider"].search([], limit=1)
-        wizard = wizard.save()
-        wizard.do_confirm()
+    def test_get_combination_info(self):
+        product = self.product_b.product_tmpl_id.with_context(website_sale_stock_get_quantity=True)
+        product._get_combination_info(product_id=self.product_b.id)
