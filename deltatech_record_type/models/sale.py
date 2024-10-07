9a78fa5a2c15e833f3cda7416af985719ff1bf9e
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2024-10-07 13:29:07 +0300

    [16.0][UPD] deltatech_record_type

--- deltatech_record_type/models/sale.py
+++ deltatech_record_type/models/sale.py
@@ -11,27 +11,14 @@ class SaleOrder(models.Model):
 
     so_type = fields.Many2one("record.type", string="Type")
 
-    def _check_so_type(self, vals):
-        if "so_type" in vals:
-            so_type_selected = self.env["record.type"].search([("id", "=", vals["so_type"]), ("model", "=", "sale")])
-            if so_type_selected:
-                for default_value in so_type_selected.default_values_ids:
-                    if default_value.field_type == "id":
-                        vals[default_value.field_name] = int(default_value.field_value)
-                    if default_value.field_type == "char":
-                        vals[default_value.field_name] = default_value.field_value
-                    if default_value.field_type == "boolean":
-                        vals[default_value.field_name] = default_value.field_value == "True"
-
-    def write(self, vals):
-        self._check_so_type(vals)
-        res = super().write(vals)
-
-        return res
-
-    @api.model_create_multi
-    def create(self, vals_list):
-        for vals in vals_list:
-            self._check_so_type(vals)
-        res = super().create(vals_list)
-        return res
+    @api.onchange("so_type")
+    def _check_so_type(self):
+        so_type_selected = self.env["record.type"].search([("id", "=", self.so_type.id), ("model", "=", "sale")])
+        if so_type_selected:
+            for default_value in so_type_selected.default_values_ids:
+                if default_value.field_type == "id":
+                    self[default_value.field_name] = int(default_value.field_value)
+                if default_value.field_type == "char":
+                    self[default_value.field_name] = default_value.field_value
+                if default_value.field_type == "boolean":
+                    self[default_value.field_name] = default_value.field_value == "True"
