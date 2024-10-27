a43063f275d26dba8c565f8b61424ce254fb849c
Dorin Hongu <dhongu@gmail.com>
2024-10-27 14:44:16 +0200

    migrare partener generic

---
+++ deltatech_partner_generic/models/res_config_settings.py
@@ -0,0 +1,16 @@
+# ©  2008-2019 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+from odoo import fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = "res.config.settings"
+
+    generic_partner_id = fields.Many2one(
+        "res.partner",
+        "Generic Partner",
+        related="company_id.generic_partner_id",
+        # config_parameter="sale.partner_generic_id"
+    )
---
+++ deltatech_partner_generic/readme/DESCRIPTION.md
@@ -0,0 +1,3 @@
+Features:
+
+- defining generic partner
