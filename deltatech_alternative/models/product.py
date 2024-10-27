fcb1e1c0a1ea7d8ce26c27b9e816d7dbfba83306
Dorin Hongu <dhongu@gmail.com>
2024-10-27 14:21:20 +0200

    migrare modul GLN

--- deltatech_alternative/models/product.py
+++ deltatech_alternative/models/product.py
@@ -3,47 +3,22 @@
 # See README.rst file on addons root folder for license details
 
 from odoo import api, fields, models
-from odoo.osv import expression
 from odoo.tools.safe_eval import safe_eval
 
 
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    search_index = fields.Char(compute="_compute_search_index", store=True, index=True, compute_sudo=True)
-
     alternative_code = fields.Char(
         string="Alternative Code",
         index=True,
         inverse="_inverse_alternative_code",
         compute="_compute_alternative_code",
+        unaccent=False,
     )
     alternative_ids = fields.One2many("product.alternative", "product_tmpl_id", string="Alternatives")
 
     used_for = fields.Char(string="Used For")
-
-    @api.depends("name", "default_code", "alternative_ids.name", "seller_ids.product_code")
-    def _compute_search_index(self):
-        langs = self.env["res.lang"].search([("active", "=", True)])
-        langs = langs.mapped("code")
-        for product in self:
-            names = [product.with_context(lang=lang).name for lang in langs]
-            name_terms = list(set(names))
-            good_terms = [term for term in name_terms if term is not False]
-            search_index = " ".join(good_terms)
-
-            if product.default_code:
-                search_index = product.default_code + " " + search_index
-
-            terms = []
-            if product.seller_ids:
-                terms += [s.product_code for s in product.seller_ids if s.product_code]
-            if product.alternative_ids:
-                terms += [a.name for a in product.alternative_ids if a.name]
-
-            terms = list(set(terms))
-            search_index += " " + " ".join(terms)
-            product.search_index = search_index[:1000]
 
     def _inverse_alternative_code(self):
         for product in self:
@@ -65,29 +40,28 @@ class ProductTemplate(models.Model):
             code = "; ".join(codes)
             product.alternative_code = code
 
-    @api.model
-    def _name_search(self, name, domain=None, operator="ilike", limit=None, order=None):
-        domain = domain or []
-        get_param = self.env["ir.config_parameter"].sudo().get_param
-        if name and safe_eval(get_param("deltatech_alternative_website.search_index", "False")):
-            domain2 = [("search_index", operator, name)]
-            return self._search(expression.AND([domain, domain2]), limit=limit)
-        else:
-            return super()._name_search(name, domain=domain, operator=operator, limit=limit, order=order)
-
 
 class ProductProduct(models.Model):
     _inherit = "product.product"
 
     @api.model
     def _name_search(self, name, domain=None, operator="ilike", limit=None, order=None):
-        domain = domain or []
+        res = super()._name_search(name, domain=domain, operator=operator, limit=limit, order=order)
         get_param = self.env["ir.config_parameter"].sudo().get_param
-        if name and safe_eval(get_param("deltatech_alternative_website.search_index", "False")):
-            domain = [("search_index", operator, name)]
-            return self._search(expression.AND([domain, domain]), limit=limit)
-        else:
-            return super()._name_search(name, domain=domain, operator=operator, limit=limit, order=order)
+        res_ids = list(res)
+        if name and safe_eval(get_param("alternative.search_name", "False")):
+            domain = [("name", operator, name)]
+            alternatives = self.env["product.alternative"].search(domain, limit=limit)
+            if alternatives:
+                product_tmpl_ids = alternatives.mapped("product_tmpl_id")
+                product_ids = self._search(
+                    [("product_tmpl_id", "in", product_tmpl_ids.ids)],
+                    limit=limit,
+                    order=order,
+                )
+                res_ids.extend(list(product_ids))
+
+        return res_ids
 
 
 class ProductAlternative(models.Model):
