8451bae7818aca29a1e7ab9c9f117c91be1efd5e
Dorin Hongu <dhongu@gmail.com>
2024-10-27 14:21:20 +0200

    migrare modul GLN

---
+++ deltatech_gln/__manifest__.py
@@ -0,0 +1,21 @@
+# ©  2024 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Deltatech Partner GLN ",
+    "summary": "Partner Global Location Number",
+    "version": "18.0.1.0.0",
+    "author": "Terrabit,Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "category": "Administration",
+    "depends": ["base"],
+    "license": "OPL-1",
+    "data": [
+        "views/res_partner_view.xml",
+    ],
+    "images": ["static/description/main_screenshot.png"],
+    "installable": True,
+    "development_status": "Mature",
+    "maintainers": ["dhongu"],
+}
