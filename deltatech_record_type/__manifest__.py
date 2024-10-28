9f7fa0de60b914090e3635734697b0a5aaaea792
Dorin Hongu <dhongu@gmail.com>
2024-10-28 07:20:01 +0200

    fix tree

---
+++ deltatech_record_type/__manifest__.py
@@ -0,0 +1,18 @@
+# ©  2015-2021 Terrabit Solutions
+#              Dan Stoica <danila(@)terrabit(.)ro
+# See README.rst file on addons root folder for license details
+
+
+{
+    "name": "Terrabit - Record Type",
+    "summary": "Manage multiple record types",
+    "version": "18.0.1.0.4",
+    "author": "Terrabit, Dan Stoica",
+    "website": "https://www.terrabit.ro",
+    "category": "Generic Modules/Other",
+    "depends": ["sale", "deltatech_select_journal", "purchase"],
+    "license": "OPL-1",
+    "data": ["views/purchase_view.xml", "views/sale.xml", "security/ir.model.access.csv"],
+    "development_status": "Beta",
+    "images": ["static/description/main_screenshot.png"],
+}
