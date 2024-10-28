cb0dc4e52aa1886324aa0c593ea175f5eba07a8c
Dorin Hongu <dhongu@gmail.com>
2024-10-28 07:20:01 +0200

    fix tree

---
+++ deltatech_product_category_group/__manifest__.py
@@ -0,0 +1,20 @@
+# ©  2008-2023 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+
+{
+    "name": "Products Category User Group",
+    "summary": "Products Category User Group",
+    "version": "18.0.1.0.4",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "category": "Sales",
+    "depends": ["product", "stock"],
+    "license": "LGPL-3",
+    "data": ["views/product_category_view.xml", "views/stock_picking_view.xml"],
+    "images": ["static/description/main_screenshot.png"],
+    "installable": True,
+    "development_status": "Mature",
+    "maintainers": ["dhongu"],
+}
