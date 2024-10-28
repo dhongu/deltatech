4b008a7c8c2e88888402b8b1df7ff7037503e282
Dorin Hongu <dhongu@gmail.com>
2024-10-28 03:03:35 +0200

    chatter

---
+++ deltatech_business_process_handover_document/__manifest__.py
@@ -0,0 +1,24 @@
+# ©  2023 Deltatech
+# See README.rst file on addons root folder for license details
+
+
+{
+    "name": "Business process handover document",
+    "summary": "Business process handover document",
+    "version": "18.0.0.0.1",
+    "author": "Terrabit, Voicu Stefan",
+    "website": "https://www.terrabit.ro",
+    "license": "OPL-1",
+    "category": "Generic Modules/Other",
+    "depends": ["deltatech_business_process"],
+    "data": [
+        "report/paperforat_a4_bp.xml",
+        "views/business_project_view.xml",
+        "report/verbal_process_report.xml",
+        "report/verbal_process_template.xml",
+    ],
+    "development_status": "Beta",
+    "images": ["static/description/main_screenshot.png"],
+    "maintainers": ["VoicuStefan2001"],
+    "application": True,
+}
---
+++ deltatech_business_process_handover_document/readme/Description.md
@@ -0,0 +1,6 @@
+Feature:
+
+- Can generate the handover document for the business process from business project.
+- The module supports english and romanian languages.
+- The document is generated in PDF format.
+-
