b259bbb79e78e6cc09c65654e8ade44d25f0ff2c
Dorin Hongu <dhongu@gmail.com>
2024-10-29 03:00:52 +0200

    migrare module

---
+++ deltatech_website_disable_fuzzy_search/__manifest__.py
@@ -0,0 +1,17 @@
+# ©  2008-2021 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Website Disable Fuzzy Search",
+    "summary": "Disable Fuzzy Search",
+    "version": "18.0.1.0.0",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "license": "OPL-1",
+    "category": "Generic Modules",
+    "depends": ["website_sale"],
+    "images": ["static/description/main_screenshot.png"],
+    "development_status": "Mature",
+    "maintainers": ["dhongu"],
+}
---
+++ deltatech_website_disable_fuzzy_search/controllers/main.py
@@ -0,0 +1,41 @@
+# ©  2008-2023 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+from odoo import http
+
+from odoo.addons.website_sale.controllers import main
+
+
+class Website(main.Website):
+    @http.route()
+    def autocomplete(
+        self,
+        search_type=None,
+        term=None,
+        order=None,
+        limit=5,
+        max_nb_chars=999,
+        options=None,
+    ):
+        options = options or {}
+        options["allowFuzzy"] = False
+        return super().autocomplete(search_type, term, order, limit, max_nb_chars, options)
+
+
+class WebsiteSale(main.WebsiteSale):
+    def _get_search_options(
+        self,
+        category=None,
+        attrib_values=None,
+        tags=None,
+        min_price=0.0,
+        max_price=0.0,
+        conversion_rate=1,
+        **post,
+    ):
+        options = super()._get_search_options(
+            category, attrib_values, tags, min_price, max_price, conversion_rate, **post
+        )
+        options["allowFuzzy"] = False
+        return options
