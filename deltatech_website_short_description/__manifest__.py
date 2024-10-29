0167dcf074456e1487312dc71caa3ef2ecb13dfc
Dorin Hongu <dhongu@gmail.com>
2024-10-29 03:00:52 +0200

    migrare module

---
+++ deltatech_website_short_description/__manifest__.py
@@ -0,0 +1,18 @@
+# ©  2008-2021 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+{
+    "name": "eCommerce Sale Short Description",
+    "category": "Website",
+    "summary": "eCommerce short description",
+    "version": "18.0.1.0.1",
+    "license": "OPL-1",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "depends": ["website_sale_stock"],
+    "data": ["views/product_view.xml"],
+    "images": ["static/description/main_screenshot.png"],
+    "installable": True,
+    "development_status": "Mature",
+    "maintainers": ["dhongu"],
+}
