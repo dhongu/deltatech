e8e0b6eb5e9adac7c0bcfd6d57b4b4b661e54b3c
Dorin Hongu <dhongu@gmail.com>
2024-10-29 03:00:52 +0200

    migrare module

---
+++ deltatech_sale_team/__manifest__.py
@@ -0,0 +1,18 @@
+# ©  2015-2019 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Sale Team Access",
+    "summary": "Sale Team Access",
+    "version": "18.0.1.0.2",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "category": "Sales",
+    "depends": ["sales_team", "account", "stock", "sale_stock"],
+    "license": "OPL-1",
+    "data": ["views/sale_team_view.xml", "security/sales_team_security.xml"],
+    "development_status": "Production/Stable",
+    "images": ["static/description/main_screenshot.png"],
+    "installable": True,
+}
