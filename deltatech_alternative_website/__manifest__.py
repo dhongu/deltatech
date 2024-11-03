1b3bd824394c4f8f0e26d3a2edaeb7e13785c408
Dorin Hongu <dhongu@gmail.com>
2024-11-03 02:15:00 +0200

    alternative website

---
+++ deltatech_alternative_website/__manifest__.py
@@ -0,0 +1,25 @@
+# ©  2008-2021 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+
+{
+    "name": "Website alternative code",
+    "summary": "Show alternative code in website",
+    "version": "18.0.1.0.6",
+    "author": "Terrabit, Dorin Hongu",
+    "license": "OPL-1",
+    "website": "https://www.terrabit.ro",
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    "category": "Website",
+    "depends": ["website", "website_sale", "deltatech_alternative"],
+    "data": [
+        # "views/product_catalog_view.xml",
+        "views/templates.xml"
+    ],
+    "images": ["images/main_screenshot.png"],
+    "development_status": "Mature",
+    "maintainers": ["dhongu"],
+}
---
+++ deltatech_alternative_website/models/product_template.py
@@ -0,0 +1,16 @@
+# ©  2008-2021 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+from odoo import api, models
+
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    @api.model
+    def _search_get_detail(self, website, order, options):
+        values = super()._search_get_detail(website, order, options)
+        values["search_fields"] += ["alternative_ids.name"]
+        values["mapping"]["alternative_ids.name"] = {"name": "alternative_ids.name", "type": "text", "match": True}
+        return values
