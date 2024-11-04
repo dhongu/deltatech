2944d73c1c5c8b02a9e32cea6677c3353dfbc649
Dorin Hongu <dhongu@gmail.com>
2024-11-04 02:53:55 +0200

    sale pallet

---
+++ deltatech_sale_pallet_website/__manifest__.py
@@ -0,0 +1,17 @@
+# ©  2008-2019 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+{
+    "name": "Sale Pallet Website",
+    "summary": "Sale pallet",
+    "version": "18.0.1.0.2",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "category": "Sales",
+    "depends": ["deltatech_sale_pallet", "website_sale"],
+    "license": "OPL-1",
+    "data": ["views/templates.xml"],
+    "images": ["static/description/main_screenshot.png"],
+    "development_status": "Production/Stable",
+    "maintainers": ["dhongu"],
+}
