2ce1a0f60846953ebb8de928bdf24160d03b950b
Dorin Hongu <dhongu@gmail.com>
2024-11-04 02:48:42 +0200

    report PRN

---
+++ deltatech_report_prn/__manifest__.py
@@ -0,0 +1,20 @@
+# ©  2008-2021 Deltatech
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Raport PRN",
+    "summary": "Raport PRN",
+    "version": "18.0.1.0.4",
+    "category": "Stock",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "depends": ["web"],
+    "license": "LGPL-3",
+    "data": [
+        # "views/assets.xml"
+    ],
+    "images": ["static/description/main_screenshot.png"],
+    "development_status": "Mature",
+    "maintainers": ["dhongu"],
+    "assets": {"web.assets_backend": ["deltatech_report_prn/static/src/js/action_manager.esm.js"]},
+}
