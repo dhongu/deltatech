79058cabfaecb96447a8b17a56bbb3e779475986
Dorin Hongu <dhongu@gmail.com>
2024-11-06 07:11:14 +0200

    price_categ

---
+++ deltatech_price_categ/__manifest__.py
@@ -0,0 +1,25 @@
+# ©  2021 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+
+{
+    "name": "Price Category ",
+    "summary": "Price List: Bronze Silver and Gold in product",
+    "version": "18p.0.1.0.1",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "category": "Generic Modules/Stock",
+    "depends": [
+        "product",
+        "account",
+        "sale",
+        "deltatech_purchase_price",
+        "website_sale",
+    ],
+    "license": "OPL-1",
+    "data": ["views/product_view.xml"],
+    "images": ["images/main_screenshot.png"],
+    "development_status": "Production/Stable",
+    "maintainers": ["dhongu"],
+}
---
+++ deltatech_price_categ/models/product.py
@@ -0,0 +1,174 @@
+# ©  2015-2019 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+
+from odoo import api, fields, models
+
+
+class PricelistItem(models.Model):
+    _inherit = "product.pricelist.item"
+
+    base = fields.Selection(
+        selection_add=[
+            ("list_price_bronze", "Bronze Price"),
+            ("list_price_copper", "Copper Price"),
+            ("list_price_silver", "Silver Price"),
+            ("list_price_gold", "Gold Price"),
+            #    ('list_price_platinum', 'Platinum Price'),
+        ],
+        ondelete={
+            "list_price_bronze": "set default",
+            "list_price_copper": "set default",
+            "list_price_silver": "set default",
+            "list_price_gold": "set default",
+        },
+    )
+
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    standard_price = fields.Float(tracking=True)
+    list_price = fields.Float(tracking=True)
+
+    list_price_base = fields.Selection(
+        [
+            ("list_price", "List price"),
+            ("standard_price", "Cost Price"),
+            ("last_purchase_price", "Last Purchase Price"),
+        ],
+        string="Base Price",
+        default="last_purchase_price",
+    )
+
+    percent_bronze = fields.Float(string="Bronze Percent")
+    percent_copper = fields.Float(string="Copper Percent")
+    percent_silver = fields.Float(string="Silver Percent")
+    percent_gold = fields.Float(string="Gold Percent")
+    # percent_platinum = fields.Float(string="Platinum Percent")
+
+    # todo: de adus valorile din listele de preturi
+    list_price_bronze = fields.Float(
+        string="Bronze Price",
+        compute="_compute_price_list",
+        tracking=True,
+        store=True,
+        readonly=True,
+        compute_sudo=True,
+    )
+
+    list_price_copper = fields.Float(
+        string="Copper Price",
+        compute="_compute_price_list",
+        tracking=True,
+        store=True,
+        readonly=True,
+        compute_sudo=True,
+    )
+
+    list_price_silver = fields.Float(
+        string="Silver Price",
+        compute="_compute_price_list",
+        tracking=True,
+        store=True,
+        readonly=True,
+        compute_sudo=True,
+    )
+    list_price_gold = fields.Float(
+        string="Gold Price",
+        compute="_compute_price_list",
+        tracking=True,
+        store=True,
+        readonly=True,
+        compute_sudo=True,
+    )
+    # list_price_platinum = fields.Float(string="Platinum Price", compute="_compute_price_list",
+    #                                    tracking=True, store=True, readonly=True, compute_sudo=True)
+
+    price_issue = fields.Boolean(compute="_compute_price_issue", store=True)
+
+    @api.depends(
+        "list_price",
+        "list_price_copper",
+        "list_price_bronze",
+        "list_price_silver",
+        "list_price_gold",
+    )
+    def _compute_price_issue(self):
+        for product in self:
+            product.price_issue = not (
+                product.list_price
+                >= product.list_price_copper
+                >= product.list_price_bronze
+                >= product.list_price_silver
+                >= product.list_price_gold
+            )
+
+    @api.depends(
+        "list_price_base",
+        "standard_price",
+        "last_purchase_price",
+        "list_price",
+        "percent_copper",
+        "percent_bronze",
+        "percent_silver",
+        "percent_gold",
+        "taxes_id",
+    )
+    def _compute_price_list(self):
+        for product in self:
+            tax_inc = False
+            # de regula este o singura  taxa
+            taxe = product.taxes_id.sudo()
+
+            for tax in taxe:
+                if tax.price_include:
+                    tax_inc = True
+
+            if product.list_price_base == "standard_price":
+                try:
+                    price = product.standard_price
+                except Exception:
+                    price = product.sudo().standard_price
+                # taxe = taxe.with_context(base_values=price)
+            elif product.list_price_base == "last_purchase_price":
+                price = product.last_purchase_price or product.standard_price
+            else:
+                price = product.list_price
+                if tax_inc:
+                    taxes = taxe.compute_all(product.list_price)
+                    price = taxes["total_excluded"]
+
+            if tax_inc:
+                tax_value = 0.0
+                for tax in taxe.sorted(key=lambda r: r.sequence):
+                    tax_value += price * tax.amount / 100
+                price += tax_value
+                price = round(price, 2)
+
+            product.list_price_bronze = price * (1 + product.percent_bronze)
+            product.list_price_copper = price * (1 + product.percent_copper)
+            product.list_price_silver = price * (1 + product.percent_silver)
+            product.list_price_gold = price * (1 + product.percent_gold)
+            # product.list_price_platinum = price * (1 + product.percent_platinum)
+
+    def _get_combination_info(
+        self,
+        combination=False,
+        product_id=False,
+        add_qty=1,
+        parent_combination=False,
+        only_template=False,
+    ):
+        combination_info = super()._get_combination_info(
+            combination=combination,
+            product_id=product_id,
+            add_qty=add_qty,
+            parent_combination=parent_combination,
+            only_template=only_template,
+        )
+
+        combination_info["web_list_price"] = combination_info["list_price"]
+
+        return combination_info
