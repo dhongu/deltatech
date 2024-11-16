fa46bca32e27407e3cce73413e9a69ea2d1ebc6e
Dorin Hongu <dhongu@gmail.com>
2024-11-16 07:29:27 +0200

    update

--- deltatech_website_product_url_image/__manifest__.py
+++ deltatech_website_product_url_image/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "eCommerce Product Image URL Link",
     "category": "Website",
     "summary": "Load image for product from URL",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.0.1",
     "author": "Terrabit, Dorin Hongu",
     "license": "OPL-1",
     "website": "https://www.terrabit.ro",
