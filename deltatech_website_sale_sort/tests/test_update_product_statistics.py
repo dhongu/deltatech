1a3d75256be9fa96e44b45054d945feecb5ee46b
Dorin Hongu <dhongu@gmail.com>
2024-11-16 08:22:40 +0200

    fix tests

--- deltatech_website_sale_sort/tests/test_update_product_statistics.py
+++ deltatech_website_sale_sort/tests/test_update_product_statistics.py
@@ -14,7 +14,7 @@ class TestUpdateProductStatistics(TransactionCase):
         self.product_a = self.env["product.product"].create(
             {
                 "name": "Test A",
-                "type": "product",
+                "is_storable": True,
                 "standard_price": 100,
                 "list_price": 150,
                 "seller_ids": seller_ids,
@@ -23,7 +23,7 @@ class TestUpdateProductStatistics(TransactionCase):
         self.product_b = self.env["product.product"].create(
             {
                 "name": "Test B",
-                "type": "product",
+                "is_storable": True,
                 "standard_price": 100,
                 "list_price": 150,
                 "seller_ids": seller_ids,
