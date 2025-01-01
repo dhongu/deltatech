441694ba5e8082605449f29c914224a7b57f1894
Dorin Hongu <dhongu@gmail.com>
2025-01-01 21:29:55 +0200

    Update icon and manifest in deltatech_business_process
    Revised the icon.svg design to improve styling and visual clarity. Updated the version in __manifest__.py from 17.0.1.3.6 to 17.0.1.3.7 to reflect these changes. No functional modifications were made.

--- deltatech_business_process/__manifest__.py
+++ deltatech_business_process/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Business process",
     "summary": "Business process",
-    "version": "17.0.1.3.6",
+    "version": "17.0.1.3.7",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "license": "OPL-1",
