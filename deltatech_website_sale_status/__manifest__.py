8d32896f646d8d4284d9c01f543e92d52d9cd634
Dorin Hongu <dhongu@gmail.com>
2025-01-06 14:30:17 +0200

    Add advanced order filtering and Sale Report enhancements
    Introduce new order filtering options in the customer portal, allowing users to filter orders by their status (e.g., open orders). Add a `stage` field to the Sale Report for enhanced reporting capabilities. Remove obsolete assets and unused JavaScript, and update templates for badge styling consistency.

--- deltatech_website_sale_status/__manifest__.py
+++ deltatech_website_sale_status/__manifest__.py
@@ -5,11 +5,11 @@
     "name": "eCommerce Sale Order status",
     "category": "Website",
     "summary": "Additional filters sales orders by status ",
-    "version": "17.0.2.0.3",
+    "version": "17.0.2.0.4",
     "license": "OPL-1",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
-    "depends": ["website_sale_stock", "deltatech_delivery_status", "sale_stock"],
+    "depends": ["portal","website_sale_stock", "deltatech_delivery_status", "sale_stock"],
     "data": [
         "views/sale_view.xml",
         "views/templates.xml",
@@ -19,9 +19,5 @@
     "installable": True,
     "development_status": "Mature",
     "maintainers": ["dhongu"],
-    "assets": {
-        "web.assets_backend": [
-            # "/eltatech_website_sale_status/static/src/js/website_sale_backend.js"
-        ],
-    },
+
 }
