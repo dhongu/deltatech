bf5aeabf9da112ea650c7bd8c6e9d94bcd2793c1
Dorin Hongu <dhongu@gmail.com>
2025-01-09 21:52:02 +0200

    Update test to validate correct payment method name
    Adjusted the expected payment method name from "Card" to "Card Payment" in the test to align with the updated naming convention. This ensures accuracy and consistency in the test validation.

--- deltatech_card_payment/tests/test_card_payment.py
+++ deltatech_card_payment/tests/test_card_payment.py
@@ -11,7 +11,7 @@ class TestAccountPaymentMethod(TransactionCase):
 
     def test_check_payment_method(self):
         # Check if the payment method is correctly set
-        self.assertEqual(self.payment_method.name, "Card", "Name is not correctly set")
+        self.assertEqual(self.payment_method.name, "Card Payment", "Name is not correctly set")
         self.assertEqual(self.payment_method.code, "card_payment", "Code is not correctly set")
         self.assertEqual(self.payment_method.payment_type, "inbound", "Payment type is not correctly set")
 
