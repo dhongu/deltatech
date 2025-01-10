86a183bf9a880da9fcb3250b710d472e14a58d48
Dorin Hongu <dhongu@gmail.com>
2025-01-10 13:48:04 +0200

    Update manifest to include product category view
    Added "views/product_category_view.xml" to the data section in the manifest file. This ensures the product category view is properly loaded and integrated into the module. Also updated the version to 17.0.1.0.4 for better version tracking.

--- deltatech_stock_account/__manifest__.py
+++ deltatech_stock_account/__manifest__.py
@@ -5,13 +5,17 @@
 {
     "name": "Stock Account Extension",
     "summary": "Stock Account Extension",
-    "version": "17.0.1.0.3",
+    "version": "17.0.1.0.4",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "category": "Generic Modules",
     "depends": ["stock_account"],
     "license": "OPL-1",
-    "data": ["security/service_security.xml", "views/stock_picking_view.xml"],
+    "data": [
+        "security/service_security.xml",
+        "views/product_category_view.xml",
+        "views/stock_picking_view.xml"
+    ],
     "images": ["static/description/main_screenshot.png"],
     "development_status": "Beta",
     "maintainers": ["dhongu"],
