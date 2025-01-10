e35462caa79c71dfd073781232e52a2284509073
Dorin Hongu <dhongu@gmail.com>
2025-01-10 13:30:20 +0200

    Update index type for 'name' field in product.alternative
    Changed the index attribute of the 'name' field from unaccent=False to btree_not_null for improved query performance and data consistency. This adjustment ensures better handling of indexed values in the database.

--- deltatech_alternative/models/product.py
+++ deltatech_alternative/models/product.py
@@ -68,7 +68,7 @@ class ProductAlternative(models.Model):
     _name = "product.alternative"
     _description = "Product alternative"
 
-    name = fields.Char(string="Code", index=True, unaccent=False)
+    name = fields.Char(string="Code", index="btree_not_null")
     sequence = fields.Integer(string="sequence", default=10)
     product_tmpl_id = fields.Many2one("product.template", string="Product Template", ondelete="cascade")
     hide = fields.Boolean(string="Hide")
