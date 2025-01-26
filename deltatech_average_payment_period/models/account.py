b28d29058a7b9ecb7ab9faeb9effe97397c220ea
Dorin Hongu <dhongu@gmail.com>
2025-01-26 13:04:46 +0200

    Improve functionality across multiple modules in Deltatech.
    Added new fields, actions, and views to enhance business process management and partner workflows. Introduced Excel report generation, updated translations, adjusted pre-commit and pylint configurations, and resolved minor code issues for better performance and maintainability.

--- deltatech_average_payment_period/models/account.py
+++ deltatech_average_payment_period/models/account.py
@@ -2,8 +2,11 @@
 #              Dorin Hongu <dhongu(@)gmail(.)com
 # See README.rst file on addons root folder for license details
 
+import logging
 
 from odoo import api, fields, models
+
+_logger = logging.getLogger(__name__)
 
 
 class AccountMoveLine(models.Model):
@@ -11,16 +14,13 @@ class AccountMoveLine(models.Model):
 
     payment_date = fields.Date(string="Payment Date", compute="_compute_payment_days", store=True)
     payment_days = fields.Integer(string="Payment Days", compute="_compute_payment_days", store=True)
+    payment_days_simple = fields.Float(string="Plain payment days", compute="_compute_payment_days_simple", store=True)
 
     @api.depends("date", "full_reconcile_id")
     def _compute_payment_days(self):
         for aml in self:
-            if aml.full_reconcile_id and aml.journal_id.type in [
-                "sale",
-                "purchase",
-                "sale_refund",
-                "purchase_refund",
-            ]:
+            if aml.full_reconcile_id and aml.journal_id.type in ["sale", "purchase", "sale_refund", "purchase_refund"]:
+                payment_date = False
                 if aml.debit > 0:
                     for line in aml.full_reconcile_id.reconciled_line_ids:
                         if line.credit > 0:
@@ -35,3 +35,26 @@ class AccountMoveLine(models.Model):
             if aml.payment_date:
                 diff = fields.Date.from_string(aml.payment_date) - fields.Date.from_string(aml.date)
                 aml.payment_days = diff.days
+
+    @api.depends("date", "full_reconcile_id")
+    def _compute_payment_days_simple(self):
+        for aml in self:
+            try:
+                if aml.full_reconcile_id:
+                    if aml.move_id.move_type in ["out_invoice", "in_invoice"] and aml.parent_state == "posted":
+                        payment_date = False
+                        if aml.debit > 0:
+                            for line in aml.full_reconcile_id.reconciled_line_ids:
+                                if line.credit > 0:
+                                    payment_date = line.date
+                                    break
+                        if aml.credit > 0:
+                            for line in aml.full_reconcile_id.reconciled_line_ids:
+                                if line.debit > 0:
+                                    payment_date = line.date
+                                    break
+                        if payment_date:
+                            diff = payment_date - aml.date
+                            aml.payment_days_simple = diff.days
+            except Exception as e:
+                _logger.info(f"Compute payment days failed for id {aml.move_id.id}: {e}")
