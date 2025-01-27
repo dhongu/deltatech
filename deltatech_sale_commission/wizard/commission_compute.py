118016205711eef3bdbaf9531eef251028c50bf6
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2025-01-27 15:36:05 +0200

    upd logic

--- deltatech_sale_commission/wizard/commission_compute.py
+++ deltatech_sale_commission/wizard/commission_compute.py
@@ -41,25 +41,27 @@ class CommissionCompute(models.TransientModel):
             else:
                 # if the invoice is paid, we will calculate the commission based on the payment date
                 if line.invoice_id.payment_state == "paid":
-                    full_payment_date = fields.Date.to_date("1999-01-01")  # took a date from the past
-                    for payment in line.invoice_id.invoice_payments_widget[
-                        "content"
-                    ]:  # sometimes the payments don't reach invoice_payments_ids so we use the widget
-                        if fields.Date.to_date(payment["date"]) > full_payment_date:
-                            full_payment_date = fields.Date.to_date(payment["date"])  # get the latest payment date
+                    # take the latest payment date
+                    last_payment = sorted(line.invoice_id.invoice_payments_widget["content"], key=lambda d: d["date"])[
+                        0
+                    ]
                     days_difference = (
-                        full_payment_date - line.invoice_id.invoice_date
+                        fields.Date.to_date(last_payment["date"]) - line.invoice_id.invoice_date
                     ).days  # calculate the days difference between the invoice date and the payment date
-                    checked = False
-                    for condition in commission_conditions:
-                        if (
-                            days_difference <= condition.less_than_days
-                        ):  # they are ordered by less_than_days so we will get the first condition that is less than the days difference
-                            checked = True
-                            value = {"commission": line.commission_computed * condition.percentage / 100}
-                            break
-                    if not checked:  # if we didn't find a condition that is less than the days difference, we will use the default commission
-                        value = {"commission": 0}
+
+                    if days_difference <= 0:
+                        value = {"commission": line.commission_computed}
+                    else:
+                        checked = False
+                        for condition in commission_conditions:
+                            if (
+                                days_difference <= condition.less_than_days
+                            ):  # they are ordered by less_than_days so we will get the first condition that is less than the days difference
+                                checked = True
+                                value = {"commission": line.commission_computed * condition.percentage / 100}
+                                break
+                        if not checked:  # if we didn't find a condition that is less than the days difference, we will use the default commission
+                            value = {"commission": 0}
                 else:
                     value = {"commission": 0}  # if the invoice is not paid, we will not calculate the commission
             # if line.purchase_price == 0 and line.product_id:
