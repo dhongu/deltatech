a8b7754cdff0966f4d4db3a8665fe2d879a1bddb
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2025-01-30 13:36:05 +0200

    [16.0][UPD] deltatech_record_type

--- deltatech_record_type/models/__init__.py
+++ deltatech_record_type/models/__init__.py
@@ -1,4 +1,4 @@
-# ©  2015-2021 Terrabit Solutions
+# ©  2024 Terrabit Solutions
 #              Dan Stoica <danila(@)terrabit(.)ro
 # See README.rst file on addons root folder for license details
 
@@ -6,3 +6,5 @@
 from . import sale
 from . import record_type
 from . import purchase
+from . import res_config_settings
+from . import account_move
