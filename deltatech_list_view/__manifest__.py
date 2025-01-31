5bf0b73e8a0ce5d354a90856d51b43035e3de7a9
Dorin Hongu <dhongu@gmail.com>
2025-01-31 08:56:14 +0200

    Add "List View Select Text" module
    Introduces functionality to prevent opening list items when selecting text. Includes JavaScript patching for list rendering, documentation, and necessary assets for module integration.

---
+++ deltatech_list_view/__manifest__.py
@@ -0,0 +1,20 @@
+# ©  2008-2022 Deltatech
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "List View Select Text",
+    "summary": "List View Select Text",
+    "version": "18.0.1.0.0",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "category": "Generic Modules",
+    "depends": ["web"],
+    "license": "OPL-1",
+    "data": [],
+    "qweb": ["static/src/xml/*.xml"],
+    "images": ["static/description/main_screenshot.png"],
+    "assets": {
+        "web.assets_backend": ["deltatech_list_view/static/src/js/list_renderer.esm.js"],
+    },
+    "development_status": "Beta",
+}
---
+++ deltatech_list_view/readme/DESCRIPTION.md
@@ -0,0 +1,7 @@
+Function:
+
+- Block opening an item from the list when selecting text
+
+Functions:
+
+- Blocheaza deschiderea unui item din lista la selecta unui text
