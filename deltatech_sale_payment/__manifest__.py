ba719465da122a1acab3f03c346f01f8bce9d7bc
Dorin Hongu <dhongu@gmail.com>
2025-02-05 05:37:39 +0200

    Refactor payment status computation in sales orders.
    Replaced redundant assignments with a single variable `payment_status` to streamline logic in `_compute_payment`. Updated module version to 17.0.1.0.7 in the manifest file.

--- deltatech_sale_payment/__manifest__.py
+++ deltatech_sale_payment/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Sale Payment",
     "summary": "Payment button in sale order",
-    "version": "17.0.1.0.6",
+    "version": "17.0.1.0.7",
     "category": "Sales",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
