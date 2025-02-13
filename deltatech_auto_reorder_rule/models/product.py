75872cd1d2376d5c7d0f1c1d18aaf04263265973
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2025-02-13 15:21:18 +0200

    fix2

--- deltatech_auto_reorder_rule/models/product.py
+++ deltatech_auto_reorder_rule/models/product.py
@@ -8,13 +8,21 @@ class ProductProduct(models.Model):
     _inherit = "product.product"
 
     def create_rule(self):
-        warehouses = self.env["stock.warehouse"].search([("generate_reorder_rules", "=", True)])
-        routes = self.env["stock.route"].search([("use_this_for_auto_rules", "=", True)])
+        warehouses = self.env["stock.warehouse"].search([
+            ("generate_reorder_rules", "=", True),
+            ("company_id", "=", self.env.user.company_id.id)
+        ])
+        routes = self.env["stock.route"].search([
+            ("use_this_for_auto_rules", "=", True),
+        ])
         route = False
         if routes:
             route = routes[0].id
         for record in self:
-            rules = record.env["stock.warehouse.orderpoint"].search([("product_id", "=", record.id)])
+            rules = record.env["stock.warehouse.orderpoint"].search([
+                ("product_id", "=", record.id),
+                ("company_id", "=", self.env.user.company_id.id)
+            ])
             if not rules:
                 values = []
                 for warehouse in warehouses:
@@ -31,7 +39,7 @@ class ProductProduct(models.Model):
                             }
                         )
                 if values:
-                    record.env["stock.warehouse.orderpoint"].sudo().create(values)
+                    record.env["stock.warehouse.orderpoint"].create(values)
 
             # if not rules and record.type == "product":
             #     record.env["stock.warehouse.orderpoint"].create(
