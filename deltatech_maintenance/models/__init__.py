c0fccb4924a928a038a564e76511d82f0987af45
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2025-02-28 12:58:36 +0200

    [18.0][UPD] deltatech_maintenance

---
+++ deltatech_maintenance/models/__init__.py
@@ -0,0 +1,9 @@
+# ©  2008-2022  Deltatech
+# See README.rst file on addons root folder for license details
+
+
+from . import maintenance
+from . import stock_picking
+
+# from . import res_company
+# from . import res_config
