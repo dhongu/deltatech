86520a1b9d9ae83d47bfb60f4a3cb502ab50eeb9
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2025-02-28 11:38:55 +0200

    [18.0][UPD] deltatech_lot

--- deltatech_lot/models/stock_picking.py
+++ deltatech_lot/models/stock_picking.py
@@ -12,7 +12,7 @@ class StockPicking(models.Model):
         for picking in self:
             picking_type = picking.picking_type_id
             if picking_type.use_create_lots or picking_type.use_existing_lots:
-                if picking_type.code == "incoming":
+                if picking_type.code in ["incoming", "dropship"]:
                     for line in picking.move_line_ids:
                         if line.product_id.tracking == "lot" and not line.lot_name:
                             line.lot_name = self.env["ir.sequence"].next_by_code("stock.lot.serial")
