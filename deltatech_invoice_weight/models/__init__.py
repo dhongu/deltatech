94ff12e55a21dc34cd967491afa043ab3f890441
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2025-02-28 09:49:34 +0200

    [18.0][UPD] deltatech_invoice_weight

--- deltatech_invoice_weight/models/__init__.py
+++ deltatech_invoice_weight/models/__init__.py
@@ -2,3 +2,6 @@
 # See README.rst file on addons root folder for license details
 
 from . import account_invoice
+from . import purchase_order
+from . import sale_order
+from . import product_template
