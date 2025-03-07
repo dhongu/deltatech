7f0a84364e51d6405f91739fc7a5cb8add0f837c
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2025-03-07 13:29:40 +0200

    [18.0][UPD] deltatech_Sale_margin

--- deltatech_sale_margin/__manifest__.py
+++ deltatech_sale_margin/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Sale Margin",
     "summary": "Check price in sale order",
-    "version": "18.0.1.0.7",
+    "version": "18.0.1.0.9",
     "category": "Sales",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
