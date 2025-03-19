aa505894af3913e8bb007650c183467f29e284aa
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2025-03-19 14:17:54 +0200

    fix

--- deltatech_saleorder_type/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# ©  2015-2021 Terrabit Solutions
-#              Dan Stoica <danila(@)terrabit(.)ro
-# See README.rst file on addons root folder for license details
-
-
-from . import sale
--- deltatech_saleorder_type/readme/DESCRIPTION.md
+++
@@ -1,4 +0,0 @@
-- Features:
-
-  - Types can be defined for sale order
-  - A journal can be defined for invoicing
