b320ddb695c4fd5a47a05013eb07b5da99255a48
Dorin Hongu <dhongu@gmail.com>
2025-03-22 01:48:01 +0200

    Actualizează denumirile câmpurilor din modele.
    S-au modificat denumirile câmpurilor `string` pentru a le face mai clare și mai consistente, adăugând "Location" în descriere. Aceste schimbări afectează atât modelul `product.template`, cât și modelul `stock.lot`. Obiectivul este îmbunătățirea lizibilității și clarității pentru utilizatori.

--- deltatech_warehouse_arrangement/models/product_template.py
+++ deltatech_warehouse_arrangement/models/product_template.py
@@ -10,7 +10,7 @@ class ProductTemplate(models.Model):
     _inherit = "product.template"
 
     loc_storehouse_id = fields.Many2one("warehouse.location.storehouse", string="Storehouse")
-    loc_zone_id = fields.Many2one("warehouse.location.zone", string="Zone")
-    loc_shelf_id = fields.Many2one("warehouse.location.shelf", string="Shelf")
-    loc_section_id = fields.Many2one("warehouse.location.section", string="Section")
-    loc_rack_id = fields.Many2one("warehouse.location.rack", string="Rack")
+    loc_zone_id = fields.Many2one("warehouse.location.zone", string="Zone Location")
+    loc_shelf_id = fields.Many2one("warehouse.location.shelf", string="Shelf Location")
+    loc_section_id = fields.Many2one("warehouse.location.section", string="Section Location")
+    loc_rack_id = fields.Many2one("warehouse.location.rack", string="Rack Location")
