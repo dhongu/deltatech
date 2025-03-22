15000ec94f8dd1fa898d4110fba5d21a70105103
Dorin Hongu <dhongu@gmail.com>
2025-03-22 12:38:06 +0200

    Refactor modele și actualizează importurile în frontend.
    Elimină câmpul `provider_id` și logica aferentă din modelul `sale.order` pentru simplificare. Adaugă importul modulului `WebsiteSaleCheckout` în scriptul frontend pentru suport funcțional extins. Aceste modificări îmbunătățesc lizibilitatea și menținabilitatea codului.

--- deltatech_website_delivery_and_payment/models/sale.py
+++ deltatech_website_delivery_and_payment/models/sale.py
@@ -1,13 +1,11 @@
 # ©  2008-2021 Deltatech
 # See README.rst file on addons root folder for license details
 
-from odoo import fields, models
+from odoo import models
 
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
-
-    provider_id = fields.Many2one("payment.provider")
 
     def _get_delivery_methods(self):
         carriers = super()._get_delivery_methods()
@@ -19,11 +17,4 @@ class SaleOrder(models.Model):
     def _check_carrier_quotation(self, force_carrier_id=None, keep_carrier=False):
         if force_carrier_id and force_carrier_id == self.carrier_id.id == int(force_carrier_id):
             return True
-        return super()._check_carrier_quotation(force_carrier_id, keep_carrier)
-
-    def _action_confirm(self):
-        for order in self:
-            tx = order.sudo().transaction_ids._get_last()
-            if tx:
-                order.write({"provider_id": tx.provider_id.id})
-        return super()._action_confirm()
+        return super()._check_carrier_quotation(force_carrier_id=force_carrier_id, keep_carrier=keep_carrier)
