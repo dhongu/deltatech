af0484bfff6399ecc2acf4c599cc2c5fb0c2747f
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2025-03-25 11:54:37 +0200

    [17.0][UPD] deltatech_contact

--- deltatech_contact/__manifest__.py
+++ deltatech_contact/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Deltatech Contacts",
     "summary": "New fields in partner",
-    "version": "18.0.1.4.6",
+    "version": "18.0.1.4.7",
     "author": "Terrabit,Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "category": "Administration",
