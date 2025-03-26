6a4f84b13ef6489e22b6ec388588a0ae34367b25
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2025-03-26 15:52:08 +0200

    reformat __init__

--- deltatech_maintenance/models/__init__.py
+++ deltatech_maintenance/models/__init__.py
@@ -2,8 +2,7 @@
 # See README.rst file on addons root folder for license details
 
 
-from . import maintenance
-from . import stock_picking
+from . import maintenance, stock_picking
 
 # from . import res_company
 # from . import res_config
