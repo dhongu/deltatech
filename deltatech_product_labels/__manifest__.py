002d913c6cd94feb7db2cfd44e37b1086fb9c19f
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2025-03-28 14:01:21 +0200

    product_lable update

--- deltatech_product_labels/__manifest__.py
+++ deltatech_product_labels/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "Product Labels",
-    "version": "18.0.1.0.6",
+    "version": "18.0.1.1.0",
     "category": "Stock",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
