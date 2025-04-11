056f7855278abbaa6de05639a3ed8a690851db8c
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2025-04-11 09:48:33 +0300

    [18.0][MIG] deltatech_invoice_report

--- deltatech_actions/__manifest__.py
+++ deltatech_actions/__manifest__.py
@@ -5,13 +5,14 @@
     "name": "Deltatech Actions",
     "category": "Other",
     "summary": "Cleaning and other actions",
-    "version": "18.0.0.0.3",
+    "version": "18.0.0.0.4",
     "author": "Terrabit, Dan Stoica",
     "website": "https://www.terrabit.ro",
     "license": "OPL-1",
     "depends": [
         "account_edi",
         "sale",
+        "product",
     ],
     "data": [
         "data/ir_cron_data.xml",
