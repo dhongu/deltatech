9176a018fb30b172d9ae550af37d94778aa0c639
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2025-04-22 12:39:53 +0300

    mig sale_return_cause

---
+++ deltatech_sale_return_cause/__manifest__.py
@@ -0,0 +1,18 @@
+# ©  2008-2021 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+{
+    "name": "Sale Return Cause",
+    "summary": "Return Cause",
+    "version": "18.0.0.0.1",
+    "author": "Terrabit, Voicu Stefan",
+    "website": "https://www.terrabit.ro",
+    "category": "Sales",
+    "depends": ["sale"],
+    "license": "OPL-1",
+    "data": ["views/sale_order_view.xml", "data/ir_cron_data.xml"],
+    "images": ["images/main_screenshot.png"],
+    "installable": True,
+    "development_status": "Beta",
+    "maintainers": ["VoicuStefan2001"],
+}
