07779d989e276a0652adc6b5052c4e21b17f1930
Dorin Hongu <dhongu@gmail.com>
2025-05-12 11:29:12 +0300

    Adaugă modulul "Price List Currency Extension"
    Acest modul permite gestionarea prețului de listă în valute diferite față de moneda principală. Au fost adăugate modele, vizualizări și configurații noi pentru a susține această funcționalitate, inclusiv câmpuri suplimentare și actualizări ale interfeței utilizator.

---
+++ deltatech_pricelist/__manifest__.py
@@ -0,0 +1,22 @@
+# ©  2008-2021 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+
+{
+    "name": "Price List Currency Extension",
+    "summary": "Price list currency",
+    "version": "18.0.1.0.0",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "category": "Sales",
+    "depends": ["base", "product", "sale"],
+    "license": "LGPL-3",
+    "data": [
+        # 'views/product_view.xml',
+        "views/res_company_view.xml"
+    ],
+    "images": ["images/main_screenshot.png"],
+    "development_status": "Mature",
+    "maintainers": ["dhongu"],
+}
