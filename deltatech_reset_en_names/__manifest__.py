c9e771fce99a17d6bb81d8b313ef22c534446b9e
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2025-05-16 12:52:13 +0300

    17.0-add-deltatech_reset_en_names

---
+++ deltatech_reset_en_names/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Product Reset EN Names",
+    "category": "Product",
+    "summary": "Will set the en names of the product templates to the names of the currently used language of the user",
+    "version": "17.0.0.0.0",
+    "author": "Terrabit",
+    "license": "LGPL-3",
+    "website": "https://www.terrabit.ro",
+    "data": [
+        "data/ir_server_action.xml",
+    ],
+    "depends": ["base", "product"],
+    "images": ["static/description/main_screenshot.png"],
+    "development_status": "Beta",
+    "maintainers": ["VoicuStefan2001"],
+}
