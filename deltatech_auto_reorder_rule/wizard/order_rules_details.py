70295a1107d03e8b0c069a9df18bd360b6726cbb
Dorin Hongu <dhongu@gmail.com>
2025-05-30 07:55:51 +0300

    Adaugă modulul "Deltatech Analytic Distribution"
    Acest modul validează distribuția analitică pe facturile furnizorilor, asigurând că totalul procentelor pe linii ajunge la 100% și câmpurile obligatorii sunt completate. Se introduce o configurare pentru activarea validării, fiind un instrument util pentru controlul și precizia alocărilor analitice.

---
+++ deltatech_auto_reorder_rule/wizard/order_rules_details.py
@@ -0,0 +1,42 @@
+from odoo import fields, models
+
+
+class OrderRulesDetailsWizard(models.TransientModel):
+    _name = "order.rules.details.wizard"
+    _description = "Order Rules Details Wizard"
+
+    min_quantity = fields.Float(string="Minimum Quantity", required=True)
+    max_quantity = fields.Float(string="Maximum Quantity", required=True)
+    stock_location_ids = fields.Many2many(
+        comodel_name="stock.location", string="Stock Locations", help="Select stock locations for the order rules."
+    )
+    trigger = fields.Selection(
+        [("auto", "Automatic"), ("manual", "Manual")], string="Trigger", required=True, default="manual"
+    )
+
+    def do_create(self):
+        product_template = self.env["product.template"].browse(self._context.get("active_id"))
+        routes = self.env["stock.route"].search(
+            [
+                ("use_this_for_auto_rules", "=", True),
+            ]
+        )
+        route = False
+        if routes:
+            route = routes[0].id
+        values = []
+        for variant in product_template.product_variant_ids:
+            for location in self.stock_location_ids:
+                values.append(
+                    {
+                        "product_id": variant.id,
+                        "product_min_qty": self.min_quantity,
+                        "product_max_qty": self.max_quantity,
+                        "qty_multiple": 0,
+                        "route_id": route,
+                        "trigger": self.trigger,
+                        "location_id": location.id,
+                    }
+                )
+        if values:
+            self.env["stock.warehouse.orderpoint"].create(values)
