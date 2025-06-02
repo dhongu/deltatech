c6327f5d958645a563e4758756fab5ce576a4962
Dorin Hongu <dhongu@gmail.com>
2025-06-02 13:35:13 +0300

    Actualizează metoda `send_mail` pentru a folosi `email_layout_xmlid`
    ---
    
    Înlocuit parametrul `notif_layout` cu `email_layout_xmlid` în metoda `send_mail` a modelului `mail.template`. Această schimbare permite o mai bună personalizare a layout-urilor emailurilor.

--- deltatech_sale_feedback/models/mail_template.py
+++ deltatech_sale_feedback/models/mail_template.py
@@ -13,11 +13,12 @@ _logger = logging.getLogger(__name__)
 class MailTemplate(models.Model):
     _inherit = "mail.template"
 
-    def send_mail(self, res_id, force_send=False, raise_exception=False, email_values=None, notif_layout=False):
+    def send_mail(self, res_id, force_send=False, raise_exception=False, email_values=None,
+                      email_layout_xmlid=False):
         record = self.env[self.model].browse(res_id)
         template = self
         if "company_id" in record and record["company_id"]:
             web_base_url = record.company_id.website
             if web_base_url:
                 template = self.with_context(web_base_url=web_base_url)
-        return super(MailTemplate, template).send_mail(res_id, force_send, raise_exception, email_values, notif_layout)
+        return super(MailTemplate, template).send_mail(res_id, force_send, raise_exception, email_values, email_layout_xmlid)
