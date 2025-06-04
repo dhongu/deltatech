0e3f9a63847e3e9434c0fab9201e2a78f4dd023f
Dorin Hongu <dhongu@gmail.com>
2025-06-04 04:02:03 +0300

    Adaugă decorări vizuale pentru câmpul `last_purchase_price`
    ---
    
    Adăugate decorări `decoration-success` și `decoration-danger` pentru câmpul `last_purchase_price`, în funcție de comparația cu `standard_price`. Acestea oferă indicii vizuale privind prețurile de achiziție. Incrementată versiunea modulului la `17.0.1.2.5`.

--- deltatech_purchase_price/__manifest__.py
+++ deltatech_purchase_price/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Purchase Price",
     "summary": "Update vendor price after reception",
-    "version": "17.0.1.2.4",
+    "version": "17.0.1.2.5",
     "author": "Terrabit, Dorin Hongu",
     "license": "OPL-1",
     "website": "https://www.terrabit.ro",
