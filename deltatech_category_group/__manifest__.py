038d7e7bdbf58be6f24ec77e38a1040d00307b0d
Dorin Hongu <dhongu@gmail.com>
2025-06-08 10:05:06 +0300

    [ADD] deltatech_category_group: introduce category type and class for internal categorization

---
+++ deltatech_category_group/__manifest__.py
@@ -0,0 +1,28 @@
+# ©  2008-now Terrabit
+#              Dan Stoica <danila(@)terrabit(.)ro
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Deltatech Category Group",
+    "summary": "Groups for internal categories",
+    "version": "17.0.0.0.2",
+    "author": "Terrabit, Dan Stoica",
+    "website": "https://www.terrabit.ro",
+    "category": "Other",
+    "depends": [
+        "deltatech_sale_commission",
+    ],
+    "license": "AGPL-3",
+    "data": [
+        "security/security.xml",
+        "views/product_category.xml",
+        "views/category_types_views.xml",
+        "views/stock_quant.xml",
+        "report/sale_margin_report.xml",
+        "report/account_invoice_report.xml",
+        "security/ir.model.access.csv",
+    ],
+    "images": ["static/description/main_screenshot.png"],
+    "development_status": "Beta",
+    "maintainers": ["danila12"],
+}
