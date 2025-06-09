2756bb018ca222fa95fef2ba0bb3cdfa307f3e45
Dorin Hongu <dhongu@gmail.com>
2025-06-10 02:03:48 +0300

    Adaugă funcționalități noi pentru gestionarea mesajelor și regulilor de stoc
    Au fost implementate metode pentru curățarea mesajelor și atașamentelor vechi în modelul `mail.message`, alături de îmbunătățirea tipului MIME pentru atașamente. În plus, a fost adăugată funcționalitatea de creare automată a regulilor de reaprovizionare pentru produse, îmbunătățind gestionarea stocurilor.

--- deltatech_analytic_distribution/models/__init__.py
+++ deltatech_analytic_distribution/models/__init__.py
@@ -1,2 +1,3 @@
 from . import account_move
 from . import res_config
+from . import res_company
--- deltatech_analytic_distribution/models/res_config.py
+++ deltatech_analytic_distribution/models/res_config.py
@@ -5,7 +5,8 @@ class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
     analytic_distribution_validation_enabled = fields.Boolean(
+        related="company_id.analytic_distribution_validation_enabled",
+        readonly=False,
         string="Enable Analytic Distribution Validation",
-        config_parameter="analytic_distribution_validation_enabled",
-        help="Enable or disable the validation of analytic distribution in account moves.",
+        help="Enable or disable analytic distribution enforcement for this company.",
     )
