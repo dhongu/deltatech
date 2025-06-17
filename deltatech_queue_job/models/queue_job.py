0fdebcf78e3227093e1b5a287998b3efd14fda9d
danila12 <danila@terrabit.ro>
2025-06-17 15:55:17 +0300

    add stock.lot label functionality, fix formatting

--- deltatech_queue_job/models/queue_job.py
+++ deltatech_queue_job/models/queue_job.py
@@ -58,7 +58,7 @@ class QueueJob(models.Model):
             WHERE  id = %(job_id)s
              FOR NO KEY UPDATE SKIP LOCKED
             """,
-            {"job_id":job_id}
+            {"job_id": job_id},
         )
         row = self.env.cr.fetchone()
         return self.browse(row and row[0])
@@ -69,7 +69,7 @@ class QueueJob(models.Model):
 
     @api.model
     def _job_runner(self, commit=True):
-        limit_jobs = self.env["ir.config_parameter"].sudo().get_param("queue_job.limit_jobs","10")
+        limit_jobs = self.env["ir.config_parameter"].sudo().get_param("queue_job.limit_jobs", "10")
         limit_jobs = int(limit_jobs)
         jobs = self.search([("state", "=", "pending")], limit=limit_jobs + 1)
 
