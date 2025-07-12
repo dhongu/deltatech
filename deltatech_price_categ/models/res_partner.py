db46a875f10952db059a3e9f79336b274ab879d2
Dorin Hongu <dhongu@gmail.com>
2025-07-12 12:04:41 +0300

    Extinde operatorii suportati pentru funcția `_search_product_pricelist`
    ---
    
    Adăugați operatorii `in` și `not in` pentru căutarea în funcția `_search_product_pricelist`. Acest lucru îmbunătățește flexibilitatea filtrării partenerilor în funcție de listele de prețuri asociate, permițând utilizarea unei liste de valori.

--- deltatech_price_categ/models/res_partner.py
+++ deltatech_price_categ/models/res_partner.py
@@ -8,23 +8,40 @@ class ResPartner(models.Model):
     )
 
     def _search_product_pricelist(self, operator, value):
-        if operator not in ('=', '!='):
+        if operator not in ('=', '!=', 'in', 'not in'):
             raise NotImplementedError(f"Operatorul '{operator}' nu este suportat.")
 
         prop_model = self.env['ir.property']
         pricelist_field = 'property_product_pricelist'
 
-        # Obținem toate proprietățile setate direct pentru parteneri
-        props = prop_model.search([
-            ('name', '=', pricelist_field),
-            ('res_model', '=', 'res.partner'),
-            ('value_reference', '=', f'product.pricelist,{value}')
-        ])
+        # Transformăm value în listă dacă nu este deja
+        if operator in ('in', 'not in'):
+            pricelist_ids = value if isinstance(value, list) else [value]
+        else:
+            pricelist_ids = [value]
 
-        # Extragem id-urile partenerilor pentru care e setat explicit
+        # Pregătim domeniul pentru căutarea în ir.property
+        domain = [
+            ('name', '=', pricelist_field),
+            ('model', '=', 'res.partner')
+        ]
+
+        # Adăugăm condiția pentru value_reference
+        if len(pricelist_ids) == 1:
+            domain.append(('value_reference', '=', f'product.pricelist,{pricelist_ids[0]}'))
+        else:
+            value_refs = [f'product.pricelist,{pid}' for pid in pricelist_ids]
+            domain.append(('value_reference', 'in', value_refs))
+
+        # Obținem toate proprietățile
+        props = prop_model.search(domain)
+
+        # Extragem id-urile partenerilor
         partner_ids = [int(prop.res_id.split(',')[1]) for prop in props if prop.res_id]
 
-        if operator == '=':
+        # Construim domeniul final pentru partner
+        if operator in ('=', 'in'):
             return [('id', 'in', partner_ids)]
-        else:
+        else:  # operator in ('!=', 'not in')
             return [('id', 'not in', partner_ids)]
+
