fad3ff8df8a889c01365d06f5ccbcd74174b6c86
Dorin Hongu <dhongu@gmail.com>
2025-07-12 12:16:46 +0300

    Optimizează filtrarea în `_search_product_pricelist`
    ---
    
    Simplificată și optimizată logica metodei `_search_product_pricelist` prin eliminarea codului redundant. Abordarea directă îmbunătățește claritatea și fiabilitatea logicului de căutare pentru partenerii asociați cu listele de prețuri, gestionând mai bine cazurile excepționale.

--- deltatech_price_categ/models/res_partner.py
+++ deltatech_price_categ/models/res_partner.py
@@ -14,34 +14,26 @@ class ResPartner(models.Model):
         prop_model = self.env['ir.property']
         pricelist_field = 'property_product_pricelist'
 
-        # Transformăm value în listă dacă nu este deja
-        if operator in ('in', 'not in'):
-            pricelist_ids = value if isinstance(value, list) else [value]
+        # Folosim o abordare directă pentru a găsi proprietățile
+        props = prop_model._get_domain(pricelist_field, self._name)
+        if props:
+            domain = props + [('value_reference', '=', f'product.pricelist,{value}')]
+            props = prop_model.search(domain)
         else:
-            pricelist_ids = [value]
+            props = prop_model.browse()
 
-        # Pregătim domeniul pentru căutarea în ir.property
-        domain = [
-            ('name', '=', pricelist_field),
-            ('model', '=', 'res.partner')
-        ]
+        # Extragem id-urile partenerilor pentru care e setat explicit
+        partner_ids = []
+        for prop in props:
+            if prop.res_id and prop.res_id.startswith('res.partner,'):
+                try:
+                    partner_id = int(prop.res_id.split(',')[1])
+                    partner_ids.append(partner_id)
+                except (ValueError, IndexError):
+                    continue
 
-        # Adăugăm condiția pentru value_reference
-        if len(pricelist_ids) == 1:
-            domain.append(('value_reference', '=', f'product.pricelist,{pricelist_ids[0]}'))
-        else:
-            value_refs = [f'product.pricelist,{pid}' for pid in pricelist_ids]
-            domain.append(('value_reference', 'in', value_refs))
-
-        # Obținem toate proprietățile
-        props = prop_model.search(domain)
-
-        # Extragem id-urile partenerilor
-        partner_ids = [int(prop.res_id.split(',')[1]) for prop in props if prop.res_id]
-
-        # Construim domeniul final pentru partner
-        if operator in ('=', 'in'):
+        if operator == '=':
             return [('id', 'in', partner_ids)]
-        else:  # operator in ('!=', 'not in')
+        else:
             return [('id', 'not in', partner_ids)]
 
