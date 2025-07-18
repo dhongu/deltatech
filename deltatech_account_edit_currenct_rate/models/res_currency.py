039644137f09b7c26958a685107780a87f5f85b9
Dorin Hongu <dhongu@gmail.com>
2025-07-18 18:41:36 +0300

    Adaugă suport pentru curs valutar personalizat pe factură
    ---
    
    Implementată posibilitatea de a seta un curs valutar personalizat pentru facturi. Include câmp nou în modelul `account.move`, extensii pentru metodele relevante și modificări în interfața utilizator, îmbunătățind flexibilitatea în gestionarea cursurilor valutare.

---
+++ deltatech_account_edit_currenct_rate/models/res_currency.py
@@ -0,0 +1,25 @@
+# ©  2025 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+from odoo import models
+
+
+class Currency(models.Model):
+    _inherit = "res.currency"
+
+    def _convert(self, from_amount, to_currency, company=None, date=None, round=True):
+        if self._context.get("currency_rate"):
+            self, to_currency = self or to_currency, to_currency or self
+            assert self, "convert amount from unknown currency"
+            assert to_currency, "convert amount to unknown currency"
+            assert company, "convert amount from unknown company"
+            assert date, "convert amount from unknown date"
+
+            if self == to_currency:
+                to_amount = from_amount
+            else:
+                to_amount = from_amount * self._context["currency_rate"]
+            return to_currency.round(to_amount) if round else to_amount
+
+        return super()._convert(from_amount, to_currency, company, date, round=round)
