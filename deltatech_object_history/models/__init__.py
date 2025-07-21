bd88079814bb874395128306f438ed2f0e620e04
Dorin Hongu <dhongu@gmail.com>
2025-07-21 15:31:27 +0300

    Elimină modulul „deltatech_object_history”
    Au fost eliminate toate fișierele și resursele asociate cu modulul „deltatech_object_history”, inclusiv modele, vizualizări, traduceri, definiții de securitate și documentație. Această eliminare indică faptul că modulul nu mai este necesar sau utilizat.

--- deltatech_object_history/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# ©  2023-now Deltatech
-# See README.rst file on addons root folder for license details
-
-
-from . import object_history
-from . import res_partner
-from . import account_move
-from . import stock_picking
--- deltatech_object_history_service/readme/DESCRIPTION.md
+++
@@ -1,3 +0,0 @@
-Features:  
-- Extends the object history functionalities to Service agreement ans
-  Service Equipment models
