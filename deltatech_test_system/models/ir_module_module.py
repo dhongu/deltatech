e999130a95845271fb96002fde0c094dc2b30864
Dorin Hongu <dhongu@gmail.com>
2025-07-31 11:56:06 +0300

    Adaugă câmp și logică pentru starea sistemului (test/production)
    ---
    
    Implementată logica pentru determinarea stării sistemului (test/production) utilizând un nou câmp `database_is_neutralized`. Actualizate vizualizările și componenta `ir.module.module` pentru a reflecta corect starea. Incrementată versiunea modulului la 17.0.0.0.5.

---
+++ deltatech_test_system/models/ir_module_module.py
@@ -0,0 +1,18 @@
+# ©  2025 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+from odoo import fields, models
+
+
+class IrModuleModule(models.Model):
+    _inherit = 'ir.module.module'
+
+    database_is_neutralized = fields.Boolean(compute='_compute_database_is_neutralized')
+
+
+    def _compute_database_is_neutralized(self):
+        """Compute if the database is neutralized based on the presence of a specific module."""
+        get_param = self.env['ir.config_parameter'].sudo().get_param
+        database_is_neutralized = get_param('database.is_neutralized', default=False)
+        self.database_is_neutralized = database_is_neutralized
