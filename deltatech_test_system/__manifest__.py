7e849aedb9ad3377371db3414b3d0bcbada0c52f
Dorin Hongu <dhongu@gmail.com>
2025-08-16 16:23:24 +0300

    Adaugă câmpul `database_is_neutralized` și actualizează vizualizările
    Câmpul `database_is_neutralized` a fost introdus în modelul `ir.module.module`, alături de o metodă de calcul corespunzătoare. Vizualizările kanban și form au fost modificate pentru a utiliza acest câmp, împreună cu indicatoare pentru starea sistemului de test sau producție. Versiunea modulului a fost incrementată la 18.0.0.0.5.

--- deltatech_test_system/__manifest__.py
+++ deltatech_test_system/__manifest__.py
@@ -5,8 +5,8 @@
 
 {
     "name": "Deltatech Test System",
-    "summary": "Utils",
-    "version": "18.0.0.0.4",
+    "summary": "Set system status: test or production",
+    "version": "18.0.0.0.5",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "category": "Tools",
@@ -14,7 +14,8 @@
     "license": "OPL-1",
     "data": [
         # "views/templates.xml",
-        "views/res_config_settings_view.xml"
+        "views/res_config_settings_view.xml",
+        "views/ir_module_module_view.xml",
     ],
     "images": ["static/description/main_screenshot.png"],
     "development_status": "Production/Stable",
