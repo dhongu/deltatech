dc4c07847c716551a3020548b6a1cb17dc60ef1a
Dorin Hongu <dhongu@gmail.com>
2025-08-28 20:44:11 +0300

    Extinde validarea email, telefon și TVA pentru portal și checkout
    ---
    
    Actualizată logica de validare pentru a include email-ul, telefonul și TVA-ul atât pe portal, cât și la checkout. Elimină spațiile albe, previne duplicatele și afișează mesaje de eroare mai clare. Această modificare îmbunătățește precizia și integritatea datelor.

--- deltatech_website_vat_validation/controllers/portal.py
+++ deltatech_website_vat_validation/controllers/portal.py
@@ -3,29 +3,30 @@
 # See README.rst file on addons root folder for license details
 
 from odoo.http import request
-
+from odoo import _
 from odoo.addons.phone_validation.tools import phone_validation
 from odoo.addons.portal.controllers.portal import CustomerPortal
 
 
 class CustomerPortalVATValidation(CustomerPortal):
     def details_form_validate(self, data, partner_creation=False):
-        if data.get("vat"):
-            data["vat"] = data.get("vat").strip()
+
+        for field in ['vat', 'email', 'phone']:
+            if field in data and data.get(field):
+                data[field] = data.get(field).strip()
 
         error, error_message = super().details_form_validate(data, partner_creation)
 
-        vat = data.get("vat", False)
-        if vat and 'vat' not in error:
-            partner = request.env['res.users'].browse(request.uid).partner_id
-            domain = [("vat", "=", vat),('id','!=',partner.id),('parent_id','=',False)]
-            partner_vat = request.env['res.partner'].sudo().search(domain, limit=1)
-            if  partner_vat:
-                error["vat"] = "error"
-                error_message.append("VAT already exist")
-            else:
-                # preluare nume din ANAF
-                pass
+        partner = request.env['res.users'].browse(request.uid).partner_id
+
+        for field in ['vat', 'email', 'phone']:
+            value = data.get(field, False)
+            if value and field not in error:
+                domain = [(field, "=", value), ('id', '!=', partner.id), ('parent_id', '=', False)]
+                partner_exists = request.env['res.partner'].search(domain, limit=1)
+                if partner_exists:
+                    error[field] = "error"
+                    error_message.append(_(f"An other partner already exists with the same {value}"))
 
 
         return error, error_message
