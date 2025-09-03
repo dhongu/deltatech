aabbb065c2641e8a66bab70d9b4bea66e4037092
Dan Stoica <danila@terrabit.ro>
2025-09-03 09:44:22 +0300

    Added `check_serial_no` field to stock location and updated logic for negative stock checks (#2122)

--- deltatech_stock_negative/__manifest__.py
+++ deltatech_stock_negative/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "No Negative Stock",
     "summary": "Negative stocks are not allowed",
-    "version": "18.0.2.0.4",
+    "version": "18.0.2.0.5",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "category": "Generic Modules/Stock",
