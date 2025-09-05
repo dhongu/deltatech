3a41ea75526f78a89ebe416c2299d2f35e841d25
danila12 <danila@terrabit.ro>
2025-09-05 10:29:07 +0300

    stock reseller: fix price calculation logic, added view references

--- deltatech_stock_reseller/__manifest__.py
+++ deltatech_stock_reseller/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Stock Report Reseller",
     "summary": "Report report reseller",
-    "version": "18.0.1.0.1",
+    "version": "18.0.1.0.2",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "category": "Generic Modules",
