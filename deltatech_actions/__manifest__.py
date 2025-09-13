daae4280e82f154c844c604e2e2f279721f18882
Dorin Hongu <dhongu@gmail.com>
2025-09-13 12:11:05 +0300

    Adaugă funcționalități de unificare și normalizare pentru parteneri
    Au fost create metode pentru unificarea contactelor duplicate după email și a companiilor duplicate după CUI. De asemenea, a fost adăugat un mecanism pentru normalizarea numelor companiilor, împreună cu job-uri cron asociate pentru aceste procese.

--- deltatech_actions/__manifest__.py
+++ deltatech_actions/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Deltatech Actions",
     "category": "Other",
     "summary": "Cleaning and other actions",
-    "version": "18.0.0.0.7",
+    "version": "18.0.0.0.9",
     "author": "Terrabit, Dan Stoica",
     "website": "https://www.terrabit.ro",
     "license": "OPL-1",
