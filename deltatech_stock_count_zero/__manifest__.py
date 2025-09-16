72a83f61686f4887620de7e420b19bf89f749bfa
Dorin Hongu <dhongu@gmail.com>
2025-09-16 20:46:41 +0300

    Adaugă modulul "Deltatech stock count zero"
    Acest modul permite setarea cantității la 0 pentru liniile de inventar când inputul este gol, calculând imediat diferența. De asemenea, aduce suport pentru gestionarea cantităților de inventar cu un flux mai simplificat.

---
+++ deltatech_stock_count_zero/__manifest__.py
@@ -0,0 +1,18 @@
+# ©  Terrabit
+#              Dan Stoica <danila(@)terrabit(.)ro
+# See README.rst file on addons root folder for license details
+{
+    "name": "Deltatech stock count zero",
+    "category": "Stock",
+    "summary": "Set inventory line to 0 when empty count is requested",
+    "version": "18.0.0.0.0",
+    "license": "OPL-1",
+    "author": "Terrabit, Voicu Stefan",
+    "website": "https://www.terrabit.ro",
+    "depends": ["stock"],
+    "data": [],
+    "images": ["static/description/main_screenshot.png"],
+    "installable": True,
+    "development_status": "Beta",
+    "maintainers": ["VoicuStefan2001"],
+}
