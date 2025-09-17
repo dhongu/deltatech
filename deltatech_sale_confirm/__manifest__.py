cddfc4bfba5df6828ae130d6b72cffaff2280dd4
Dorin Hongu <dhongu@gmail.com>
2025-09-17 05:06:13 +0300

    Adaugă noul modul `deltatech_sale_confirm` în proiect
    ---
    
    Creat modulul `deltatech_sale_confirm`, configurat cu manifest, dependințe și metadate relevante. Actualizată versiunea copyright-ului în fișierele Python aferente. Modulul este pregătit pentru gestionarea noilor funcționalități de confirmare a vânzărilor.

---
+++ deltatech_sale_confirm/__manifest__.py
@@ -0,0 +1,17 @@
+# ©  2008-2021 Deltatech
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Sale Confirm",
+    "summary": "Sale Confirm",
+    "version": "17.0.1.4.0",
+    "category": "Sales",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "depends": ["sale"],
+    "license": "OPL-1",
+
+    "images": ["static/description/main_screenshot.png"],
+    "development_status": "Mature",
+    "maintainers": ["dhongu"],
+}
