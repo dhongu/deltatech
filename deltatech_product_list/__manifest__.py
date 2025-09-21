31d06c29e38af60c1449e685cd0b99b2d71a933f
Dorin Hongu <dhongu@gmail.com>
2025-09-21 19:11:28 +0300

    Actualizează versiunea modulului la 19.0.1.0.1
    Versiunea specificată în manifest a fost actualizată pentru a reflecta migrarea la Odoo 19.0. De asemenea, fișierul imaginii asociate a fost modificat pentru a se alinia structurii existente.

--- deltatech_product_list/__manifest__.py
+++ deltatech_product_list/__manifest__.py
@@ -6,7 +6,7 @@
 {
     "name": "Product List",
     "summary": "Define products lists",
-    "version": "18.0.1.0.1",
+    "version": "19.0.1.0.1",
     "category": "Sale",
     "author": "Terrabit, Dorin Hongu",
     "license": "LGPL-3",
