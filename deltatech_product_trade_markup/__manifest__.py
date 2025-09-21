2057b15b7c7e7859a32537e7161c0406c6553c1e
Dorin Hongu <dhongu@gmail.com>
2025-09-21 19:04:35 +0300

    Actualizează versiunea modulului la 19.0.1.0.1
    A fost modificată versiunea în fișierul manifest pentru a reflecta migrarea la Odoo 19.0. De asemenea, fișierul imaginii asociate a fost actualizat în cadrul structurii existente.

--- deltatech_product_trade_markup/__manifest__.py
+++ deltatech_product_trade_markup/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Product trade markup",
     "summary": "Product trade markup",
-    "version": "18.0.1.0.1",
+    "version": "19.0.1.0.1",
     "author": "Terrabit, Dan Stoica",
     "website": "https://www.terrabit.ro",
     "support": "odoo@terrabit.ro",
