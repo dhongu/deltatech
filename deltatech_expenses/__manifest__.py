baebe4b1d260e42d553a1632333a4aead865bb62
Dorin Hongu <dhongu@gmail.com>
2025-09-22 13:08:50 +0300

    Adaugă câmpuri și refactorizează logica pentru deducerea cheltuielilor
    Au fost adăugate câmpurile `analytic_distribution` și `expense_journal_id` pentru o gestionare mai detaliată a jurnalelor de cheltuieli. De asemenea, logica existentă a fost refactorizată pentru a utiliza aceste câmpuri și a îmbunătăți procesarea deducerilor și avansurilor.

--- deltatech_expenses/__manifest__.py
+++ deltatech_expenses/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Expenses Deduction",
     "summary": "Expenses Deduction & Disposition of Cashing",
-    "version": "18.0.2.0.4",
+    "version": "18.0.2.2.0",
     "category": "Accounting & Finance",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
