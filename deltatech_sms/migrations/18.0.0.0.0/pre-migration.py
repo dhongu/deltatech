a39bc16e11f48436edeacfcd26661b286c22b109
Dorin Hongu <dhongu@gmail.com>
2025-09-25 15:34:46 +0300

    Comentează linia de actualizare a tabelelor în scriptul de migrare
    Linia de SQL care modifica câmpul `technical_name` din `iap_service` a fost comentată în scriptul de migrare pre-18.0. Acest lucru previne aplicarea acestei modificări în timpul migrației.

--- deltatech_sms/migrations/18.0.0.0.0/pre-migration.py
+++ deltatech_sms/migrations/18.0.0.0.0/pre-migration.py
@@ -6,6 +6,6 @@ _logger = logging.getLogger(__name__)
 def migrate(cr, version):
     _logger.info("_____________ Migration pre-script for IAP ANAF _____________")
 
-    cr.execute("""
-        UPDATE iap_service SET technical_name = 'sms_old' WHERE technical_name = 'sms';
-    """)
+    # cr.execute("""
+    #     UPDATE iap_service SET technical_name = 'sms_old' WHERE technical_name = 'sms';
+    # """)
