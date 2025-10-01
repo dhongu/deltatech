43efa72e145802c1acd4fbdc616ac6114da5eede
Dorin Hongu <dhongu@gmail.com>
2025-10-01 14:11:27 +0300

    Adaugă suport pentru automatizarea liniilor suplimentare în POS și îmbunătățiri documentație
    Integrează automatizarea logicii pentru adăugarea liniilor suplimentare direct în Point of Sale, incluzând calcule dinamice de cantitate și consolidare automată a liniilor. Actualizează documentația pentru a reflecta noile funcționalități și îmbunătățește claritatea descrierilor README. Incrementarea versiunii modulului la `18.0.0.0.2` pentru a marca modificările.

--- deltatech_sale_add_extra_line_pos/static/src/js/models.esm.js
+++ deltatech_sale_add_extra_line_pos/static/src/js/models.esm.js
@@ -1,52 +1,61 @@
 /** @odoo-module **/
 
-import {Order, Orderline} from "@point_of_sale/app/store/models";
+import {PosOrder} from "@point_of_sale/app/models/pos_order";
+import {PosOrderline} from "@point_of_sale/app/models/pos_order_line";
+import {PosStore} from "@point_of_sale/app/store/pos_store";
 import {patch} from "@web/core/utils/patch";
 
-patch(Orderline.prototype, {
-    set_quantity(...args) {
-        const res = super.set_quantity(...args);
-        if (this.product.extra_product_id) {
-            const extra_product_id = this.pos.db.get_product_by_id(this.product.extra_product_id[0]);
-            if (extra_product_id) {
-                this.order.add_extra_product(extra_product_id);
+patch(PosStore.prototype, {
+    async addLineToCurrentOrder(vals, opt = {}, configure = true) {
+        await super.addLineToCurrentOrder(vals, opt, configure);
+
+        const product = vals.product_id;
+        const extra_product_id = product.product_tmpl_id.extra_product_id;
+
+        if (extra_product_id) {
+            const order = this.get_order();
+            const extra_line = order.add_extra_product(extra_product_id);
+            if (!extra_line) {
+                let qty = vals.qty || 1;
+                qty *= product.product_tmpl_id.extra_qty;
+                this.addLineToCurrentOrder({product_id: extra_product_id, qty: qty});
             }
         }
-        return res;
     },
 });
 
-patch(Order.prototype, {
-    add_product(product, ...args) {
-        const res = super.add_product(product, ...args);
-        if (product.extra_product_id) {
-            const extra_product_id = this.pos.db.get_product_by_id(product.extra_product_id[0]);
-            if (extra_product_id) {
-                this.add_extra_product(extra_product_id);
-            }
-        }
-        return res;
-    },
+patch(PosOrder.prototype, {
     add_extra_product(extra_product_id) {
         let extra_line = false;
         let qty = 0;
-        let options = {};
-        for (const line of this.get_orderlines()) {
-            if (line.product.extra_product_id) {
-                if (line.product.extra_product_id[0] === extra_product_id.id) {
-                    qty += line.quantity * line.product.extra_qty;
+
+        for (const line of this.lines) {
+            const line_extra_product = line.product_id.product_tmpl_id.extra_product_id;
+            if (line_extra_product) {
+                if (line_extra_product.id === extra_product_id.id) {
+                    qty += line.qty * line.product_id.product_tmpl_id.extra_qty;
                 }
             }
-            if (line.product.id === extra_product_id.id) {
-                line.quantity = 0;
+            if (line.product_id.id === extra_product_id.id) {
+                line.qty = 0;
                 extra_line = line;
             }
         }
         if (extra_line !== false) {
-            extra_line.set_quantity(qty);
-        } else {
-            options = {quantity: qty};
-            this.add_product(extra_product_id, options);
+            extra_line.set_quantity(qty, true);
         }
+        return extra_line;
+    },
+});
+
+patch(PosOrderline.prototype, {
+    set_quantity(quantity, keep_price) {
+        const res = super.set_quantity(quantity, keep_price);
+        const extra_product_id = this.product_id.product_tmpl_id.extra_product_id;
+        if (extra_product_id) {
+            const order = this.order_id;
+            order.add_extra_product(extra_product_id);
+        }
+        return res;
     },
 });
