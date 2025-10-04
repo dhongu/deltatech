55907153348ce7e24e69e53e86e0a4a35a3b25c0
Dorin Hongu <dhongu@gmail.com>
2025-10-04 09:17:30 +0300

    Actualizează referințele la `context` cu `env.context`
    Înlocuiește utilizarea `self._context` cu `self.env.context` în întreg codul pentru consistență și respectarea standardelor Odoo. Aceste modificări nu afectează funcționalitatea, dar îmbunătățesc lizibilitatea și alinierea codului la bunele practici.

--- deltatech_sale_pallet/models/invoice.py
+++ deltatech_sale_pallet/models/invoice.py
@@ -29,7 +29,7 @@ class AccountInvoice(models.Model):
             "pivot_measures": ["qty_delivered", "qty_invoiced", "price_unit"],
             "pivot_column_groupby": [],
             "pivot_row_groupby": ["product_id", "price_unit"],
-            "active_id": self._context.get("active_id"),
+            "active_id": self.env.context.get("active_id"),
             "active_model": "sale.report",
         }
         # action['target'] = 'new'
