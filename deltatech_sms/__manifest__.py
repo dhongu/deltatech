6a49a8718045a37dfbd12c4991c408bdffc8a294
Dorin Hongu <dhongu@gmail.com>
2025-10-04 09:05:26 +0300

    [IMP] deltatech_sms: actualizează versiunea și curăță codul
    S-a incrementat versiunea modulului la 19.0.1.0.1 pentru a reflecta noile modificări. În plus, a fost eliminată definiția redundantă a modulei în fișierul JavaScript, optimizând astfel codul.

--- deltatech_sms/__manifest__.py
+++ deltatech_sms/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Deltatech SMS",
     "summary": "Send SMS to custom endpoint",
-    "version": "18.0.1.0.1",
+    "version": "19.0.1.0.1",
     "author": "Terrabit, Dorin Hongu, Dan Stoica",
     "website": "https://www.terrabit.ro",
     "category": "Extra Tools",
