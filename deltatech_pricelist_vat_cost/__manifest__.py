615f6afa75b1427b1c2f79c9ab4d8378aabb9d40
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2025-10-06 13:25:12 +0300

    [18.0][UPD] deltatech_pricelist_vat_cost

--- deltatech_pricelist_vat_cost/__manifest__.py
+++ deltatech_pricelist_vat_cost/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Base pricelist on cost with vat",
     "summary": "Base pricelist on cost with vat",
-    "version": "18.0.0.0.1",
+    "version": "18.0.0.0.2",
     "author": "Terrabit, Voicu Stefan",
     "website": "https://www.terrabit.ro",
     "category": "Generic Modules",
