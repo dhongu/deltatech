d66922f4e7ae57977e82ead8e96b510df9e588ca
Dorin Hongu <dhongu@gmail.com>
2025-10-16 09:49:42 +0300

    Actualizează versiunea modulului la 18.0.1.0.1
    Modifică versiunea în fișierul `__manifest__.py` pentru a reflecta upgrade-ul la versiunea 18.0.1.0.1. Această actualizare asigură compatibilitatea cu noile schimbări din fluxul de dezvoltare.

--- deltatech_purchase_add_extra_line/__manifest__.py
+++ deltatech_purchase_add_extra_line/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Purchase Add Extra Line",
     "summary": "Purchase Add Extra Line",
-    "version": "17.0.1.0.1",
+    "version": "18.0.1.0.1",
     "category": "Sales",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
