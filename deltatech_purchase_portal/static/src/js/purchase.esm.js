adfb222895de21393c7765e9d7b3c3bdeb038386
Dorin Hongu <dhongu@gmail.com>
2025-10-20 14:13:00 +0300

    [ADD] Introduce `deltatech_purchase_portal` module
    ---
    
    Added a new module to enhance vendor interaction via the purchase portal. Vendors can now update product prices and descriptions directly through the portal. Includes new views, controllers, models, assets, and documentation.

---
+++ deltatech_purchase_portal/static/src/js/purchase.esm.js
@@ -0,0 +1,61 @@
+/** @odoo-module */
+import PublicWidget from "@web/legacy/js/public/public_widget";
+
+function debounce(fn, delay) {
+    let t;
+    return function (...args) {
+        clearTimeout(t);
+        t = setTimeout(() => fn.apply(this, args), delay);
+    };
+}
+
+export const PurchasePrice = PublicWidget.Widget.extend({
+    selector: ".o-purchase-price",
+    init() {
+        this._super(...arguments);
+        this.rpc = this.bindService("rpc");
+        this._onChange = debounce(this._onChange.bind(this), 500);
+    },
+    start() {
+        this.el.addEventListener("change", this._onChange);
+        this.el.addEventListener("input", this._onChange);
+        return this._super(...arguments);
+    },
+    _onChange() {
+        const accessToken = this.el.dataset.accessToken || this.el.dataset.access_token || this.el.getAttribute("data-access-token");
+        const orderId = this.el.dataset.orderId || this.el.getAttribute("data-order-id");
+        const lineId = this.el.dataset.lineId || this.el.getAttribute("data-line-id");
+        const value = this.el.value;
+        if (!orderId || !accessToken || !lineId) return;
+        const payload = {};
+        payload[`price_${lineId}`] = value;
+        this.rpc(`/my/purchase/${orderId}/update_price_note?access_token=${accessToken}`, payload);
+    },
+});
+
+export const PurchaseProductName = PublicWidget.Widget.extend({
+    selector: ".o-purchase-product-name",
+    init() {
+        this._super(...arguments);
+        this.rpc = this.bindService("rpc");
+        this._onChange = debounce(this._onChange.bind(this), 500);
+    },
+    start() {
+        this.el.addEventListener("change", this._onChange);
+        this.el.addEventListener("input", this._onChange);
+        return this._super(...arguments);
+    },
+    _onChange() {
+        const accessToken = this.el.dataset.accessToken || this.el.dataset.access_token || this.el.getAttribute("data-access-token");
+        const orderId = this.el.dataset.orderId || this.el.getAttribute("data-order-id");
+        const lineId = this.el.dataset.lineId || this.el.getAttribute("data-line-id");
+        const value = this.el.value;
+        if (!orderId || !accessToken || !lineId) return;
+        const payload = {};
+        payload[`name_${lineId}`] = value;
+        this.rpc(`/my/purchase/${orderId}/update_price_note?access_token=${accessToken}`, payload);
+    },
+});
+
+PublicWidget.registry.PurchasePrice = PurchasePrice;
+PublicWidget.registry.PurchaseProductName = PurchaseProductName;
