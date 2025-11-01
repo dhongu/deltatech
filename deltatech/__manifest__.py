b79cf9c7f6915e10e69fbee63db035246a5c393c
Dorin Hongu <dhongu@gmail.com>
2025-11-01 14:24:25 +0200

    [IMP] Actualizează versiunea modulului `deltatech`
    Modifică versiunea din `18.0.1.0.1` în `19.0.1.0.1` în fișierul manifest. Această actualizare reflectă schimbările și progresul modulului conform noilor implementări și îmbunătățiri.

--- deltatech/__manifest__.py
+++ deltatech/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Deltatech",
-    "version": "18.0.1.0.1",
+    "version": "19.0.1.0.1",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "summary": "Generic module",
