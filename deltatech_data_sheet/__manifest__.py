26fe22488f3c0ee0fd43e500b7e0cd9a5a149e07
Dorin Hongu <dhongu@gmail.com>
2025-11-01 20:44:50 +0200

    Actualizează versiunea modulului Data Sheet
    S-a modificat versiunea în fișierul `__manifest__.py` de la 18.0.1.0.0 la 19.0.1.0.0 pentru a reflecta noile actualizări sau schimbări structurale aduse modulului.

--- deltatech_data_sheet/__manifest__.py
+++ deltatech_data_sheet/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Product Data Sheet",
     "summary": "Data Sheet",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "category": "Administration",
