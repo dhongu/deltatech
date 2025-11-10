feeb2270ade27fb8756b29a8a7398de378a7ed3e
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2025-11-10 09:24:39 +0200

    [19.0][UPD] reparare trimitere mail

--- deltatech_business_process/__manifest__.py
+++ deltatech_business_process/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Business process",
     "summary": "Business process",
-    "version": "19.0.1.4.5",
+    "version": "19.0.1.4.6",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "license": "OPL-1",
