8f09e3851f90513afa1e2d40562e2596826f09f6
Dan Stoica <danila@terrabit.ro>
2025-11-11 15:58:58 +0200

    remove tag dependency (#2209)

--- deltatech_followup/__manifest__.py
+++ deltatech_followup/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Invoice Followup",
     "summary": "Simple invoice followup, with automatic e-mails",
-    "version": "18.0.0.1.0",
+    "version": "18.0.0.1.1",
     "author": "Terrabit, Dan Stoica",
     "website": "https://www.terrabit.ro",
     "support": "odoo@terrabit.ro",
