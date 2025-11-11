3361d560c73e3a56c92ba2793263deeed3033101
danila12 <danila@terrabit.ro>
2025-11-11 15:42:13 +0200

    remove tag dependency

--- deltatech_followup/wizard/followup_send.py
+++ deltatech_followup/wizard/followup_send.py
@@ -84,7 +84,7 @@ class FollowupSendWizard(models.TransientModel):
                             continue
                         invoices_content = ""
                         for invoice in invoices_to_process:
-                            crt_row = Template(followup.invoice_html).substitute(
+                            crt_row = Template(followup.invoice_html or "").substitute(
                                 number=invoice.name,
                                 payment_term_id=invoice.invoice_payment_term_id,
                                 date_invoice=invoice.invoice_date.strftime(lang_id.date_format),
@@ -97,48 +97,44 @@ class FollowupSendWizard(models.TransientModel):
                             )
                             invoices_content += crt_row
                             partner_debit += invoice.amount_residual
-                        email_values = {}
-                        if "[invoices]" in followup.mail_template.body_html:
+                        # email_values = {}
+                        override_partner_id = None
+                        get_param = self.env["ir.config_parameter"].sudo().get_param
+                        override_value = safe_eval(get_param("followup.override_partner_id", "False"))
+                        if override_value:
+                            try:
+                                override_partner_id = int(override_value)
+                            except ValueError:
+                                override_partner_id = None
+
                             # todo: de corectat metoda de transmitere email
-                            mail_values = followup.mail_template.with_context(
-                                template_preview_lang=partner.lang
-                            )._generate_template(
-                                [partner.id],
-                                [
-                                    "subject",
-                                    "body_html",
-                                    "email_from",
-                                    "email_to",
-                                    "partner_to",
-                                    "email_cc",
-                                    "reply_to",
-                                    "scheduled_date",
-                                ],
-                            )
-                            new_body = mail_values[partner.id]["body_html"]
-                            get_param = self.env["ir.config_parameter"].sudo().get_param
-                            override_id = safe_eval(get_param("followup.override_partner_id", "False"))
-                            # override_id = (
-                            #     self.env["ir.config_parameter"]
-                            #     .sudo()
-                            #     .get_param("followup.override_partner_id", default=False)
-                            # )
-                            if override_id:
-                                try:
-                                    partner_id = int(override_id)
-                                except ValueError:
-                                    partner_id = partner.id
-                            else:
-                                partner_id = partner.id
-                            body = new_body.replace("[invoices]", invoices_content)
-                            body = body.replace("${object.name}", partner.name)
-                            body = body.replace("$total_debit", f"{partner_debit:,.2f}")
-                            body = body.replace("$total_all_debit", f"{partner_all_debit:,.2f}")
-                            body = body.replace("$total_due_debit", f"{partner_due_debit:,.2f}")
-                            body = body.replace("$currency", inv_currency.name)
-                            body = html.unescape(body)
-                            email_values = {
-                                "body_html": body,
-                                "recipient_ids": [(4, partner_id)],
-                            }
+                        mail_values = followup.mail_template.with_context(
+                            template_preview_lang=partner.lang
+                        )._generate_template(
+                            [partner.id],
+                            [
+                                "subject",
+                                "body_html",
+                                "email_from",
+                                "email_to",
+                                "partner_to",
+                                "email_cc",
+                                "reply_to",
+                                "scheduled_date",
+                            ],
+                        )
+                        new_body = mail_values[partner.id]["body_html"]
+                        body = new_body.replace("${object.name}", partner.name)
+                        body = body.replace("$total_debit", f"{partner_debit:,.2f}")
+                        body = body.replace("$total_all_debit", f"{partner_all_debit:,.2f}")
+                        body = body.replace("$total_due_debit", f"{partner_due_debit:,.2f}")
+                        body = body.replace("$currency", inv_currency.name)
+                        if "[invoices]" in followup.mail_template.body_html:
+                            body = body.replace("[invoices]", invoices_content)
+                        body = html.unescape(body)
+                        email_values = {
+                            "body_html": body,
+                        }
+                        if override_partner_id:
+                            email_values["recipient_ids"] = [(6, 0, [override_partner_id])]
                         followup.mail_template.send_mail(partner.id, False, False, email_values)
