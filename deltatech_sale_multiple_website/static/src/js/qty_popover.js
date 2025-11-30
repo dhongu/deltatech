eebf853f1b604d4bcc26a61df73cbd2034efc2fb
Popa Marius Daniel <105748764+danyyy10@users.noreply.github.com>
2025-11-30 15:45:46 +0200

    Refactor JavaScript and XML templates for improved readability and consistency

--- deltatech_sale_multiple_website/static/src/js/qty_popover.js
+++ deltatech_sale_multiple_website/static/src/js/qty_popover.js
@@ -3,20 +3,20 @@
 import publicWidget from "@web/legacy/js/public/public_widget";
 
 publicWidget.registry.QtyInfoPopover = publicWidget.Widget.extend({
-    selector: '#product_qty_restrictions',
-    
+    selector: "#product_qty_restrictions",
+
     start: function () {
         var self = this;
         // Use Bootstrap from window object (loaded by Odoo)
         var Popover = window.Popover || (window.bootstrap && window.bootstrap.Popover);
         if (Popover) {
-            this.$('.qty-info-popover').each(function() {
+            this.$(".qty-info-popover").each(function () {
                 new Popover(this);
             });
         } else {
             // Fallback: use jQuery popover if available
-            if (this.$('.qty-info-popover').popover) {
-                this.$('.qty-info-popover').popover();
+            if (this.$(".qty-info-popover").popover) {
+                this.$(".qty-info-popover").popover();
             }
         }
         return this._super.apply(this, arguments);
