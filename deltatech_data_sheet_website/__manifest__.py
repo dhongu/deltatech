b2f460895a8d50865429ace0c4e6b949e9d74f2c
Dorin Hongu <dhongu@gmail.com>
2025-12-01 09:52:11 +0200

    [IMP] Actualizează versiunea modulului deltatech_data_sheet_website
    Modifică versiunea modulului de la 18.0.1.0.0 la 19.0.1.0.0 în fișierul manifest. Această actualizare reflectă modificările din cadrul ciclului curent de dezvoltare.

--- deltatech_data_sheet_website/__manifest__.py
+++ deltatech_data_sheet_website/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Product Data Sheet Website",
     "summary": "Data Sheet",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "author": "Terrabit, Dorin Hongu",
     "license": "OPL-1",
     "website": "https://www.terrabit.ro",
