f9f5f419421010e42656180275d6a7cd811375ca
Dorin Hongu <dhongu@gmail.com>
2026-01-07 09:24:48 +0200

    [IMP] Actualizează versiunea și dependințele modulului
    Modifică versiunea modulului `deltatech_putaway_strategy` la `18.0.1.0.2` și adaugă secțiunea `[project]` în fișierul `pyproject.toml`. Această actualizare îmbunătățește claritatea structurii și compatibilitatea pentru versiuni mai recente.

--- deltatech_putaway_strategy/__manifest__.py
+++ deltatech_putaway_strategy/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Deltatech Putaway Strategy",
-    "version": "17.0.1.0.2",
+    "version": "18.0.1.0.2",
     "summary": "Location capacities and enhanced putaway strategy for Inventory",
     "author": "Deltatech, Terrabit",
     "website": "https://www.terrabit.ro",
