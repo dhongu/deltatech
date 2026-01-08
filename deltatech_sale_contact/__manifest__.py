75d58c170451bbe8a67718670278f064063ea207
Dorin Hongu <dhongu@gmail.com>
2026-01-08 11:35:49 +0200

    19.0 mig deltatech sale contact (#2270)
    * [18.0][UPD] deltatech_Sale_contact
    
    * Uniformizează formatările în fișierele README
    
    Corectează și aliniază formatările din fișierele README pentru a menține un stil consistent. Modificările includ ajustări de indentare, aliniamente și corecturi minore pentru claritate și lizibilitate.
    
    * pre-commit
    
    * Actualizează formatarea fișierelor README
    
    S-au corectat spațierile, alinierea și formatarea listelor în multiple fișiere README din modulele Odoo. Aceste modificări îmbunătățesc lizibilitatea și claritatea conținutului fără să afecteze funcționalitatea.
    
    * Adaugă script pentru copierea imaginilor în module
    
    A fost adăugat un script pentru automatizarea copierii imaginilor principale în toate modulele `deltatech`. Această modificare asigură consistența fișierelor de descriere și reduce erorile manuale.
    
    * Elimină definirea licenței din fișierele de documentație
    
    A fost eliminat indicatorul de licență OPL-1 din fișierele README și HTML ale mai multor module. Modificările asigură consistența documentației cu politicile curente ale proiectului.
    
    * [IMP] Adaugă metadate pentru proiecte și elimină dependințe
    
    Introduce informații de metadate `project` în fișierele `pyproject.toml` pentru fiecare modul din folderul `deltatech`. Șterge fișierul `requirements.txt`, consolidând gestionarea dependințelor în cadrul fiecărui modul. Aceste schimbări standardizează structura proiectului și îmbunătățesc gestionarea dependințelor.
    
    * [IMP] Actualizează versiunea modulului în manifest
    
    Modifică versiunea modulului `deltatech_sale_contact` la `19.0.1.0.21` pentru a reflecta compatibilitatea cu Odoo 19.0. Această actualizare asigură conformitatea cu politica de versionare și menține claritatea în gestionarea versiunilor.
    
    * [ADD] Adaugă teste unitare pentru `deltatech_sale_contact`
    
    Introduce o suită de teste unitare pentru validarea comportamentului `contact_default`, domeniilor definite pe parteneri și funcționalitățile legate de comenzile de vânzare și facturi. Testele sporesc acoperirea codului și asigură funcționarea corectă a domeniilor și metodelor specifice partenerilor.
    
    ---------
    
    Co-authored-by: VoicuStefan2001 <stefanvoicu2009@gmail.com>

---
+++ deltatech_sale_contact/__manifest__.py
@@ -0,0 +1,22 @@
+# ©  2008-2023 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+
+{
+    "name": "Sale Contact",
+    "summary": "Limit contacts insale order",
+    "version": "19.0.1.0.21",
+    "category": "Sales",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "depends": ["sale", "contacts", "deltatech_contact", "account"],
+    "license": "OPL-1",
+    "data": [
+        "views/res_partner_view.xml",
+        # "views/sale_order_view.xml",
+    ],
+    "development_status": "Beta",
+    "images": ["static/description/main_screenshot.png"],
+    "maintainers": ["dhongu"],
+}
