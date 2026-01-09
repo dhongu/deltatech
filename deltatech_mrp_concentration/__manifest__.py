68639a87e8a85812bb6c5a5ad80dd3a3d9822800
Dorin Hongu <dhongu@gmail.com>
2026-01-09 08:25:14 +0200

    [IMP] Actualizează versiunea modulului la 19.0.1.0.1
    Modificarea versiunii reflectă compatibilitatea modulului `deltatech_mrp_concentration` cu Odoo 19. Actualizarea asigură alinierea cu politicile de versionare și menține consistența în cadrul proiectului.

--- deltatech_mrp_concentration/__manifest__.py
+++ deltatech_mrp_concentration/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "MRP Concentration",
     "summary": "MRP Concentration",
-    "version": "18.0.1.0.1",
+    "version": "19.0.1.0.1",
     "author": "Terrabit, Dorin Hongu",
     "license": "OPL-1",
     "website": "https://www.terrabit.ro",
