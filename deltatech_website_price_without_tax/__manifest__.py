5df8bc0eebed056af6d7c54ae833f5a459cd3005
Dorin Hongu <dhongu@gmail.com>
2026-01-10 01:56:44 +0200

    [IMP] Actualizează versiunea modulului deltatech_website_price_without_tax
    Incrementarea versiunii de la 18.0.1.0.2 la 19.0.1.0.2 în manifest reflectă compatibilitatea modulului cu ultima versiune și alinierea sa la standardele proiectului.

--- deltatech_website_price_without_tax/__manifest__.py
+++ deltatech_website_price_without_tax/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "eCommerce Product Price Without Tax",
     "summary": "Display Product Price Without Tax",
     "category": "Website",
-    "version": "18.0.1.0.2",
+    "version": "19.0.1.0.2",
     "author": "Terrabit, Dorin Hongu",
     "license": "OPL-1",
     "website": "https://www.terrabit.ro",
