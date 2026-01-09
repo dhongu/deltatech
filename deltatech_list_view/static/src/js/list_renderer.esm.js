ff5609253ac30b25c7a60af9c42461761b7b8e8c
Dorin Hongu <dhongu@gmail.com>
2026-01-09 09:45:04 +0200

    19.0 mig deltatech list view (#2279)
    * Add "List View Select Text" module
    
    Introduces functionality to prevent opening list items when selecting text. Includes JavaScript patching for list rendering, documentation, and necessary assets for module integration.
    
    * Update references to version 18.0 for deltatech_list_view
    
    Updated links across documentation, description, and README files to point to the 18.0 version of the deltatech_list_view module. Added the module
    
    * Corectează formatarea și adaugă funcționalități noi.
    
    Revizuirea fișierelor README pentru a corecta formatarea listelor și titlurilor. Adăugate îmbunătățiri în afișarea disponibilității stocului pe website, inclusiv ajustarea fonturilor și a mesajelor traduse. Extinsă funcționalitatea pentru timpii de livrare în magazinul online și consolidate template-urile XML asociate.```
    
    * [18.0][UPD] deltatech_alternative
    
    * [18.0][UPD] deltatech_list_view
    
    * Normalize whitespace in README content across modules
    
    S-au corectat aliniamentele și spațierile din fișierele README pentru a îmbunătăți lizibilitatea și consistența. Modificările nu afectează funcționalitatea codului, fiind limitate la documentație.
    
    * [18.0][UPD deltatech_picking_transit
    
    * Uniformizează formatările în fișierele README
    
    Corectează și aliniază formatările din fișierele README pentru a menține un stil consistent. Modificările includ ajustări de indentare, aliniamente și corecturi minore pentru claritate și lizibilitate.
    
    * pre-commit
    
    * Actualizează formatarea fișierelor README
    
    S-au corectat spațierile, alinierea și formatarea listelor în multiple fișiere README din modulele Odoo. Aceste modificări îmbunătățesc lizibilitatea și claritatea conținutului fără să afecteze funcționalitatea.
    
    * Adaugă script pentru copierea imaginilor în module
    
    A fost adăugat un script pentru automatizarea copierii imaginilor principale în toate modulele `deltatech`. Această modificare asigură consistența fișierelor de descriere și reduce erorile manuale.
    
    * Elimină definirea licenței din fișierele de documentație
    
    A fost eliminat indicatorul de licență OPL-1 din fișierele README și HTML ale mai multor module. Modificările asigură consistența documentației cu politicile curente ale proiectului.
    
    * [IMP] Adaugă metadate pentru proiecte și elimină dependințe
    
    Introduce informații de metadate `project` în fișierele `pyproject.toml` pentru fiecare modul din folderul `deltatech`. Șterge fișierul `requirements.txt`, consolidând gestionarea dependințelor în cadrul fiecărui modul. Aceste schimbări standardizează structura proiectului și îmbunătățesc gestionarea dependințelor.
    
    * [IMP] Actualizează versiuni și îmbunătățește configurațiile
    
    Actualizată versiunea modulului `deltatech_list_view` la `19.0.1.0.1` pentru compatibilitate cu Odoo 19.0. Revizuită configurația fișierului `.pre-commit-config.yaml`, incluzând actualizarea hook-urilor și schimbarea versiunii pentru `pylint-odoo` la `v9.2.0`. De asemenea, s-au uniformizat comentariile din modulele JavaScript pentru o consistență mai bună.
    
    ---------
    
    Co-authored-by: VoicuStefan2001 <stefanvoicu2009@gmail.com>

---
+++ deltatech_list_view/static/src/js/list_renderer.esm.js
@@ -0,0 +1,21 @@
+/* @odoo-module */
+
+import {ListRenderer} from "@web/views/list/list_renderer";
+import {patch} from "@web/core/utils/patch";
+
+patch(ListRenderer.prototype, {
+    onCellClicked() {
+        if (this.getSelectedText() === "") {
+            super.onCellClicked(...arguments);
+        }
+    },
+
+    getSelectedText() {
+        if (window.getSelection) {
+            return window.getSelection().toString();
+        } else if (document.selection) {
+            return document.selection.createRange().text;
+        }
+        return "";
+    },
+});
