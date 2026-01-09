8cef3f8790d78d9cdc8438b5d594dfb6cde0ffff
Dorin Hongu <dhongu@gmail.com>
2026-01-09 13:37:04 +0200

    [IMP] deltatech_fast_sale: pre-commit stuff

--- deltatech_fast_sale/__manifest__.py
+++ deltatech_fast_sale/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Fast Sale",
-    "version": "18.0.1.0.2",
+    "version": "19.0.1.0.2",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "summary": "Vanzare rapida",
