58d3f8258105fedd0f05699395a69e1df53f3904
Dorin Hongu <dhongu@gmail.com>
2026-01-11 06:33:10 +0200

    Actualizează versiunea modulului `deltatech_website_fixed_price`.
    Incrementarea versiunii la `19.0.0.0.0` reflectă compatibilitatea cu Odoo 19.0. Nu au fost efectuate alte modificări funcționale sau structurale.

--- deltatech_website_fixed_price/__manifest__.py
+++ deltatech_website_fixed_price/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "eCommerce Display Fixed Price As Discount",
     "category": "Website",
     "summary": "Display the cut off price for fixed pricelist rule if the price is lower than the list price",
-    "version": "18.0.0.0.0",
+    "version": "19.0.0.0.0",
     "license": "OPL-1",
     "author": "Terrabit, Voicu Stefan",
     "website": "https://www.terrabit.ro",
