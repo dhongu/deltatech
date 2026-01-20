4ad35dec9181bc72f1e652ead048d651e17fccb3
Dorin Hongu <dhongu@gmail.com>
2026-01-20 12:20:03 +0200

    [ADD] Funcție pentru căutarea numelui bazată pe alternative
    Adaugă metoda `_search_display_name` pentru modelele `product.template` și `product.product`, extinzând căutarea numelor să includă produsele alternative. În plus, actualizează versiunile modulelor implicate pentru a reflecta aceste modificări.

--- deltatech_alternative/__manifest__.py
+++ deltatech_alternative/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Products Alternative",
-    "version": "18.0.2.1.2",
+    "version": "18.0.2.1.3",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "summary": "Alternative product codes",
--- deltatech_stock_inventory/__manifest__.py
+++ deltatech_stock_inventory/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Stock Inventory",
     "summary": "Inventory Old Method",
-    "version": "18.0.2.3.7",
+    "version": "18.0.2.3.8",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "category": "Warehouse",
