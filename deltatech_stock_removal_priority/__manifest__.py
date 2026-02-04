b89e93619635b93abac1748153071f5abfcec9b7
Dorin Hongu <dhongu@gmail.com>
2026-02-04 20:20:33 +0200

    [ADD] Improve removal priority with default values and category rules
    Enhanced the stock quant removal priority calculation by adding default priority values and category-based fallback rules. Updated the logic to account for missing product-specific rules and included new test cases to validate the changes. Incremented the module version to 17.0.1.0.3.

--- deltatech_stock_removal_priority/__manifest__.py
+++ deltatech_stock_removal_priority/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Stock Removal Location by Priority",
     "summary": "Stock Removal Location by Priority",
-    "version": "17.0.1.0.2",
+    "version": "17.0.1.0.3",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "category": "Warehouse",
