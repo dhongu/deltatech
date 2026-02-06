02072851117212b116c26a8d150887d410a9ffa7
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2026-02-06 11:56:45 +0200

    redenumire functie pentru evitare conflicte

--- deltatech_stock_picking_activity_report/__manifest__.py
+++ deltatech_stock_picking_activity_report/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Stock Picking Activity Report",
-    "version": "18.0.0.0.1",
+    "version": "18.0.0.0.2",
     "author": "Terrabit, Voicu Stefan",
     "website": "https://www.terrabit.ro",
     "category": "Inventory",
