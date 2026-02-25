9a734c3add0319b75e182fae52c2c13897edbea0
Dorin Hongu <dhongu@gmail.com>
2026-02-25 17:35:05 +0200

    18.0 deltatech account edi ubl advice (#2368)
    * [IMP] Refactorizare și redenumire modul UBL Advice
    
    Redenumirea modulului din `deltatech_account_edi_ub_advice` în `deltatech_account_edi_ubl_advice` pentru consistență și claritate. Adaugă îmbunătățiri la generarea documentelor UBL prin gestionarea mai bună a câmpului `DespatchDocumentReference` și refactorizează modele și metode pentru alinierea la formatul UBL BIS3. Fișierele statice și clasele afectate au fost actualizate corespunzător.
    
    * [IMP] Activează logică pentru UBL și actualizează metode
    
    Activează clasa `AccountEdiXmlUBL20` și adaugă logică pentru câmpul `DespatchDocumentReference`. Refactorizează metodele de export și generare noduri pentru alinierea cu formatul UBL BIS3.
    
    * [IMP] Ajustează logica pentru generarea referințelor la documente
    
    Refactorizează metoda `_get_document_template` pentru a adăuga suport condiționat pentru câmpul `DespatchDocumentReference`. Această modificare îmbunătățește alinierea cu structura formatului UBL BIS3.

---
+++ deltatech_account_edi_ubl_advice/models/account_edi_ubl.py
@@ -0,0 +1,65 @@
+# ©  2024 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+
+from odoo import models
+
+
+class AccountEdiXmlUBL20(models.AbstractModel):
+    _inherit = "account.edi.xml.ubl_20"
+
+    # class AccountEdiXmlUBLBIS3(models.AbstractModel):
+    #     _inherit = "account.edi.xml.ubl_bis3"
+
+    def _export_invoice_vals(self, invoice):
+        # old helper
+        vals_list = super()._export_invoice_vals(invoice)
+
+        pickings = self.env["stock.picking"]
+        for line in invoice.invoice_line_ids:
+            for sale_line in line.sale_line_ids:
+                for move in sale_line.move_ids:
+                    if move.picking_id.state == "done":
+                        pickings |= move.picking_id
+        if pickings:
+            names = pickings.mapped("name")
+            vals_list["vals"]["despatch_advice"] = ", ".join(names)
+
+        return vals_list
+
+    def _export_invoice_new(self, invoice):
+        res = super()._export_invoice_new(invoice)
+        return res
+
+    def _add_invoice_header_nodes(self, document_node, vals):
+        res = super()._add_invoice_header_nodes(document_node, vals)
+        invoice = vals["invoice"]
+        pickings = self.env["stock.picking"]
+        for line in invoice.invoice_line_ids:
+            for sale_line in line.sale_line_ids:
+                for move in sale_line.move_ids:
+                    if move.picking_id.state == "done":
+                        pickings |= move.picking_id
+        if pickings:
+            names = pickings.mapped("name")
+            vals["despatch_advice"] = ", ".join(names)
+            document_node["cac:DespatchDocumentReference"] = {"cbc:ID": {"_text": ", ".join(names)}}
+        return res
+
+    def _get_document_template(self, vals):
+        res = super()._get_document_template(vals)
+        if vals["document_type"] == "invoice":
+            new_res = {}
+            for key, _value in res.items():
+                new_res[key] = res[key]
+                if key == "cac:OrderReference":
+                    new_res["cac:DespatchDocumentReference"] = {"cbc:ID": {}}
+            res = new_res
+
+        return res
+
+    def _add_invoice_delivery_nodes(self, document_node, vals):
+        res = super()._add_invoice_delivery_nodes(document_node, vals)
+
+        return res
