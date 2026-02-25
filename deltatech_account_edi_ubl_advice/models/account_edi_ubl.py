92bc63bfa9bbf02c4df2e64f56a3ec5de19f7b2c
Dorin Hongu <dhongu@gmail.com>
2026-02-25 16:54:57 +0200

    [IMP] Activează logică pentru UBL și actualizează metode
    Activează clasa `AccountEdiXmlUBL20` și adaugă logică pentru câmpul `DespatchDocumentReference`. Refactorizează metodele de export și generare noduri pentru alinierea cu formatul UBL BIS3.

--- deltatech_account_edi_ubl_advice/models/account_edi_ubl.py
+++ deltatech_account_edi_ubl_advice/models/account_edi_ubl.py
@@ -5,12 +5,12 @@
 
 from odoo import models
 
-# class AccountEdiXmlUBL20(models.AbstractModel):
-#     _inherit = "account.edi.xml.ubl_20"
 
+class AccountEdiXmlUBL20(models.AbstractModel):
+    _inherit = "account.edi.xml.ubl_20"
 
-class AccountEdiXmlUBLBIS3(models.AbstractModel):
-    _inherit = "account.edi.xml.ubl_bis3"
+    # class AccountEdiXmlUBLBIS3(models.AbstractModel):
+    #     _inherit = "account.edi.xml.ubl_bis3"
 
     def _export_invoice_vals(self, invoice):
         # old helper
@@ -25,6 +25,7 @@ class AccountEdiXmlUBLBIS3(models.AbstractModel):
         if pickings:
             names = pickings.mapped("name")
             vals_list["vals"]["despatch_advice"] = ", ".join(names)
+
         return vals_list
 
     def _export_invoice_new(self, invoice):
@@ -33,10 +34,6 @@ class AccountEdiXmlUBLBIS3(models.AbstractModel):
 
     def _add_invoice_header_nodes(self, document_node, vals):
         res = super()._add_invoice_header_nodes(document_node, vals)
-        return res
-
-    def _add_invoice_delivery_nodes(self, document_node, vals):
-        res = super()._add_invoice_delivery_nodes(document_node, vals)
         invoice = vals["invoice"]
         pickings = self.env["stock.picking"]
         for line in invoice.invoice_line_ids:
@@ -47,4 +44,16 @@ class AccountEdiXmlUBLBIS3(models.AbstractModel):
         if pickings:
             names = pickings.mapped("name")
             vals["despatch_advice"] = ", ".join(names)
+            document_node["cac:DespatchDocumentReference"] = {"cbc:ID": {"_text": ", ".join(names)}}
+        return res
+
+    def _get_document_template(self, vals):
+        res = super()._get_document_template(vals)
+        if vals["document_type"] == "invoice":
+            res["cac:DespatchDocumentReference"] = {"cbc:ID": {}}
+        return res
+
+    def _add_invoice_delivery_nodes(self, document_node, vals):
+        res = super()._add_invoice_delivery_nodes(document_node, vals)
+
         return res
