853e0620970bc66b264790f1016b1a8970b32ffc
Dorin Hongu <dhongu@gmail.com>
2026-03-04 12:51:16 +0200

    [IMP] Ștergere cheie inutilă din `ir_config_parameter` (#2381)
    Elimină înregistrarea cu cheia `database_notification_banner` din tabelul `ir_config_parameter`. Această modificare curăță datele redundante și previne potențiale conflicte sau mesaje necorespunzătoare în timpul utilizării sistemului.

--- deltatech_test_system/data/neutralize.sql
+++ deltatech_test_system/data/neutralize.sql
@@ -2,3 +2,5 @@ INSERT INTO ir_config_parameter (key, value)
 VALUES ('database.is_neutralized', 'True')
 ON CONFLICT (key) DO UPDATE
 SET value = 'True';
+
+DELETE FROM ir_config_parameter WHERE key = 'database_notification_banner';
