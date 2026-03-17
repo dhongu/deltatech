ac62ac6e8771e7db7e06d15046c53966e8538def
Dan Stoica <danila@terrabit.ro>
2026-03-17 12:19:13 +0200

    fix js (#2400)

--- deltatech_website_delivery_and_payment/__manifest__.py
+++ deltatech_website_delivery_and_payment/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Delivery and Payment",
     "category": "Website",
     "summary": "eCommerce Delivery and Payment constrains",
-    "version": "18.0.2.1.4",
+    "version": "18.0.2.1.5",
     "author": "Terrabit, Dorin Hongu",
     "license": "OPL-1",
     "website": "https://www.terrabit.ro",
--- deltatech_website_delivery_and_payment/static/src/js/payment_form.esm.js
+++ deltatech_website_delivery_and_payment/static/src/js/payment_form.esm.js
@@ -1,44 +1,72 @@
 /** @odoo-module */
 
-// import WebsiteSaleCheckout from "@website_sale/js/checkout";
+import WebsiteSaleCheckout from "@website_sale/js/checkout";
 
-// Import "@website_sale/js/website_sale_delivery";
-// import publicWidget from "@web/legacy/js/public/public_widget";
+WebsiteSaleCheckout.include({
+    /**
+     * @override
+     */
+    async start() {
+        await this._super(...arguments);
+        const checkedRadio = this.el.querySelector('input[name="o_delivery_radio"]:checked');
+        if (checkedRadio) {
+            this._updatePaymentOptions(checkedRadio);
+        }
+    },
 
-// PublicWidget.registry.websiteSaleDelivery.include({
-//     start: function () {
-//         this.paymentOptions = document.querySelectorAll('input[name="o_payment_radio"]');
-//         return this._super.apply(this, ...arguments);
-//     },
-//
-//     _setEnablePaymentOption(radio, enabled) {
-//         const node = radio.closest('[name="o_payment_option"]');
-//         if (enabled) {
-//             node.classList.remove("d-none");
-//             node.classList.add("list-group-item");
-//         } else {
-//             node.classList.add("d-none");
-//             node.classList.remove("list-group-item");
-//             radio.checked = false;
-//         }
-//     },
-//
-//     _onCarrierClick: function (ev) {
-//         const input = ev.currentTarget.querySelector("input");
-//         // Data-acquirer-allowed-ids is a comma-separated list of acquirer ids : ex: "[1,2,3]"
-//         let acquirerAllowedIds = input.getAttribute("data-acquirer-allowed-ids");
-//         if (acquirerAllowedIds) {
-//             acquirerAllowedIds = acquirerAllowedIds.replace(/[\[\] ]/g, "").split(",");
-//         }
-//         for (const option of this.paymentOptions) {
-//             var isEnable = true;
-//             if (acquirerAllowedIds) {
-//                 const acquirerId = option.dataset.providerId;
-//                 isEnable = acquirerAllowedIds.includes(acquirerId);
-//             }
-//             this._setEnablePaymentOption(option, isEnable);
-//         }
-//
-//         this._super(...arguments);
-//     },
-// });
+    /**
+     * @override
+     */
+    async _selectDeliveryMethod(ev) {
+        await this._super(...arguments);
+        this._updatePaymentOptions(ev.currentTarget);
+    },
+
+    /**
+     * Update the payment options visibility based on the selected delivery carrier.
+     *
+     * @private
+     * @param {HTMLInputElement} deliveryRadio
+     * @returns {void}
+     */
+    _updatePaymentOptions(deliveryRadio) {
+        let acquirerAllowedIds = deliveryRadio.getAttribute("data-acquirer-allowed-ids");
+        if (acquirerAllowedIds) {
+            acquirerAllowedIds = JSON.parse(acquirerAllowedIds);
+        }
+
+        const paymentOptions = document.querySelectorAll('input[name="o_payment_radio"]');
+        for (const option of paymentOptions) {
+            let isVisible = true;
+            if (acquirerAllowedIds && acquirerAllowedIds.length > 0) {
+                const providerId = parseInt(option.dataset.providerId, 10);
+                isVisible = acquirerAllowedIds.includes(providerId);
+            }
+            this._setPaymentOptionVisibility(option, isVisible);
+        }
+    },
+
+    /**
+     * Show or hide a payment option.
+     *
+     * @private
+     * @param {HTMLInputElement} radio
+     * @param {Boolean} visible
+     * @returns {void}
+     */
+    _setPaymentOptionVisibility(radio, visible) {
+        const container = radio.closest('[name="o_payment_option"]');
+        if (container) {
+            if (visible) {
+                container.classList.remove("d-none");
+            } else {
+                container.classList.add("d-none");
+                if (radio.checked) {
+                    radio.checked = false;
+                    // Trigger change to let other scripts know it's unchecked if needed
+                    radio.dispatchEvent(new Event("change", {bubbles: true}));
+                }
+            }
+        }
+    },
+});
