9cbc768ba22e0b4e7b505ff181ae318e18238a9c
Dorin Hongu <dhongu@gmail.com>
2026-03-18 13:27:07 +0200

    17.0 deltatech queue job (#2404)
    * [ADD] Enhance queue job processing and external API integration
    
    Added support for external API-triggered queue job processing with detailed configuration options. Introduced settings for API key generation, batch size, and processing time, along with new methods for threaded execution and improved CRON synchronization. Updated manifest and documentation accordingly.
    
    * [ADD] Add controllers to deltatech_queue_job module
    
    Introduced a new `controllers` package in the `deltatech_queue_job` module to support upcoming functionality. Updated the manifest file to increment the version to 17.0.1.1.1, reflecting this structural enhancement.

--- deltatech_queue_job/controllers/__init__.py
+++ deltatech_queue_job/controllers/__init__.py
@@ -1,6 +1,5 @@
-# ©  2024 Deltatech
+# ©  2025 Deltatech
 #              Dorin Hongu <dhongu(@)gmail(.)com
 # See README.rst file on addons root folder for license details
 
-from . import models
-from . import controllers
+from . import main
