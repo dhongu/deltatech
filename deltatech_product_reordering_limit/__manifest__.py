f02c008c21839aa6dcdc094694cde676cda5c23a
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2026-03-23 16:05:45 +0200

    [18.0][ADD] deltatech_product_reordering_limit

---
+++ deltatech_product_reordering_limit/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Product Reordering Limit",
+    "summary": "Custom reordering limits for products",
+    "version": "18.0.1.0.0",
+    "author": "Terrabit, Voicu Stefan",
+    "website": "https://www.terrabit.ro",
+    "category": "Inventory",
+    "depends": ["product", "stock"],
+    "license": "OPL-1",
+    "data": [
+        "views/product_template_view.xml",
+    ],
+    "installable": True,
+    "development_status": "Beta",
+    "maintainers": ["VoicuStefan2001"],
+}
