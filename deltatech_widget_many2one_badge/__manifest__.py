78a6235366b0274a3a1b8b19d9fdf0f842de4cfa
Dorin Hongu <dhongu@gmail.com>
2026-03-25 10:37:29 +0200

    [IMP] deltatech_widget_many2one_badge: pre-commit stuff

--- deltatech_widget_many2one_badge/__manifest__.py
+++ deltatech_widget_many2one_badge/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Many2one Badge Widget",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "category": "Web",
     "summary": "Many2one field widget displayed as colored badge, similar to many2many_tags",
     "author": "Terrabit, Dorin Hongu",
