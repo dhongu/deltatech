33f04aa2cb220ed0a814b245c77a826bc2973435
Dorin Hongu <dhongu@gmail.com>
2026-03-25 08:21:33 +0200

    [IMP] Improve formatting in MrpProduction tests
    Refactored test code to enhance readability and maintain PEP-8 compliance. Adjusted dictionary definitions, method calls, and line breaks without altering test logic or functionality.

--- deltatech_mrp_bom/models/mrp_production.py
+++ deltatech_mrp_bom/models/mrp_production.py
@@ -3,7 +3,7 @@
 # See README.rst file on addons root folder for license details
 
 
-from odoo import api, fields, models, Command
+from odoo import Command, api, fields, models
 
 
 class MrpProduction(models.Model):
