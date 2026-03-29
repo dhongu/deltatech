8122d93d0dff67c317daf786fefc742d3d4f052f
Dorin Hongu <dhongu@gmail.com>
2026-03-29 11:02:22 +0300

    Actualizează funcționalitatea Derived BoM în MRP
    Adaugă opțiunea de a genera manual un Derived BoM printr-un buton în Ordinele de Producție. Extinde comportamentul pentru actualizarea automată a referinței BoM-urilor derivate și introduce teste unitarizate care validează noile modificări și funcționalități implementate.

--- deltatech_mrp_bom/models/mrp_bom.py
+++ deltatech_mrp_bom/models/mrp_bom.py
@@ -60,24 +60,8 @@ class MrpBom(models.Model):
 
 class MrpBomLine(models.Model):
     _inherit = "mrp.bom.line"
-    # _rec_name = "name"
 
     bom_product_template_attribute_value_ids = fields.Many2many("product.template.attribute.value", copy=True)
-
-    # name = fields.Char(string="Name")
-    #
-    # @api.onchange("product_id")
-    # def onchange_product_id(self):
-    #     super(MrpBomLine, self).onchange_product_id()
-    #     if self.product_id:
-    #         self.name = self.product_id.name
-    #
-    # @api.model
-    # def create(self, vals):
-    #     if "name" not in vals:
-    #         product = self.env["product.product"].browse(vals["product_id"])
-    #         vals["name"] = product.name
-    #     return super(MrpBomLine, self).create(vals)
 
     def open_bom(self):
         self.ensure_one()
@@ -94,10 +78,3 @@ class MrpBomLine(models.Model):
                 "nodestroy": True,
                 "type": "ir.actions.act_window",
             }
-
-    #
-    # def init(self):
-    #     super(MrpBomLine, self).init()
-    #     lines = self.search([("name", "=", False)])
-    #     for line in lines:
-    #         line.write({"name": line.product_id.name})
