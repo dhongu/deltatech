734e9ff08cea05b889322d9aa0b7ab073ed1a7c4
Dorin Hongu <dhongu@gmail.com>
2026-04-02 19:57:04 +0300

    Adaugă modulul `deltatech_pos_fix` pentru recalcularea corectă a totalurilor în POS
    Introduce un nou modul pentru corectarea tratamentului fiscal în POS atunci când o poziție fiscală remapează taxe incluse către taxe neincluse (ex. 0% reverse charge). Modulul aliniază comportamentul POS cu backend-ul Vânzări, asigurând consistența calculelor de preț și taxe.

---
+++ deltatech_pos_fix/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Deltatech POS Fix",
+    "version": "18.0.1.0.0",
+    "category": "Point of Sale",
+    "summary": "Fix POS total calculation when using tax-included fiscal position mapping",
+    "license": "LGPL-3",
+    "development_status": "Production/Stable",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "depends": ["point_of_sale"],
+    "data": [],
+    "assets": {
+        "point_of_sale._assets_pos": ["deltatech_pos_fix/static/src/app/models/pos_order_line.esm.js"],
+    },
+    "installable": True,
+}
