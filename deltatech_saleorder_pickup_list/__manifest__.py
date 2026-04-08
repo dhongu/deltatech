6b5da84d569e593cbd7cc4ba3d35a7066cfbeb17
Dorin Hongu <dhongu@gmail.com>
2026-04-08 05:43:16 +0300

    Actualizează versiunea modulului și adaugă resurse statice
    Actualizează versiunea la 19.0.1.0.0 în fișierul manifest pentru a reflecta compatibilitatea cu noua versiune Odoo. Adaugă o pictogramă pentru modul, îmbunătățind prezentarea vizuală în listările aplicațiilor.

--- deltatech_saleorder_pickup_list/__manifest__.py
+++ deltatech_saleorder_pickup_list/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Deltatech Sale Order Pickup List",
     "summary": "Pickup list report from sale order",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "author": "Terrabit, Dorin Hongu, Dan Stoica",
     "website": "https://www.terrabit.ro",
     "category": "Sales",
