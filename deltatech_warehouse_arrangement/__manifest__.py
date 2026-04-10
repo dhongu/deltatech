c24c5993792d6fa9266264ef6dd03a6c87382a42
Dorin Hongu <dhongu@gmail.com>
2026-04-10 07:18:09 +0300

    Actualizează versiunea modulului în manifest
    Incrementează versiunea din `18.0.0.1.2` la `19.0.0.1.2` în `__manifest__.py` pentru a reflecta actualizările efectuate. Modificarea menține conformitatea cu practicile de versiune Odoo și facilitează urmărirea schimbărilor.

--- deltatech_warehouse_arrangement/__manifest__.py
+++ deltatech_warehouse_arrangement/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Deltatech Warehouse Arrangement",
     "category": "Stock",
     "summary": "Manages warehouse locations, parallel to standard Odoo locations",
-    "version": "18.0.0.1.2",
+    "version": "19.0.0.1.2",
     "author": "Terrabit, Dan Stoica",
     "website": "https://www.terrabit.ro",
     "license": "OPL-1",
