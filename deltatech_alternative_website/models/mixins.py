7c08669a39c75a3952f39c812957329a6dd28dd1
Dorin Hongu <dhongu@gmail.com>
2026-04-20 12:07:31 +0300

    Elimină cod comentat inutil din `_search_build_domain`
    Eliminarea fragmentelor de cod comentat îmbunătățește lizibilitatea și mentenabilitatea funcției `_search_build_domain`. Modificarea asigură un cod mai curat și mai ușor de înțeles pentru dezvoltatori.

--- deltatech_alternative_website/models/mixins.py
+++ deltatech_alternative_website/models/mixins.py
@@ -16,4 +16,27 @@ class WebsiteSearchableMixin(models.AbstractModel):
             search = search.strip().split(" ")
             search = " ".join(s.strip() for s in search if s.strip())
 
+        # from odoo.osv import expression
+        # from odoo.tools import escape_psql
+        # domains = domain_list.copy()
+        # if search:
+        #     get_param = self.env["ir.config_parameter"].sudo().get_param
+        #     alternative_length_min = int(get_param("alternative.length_min", "3"))
+        #     fields_to_search_short = [
+        #         f for f in fields if f not in ["default_code", "alternative_code", "product_variant_ids.default_code"]
+        #     ]
+        #     for search_term in search.split(" "):
+        #         if len(search_term) < alternative_length_min:
+        #             # pentru siruri scurte de caractere nu se cauta in campurile de tip cod
+        #             fields_to_search = fields_to_search_short
+        #         else:
+        #             fields_to_search = fields
+        #
+        #         subdomains = [[(field, "ilike", escape_psql(search_term))] for field in fields_to_search]
+        #         if extra:
+        #             subdomains.append(extra(self.env, search_term))
+        #         if subdomains:
+        #             domains.append(expression.OR(subdomains))
+        # return expression.AND(domains)
+
         return super()._search_build_domain(domain_list, search, fields, extra)
