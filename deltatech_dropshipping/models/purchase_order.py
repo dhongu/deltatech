967b92f3befc89f77be7b59a9abf44a5134e305b
Dorin Hongu <dhongu@gmail.com>
2026-04-21 07:17:27 +0300

    Adaugă avertizări pentru prețurile de achiziție mai mari decât cele de vânzare
    Extinde funcționalitatea modulului `deltatech_dropshipping` prin adăugarea unei comparații automate între prețurile de achiziție și prețurile de vânzare fără taxe, afișând avertizări pentru diferențele nefavorabile. Adaugă teste unitare pentru validarea noii funcționalități și modifică interfața utilizatorului pentru a include un mesaj de avertizare în form-ul de comandă de achiziție. Incrementul versiunii reflectă aceste îmbunătățiri.

---
+++ deltatech_dropshipping/models/purchase_order.py
@@ -0,0 +1,57 @@
+# ©  2008-2021 Deltatech
+# See README.rst file on addons root folder for license details
+
+from odoo import _, api, fields, models
+
+
+class PurchaseOrder(models.Model):
+    _inherit = "purchase.order"
+
+    purchase_price_warning = fields.Html(compute="_compute_purchase_price_warning")
+
+    @api.depends(
+        "order_line.price_unit",
+        "order_line.taxes_id",
+        "order_line.sale_line_id.price_unit",
+        "order_line.sale_line_id.tax_id",
+    )
+    def _compute_purchase_price_warning(self):
+        for order in self:
+            warnings = []
+            for line in order.order_line:
+                if line.sale_line_id:
+                    # Prețul de achiziție din PO fără taxe
+                    taxes_p = line.taxes_id.compute_all(
+                        line.price_unit,
+                        line.order_id.currency_id,
+                        1.0,
+                        product=line.product_id,
+                        partner=order.partner_id,
+                    )
+                    purchase_price = taxes_p["total_excluded"]
+
+                    # Prețul de vânzare din SO fără taxe
+                    taxes_s = line.sale_line_id.tax_id.compute_all(
+                        line.sale_line_id.price_unit,
+                        line.sale_line_id.order_id.currency_id,
+                        1.0,
+                        product=line.sale_line_id.product_id,
+                        partner=line.sale_line_id.order_id.partner_id,
+                    )
+                    sale_price = taxes_s["total_excluded"]
+
+                    if purchase_price > sale_price:
+                        diff = purchase_price - sale_price
+                        warnings.append(
+                            _(
+                                "<li><strong>%(product)s</strong>: Purchase %(purchase)s > Sale %(sale)s (Diff: %(diff)s)</li>",
+                                product=line.product_id.display_name,
+                                purchase=purchase_price,
+                                sale=sale_price,
+                                diff=diff,
+                            )
+                        )
+            if warnings:
+                order.purchase_price_warning = "<ul>" + "".join(warnings) + "</ul>"
+            else:
+                order.purchase_price_warning = False
--- deltatech_dropshipping/tests/__init__.py
+++ deltatech_dropshipping/tests/__init__.py
@@ -2,3 +2,4 @@
 #              Dorin Hongu <dhongu(@)gmail(.)com
 # See README.rst file on addons root folder for license details
 from . import test_dropshipping
+from . import test_purchase_order
