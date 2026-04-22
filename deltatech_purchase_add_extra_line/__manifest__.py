7572ddda4eb361b60bae304efc4a59c03c2a6d17
Dorin Hongu <dhongu@gmail.com>
2026-04-22 14:59:54 +0300

    Adaugă gestionarea produselor suplimentare în comenzi de achiziție
    Extinde logica pentru produse suplimentare, incluzând verificări și sincronizări ale liniilor principale și suplimentare. Adaugă teste unitare pentru validarea funcționalității și incrementează versiunea modulului la **18.0.1.0.2** pentru a reflecta modificările.

--- deltatech_purchase_add_extra_line/__manifest__.py
+++ deltatech_purchase_add_extra_line/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Purchase Add Extra Line",
     "summary": "Purchase Add Extra Line",
-    "version": "18.0.1.0.1",
+    "version": "18.0.1.0.2",
     "category": "Sales",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
