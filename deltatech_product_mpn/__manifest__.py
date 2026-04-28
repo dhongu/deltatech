778f896ce1ed9780906d689860345a867d497d15
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2026-04-28 15:04:54 +0300

    [18.0][ADD] deltatech_product_mpn

---
+++ deltatech_product_mpn/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Product MPN",
+    "summary": "Add MPN to product",
+    "version": "18.0.0.0.0",
+    "author": "Terrabit, Voicu Stefan",
+    "license": "OPL-1",
+    "website": "https://www.terrabit.ro",
+    "category": "Sales",
+    "depends": ["product"],
+    "data": ["views/product_template_view.xml"],
+    "images": ["static/description/main_screenshot.png"],
+    "development_status": "Mature",
+    "maintainers": ["VoicuStefan2001"],
+}
