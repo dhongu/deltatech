5a225df14666f92a2a9aa898627402b8ca4622de
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2026-04-29 12:28:21 +0300

    module pentru website

---
+++ deltatech_website_sale_attribute_filter/__manifest__.py
@@ -0,0 +1,18 @@
+# © 2024 Deltatech
+# See README.rst file on addons root folder for license details
+{
+    "name": "Website Sale Attribute Filter",
+    "version": "18.0.0.0.0",
+    "category": "Website",
+    "summary": "Filter attribute values based on displayed products",
+    "author": "Terrabit, Voicu Stefan",
+    "website": "https://www.terrabit.ro",
+    "license": "OPL-1",
+    "depends": ["website_sale"],
+    "data": [
+        "views/templates.xml",
+    ],
+    "installable": True,
+    "development_status": "Beta",
+    "maintainers": ["VoicuStefan2001"],
+}
