b7c2cd8d61b03fa16c97a1794682e1e5a2ba835c
CosminAndreiCojocaru <cosminandreicojocaru@gmail.com>
2026-05-08 13:29:25 +0300

    [19.0][ADD] added module deltatech_website_rmv_sort

---
+++ deltatech_website_rmv_sort_new/__init__.py
@@ -0,0 +1,5 @@
+# ©  2008-2021 Deltatech
+#              Cojocaru Cosmin-Andrei <cosminandreicojocaru(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+
---
+++ deltatech_website_rmv_sort_new/__manifest__.py
@@ -0,0 +1,18 @@
+# ©  2008-2021 Deltatech
+#              Cojocaru Cosmin-Andrei <cosminandreicojocaru(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
+{
+    "name": "Deltatech website rmv sort new",
+    "version": "19.0.0.0.0",
+    "author": "Terrabit, Cojocaru Cosmin-Andrei",
+    "website": "https://www.terrabit.ro",
+    "summary": "Generic module",
+    "category": "Tools",
+    "depends": ["web", "base"],
+    "data": ["views/rmv_sort_new.xml"],
+    "license": "OPL-1",
+    "images": ["images/main_screenshot.png"],
+    "development_status": "Mature",
+    "maintainers": ["CosminAndreiCojocaru"],
+}
