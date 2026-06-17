9739f26df8fbc95cabab337a7e86bf72e35c82c6
Dorin Hongu <dhongu@gmail.com>
2026-06-17 17:49:53 +0300

    [IMP] Actualizare șablon raport și versiune modul
    Actualizat fișierul `report_dc.xml` pentru a corecta indentarea, styling-ul și structura HTML, asigurând o generare mai clară și corectă a formatului raportului. Incrementată versiunea modulului la `19.0.1.0.10` în `__manifest__.py` pentru a reflecta modificările.

--- deltatech_dc/__manifest__.py
+++ deltatech_dc/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Declaration of Conformity",
     "summary": "Print Declaration of Conformity",
-    "version": "19.0.1.0.7",
+    "version": "19.0.1.0.10",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "license": "OPL-1",
