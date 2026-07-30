e15f64c14fa9153a3eb40473e203251a932f226c
Dorin Hongu <dhongu@gmail.com>
2026-07-30 08:39:38 +0300

    [MIG] deltatech_sale_xls: migrare 19.0

--- deltatech_sale_xls/__manifest__.py
+++ deltatech_sale_xls/__manifest__.py
@@ -6,7 +6,7 @@
     "name": "Deltatech Sale XLS",
     "summary": "Import/export sale line from/to Excel",
     "author": "Terrabit, Voicu Stefan",
-    "version": "18.0.0.0.0",
+    "version": "19.0.1.0.0",
     "license": "OPL-1",
     "website": "https://www.terrabit.ro",
     "category": "Sales",
